ecaf315844b6e0ff69ef5dc04705dbd2ef0ce094
nsklyar <nsklyar@ebi.ac.uk>
2012-08-17 16:08:51 +0100

    project created

---
+++ web/src/main/java/uk/ac/ebi/atlas/services/DummyService.java
@@ -0,0 +1,13 @@
+package uk.ac.ebi.atlas.services;
+
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * Date: 17/08/2012
+ */
+public class DummyService {
+    public Collection<String> getGeneNames() {
+        return Collections.emptyList();
+    }
+}
---
+++ web/src/test/java/uk/ac/ebi/atlas/services/DummyServiceTest.java
@@ -0,0 +1,18 @@
+package uk.ac.ebi.atlas.services;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Date: 17/08/2012
+ */
+public class DummyServiceTest{
+
+    @Test
+    public void testGetGeneNames() throws Exception {
+        assertTrue(true);
+    }
+
+
+}
