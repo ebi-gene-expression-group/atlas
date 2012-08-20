867a3be00501e415bc081f25032b273f44713672
bennyfio <bennyfio@ebi.ac.uk>
2012-08-20 13:11:15 +0100

    added selenium test for experiment page and some initial selenium infrastructure (AtlasPage abstract class). Also reconfigured poms to enhance integration testing.

---
+++ web/src/test/java/uk/ac/ebi/atlas/acceptance/selenium/VisualizeTopTenExpressionLevels.java
@@ -0,0 +1,38 @@
+package uk.ac.ebi.atlas.acceptance.selenium;
+
+import uk.ac.ebi.atlas.acceptance.selenium.pages.ExperimentPage;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class VisualizeTopTenExpressionLevels {
+
+	private WebDriver driver = new HtmlUnitDriver();
+
+	private ExperimentPage subject;
+	
+	@Before
+	public void initializeTest(){
+        subject = new ExperimentPage(driver);
+        subject.get();
+    }
+
+    @Test
+    public void titleShallBeExperiment(){
+    	assertThat(driver.getTitle(), is(subject.getTitle()));
+    }
+
+    @Test
+    public void verifyMostExpressedTranscriptId(){
+        assertThat(subject.getMostExpressedTranscriptId(), is("ENST11111"));
+    }
+
+    @Test
+    public void numberOfRowsInExpressionLevelsTableShallBeTen(){
+        assertThat(subject.getTableRowCount(), is(10));
+    }
+}
---
+++ web/src/test/java/uk/ac/ebi/atlas/acceptance/selenium/pages/ExperimentPage.java
@@ -0,0 +1,50 @@
+package uk.ac.ebi.atlas.acceptance.selenium.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.endsWith;
+import static org.junit.Assert.assertThat;
+
+
+public class ExperimentPage  extends AtlasPage {
+
+    private static final String PAGE_URI = "/experiment.html";
+
+    @FindBy(id = "topTenExpressionLevels")
+    WebElement tableElement;
+
+
+    public ExperimentPage(WebDriver driver){
+        super(driver);
+    }
+
+    public int getTableRowCount() {
+       List<WebElement> tableRows = tableElement.findElements(By.xpath("tbody/tr"));
+       return tableRows.size();
+    }
+    
+    public String getMostExpressedTranscriptId() {
+    	return tableElement.findElement(By.xpath("tbody/tr/td[1]")).getText();
+	}
+
+    public String getTitle() {
+        return "Experiment Page";
+    }
+
+    @Override
+    protected String getPageURI() {
+        return PAGE_URI;
+    }
+
+    @Override
+    protected void isLoaded() throws Error {
+        String url = driver.getCurrentUrl();
+        assertThat(url, endsWith(PAGE_URI));
+    }
+
+}
