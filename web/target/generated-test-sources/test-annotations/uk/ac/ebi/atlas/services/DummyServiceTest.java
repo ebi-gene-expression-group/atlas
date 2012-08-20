ccd0cd10af615c7237b0961cb6323193624c6d62
nsklyar <nsklyar@ebi.ac.uk>
2012-08-20 13:30:56 +0100

    ignore this commit

---
+++ web/target/generated-test-sources/test-annotations/uk/ac/ebi/atlas/services/DummyServiceTest.java
@@ -0,0 +1,12 @@
+package uk.ac.ebi.atlas.services;
+
+import junit.framework.TestCase;
+
+/**
+ * Date: 17/08/2012
+ */
+public class DummyServiceTest extends TestCase {
+    public void testGetGeneNames() throws Exception {
+
+    }
+}
