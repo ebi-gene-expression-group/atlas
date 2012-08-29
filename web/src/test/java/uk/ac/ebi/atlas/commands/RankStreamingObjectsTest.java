33ca2a1ea1533cee937253f434566c7cee14425f
bennyfio <bennyfio@ebi.ac.uk>
2012-08-29 12:40:09 +0100

    added tomcat plugin configuration for deploying on lime tomcat 7 with mvn deploy

---
+++ web/src/test/java/uk/ac/ebi/atlas/commands/RankStreamingObjectsTest.java
@@ -0,0 +1,16 @@
+package uk.ac.ebi.atlas.commands;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class RankStreamingObjectsTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testApply() throws Exception {
+
+    }
+}
