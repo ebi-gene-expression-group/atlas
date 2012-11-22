0129202cfeb35f178527e5093253e2a8d2f6cf6a
nsklyar <nsklyar@ebi.ac.uk>
2012-11-22 11:31:25 +0000

    added new test

---
+++ web/src/test/java/uk/ac/ebi/atlas/acceptance/selenium/HeatmapTableWithCutoff05AndGenePropertyFilterIT.java
@@ -0,0 +1,45 @@
+package uk.ac.ebi.atlas.acceptance.selenium;
+
+import org.junit.Test;
+import uk.ac.ebi.atlas.acceptance.selenium.pages.HeatmapTablePage;
+
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class HeatmapTableWithCutoff05AndGenePropertyFilterIT extends SeleniumFixture {
+
+    protected HeatmapTablePage subject;
+
+    public void getStartingPage() {
+//
+    }
+
+    @Test
+    public void verifyResultOnSinglePropertyQuery() {
+        subject = new HeatmapTablePage(firefoxDriver, "geneQuery=regulation&cutoff=0.5");
+        subject.get();
+        assertThat(subject.getGeneCount().contains("62"), is(true));
+
+    }
+
+    @Test
+    public void verifyResultOnMultiplePropertyQuery() {
+        subject = new HeatmapTablePage(firefoxDriver, "geneQuery=regulation+%22protein+binding%22&cutoff=0.5");
+        subject.get();
+        assertThat(subject.getGeneCount().contains("82"), is(true));
+
+    }
+
+    @Test
+    public void verifyResultOnMultiplePropertyAndOrganismPartQuery() {
+        subject = new HeatmapTablePage(firefoxDriver, "geneQuery=regulation+%22protein+binding%22&" +
+                "organismParts=skeletal+muscle&organismParts=thyroid&_organismParts=1&cutoff=0.5");
+        subject.get();
+        assertThat(subject.getGeneCount().contains("1"), is(true));
+        assertThat(subject.getFirstGeneProfile(), contains("2", "0.7"));
+
+
+    }
+
+}
