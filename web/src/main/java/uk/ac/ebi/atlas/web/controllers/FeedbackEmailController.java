a3086ef51ae401ee3668c619a27b1f3c5657064e
Benedetto Fiorelli <bennyfio@ebi.ac.uk>
2012-12-11 12:08:37 +0000

    added some logging to feedback mail controller

--- web/src/main/java/uk/ac/ebi/atlas/web/controllers/FeedbackEmailController.java
+++ web/src/main/java/uk/ac/ebi/atlas/web/controllers/FeedbackEmailController.java
@@ -22,6 +22,7 @@
 
 package uk.ac.ebi.atlas.web.controllers;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -42,6 +43,9 @@ import java.util.Properties;
 
 @Controller
 public class FeedbackEmailController {
+
+    private static final Logger logger = Logger.getLogger(FeedbackEmailController.class);
+
 
     class EmailMessage {
         public String sender;
@@ -116,8 +120,11 @@ public class FeedbackEmailController {
 
         // capture input data
         String feedback = body.get("feedback").get(0);
+        logger.info("<put> feedback = " + feedback);
         String email = body.get("email").get(0);
+        logger.info("<put> email = " + email);
         boolean sendemail = Boolean.parseBoolean(body.get("sendemail").get(0));
+        logger.info("<put> mail sent = " + sendemail);
 
         // compose email
         EmailMessage message;
