0f2d84e5ecd1c5032db203271a04cadd1ca19c48
jtaubert <jtaubert@ebi.ac.uk>
2012-12-20 15:00:17 +0000

    added tests for refactored request preferences

--- web/src/test/java/uk/ac/ebi/atlas/web/RequestPreferencesTest.java
+++ web/src/test/java/uk/ac/ebi/atlas/web/RequestPreferencesTest.java
@@ -22,14 +22,19 @@
 
 package uk.ac.ebi.atlas.web;
 
+import com.google.common.collect.Sets;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import uk.ac.ebi.atlas.model.FactorValue;
 import uk.ac.ebi.atlas.utils.NumberUtils;
 
+import java.util.TreeSet;
+
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.is;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -71,7 +76,24 @@ public class RequestPreferencesTest {
         subject.setHeatmapMatrixSize(33);
         //then
         assertThat(subject.getHeatmapMatrixSize(), is(33));
+    }
 
+    @Test
+    public void getFilterFactorValuesAsObjects() {
+        TreeSet<String> filterFactorValuesStrings = Sets.newTreeSet();
+        filterFactorValuesStrings.add("MATERIAL_TYPE:total rna");
+        filterFactorValuesStrings.add("CELLULAR_COMPONENT:whole cell");
 
+        // given
+        subject.setFilterFactorValues(filterFactorValuesStrings);
+        //then
+        assertThat(subject.getFilterFactorValues().size(), is(2));
+        //and
+        assertThat(subject.getFilterFactorValues(), hasItems("MATERIAL_TYPE:total rna", "CELLULAR_COMPONENT:whole cell"));
+        //and
+        assertThat(subject.getFilterFactorValuesAsObjects().size(), is(2));
+        //and
+        assertThat(subject.getFilterFactorValuesAsObjects(), hasItems(
+                new FactorValue("CELLULAR_COMPONENT", "", "whole cell"), new FactorValue("MATERIAL_TYPE", "", "total rna")));
     }
 }
