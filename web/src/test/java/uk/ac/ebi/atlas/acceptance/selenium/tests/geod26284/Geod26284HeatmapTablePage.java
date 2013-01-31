75e00c557674e899a3d49f74bc33680183f16fd8
jtaubert <jtaubert@ebi.ac.uk>
2013-01-31 12:53:26 +0000

    first menu tests, getting a handle on the whole menu structure now

--- web/src/test/java/uk/ac/ebi/atlas/acceptance/selenium/tests/geod26284/Geod26284HeatmapTablePage.java
+++ web/src/test/java/uk/ac/ebi/atlas/acceptance/selenium/tests/geod26284/Geod26284HeatmapTablePage.java
@@ -23,94 +23,85 @@
 package uk.ac.ebi.atlas.acceptance.selenium.tests.geod26284;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Action;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import uk.ac.ebi.atlas.acceptance.selenium.pages.HeatmapTablePage;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Geod26284HeatmapTablePage extends HeatmapTablePage {
 
-    private final Actions builder;
+    private static final String XPATH_TEMPLATE = "ul/li[%d]";
+    ;
 
     @FindBy(xpath = "//ul[@id = 'filterBy']/li")
     private WebElement filterByMenu;
 
     public Geod26284HeatmapTablePage(WebDriver driver) {
         super(driver);
-        builder = new Actions(driver);
     }
 
     public Geod26284HeatmapTablePage(WebDriver driver, String httpParameters) {
         super(driver, httpParameters);
-        builder = new Actions(driver);
     }
 
     protected String getPageURI() {
         return "/gxa/experiments/E-GEOD-26284";
     }
 
-    protected List<WebElement> moveMouseOntoMenuElement(final WebElement menuElement) {
-
-        WebElement linkElement = menuElement.findElement(By.xpath("a"));
-        Action mouseMove = builder.moveToElement(linkElement).build();
-        mouseMove.perform();
-
-        System.out.println(menuElement + " " + menuElement.getTagName() + " " + menuElement.getText());
-        for (WebElement element : menuElement.findElements(By.xpath("ul/li"))) {
-            System.out.println(element + " " + element.getTagName() + " " + element.getText());
-        }
-
-        // wait until jQuery has rendered the child menu
-        WebDriverWait wait = new WebDriverWait(driver, 5);
-        wait.until(new ExpectedCondition<Boolean>() {
-            @Override
-            public Boolean apply(WebDriver webDriver) {
-                // this is the text of the first child
-                return !menuElement.findElement(By.xpath("ul/li")).getText().equals("");
-            }
-        });
-
-        for (WebElement element : menuElement.findElements(By.xpath("ul/li"))) {
-            System.out.println(element + " " + element.getTagName() + " " + element.getText());
-        }
-
-        System.out.println();
-
-        // return all child elements in current menu element
-        return menuElement.findElements(By.xpath("ul/li"));
-    }
-
-    protected WebElement recurseThroughMenu(WebElement menuElement, int[] indices) {
-
-        int index = indices[0];
-        WebElement childElement = moveMouseOntoMenuElement(menuElement).get(index);
-
-        // this is the base case
-        if (indices.length == 1) {
-            return childElement;
-        }
-
-        // this is the recursion case
-        int[] newIndices = Arrays.copyOfRange(indices, 1, indices.length);
-        return recurseThroughMenu(childElement, newIndices);
-    }
-
     public String getFilterByMenuTopText() {
         return filterByMenu.getText();
     }
 
+    protected List<WebElement> getWebElementPath(List<WebElement> list, WebElement element, int[] indices) {
+
+        list.add(element);
+
+        // for xpath indices seem to start at 1
+        String xpath = String.format(XPATH_TEMPLATE, indices[0] + 1);
+        WebElement childElement = element.findElement(By.xpath(xpath));
+
+        // base case
+        if (indices.length == 1) {
+            list.add(childElement);
+            return list;
+        }
+
+        // recursion case
+        int[] newIndices = Arrays.copyOfRange(indices, 1, indices.length);
+        return getWebElementPath(list, childElement, newIndices);
+    }
+
     public String getFilterByMenuText(int... indicies) {
 
-        WebElement element = recurseThroughMenu(filterByMenu, indicies);
-        System.out.println(element + " " + element.getTagName() + " " + element.getText());
+        // get path to click through menu
+        List<WebElement> menuElements = getWebElementPath(new ArrayList<WebElement>(), filterByMenu, indicies);
 
-        return element.getText();
+        // make whole menu appear
+        ((JavascriptExecutor) driver).executeScript("$(\"ul:hidden\").show();");
+
+        final WebElement lastMenuElement = menuElements.get(menuElements.size() - 1);
+
+        // wait until jQuery has rendered the menu
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(new ExpectedCondition<Boolean>() {
+            @Override
+            public Boolean apply(WebDriver webDriver) {
+                return !lastMenuElement.getText().equals("");
+            }
+        });
+
+        for (WebElement menuElement : menuElements) {
+            WebElement link = menuElement.findElement(By.xpath("a"));
+            System.out.println(link + " " + link.getTagName() + " " + link.getText());
+        }
+
+        return lastMenuElement.findElement(By.xpath("a")).getText();
     }
 }
