89ac3e6bc7128159ca5d6543164c32d55fe5e30c
Benedetto Fiorelli <bennyfio@ebi.ac.uk>
2013-03-15 09:25:03 +0000

    added some DifferentialProfilesInputStreamIT test cases

--- web/src/test/java/uk/ac/ebi/atlas/streams/differential/DifferentialProfilesInputStreamIT.java
+++ web/src/test/java/uk/ac/ebi/atlas/streams/differential/DifferentialProfilesInputStreamIT.java
@@ -45,9 +45,17 @@ public class DifferentialProfilesInputStreamIT {
 
     public static final String EXPERIMENT_ACCESSION = "E-GEOD-22351";
 
-    private static final String GENE_ID_1 = "ENSMUSG00000050370";
-    private static final String GENE_ID_2 = "ENSMUSG00000024799";
-    private static final String GENE_ID_3 = "ENSMUSG00000018930";
+    private static final String GENE_ID_UPDOWN_1 = "ENSMUSG00000050370";
+    private static final String GENE_ID_UPDOWN_2 = "ENSMUSG00000024799";
+    private static final String GENE_ID_UPDOWN_3 = "ENSMUSG00000018930";
+
+    private static final String GENE_ID_UP_1 = "ENSMUSG00000050370";
+    private static final String GENE_ID_UP_2 = "ENSMUSG00000018930";
+    private static final String GENE_ID_UP_3 = "ENSMUSG00000036896";
+
+    private static final String GENE_ID_DOWN_1 = "ENSMUSG00000024799";
+    private static final String GENE_ID_DOWN_2 = "ENSMUSG00000002100";
+    private static final String GENE_ID_DOWN_3 = "ENSMUSG00000052468";
 
     @Inject
     private InputStreamFactory inputStreamFactory;
@@ -61,17 +69,18 @@ public class DifferentialProfilesInputStreamIT {
 
     @Before
     public void initSubject() throws Exception {
-        subject = inputStreamFactory.createDifferentialProfileInputStream(EXPERIMENT_ACCESSION, 0.05D, Regulation.UP_DOWN);
         DifferentialExperiment differentialExperiment = differentialExperimentsCache.getExperiment(EXPERIMENT_ACCESSION);
         contrast = differentialExperiment.getContrasts().first();
     }
 
     @Test
-    public void readNextShouldReturnNextExpression() throws IOException {
+    public void readNextWithUpDownRegulation() throws IOException {
+        subject = inputStreamFactory.createDifferentialProfileInputStream(EXPERIMENT_ACCESSION, 0.05D, Regulation.UP_DOWN);
+
         //given
         DifferentialProfile differentialProfile = subject.readNext();
         //then
-        assertThat(differentialProfile.getGeneId(), is(GENE_ID_1));
+        assertThat(differentialProfile.getGeneId(), is(GENE_ID_UPDOWN_1));
         assertThat(differentialProfile.getSpecificity(), is(1));
         double expressionLevel = differentialProfile.getExpressionLevel(contrast);
         assertThat(expressionLevel, is(1.70428798138445E-6));
@@ -81,7 +90,7 @@ public class DifferentialProfilesInputStreamIT {
         //given we poll again
         differentialProfile = subject.readNext();
         //then
-        assertThat(differentialProfile.getGeneId(), is(GENE_ID_2));
+        assertThat(differentialProfile.getGeneId(), is(GENE_ID_UPDOWN_2));
         assertThat(differentialProfile.getSpecificity(), is(1));
         expressionLevel = differentialProfile.getExpressionLevel(contrast);
         assertThat(expressionLevel, is(0.0137444998099392));
@@ -92,7 +101,7 @@ public class DifferentialProfilesInputStreamIT {
         differentialProfile = subject.readNext();
 
         //given we poll again
-        assertThat(differentialProfile.getGeneId(), is(GENE_ID_3));
+        assertThat(differentialProfile.getGeneId(), is(GENE_ID_UPDOWN_3));
         assertThat(differentialProfile.getSpecificity(), is(1));
         expressionLevel = differentialProfile.getExpressionLevel(contrast);
         assertThat(expressionLevel, is(0.00188954906204623));
@@ -101,9 +110,84 @@ public class DifferentialProfilesInputStreamIT {
 
     }
 
+    @Test
+    public void readNextWithUpRegulation() throws IOException {
+        subject = inputStreamFactory.createDifferentialProfileInputStream(EXPERIMENT_ACCESSION, 0.05D, Regulation.UP);
+
+        //given
+        DifferentialProfile differentialProfile = subject.readNext();
+        //then
+        assertThat(differentialProfile.getGeneId(), is(GENE_ID_UP_1));
+        assertThat(differentialProfile.getSpecificity(), is(1));
+        double expressionLevel = differentialProfile.getExpressionLevel(contrast);
+        assertThat(expressionLevel, is(1.70428798138445E-6));
+        DifferentialExpression differentialExpression = differentialProfile.getExpression(contrast);
+        assertThat(differentialExpression.getFoldChange(), is(3.01033089730209));
+
+        //given we poll again
+        differentialProfile = subject.readNext();
+        //then
+        assertThat(differentialProfile.getGeneId(), is(GENE_ID_UP_2));
+        assertThat(differentialProfile.getSpecificity(), is(1));
+        expressionLevel = differentialProfile.getExpressionLevel(contrast);
+        assertThat(expressionLevel, is(0.00188954906204623));
+
+        differentialExpression = differentialProfile.getExpression(contrast);
+        assertThat(differentialExpression.getFoldChange(), is(3.2943463328998));
+
+        differentialProfile = subject.readNext();
+
+        //given we poll again
+        assertThat(differentialProfile.getGeneId(), is(GENE_ID_UP_3));
+        assertThat(differentialProfile.getSpecificity(), is(1));
+        expressionLevel = differentialProfile.getExpressionLevel(contrast);
+        assertThat(expressionLevel, is(1.72296964769093E-4));
+        differentialExpression = differentialProfile.getExpression(contrast);
+        assertThat(differentialExpression.getFoldChange(), is(1.38380288897256));
+
+    }
+
+
+    @Test
+    public void readNextWithDownRegulation() throws IOException {
+        subject = inputStreamFactory.createDifferentialProfileInputStream(EXPERIMENT_ACCESSION, 0.05D, Regulation.DOWN);
+
+        //given
+        DifferentialProfile differentialProfile = subject.readNext();
+        //then
+        assertThat(differentialProfile.getGeneId(), is(GENE_ID_DOWN_1));
+        assertThat(differentialProfile.getSpecificity(), is(1));
+        double expressionLevel = differentialProfile.getExpressionLevel(contrast);
+        assertThat(expressionLevel, is(0.0137444998099392));
+        DifferentialExpression differentialExpression = differentialProfile.getExpression(contrast);
+        assertThat(differentialExpression.getFoldChange(), is(-1.15843751459071));
+
+        //given we poll again
+        differentialProfile = subject.readNext();
+        //then
+        assertThat(differentialProfile.getGeneId(), is(GENE_ID_DOWN_2));
+        assertThat(differentialProfile.getSpecificity(), is(1));
+        expressionLevel = differentialProfile.getExpressionLevel(contrast);
+        assertThat(expressionLevel, is(0.0206187013724948));
+
+        differentialExpression = differentialProfile.getExpression(contrast);
+        assertThat(differentialExpression.getFoldChange(), is(-2.37961321555458));
+
+        differentialProfile = subject.readNext();
+
+        //given we poll again
+        assertThat(differentialProfile.getGeneId(), is(GENE_ID_DOWN_3));
+        assertThat(differentialProfile.getSpecificity(), is(1));
+        expressionLevel = differentialProfile.getExpressionLevel(contrast);
+        assertThat(expressionLevel, is(0.00304596230892978));
+        differentialExpression = differentialProfile.getExpression(contrast);
+        assertThat(differentialExpression.getFoldChange(), is(-1.46618405734343));
+
+    }
 
     @Test
     public void readNextShouldReturnNullGivenAllExpressionLevelsHaveBeenRead() throws Exception {
+        subject = inputStreamFactory.createDifferentialProfileInputStream(EXPERIMENT_ACCESSION, 0.05D, Regulation.UP_DOWN);
 
         long countProfiles = 0;
         while(subject.readNext() != null){
