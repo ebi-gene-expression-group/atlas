45ba6b8203796cfd7417632eb93d3f3be6a86e5b
nsklyar <nsklyar@ebi.ac.uk>
2013-03-26 15:13:35 +0000

    work in progress selenium tests

--- web/src/test/java/uk/ac/ebi/atlas/acceptance/selenium/tests/geod38400/SpecificNoContrastSelected.java
+++
@@ -1,4 +0,0 @@
-package uk.ac.ebi.atlas.acceptance.selenium.tests.geod38400;
-
-public class SpecificNoContrastSelected {
-}
---
+++ web/src/test/java/uk/ac/ebi/atlas/acceptance/selenium/tests/geod38400/SpecificNoContrastSelectedIT.java
@@ -0,0 +1,73 @@
+package uk.ac.ebi.atlas.acceptance.selenium.tests.geod38400;
+
+import org.junit.Test;
+import uk.ac.ebi.atlas.acceptance.selenium.pages.HeatmapTablePage;
+import uk.ac.ebi.atlas.acceptance.selenium.utils.SeleniumFixture;
+
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.core.StringContains.containsString;
+import static org.junit.Assert.assertThat;
+
+public class SpecificNoContrastSelectedIT extends SeleniumFixture {
+
+    private static final String E_E_GEOD_38400_ACCESSION = "E-GEOD-38400";
+    protected HeatmapTablePage subject;
+
+    @Test
+    public void verifyQueryFactorLableAndHeatmapHeaders() {
+        subject = new HeatmapTablePage(driver, E_E_GEOD_38400_ACCESSION, "regulation=UP&displayLevels=true");
+        subject.get();
+
+        assertThat(subject.getQueryFactorLabel(), is("Contrast"));
+
+        assertThat(subject.getFactorValueHeaders().size(), is(3));
+        assertThat(subject.getFactorValueHeaders().get(0), startsWith("idn2"));
+    }
+
+    @Test
+    public void verifyResultsWithRegulationUp() {
+        subject = new HeatmapTablePage(driver, E_E_GEOD_38400_ACCESSION, "regulation=UP&displayLevels=true");
+        subject.get();
+        assertThat(subject.getGeneCount(), containsString("of 36"));
+
+        assertThat(subject.getSelectedGenes().size(), is(36));
+        assertThat(subject.getSelectedGenes().subList(0, 3), contains("AT1G33840", "AT1G33850", "AT3G54770"));
+
+        assertThat(subject.getGeneProfile(1).size(), is(3));
+        assertThat(subject.getGeneProfile(1).get(2), is("<10-10"));
+
+    }
+
+//    @Test
+//    public void verifyResultsWithRegulationDown() {
+//        subject = new HeatmapTablePage(driver, E_E_GEOD_38400_ACCESSION, "regulation=DOWN&displayLevels=true");
+//        subject.get();
+//        assertThat(subject.getGeneCount(), containsString("of 9"));
+//
+//        assertThat(subject.getSelectedGenes().size(), is(9));
+//        assertThat(subject.getSelectedGenes().subList(0, 3), contains("Gm15512", "Pla2g3", "Pmp2"));
+//
+//        assertThat(subject.getGeneProfile(1).size(), is(1));
+//        assertThat(subject.getGeneProfile(1).get(0), is("6.61"+ " \u00D7 " + "10-5"));
+//
+//        assertThat(subject.getLastGeneProfile().size(), is(1));
+//        assertThat(subject.getLastGeneProfile().get(0), is("0.041"));
+//    }
+//
+//    @Test
+//    public void verifyResultsWithRegulationUpDown() {
+//        subject = new HeatmapTablePage(driver, E_E_GEOD_38400_ACCESSION, "regulation=UP_DOWN&displayLevels=true");
+//        subject.get();
+//        assertThat(subject.getGeneCount(), containsString("of 49"));
+//
+//        assertThat(subject.getSelectedGenes().size(), is(49));
+//        assertThat(subject.getSelectedGenes().subList(0, 3), contains("Gpnmb", "Cst7", "Itgax"));
+//
+//        assertThat(subject.getGeneProfile(1).size(), is(1));
+//        assertThat(subject.getGeneProfile(1).get(0), is("<10-10"));
+//
+//        assertThat(subject.getLastGeneProfile().size(), is(1));
+//        assertThat(subject.getLastGeneProfile().get(0), is("0.041"));
+//    }
+
+}
