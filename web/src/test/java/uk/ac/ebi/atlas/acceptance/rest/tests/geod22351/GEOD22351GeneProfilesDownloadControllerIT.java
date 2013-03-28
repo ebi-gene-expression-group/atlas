d697a509a5a29b7832d9f36f208d977ce9f2753c
nsklyar <nsklyar@ebi.ac.uk>
2013-03-28 11:26:13 +0000

    fixed test

--- web/src/test/java/uk/ac/ebi/atlas/acceptance/rest/tests/geod22351/GEOD22351GeneProfilesDownloadControllerIT.java
+++ web/src/test/java/uk/ac/ebi/atlas/acceptance/rest/tests/geod22351/GEOD22351GeneProfilesDownloadControllerIT.java
@@ -57,7 +57,7 @@ public class GEOD22351GeneProfilesDownloadControllerIT {
         List<String> firstLine = subject.getRowValues(0);
 
         assertThat(firstLine,
-                contains("Gene name", "Gene Id", "g1_g2.p-value", "g1_g2.log2foldchange")
+                contains("Gene name", "Gene Id", "genotype:'expressing human TDP-43' vs 'non transgenic'.p-value", "genotype:'expressing human TDP-43' vs 'non transgenic'.log2foldchange")
         );
 
     }
