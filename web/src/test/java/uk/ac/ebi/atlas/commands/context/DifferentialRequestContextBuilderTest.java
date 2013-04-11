f3f1c540896260f11c55e68a18bf1119942ca060
jtaubert <jtaubert@ebi.ac.uk>
2013-04-11 15:24:23 +0100

    last unit tests for context

---
+++ web/src/test/java/uk/ac/ebi/atlas/commands/context/DifferentialRequestContextBuilderTest.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2008-2013 Microarray Informatics Team, EMBL-European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ * For further details of the Gene Expression Atlas project, including source code,
+ * downloads and documentation, please see:
+ *
+ * http://gxa.github.com/gxa
+ */
+
+package uk.ac.ebi.atlas.commands.context;
+
+import com.google.common.collect.Sets;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import uk.ac.ebi.atlas.model.differential.Contrast;
+import uk.ac.ebi.atlas.model.differential.DifferentialExperiment;
+import uk.ac.ebi.atlas.web.DifferentialRequestPreferences;
+
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DifferentialRequestContextBuilderTest {
+
+    public static final String ORGANISM = "homo sapiens";
+    public static final String CONTRAST_NAME1 = "a";
+    public static final String CONTRAST_NAME2 = "b";
+
+    @Mock
+    DifferentialExperiment experimentMock;
+
+    @Mock
+    DifferentialRequestPreferences preferencesMock;
+
+    @Mock
+    Contrast contrastMock1;
+
+    @Mock
+    Contrast contrastMock2;
+
+    DifferentialRequestContextBuilder subject;
+
+    @Before
+    public void setUp() throws Exception {
+        subject = new DifferentialRequestContextBuilder(new DifferentialRequestContext());
+
+        when(experimentMock.getFirstSpecies()).thenReturn(ORGANISM);
+
+        when(contrastMock1.getDisplayName()).thenReturn(CONTRAST_NAME1);
+        when(contrastMock2.getDisplayName()).thenReturn(CONTRAST_NAME2);
+        SortedSet<Contrast> sortedSet = new TreeSet(Contrast.orderByDisplayName());
+        sortedSet.add(contrastMock1);
+        sortedSet.add(contrastMock2);
+
+        when(experimentMock.getContrasts()).thenReturn(sortedSet);
+        when(preferencesMock.getQueryFactorValues()).thenReturn(Sets.newTreeSet(Sets.newHashSet("a")));
+        when(experimentMock.getContrast(CONTRAST_NAME1)).thenReturn(contrastMock1);
+
+    }
+
+    @Test
+    public void testBuild() throws Exception {
+        DifferentialRequestContext context = subject.forExperiment(experimentMock).withPreferences(preferencesMock).build();
+        assertThat(context.getSelectedQueryFactors(), hasItem(contrastMock1));
+        assertThat(context.getFilteredBySpecies(), is(ORGANISM));
+        assertThat(context.getAllQueryFactors(), hasItems(contrastMock1, contrastMock2));
+    }
+}
