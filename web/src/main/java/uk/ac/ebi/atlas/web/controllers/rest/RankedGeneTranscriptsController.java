0cf69727cf7d31cd4091ebb28a4bef61129446e3
Benedetto Fiorelli <bennyfio@ebi.ac.uk>
2013-04-12 16:43:28 +0100

    upgraded Flot version (now 0.8) and created new REST controller for rankedTranscripts by gene id

---
+++ web/src/main/java/uk/ac/ebi/atlas/web/controllers/rest/RankedGeneTranscriptsController.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2008-2013 Microarray Informatics Team, EMBL-European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ * For further details of the Gene Expression Atlas project, including source code,
+ * downloads and documentation, please see:
+ *
+ * http://gxa.github.com/gxa
+ */
+
+package uk.ac.ebi.atlas.web.controllers.rest;
+
+import com.google.gson.Gson;
+import org.springframework.context.annotation.Scope;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import uk.ac.ebi.atlas.model.baseline.Factor;
+import uk.ac.ebi.atlas.model.baseline.barcharts.BarChartTrader;
+import uk.ac.ebi.atlas.model.cache.baseline.BarChartTradersCache;
+import uk.ac.ebi.atlas.web.FilterFactorsConverter;
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.Set;
+
+@Controller
+@Scope("request")
+public class RankedGeneTranscriptsController {
+
+    private BarChartTradersCache barChartTradersCache;
+
+    private FilterFactorsConverter filterFactorsConverter;
+
+    @Inject
+    public RankedGeneTranscriptsController(BarChartTradersCache barChartTradersCache, FilterFactorsConverter filterFilterFactorsConverter) {
+        this.barChartTradersCache = barChartTradersCache;
+        this.filterFactorsConverter = filterFilterFactorsConverter;
+    }
+
+    @RequestMapping(value = "/json/barchart/{experimentAccession}", method = RequestMethod.GET, produces = "application/json")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public String getMap(HttpServletRequest request, @PathVariable String experimentAccession,
+                         @RequestParam(value = "queryFactorValues[]", required = false) Set<String> queryFactorValues,
+                         @RequestParam String queryFactorType, @RequestParam(required = false) String serializedFilterFactors) {
+
+        BarChartTrader barchartTrader = barChartTradersCache.getBarchartTrader(experimentAccession);
+
+        Set<Factor> queryFactors = new HashSet<>();
+        if (queryFactorValues != null) {
+            for (String queryFactorValue : queryFactorValues) {
+                queryFactors.add(new Factor(queryFactorType, queryFactorValue));
+            }
+        }
+
+        Set<Factor> filterFactors = filterFactorsConverter.deserialize(serializedFilterFactors);
+
+        NavigableMap<Double, Integer> chartData = barchartTrader.getChart(filterFactors, queryFactors);
+
+        Gson gson = new Gson();
+
+        // changed to more generic Map interface as per Sonar recommendation
+        return gson.toJson(chartData, Map.class);
+
+    }
+
+}
