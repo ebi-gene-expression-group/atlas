ce5dcbe2f9362b0a7f3df5382b287ca725c52495
Benedetto Fiorelli <bennyfio@ebi.ac.uk>
2013-04-15 16:10:31 +0100

    updated schema

--- web/src/main/resources/atlas-schema.sql
+++ web/src/main/resources/atlas-schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS experiment_transcripts(
     experiment_accession VARCHAR(50) NOT NULL,
     gene_id VARCHAR(255) NOT NULL,
-    transcript_profiles CLOB,
+    transcript_profiles BLOB,
     PRIMARY KEY (experiment_accession, gene_id)
 );
