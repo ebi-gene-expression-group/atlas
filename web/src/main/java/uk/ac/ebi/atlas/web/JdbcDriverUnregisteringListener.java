2b4d84889e76d168af731698df05cef28a927414
Benedetto Fiorelli <bennyfio@ebi.ac.uk>
2013-04-18 11:40:21 +0100

    replaced bdcp with c3p0 datasource and added ServletContextListener that deregisters JDBC drivers on contextDestroyed

---
+++ web/src/main/java/uk/ac/ebi/atlas/web/JdbcDriverUnregisteringListener.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2008-2013 Microarray Informatics Team, EMBL-European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ * For further details of the Gene Expression Atlas project, including source code,
+ * downloads and documentation, please see:
+ *
+ * http://gxa.github.com/gxa
+ */
+package uk.ac.ebi.atlas.web;
+
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+
+
+public class JdbcDriverUnregisteringListener implements ServletContextListener {
+
+
+    @Override
+    public void contextInitialized(ServletContextEvent event) {
+    }
+
+
+    @Override
+    public void contextDestroyed(ServletContextEvent event) {
+        deregisterDrivers(getDrivers());
+    }
+
+    /**
+     * Retrieves an Enumeration with all of the currently loaded JDBC drivers.
+     *
+     * @return the list of JDBC Drivers
+     */
+    Enumeration<Driver> getDrivers() {
+        return DriverManager.getDrivers();
+    }
+
+    /**
+     * Unregistering JDBC drivers given as param.
+     *
+     * @param drivers {@link Enumeration} of {@link Driver} to unregister
+     */
+    void deregisterDrivers(Enumeration<Driver> drivers) {
+        while (drivers.hasMoreElements()) {
+            deregisterDriver(drivers.nextElement());
+        }
+    }
+
+    /**
+     * Unregistering single JDBC driver given as param.
+     *
+     * @param driver to unregister
+     */
+    void deregisterDriver(Driver driver) {
+        try {
+            DriverManager.deregisterDriver(driver);
+            System.out.println("Successfully deregistered JDBC driver: " + driver); //System.out because log4j doesn't work in context listeners
+        } catch (SQLException e) {
+            e.printStackTrace(); //System.out because log4j doesn't work in context listeners
+        }
+    }
+}
+
+
+
