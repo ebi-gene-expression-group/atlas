289f8a748b81c2e7d61807965978a1659f39f72e
jtaubert <jtaubert@ebi.ac.uk>
2013-04-23 13:07:15 +0100

    more unit and integration tests for microarray experiment E-MTAB-1066

--- web/src/test/java/uk/ac/ebi/atlas/streams/differential/microarray/MicroarrayExpressionsBufferTest.java
+++ web/src/test/java/uk/ac/ebi/atlas/streams/differential/microarray/MicroarrayExpressionsBufferTest.java
@@ -22,17 +22,149 @@
 
 package uk.ac.ebi.atlas.streams.differential.microarray;
 
+import com.google.common.collect.Lists;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import uk.ac.ebi.atlas.model.differential.Contrast;
+import uk.ac.ebi.atlas.model.differential.microarray.MicroarrayExpression;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+@RunWith(MockitoJUnitRunner.class)
 public class MicroarrayExpressionsBufferTest {
+
+    public static final String P_VAL_1 = "1";
+    public static final String T_VAL_1 = "0.5";
+    public static final String FOLD_CHANGE_1 = "0.474360080385946";
+
+    public static final String P_VAL_2 = "1";
+    public static final String T_VAL_2 = "-0.5";
+    public static final String FOLD_CHANGE_2 = "-Inf";
+
+    private static final String[] TWO_CONTRASTS = new String[]{P_VAL_1, T_VAL_1, FOLD_CHANGE_1, P_VAL_2, T_VAL_2, FOLD_CHANGE_2};
+
+    private MicroarrayExpressionsBuffer subject;
+
+    @Mock
+    private Contrast contrast1Mock;
+    @Mock
+    private Contrast contrast2Mock;
+
     @Before
-    public void setUp() throws Exception {
+    public void initializeSubject() {
+        subject = new MicroarrayExpressionsBuffer(Lists.newArrayList(contrast1Mock, contrast2Mock));
 
     }
 
     @Test
-    public void testPollExpression() throws Exception {
+    public void pollShouldReturnExpressionsInTheRightOrder() throws Exception {
+        //given we load the buffer with three expressions
+        subject.reload(TWO_CONTRASTS);
 
+        //when
+        MicroarrayExpression expression = subject.poll();
+        //then we expect first expression
+        assertThat(expression.getLevel(), is(Double.valueOf(P_VAL_1)));
+        assertThat(expression.getTstatistic(), is(Double.valueOf(T_VAL_1)));
+        assertThat(expression.getFoldChange(), is(Double.valueOf(FOLD_CHANGE_1)));
+        assertThat(expression.getContrast(), is(contrast1Mock));
+
+        //given we poll again
+        expression = subject.poll();
+        assertThat(expression.getLevel(), is(Double.valueOf(P_VAL_2)));
+        assertThat(expression.getTstatistic(), is(Double.valueOf(T_VAL_2)));
+        assertThat(expression.getFoldChange(), is(Double.NEGATIVE_INFINITY));
+        assertThat(expression.getContrast(), is(contrast2Mock));
+
+
+        assertThat(subject.poll(), is(nullValue()));
     }
+
+    @Test
+    public void bufferShouldReturnNullWhenExhausted() throws Exception {
+        //given we load the buffer with three expressions
+        subject.reload(TWO_CONTRASTS);
+
+        //when
+        subject.poll();
+        subject.poll();
+        subject.poll();
+        //then we expect next poll to return null
+        assertThat(subject.poll(), Matchers.is(nullValue()));
+    }
+
+    @Test
+    public void reloadShouldRefillAnExhaustedBuffer() throws Exception {
+        //given we load the buffer with three expressions
+        subject.reload(TWO_CONTRASTS);
+
+        //when we poll until exhaustion
+        MicroarrayExpression run;
+        do {
+            run = subject.poll();
+        } while (run != null);
+        //and we reload again with new values
+        subject.reload("1", "0.5", "2");
+        //and we poll
+        MicroarrayExpression expression = subject.poll();
+        //then we expect to find the new values
+        assertThat(expression.getLevel(), is(1d));
+        assertThat(expression.getTstatistic(), is(0.5d));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void reloadShouldThrowExceptionIfBufferIsNotEmpty() throws Exception {
+        //given we load the buffer with three expressions
+        subject.reload(TWO_CONTRASTS);
+        //and we poll only a single expression
+        subject.poll();
+
+        //when we reload again
+        subject.reload(TWO_CONTRASTS);
+
+        //then we expect an IllegalArgumentException
+    }
+
+    @Test
+    public void skipNALines() {
+
+        subject.reload("NA", "NA", "NA");
+
+        MicroarrayExpression expression = subject.poll();
+
+        assertThat(expression, is(CoreMatchers.nullValue()));
+
+        subject.reload("1", "NA", "NA");
+
+        expression = subject.poll();
+
+        assertThat(expression, is(CoreMatchers.nullValue()));
+
+        subject.reload("NA", "1", "NA");
+
+        expression = subject.poll();
+
+        assertThat(expression, is(CoreMatchers.nullValue()));
+    }
+
+    @Test
+    public void skipNALinesKeepsCorrespondingContrasts() {
+
+        subject.reload("NA", "NA", "1", P_VAL_2, T_VAL_2, "-Inf");
+
+        MicroarrayExpression expression = subject.poll();
+
+        assertThat(expression.getLevel(), is(Double.valueOf(P_VAL_2)));
+        assertThat(expression.getTstatistic(), is(Double.valueOf(T_VAL_2)));
+        assertThat(expression.getFoldChange(), is(Double.NEGATIVE_INFINITY));
+        assertThat(expression.getContrast(), is(contrast2Mock));
+    }
+
 }
---
+++ web/src/test/java/uk/ac/ebi/atlas/streams/differential/microarray/MicroarrayProfilesInputStreamIT.java
@@ -0,0 +1,234 @@
+/*
+ * Copyright 2008-2013 Microarray Informatics Team, EMBL-European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ * For further details of the Gene Expression Atlas project, including source code,
+ * downloads and documentation, please see:
+ *
+ * http://gxa.github.com/gxa
+ */
+
+package uk.ac.ebi.atlas.streams.differential.microarray;
+
+import com.google.common.collect.Sets;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import uk.ac.ebi.atlas.commands.context.MicroarrayRequestContext;
+import uk.ac.ebi.atlas.commons.streams.ObjectInputStream;
+import uk.ac.ebi.atlas.model.cache.microarray.MicroarrayExperimentsCache;
+import uk.ac.ebi.atlas.model.differential.Contrast;
+import uk.ac.ebi.atlas.model.differential.Regulation;
+import uk.ac.ebi.atlas.model.differential.microarray.MicroarrayExperiment;
+import uk.ac.ebi.atlas.model.differential.microarray.MicroarrayExpression;
+import uk.ac.ebi.atlas.model.differential.microarray.MicroarrayProfile;
+import uk.ac.ebi.atlas.streams.InputStreamFactory;
+import uk.ac.ebi.atlas.web.MicroarrayRequestPreferences;
+
+import javax.inject.Inject;
+import java.io.IOException;
+import java.util.TreeSet;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(locations = "classpath:applicationContext.xml")
+public class MicroarrayProfilesInputStreamIT {
+
+    public static final String EXPERIMENT_ACCESSION = "E-MTAB-1066";
+
+    private static final String GENE_ID_UPDOWN_1 = "FBgn0038465";
+    private static final String GENE_ID_UPDOWN_2 = "FBgn0053054";
+    private static final String GENE_ID_UPDOWN_3 = "FBgn0029134";
+
+    private static final String GENE_ID_UP_1 = "FBgn0038465";
+    private static final String GENE_ID_UP_2 = "FBgn0053054";
+    private static final String GENE_ID_UP_3 = "FBgn0040393";
+
+    private static final String GENE_ID_DOWN_1 = "FBgn0029134";
+    private static final String GENE_ID_DOWN_2 = "FBgn0033770";
+    private static final String GENE_ID_DOWN_3 = "FBgn0032636";
+    public static final String ARRAY_DESIGN_ACCESSION = "A-AFFY-35";
+
+    @Inject
+    private InputStreamFactory inputStreamFactory;
+
+    @Inject
+    private MicroarrayExperimentsCache microarrayExperimentsCache;
+
+    @Inject
+    private MicroarrayRequestContext microarrayRequestContext;
+
+    private ObjectInputStream<MicroarrayProfile> subject;
+
+    private Contrast contrast;
+
+    private MicroarrayRequestPreferences microarrayRequestPreferences = new MicroarrayRequestPreferences();
+
+    @Before
+    public void initSubject() throws Exception {
+        microarrayRequestContext.setRequestPreferences(microarrayRequestPreferences);
+
+        subject = inputStreamFactory.createMicroarrayProfileInputStream(EXPERIMENT_ACCESSION, ARRAY_DESIGN_ACCESSION);
+
+        MicroarrayExperiment microarrayExperiment = microarrayExperimentsCache.getExperiment(EXPERIMENT_ACCESSION);
+
+        contrast = microarrayExperiment.getContrasts().first();
+
+        TreeSet<Contrast> allQueryFactors = Sets.newTreeSet(Contrast.orderByDisplayName());
+        allQueryFactors.add(contrast);
+        microarrayRequestContext.setAllQueryFactors(allQueryFactors);
+        microarrayRequestContext.setSelectedQueryFactors(Sets.newHashSet());
+    }
+
+    @Test
+    public void readNextWithUpDownRegulation() throws IOException {
+        microarrayRequestContext.setRegulation(Regulation.UP_DOWN);
+
+
+        //given
+        MicroarrayProfile microarrayProfile = subject.readNext();
+        //then
+        assertThat(microarrayProfile.getGeneId(), is(GENE_ID_UPDOWN_1));
+        assertThat(microarrayProfile.getSpecificity(), is(1));
+        double expressionLevel = microarrayProfile.getExpressionLevel(contrast);
+        assertThat(expressionLevel, is(0.0));
+
+        //given we poll again
+        microarrayProfile = subject.readNext();
+        //then
+        assertThat(microarrayProfile.getGeneId(), is(GENE_ID_UPDOWN_2));
+        assertThat(microarrayProfile.getSpecificity(), is(2));
+        expressionLevel = microarrayProfile.getExpressionLevel(contrast);
+        assertThat(expressionLevel, is(0.0103104291771865));
+
+        MicroarrayExpression microarrayExpression = microarrayProfile.getExpression(contrast);
+        assertThat(microarrayExpression.getFoldChange(), is(0.386948166666667));
+        assertThat(microarrayExpression.getLevel(), is(0.0103104291771865));
+        assertThat(microarrayExpression.getTstatistic(), is(4.61680601729325));
+
+        microarrayProfile = subject.readNext();
+
+        //given we poll again
+        assertThat(microarrayProfile.getGeneId(), is(GENE_ID_UPDOWN_3));
+        assertThat(microarrayProfile.getSpecificity(), is(2));
+        expressionLevel = microarrayProfile.getExpressionLevel(contrast);
+        assertThat(expressionLevel, is(0.00146761846818228));
+        microarrayExpression = microarrayProfile.getExpression(contrast);
+        assertThat(microarrayExpression.getFoldChange(), is(-0.672350666666665));
+        assertThat(microarrayExpression.getLevel(), is(0.00146761846818228));
+        assertThat(microarrayExpression.getTstatistic(), is(-7.07897827772072));
+
+    }
+
+    @Test
+    public void readNextWithUpRegulation() throws IOException {
+        microarrayRequestContext.setRegulation(Regulation.UP);
+
+        //given
+        MicroarrayProfile microarrayProfile = subject.readNext();
+        //then
+        assertThat(microarrayProfile.getGeneId(), is(GENE_ID_UP_1));
+        assertThat(microarrayProfile.getSpecificity(), is(1));
+        double expressionLevel = microarrayProfile.getExpressionLevel(contrast);
+        assertThat(expressionLevel, is(0.0));
+
+        //given we poll again
+        microarrayProfile = subject.readNext();
+        //then
+        assertThat(microarrayProfile.getGeneId(), is(GENE_ID_UP_2));
+        assertThat(microarrayProfile.getSpecificity(), is(2));
+        expressionLevel = microarrayProfile.getExpressionLevel(contrast);
+        assertThat(expressionLevel, is(0.0103104291771865));
+
+        MicroarrayExpression microarrayExpression = microarrayProfile.getExpression(contrast);
+        assertThat(microarrayExpression.getFoldChange(), is(0.386948166666667));
+        assertThat(microarrayExpression.getLevel(), is(0.0103104291771865));
+        assertThat(microarrayExpression.getTstatistic(), is(4.61680601729325));
+
+        microarrayProfile = subject.readNext();
+
+        //given we poll again
+        assertThat(microarrayProfile.getGeneId(), is(GENE_ID_UP_3));
+        assertThat(microarrayProfile.getSpecificity(), is(2));
+        expressionLevel = microarrayProfile.getExpressionLevel(contrast);
+        assertThat(expressionLevel, is(1.46552279262956E-5));
+        microarrayExpression = microarrayProfile.getExpression(contrast);
+        assertThat(microarrayExpression.getFoldChange(), is(2.858022));
+        assertThat(microarrayExpression.getLevel(), is(1.46552279262956E-5));
+        assertThat(microarrayExpression.getTstatistic(), is(19.8487440997079));
+
+    }
+
+
+    @Test
+    public void readNextWithDownRegulation() throws IOException {
+        microarrayRequestContext.setRegulation(Regulation.DOWN);
+
+
+        //given
+        MicroarrayProfile microarrayProfile = subject.readNext();
+        //then
+        assertThat(microarrayProfile.getGeneId(), is(GENE_ID_DOWN_1));
+        assertThat(microarrayProfile.getSpecificity(), is(2));
+        double expressionLevel = microarrayProfile.getExpressionLevel(contrast);
+        assertThat(expressionLevel, is(0.00146761846818228));
+        MicroarrayExpression microarrayExpression = microarrayProfile.getExpression(contrast);
+        assertThat(microarrayExpression.getFoldChange(), is(-0.672350666666665));
+        assertThat(microarrayExpression.getLevel(), is(0.00146761846818228));
+        assertThat(microarrayExpression.getTstatistic(), is(-7.07897827772072));
+
+        //given we poll again
+        microarrayProfile = subject.readNext();
+        //then
+        assertThat(microarrayProfile.getGeneId(), is(GENE_ID_DOWN_2));
+        assertThat(microarrayProfile.getSpecificity(), is(1));
+        expressionLevel = microarrayProfile.getExpressionLevel(contrast);
+        assertThat(expressionLevel, is(0.0));
+
+        microarrayProfile = subject.readNext();
+
+        //given we poll again
+        assertThat(microarrayProfile.getGeneId(), is(GENE_ID_DOWN_3));
+        assertThat(microarrayProfile.getSpecificity(), is(2));
+        expressionLevel = microarrayProfile.getExpressionLevel(contrast);
+        assertThat(expressionLevel, is(0.00844182480286779));
+        microarrayExpression = microarrayProfile.getExpression(contrast);
+        assertThat(microarrayExpression.getFoldChange(), is(-0.6649263));
+        assertThat(microarrayExpression.getLevel(), is(0.00844182480286779));
+        assertThat(microarrayExpression.getTstatistic(), is(-4.82652565702941));
+
+    }
+
+    @Test
+    public void readNextShouldReturnNullGivenAllExpressionLevelsHaveBeenRead() throws Exception {
+        microarrayRequestContext.setRegulation(Regulation.UP_DOWN);
+
+
+        long countProfiles = 0;
+        while (subject.readNext() != null) {
+            ++countProfiles;
+        }
+
+        assertThat(countProfiles, is(175L));
+    }
+
+
+}
