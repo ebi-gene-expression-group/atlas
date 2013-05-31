1db35be6fda704375725c78045a7fa3d2d26677a
Benedetto Fiorelli <bennyfio@ebi.ac.uk>
2013-05-31 16:53:19 +0100

    corrected failing unit test

--- web/src/test/java/uk/ac/ebi/atlas/web/controllers/page/GenePageControllerTest.java
+++ web/src/test/java/uk/ac/ebi/atlas/web/controllers/page/GenePageControllerTest.java
@@ -108,7 +108,7 @@ public class GenePageControllerTest {
     public void testShowGenePage() throws Exception {
         when(differentialGeneProfileServiceMock.getDifferentialProfilesListMapForIdentifier(IDENTIFIER, GenePageController.DEFAULT_CUTOFF)).thenReturn(differentialGeneProfilePropertiesMock);
         when(differentialGeneProfilePropertiesMock.getFdrCutoff()).thenReturn(GenePageController.DEFAULT_CUTOFF);
-        assertThat(subject.showGenePage(IDENTIFIER, modelMock), is("bioEntity"));
+        assertThat(subject.showGenePage(null,IDENTIFIER, modelMock), is("bioEntity"));
         verify(modelMock).addAttribute(GenePageController.PROPERTY_TYPE_SYMBOL, SYMBOL);
         verify(differentialGeneProfileServiceMock).getDifferentialProfilesListMapForIdentifier(IDENTIFIER, GenePageController.DEFAULT_CUTOFF);
     }
