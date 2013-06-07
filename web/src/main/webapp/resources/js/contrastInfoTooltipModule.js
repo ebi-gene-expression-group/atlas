7cf324661752c9a286a2d91a5e05fc43ca555b6e
Benedetto Fiorelli <bennyfio@ebi.ac.uk>
2013-06-07 11:58:40 +0100

    started story about contrast summary on mouseover of differential gene page heatmap

---
+++ web/src/main/webapp/resources/js/contrastInfoTooltipModule.js
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2008-2013 Microarray Informatics Team, EMBL-European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ * For further details of the Gene Expression Atlas project, including source code,
+ * downloads and documentation, please see:
+ *
+ * http://gxa.github.com/gxa
+ */
+
+/*global $,jQuery,console,loadSliderAndPlot: false */
+/*jslint browser:true */
+/*jslint nomen: true*/
+var contrastInfoTooltipModule = (function($) {
+    "use strict";
+
+    function initTooltip(contextRoot){
+        var experimentAccession = $(this).attr("data-experiment-accession"),
+            contrastId = $(this).attr("data-contrast-id");
+
+        $(".contrastNameCell").attr("title","").tooltip({
+
+            hide:false,
+            show:false,
+            tooltipClass:"help-tooltip pvalue-tooltip-styling",
+            content: function (callback) {
+
+                //callback($("#contrastInfo").html());
+
+                $.ajax({
+                    url:contextRoot + "/rest/contrast-summary?experimentAccession=" + experimentAccession + "&contrastId=" + contrastId,
+                    type:"GET",
+                    success:function (data) {
+                        var experimentDescription = data.experimentDescription,
+                            contrastDescription = data.contrastDescription;
+
+                        $('#experimentDescription').text(experimentDescription);
+                        $('#contrastDescription').text(contrastDescription);
+                        for(var i = 0; i < data.properties.length; i++){
+                            var property = data.properties[i];
+                            var propertyName = property.propertyName;
+                            var testValue = property.testValue !== undefined ? property.testValue : "";
+                            var referenceValue = property.referenceValue !== undefined ? property.referenceValue : "";
+                            var tableRow = $("<tr><td>" + propertyName + "</td><td>" + testValue + "</td><td>" + referenceValue + "</td></tr>");
+                            if(testValue === referenceValue) {
+                                tableRow.css("font-weight","bold");
+                            } else {
+                                tableRow.css("font-color","gray");
+                            }
+                            $("#contrastInfo tbody").append(tableRow);
+                        }
+                        callback($("#contrastInfo").html());
+
+                    }
+                }).fail(function (data) {
+                        //"Sorry but there was an error: " + xhr.status + " " + xhr.statusText
+                        console.log("ERROR:  " + data);
+                        callback("ERROR: " + data);
+                });
+
+            }
+        });
+    }
+    return {
+        init:  function(contextRoot) {
+            initTooltip(contextRoot);
+
+        }
+
+    };
+}(jQuery));
---
+++ web/src/test/java/uk/ac/ebi/atlas/model/differential/ContrastPropertyTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2008-2013 Microarray Informatics Team, EMBL-European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ * For further details of the Gene Expression Atlas project, including source code,
+ * downloads and documentation, please see:
+ *
+ * http://gxa.github.com/gxa
+ */
+
+package uk.ac.ebi.atlas.model.differential;
+
+import com.google.common.collect.Sets;
+import org.junit.Test;
+
+import java.util.SortedSet;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.is;
+
+public class ContrastPropertyTest {
+    @Test
+    public void testHasEqualValues() throws Exception {
+        ContrastProperty property = new ContrastProperty("a","a","a");
+        assertThat(property.hasEqualValues(), is(true));
+
+        property = new ContrastProperty("a","z","a");
+        assertThat(property.hasEqualValues(), is(false));
+
+        property = new ContrastProperty("a","z",null);
+        assertThat(property.hasEqualValues(), is(false));
+    }
+
+    @Test
+    public void testCompareTo() throws Exception {
+        ContrastProperty property1 = new ContrastProperty("b","z","a");
+        ContrastProperty property2 = new ContrastProperty("a","a","a");
+        ContrastProperty property3 = new ContrastProperty("c",null,"a");
+        ContrastProperty property4 = new ContrastProperty("q","a","z");
+        ContrastProperty property5 = new ContrastProperty("d","a","a");
+
+        SortedSet<ContrastProperty> properties = Sets.newTreeSet(Sets.newHashSet(property1,property2,property3,property4,property5));
+
+        assertThat(properties, contains(property2,property5, property1, property3, property4));
+    }
+}
