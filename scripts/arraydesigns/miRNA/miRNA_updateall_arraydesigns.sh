899ac3a2adfa89a84f352e19a3478c73d7251e5d
Robert Petryszak <rpetry@ebi.ac.uk>
2013-07-01 16:26:19 +0100

    Imported Atlas-specific scripts from ereap; eliminated absolute paths in scripts

--- scripts/arraydesigns/miRNA/miRNA_updateall_arraydesigns.sh
+++ scripts/arraydesigns/miRNA/miRNA_updateall_arraydesigns.sh
@@ -2,7 +2,7 @@
 # A wrapper script to update all array designs in the current directory to the latest miRBase release
 # Author: rpetry@ebi.ac.uk
 
-pushd /nfs/ma/home/atlas3-production/arraydesigns/microRNA
+pushd $ATLAS_PROD/arraydesigns/microRNA
 
 wget ftp://anonymous:anonymous\@mirbase.org/pub/mirbase/CURRENT/aliases.txt.gz
 gunzip aliases.txt.gz
