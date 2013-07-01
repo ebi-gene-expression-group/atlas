d1493167d66c1c2fa1783b7d78e4e27f9a76f095
Robert Petryszak <rpetry@ebi.ac.uk>
2013-07-01 08:33:19 +0100

    Re-structured data production code directories

--- scripts/arraydesigns/miRNA/updateall.sh
+++ scripts/arraydesigns/miRNA/updateall.sh
@@ -2,13 +2,11 @@
 # A wrapper script to update all array designs in the current directory to the latest miRBase release
 # Author: rpetry@ebi.ac.uk
 
-pushd /nfs/ma/home/atlas3-production/arraydesigns/microRNA
-
 wget ftp://anonymous:anonymous\@mirbase.org/pub/mirbase/CURRENT/aliases.txt.gz
 gunzip aliases.txt.gz
 
 for ad in $(ls -d A-* ); do
    scripts/miRNA_arraydesign_to_mirBase.pl -adf $ad/$ad.adf.txt -out $ad/${ad}_reporter_mirbase.txt
 done
+
 mv aliases.txt miRBase
-popd
