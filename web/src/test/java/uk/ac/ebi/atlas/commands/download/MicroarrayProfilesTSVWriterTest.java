ad7f630e130a6ad84da864d481116f8c2cb1a83c
jtaubert <jtaubert@ebi.ac.uk>
2013-08-09 12:29:38 +0100

    merger with build index and switch to new solr

---
+++ web/src/test/java/uk/ac/ebi/atlas/commands/download/MicroarrayProfilesTSVWriterTest.java
@@ -0,0 +1,190 @@
+/*
+ * Copyright 2008-2013 Microarray Informatics Team, EMBL-European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ * For further details of the Gene Expression Atlas project, including source code,
+ * downloads and documentation, please see:
+ *
+ * http://gxa.github.com/gxa
+ */
+
+package uk.ac.ebi.atlas.commands.download;
+
+import com.google.common.collect.Sets;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.core.io.Resource;
+import uk.ac.ebi.atlas.commands.context.MicroarrayRequestContext;
+import uk.ac.ebi.atlas.commons.streams.ObjectInputStream;
+import uk.ac.ebi.atlas.geneannotation.GeneNamesProvider;
+import uk.ac.ebi.atlas.model.differential.Contrast;
+import uk.ac.ebi.atlas.model.differential.Regulation;
+import uk.ac.ebi.atlas.model.differential.microarray.MicroarrayExperiment;
+import uk.ac.ebi.atlas.model.differential.microarray.MicroarrayExpression;
+import uk.ac.ebi.atlas.model.differential.microarray.MicroarrayProfile;
+
+import java.io.ByteArrayInputStream;
+import java.io.PrintWriter;
+import java.util.Collections;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.arrayContaining;
+import static org.hamcrest.Matchers.contains;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MicroarrayProfilesTSVWriterTest {
+
+    private static final String HEADER_TEMPLATE = "# Expression Atlas version: 0.1.4-SNAPSHOT\n" +
+            "# Query: Genes matching: ''{0}''{1},{2}{3} differentially expressed in {4} given the False Discovery Rate cutoff: {5} in experiment {6}\n" +
+            "# Timestamp: {7}";
+    @Mock
+    private GeneNamesProvider geneNamesProviderMock;
+
+    @Mock
+    private MicroarrayProfile geneProfileMock;
+
+    @Mock
+    private MicroarrayExpression expressionMock;
+
+    @Mock
+    private MicroarrayRequestContext microarrayRequestContextMock;
+
+    @Mock
+    private ObjectInputStream<MicroarrayProfile> inputStreamMock;
+
+    @Mock
+    private PrintWriter responeWriterMock;
+
+    @Mock
+    private MicroarrayExperiment experimentMock;
+
+    @Mock
+    private Resource resourceMock;
+
+    private MicroarrayProfilesTSVWriter subject;
+
+    private TreeSet<Contrast> conditions;
+
+
+    @Before
+    public void initMocks() throws Exception {
+        when(geneProfileMock.getExpression(any(Contrast.class))).thenReturn(expressionMock);
+        when(microarrayRequestContextMock.getGeneQuery()).thenReturn("");
+        when(microarrayRequestContextMock.getSelectedQueryFactors()).thenReturn(Collections.EMPTY_SET);
+        when(microarrayRequestContextMock.getRegulation()).thenReturn(Regulation.UP);
+        when(microarrayRequestContextMock.getExperiment()).thenReturn(experimentMock);
+        when(experimentMock.getAccession()).thenReturn("ACCESSION");
+
+        when(resourceMock.getInputStream()).thenReturn(new ByteArrayInputStream(HEADER_TEMPLATE.getBytes()));
+
+        subject = new MicroarrayProfilesTSVWriter();
+        subject.setRequestContext(microarrayRequestContextMock);
+        subject.setTsvFileMastheadTemplateResource(resourceMock);
+        subject.initTsvFileMastheadTemplate();
+
+        Contrast contrast1 = mock(Contrast.class);
+        when(contrast1.getDisplayName()).thenReturn("cond1");
+        Contrast contrast2 = mock(Contrast.class);
+        when(contrast2.getDisplayName()).thenReturn("cond2");
+
+        conditions = Sets.newTreeSet();
+        conditions.add(contrast1);
+        conditions.add(contrast2);
+
+        //given
+        when(expressionMock.getFoldChange()).thenReturn(-0.978932452151424);
+        when(expressionMock.getLevel()).thenReturn(0.134707651014487);
+        when(expressionMock.getTstatistic()).thenReturn(0.0099999999);
+    }
+
+    @Test
+    public void buildConditionExpressionsHeaders() throws Exception {
+
+        String[] columnNames = subject.getConditionColumnHeaders(conditions);
+        assertThat(columnNames.length, is(6));
+        assertThat(columnNames, arrayContaining("cond1.p-value", "cond1.log2foldchange", "cond1.t-statistic", "cond2.p-value", "cond2.log2foldchange", "cond2.t-statistic"));
+    }
+
+    @Test
+    public void testBuildExpressionsRow() throws Exception {
+
+        SortedSet<Contrast> contrasts = new TreeSet<>();
+        contrasts.add(new Contrast("id1", null, null, null, "name"));
+
+        //when
+        String[] expressions = subject.extractConditionLevels(geneProfileMock, contrasts);
+
+        //then
+        assertThat(expressions.length, Matchers.is(3));
+        assertThat(expressions, is(new String[]{"0.134707651014487", "-0.978932452151424", "0.0099999999"}));
+
+    }
+
+    @Test
+    public void testGetValueToString() throws Exception {
+        assertThat(subject.getValueToString(0.134707651014487), is("0.134707651014487"));
+        assertThat(subject.getValueToString(Double.POSITIVE_INFINITY), is("NA"));
+        assertThat(subject.getValueToString(Double.NEGATIVE_INFINITY), is("NA"));
+    }
+
+    @Test
+    public void testRemoveTrailingZero() {
+        assertThat(subject.removeTrailingZero(1.111111), is("1.1111"));
+        assertThat(subject.removeTrailingZero(1.100111), is("1.1001"));
+        assertThat(subject.removeTrailingZero(1.111100), is("1.1111"));
+        assertThat(subject.removeTrailingZero(1.110000), is("1.11"));
+    }
+
+    @Test
+    public void testWrite() throws Exception {
+        subject.setResponseWriter(responeWriterMock);
+        subject.write(inputStreamMock, conditions);
+
+        verify(responeWriterMock).write(subject.getTsvFileMasthead() + "\n");
+        verify(responeWriterMock).write("Gene name\tDesign Element\tcond1.p-value\tcond1.log2foldchange\tcond1.t-statistic\tcond2.p-value\tcond2.log2foldchange\tcond2.t-statistic\n", 0, 131);
+
+        verify(responeWriterMock, times(1)).flush();
+    }
+
+    @Test
+    public void testGetExpressionColumnsHeaders() {
+        assertThat(subject.getExpressionColumnsHeaders(), contains("p-value", "log2foldchange", "t-statistic"));
+    }
+
+    @Test
+    public void testGetExpressionLevelData() {
+        assertThat(subject.getExpressionLevelData(expressionMock), contains(0.134707651014487, -0.978932452151424, 0.0099999999));
+    }
+
+    @Test
+    public void testGetProfileIdColumnHeaders() {
+        assertThat(subject.getProfileIdColumnHeaders(), arrayContaining(HeaderBuilder.GENE_NAME_COLUMN_NAME, HeaderBuilder.DESIGN_ELEMENT));
+    }
+
+    @Test
+    public void testGetSecondaryRowHeader() {
+        when(geneProfileMock.getDesignElementName()).thenReturn("NAME");
+        assertThat(subject.getSecondaryRowHeader(geneProfileMock), Matchers.is("NAME"));
+    }
+}
