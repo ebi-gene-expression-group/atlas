276b74e7454f64c71d70865fcb4f54061abdc893
nsklyar <nsklyar@ebi.ac.uk>
2013-09-09 15:52:01 +0100

    rolled back to single-core solr

--- web/src/main/java/uk/ac/ebi/atlas/solr/admin/index/conditions/ConditionsIndex.java
+++ web/src/main/java/uk/ac/ebi/atlas/solr/admin/index/conditions/ConditionsIndex.java
@@ -3,14 +3,12 @@ package uk.ac.ebi.atlas.solr.admin.index.conditions;
 import org.apache.log4j.Logger;
 import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.client.solrj.SolrServerException;
-import org.apache.solr.client.solrj.impl.HttpSolrServer;
 import org.springframework.context.annotation.Scope;
 import uk.ac.ebi.atlas.model.differential.DifferentialExperiment;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.IOException;
-import java.util.Collection;
 
 @Named
 @Scope("prototype")
@@ -30,24 +28,24 @@ public class ConditionsIndex {
 
     public void indexExperiment(DifferentialExperiment experiment) {
 
-        LOGGER.info("<indexExperiment> " + experiment.getAccession());
-
-        try {
-            deleteExperiment(experiment.getAccession());
-
-            Collection<ConditionProperty> conditionProperties = conditionPropertiesBuilder.buildProperties(experiment);
-
-            conditionsSolrServer.addBeans(conditionProperties);
-
-            conditionsSolrServer.commit();
-
-            //ToDO: (NK) Do we need to optimise after every experiment?
-            optimize();
-
-        } catch (SolrServerException | IOException e) {
-            LOGGER.error(e.getMessage(), e);
-            throw new IllegalStateException(e);
-        }
+//        LOGGER.info("<indexExperiment> " + experiment.getAccession());
+//
+//        try {
+//            deleteExperiment(experiment.getAccession());
+//
+//            Collection<ConditionProperty> conditionProperties = conditionPropertiesBuilder.buildProperties(experiment);
+//
+//            conditionsSolrServer.addBeans(conditionProperties);
+//
+//            conditionsSolrServer.commit();
+//
+//            //ToDO: (NK) Do we need to optimise after every experiment?
+//            optimize();
+//
+//        } catch (SolrServerException | IOException e) {
+//            LOGGER.error(e.getMessage(), e);
+//            throw new IllegalStateException(e);
+//        }
 
     }
 
--- web/src/main/java/uk/ac/ebi/atlas/solr/query/conditions/ConditionsSearchService.java
+++ web/src/main/java/uk/ac/ebi/atlas/solr/query/conditions/ConditionsSearchService.java
@@ -1,17 +1,12 @@
 package uk.ac.ebi.atlas.solr.query.conditions;
 
-import com.google.common.base.Function;
-import com.google.common.collect.Collections2;
+import com.google.common.collect.Lists;
 import org.apache.solr.client.solrj.SolrServer;
-import org.apache.solr.client.solrj.SolrServerException;
-import org.apache.solr.client.solrj.response.QueryResponse;
 import org.springframework.context.annotation.Scope;
-import uk.ac.ebi.atlas.solr.admin.index.conditions.ConditionProperty;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.util.Collection;
-import java.util.List;
 
 @Named
 @Scope("singleton")
@@ -28,21 +23,22 @@ public class ConditionsSearchService {
     }
 
     public Collection<IndexedContrast> findContrasts(String queryString) {
-        try {
-            QueryResponse queryResponse = conditionsSolrServer.query(queryBuilder.buildFullTestSearchQuery(queryString));
-            List<ConditionProperty> beans = queryResponse.getBeans(ConditionProperty.class);
-
-            Collection<IndexedContrast> result = Collections2.transform(beans, new Function<ConditionProperty, IndexedContrast>() {
-                @Override
-                public IndexedContrast apply(ConditionProperty conditionProperty) {
-                    return new IndexedContrast(conditionProperty.getExperimentAccession(), conditionProperty.getContrastId());
-                }
-            });
-
-            return result;
-        } catch (SolrServerException e) {
-            throw new IllegalStateException("Conditions index query failed!", e);
-        }
+//        try {
+//            QueryResponse queryResponse = conditionsSolrServer.query(queryBuilder.buildFullTestSearchQuery(queryString));
+//            List<ConditionProperty> beans = queryResponse.getBeans(ConditionProperty.class);
+//
+//            Collection<IndexedContrast> result = Collections2.transform(beans, new Function<ConditionProperty, IndexedContrast>() {
+//                @Override
+//                public IndexedContrast apply(ConditionProperty conditionProperty) {
+//                    return new IndexedContrast(conditionProperty.getExperimentAccession(), conditionProperty.getContrastId());
+//                }
+//            });
+//
+//            return result;
+//        } catch (SolrServerException e) {
+//            throw new IllegalStateException("Conditions index query failed!", e);
+//        }
+        return Lists.newArrayList();
     }
 
 }
