8d3a1ad1e3eba0dcb5234257e00e8ee0ef077170
Benedetto Fiorelli <bennyfio@ebi.ac.uk>
2013-09-10 11:36:34 +0100

    Restored multicore configurations

--- web/src/main/java/uk/ac/ebi/atlas/solr/query/conditions/ConditionsSearchService.java
+++ web/src/main/java/uk/ac/ebi/atlas/solr/query/conditions/ConditionsSearchService.java
@@ -1,12 +1,39 @@
+/*
+ * Copyright 2008-2013 Microarray Informatics Team, EMBL-European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ * For further details of the Gene Expression Atlas project, including source code,
+ * downloads and documentation, please see:
+ *
+ * http://gxa.github.com/gxa
+ */
+
 package uk.ac.ebi.atlas.solr.query.conditions;
 
-import com.google.common.collect.Lists;
+import com.google.common.base.Function;
+import com.google.common.collect.Collections2;
 import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.response.QueryResponse;
 import org.springframework.context.annotation.Scope;
+import uk.ac.ebi.atlas.solr.admin.index.conditions.ConditionProperty;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.util.Collection;
+import java.util.List;
 
 @Named
 @Scope("singleton")
@@ -23,22 +50,21 @@ public class ConditionsSearchService {
     }
 
     public Collection<IndexedContrast> findContrasts(String queryString) {
-//        try {
-//            QueryResponse queryResponse = conditionsSolrServer.query(queryBuilder.buildFullTestSearchQuery(queryString));
-//            List<ConditionProperty> beans = queryResponse.getBeans(ConditionProperty.class);
-//
-//            Collection<IndexedContrast> result = Collections2.transform(beans, new Function<ConditionProperty, IndexedContrast>() {
-//                @Override
-//                public IndexedContrast apply(ConditionProperty conditionProperty) {
-//                    return new IndexedContrast(conditionProperty.getExperimentAccession(), conditionProperty.getContrastId());
-//                }
-//            });
-//
-//            return result;
-//        } catch (SolrServerException e) {
-//            throw new IllegalStateException("Conditions index query failed!", e);
-//        }
-        return Lists.newArrayList();
+        try {
+            QueryResponse queryResponse = conditionsSolrServer.query(queryBuilder.buildFullTestSearchQuery(queryString));
+            List<ConditionProperty> beans = queryResponse.getBeans(ConditionProperty.class);
+
+            Collection<IndexedContrast> result = Collections2.transform(beans, new Function<ConditionProperty, IndexedContrast>() {
+                @Override
+                public IndexedContrast apply(ConditionProperty conditionProperty) {
+                    return new IndexedContrast(conditionProperty.getExperimentAccession(), conditionProperty.getContrastId());
+                }
+            });
+
+            return result;
+        } catch (SolrServerException e) {
+            throw new IllegalStateException("Conditions index query failed!", e);
+        }
     }
 
 }
