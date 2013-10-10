ba828116cb81bca1b1eab9544e54cf5d15f3abf7
nsklyar <nsklyar@ebi.ac.uk>
2013-10-10 14:26:27 +0100

    deleted unused classes

--- web/src/main/java/uk/ac/ebi/atlas/commands/download/MicroarrayAnalyticsDataHeaderBuilder.java
+++
@@ -1,9 +0,0 @@
-//package uk.ac.ebi.atlas.commands.download;
-//
-//class MicroarrayAnalyticsDataHeaderBuilder extends AnalyticsDataHeaderBuilder {
-//
-//    @Override
-//    protected String getSecondColumnName() {
-//        return DESIGN_ELEMENT;
-//    }
-//}
