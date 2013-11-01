8847e0a8fc66aa7ee6478812d4a05ef458bd587e
Robert Petryszak <rpetry@ebi.ac.uk>
2013-11-01 16:18:23 +0000

    Turned VW_DIFFANALYTICS to materialized view to speed up the query

--- web/src/main/resources/atlas3-schema.sql
+++ web/src/main/resources/atlas3-schema.sql
@@ -77,15 +77,20 @@ CREATE INDEX RNASEQ_DIFF_ANALYTICS_IDX ON RNASEQ_DIFF_ANALYTICS (EXPERIMENT, CON
 );
 
 -- Differential analitics across all types of experiments, with additional gene name and organism information
-CREATE OR REPLACE FORCE VIEW VW_DIFFANALYTICS (IDENTIFIER, DESIGNELEMENT, NAME, ORGANISM, EXPERIMENT, CONTRASTID, ISACTIVE, PVAL, LOG2FOLD, TSTAT) AS
-select dem.IDENTIFIER, mda.DESIGNELEMENT, bn.NAME, o.NAME, mda.EXPERIMENT, mda.CONTRASTID, mda.ISACTIVE, mda.PVAL, mda.LOG2FOLD, mda.TSTAT 
+DROP MATERIALIZED VIEW VW_DIFFANALYTICS;
+CREATE MATERIALIZED VIEW VW_DIFFANALYTICS 
+NOLOGGING PARALLEL 16
+BUILD DEFERRED
+REFRESH COMPLETE ON DEMAND 
+ENABLE QUERY REWRITE AS
+select dem.IDENTIFIER, mda.DESIGNELEMENT, bn.NAME AS NAME, o.NAME AS ORGANISM, mda.EXPERIMENT, mda.CONTRASTID, mda.ISACTIVE, mda.PVAL, mda.LOG2FOLD, mda.TSTAT 
 from MICROARRAY_DIFF_ANALYTICS subpartition( BELOW_DEFAULT_FDR ) mda
 join DESIGNELEMENT_MAPPING dem on mda.designelement=dem.designelement and mda.arraydesign = dem.arraydesign
 join BIOENTITY_NAME bn on dem.identifier=bn.identifier  
 join BIOENTITY_ORGANISM o on bn.organismid = o.organismid
 join EXPERIMENT_ORGANISM eo on o.name = eo.bioentity_organism and eo.experiment = mda.experiment
 union all
-select rda.IDENTIFIER, null, bn.NAME, o.name, rda.EXPERIMENT, rda.CONTRASTID, rda.ISACTIVE, rda.PVAL, rda.LOG2FOLD, null 
+select rda.IDENTIFIER, null, bn.NAME AS NAME, o.name AS ORGANISM, rda.EXPERIMENT, rda.CONTRASTID, rda.ISACTIVE, rda.PVAL, rda.LOG2FOLD, null 
 from RNASEQ_DIFF_ANALYTICS subpartition( BELOW_DEFAULT_FDR ) rda
 join BIOENTITY_NAME bn on rda.IDENTIFIER=bn.identifier
 join BIOENTITY_ORGANISM o on bn.organismid = o.organismid
