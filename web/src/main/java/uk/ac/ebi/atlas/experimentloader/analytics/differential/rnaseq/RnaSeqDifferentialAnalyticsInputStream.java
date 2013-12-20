6f959e698e75726af3e2ec8f17151b39dcd29c54
Oliver Mannion <omannion@ebi.ac.uk>
2013-12-20 11:53:19 +0000

    add RnaSeqDifferentialAnalyticsInputStream

--- web/src/main/java/uk/ac/ebi/atlas/experimentloader/analytics/differential/rnaseq/RnaSeqDifferentialAnalyticsInputStream.java
+++ web/src/main/java/uk/ac/ebi/atlas/experimentloader/analytics/differential/rnaseq/RnaSeqDifferentialAnalyticsInputStream.java
@@ -1,4 +1,4 @@
-package uk.ac.ebi.atlas.experimentloader.analytics.differential.microarray;
+package uk.ac.ebi.atlas.experimentloader.analytics.differential.rnaseq;
 
 import au.com.bytecode.opencsv.CSVReader;
 import com.google.common.collect.ImmutableList;
@@ -12,36 +12,35 @@ import java.io.IOException;
 import java.util.LinkedList;
 import java.util.Queue;
 
-import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkState;
 
 /*
  * Reads tsv input of (NB: there may be multiple contrasts defined by the header):
  *
- * Gene ID	            Gene Name   Design Element  g2_g1.p-value   g2_g1.t-statistic   g2_g1.log2foldchange    g2_g3.p-value   g2_g3.t-statistic   g2_g3.log2foldchange
- * ENSMUSG00000041538   H2-Ob       1422201_at      0.1             0.2                 0.3                     0.7             0.8                 0.9
+ * Gene ID	            Gene Name   g2_g1.p-value   g2_g1.log2foldchange    g2_g3.p-value   g2_g3.log2foldchange
+ * ENSMUSG00000041538   H2-Ob       0.1             0.3                     0.7             0.9
  *
- * and returns MicroarrayDifferentialAnalytics of:
+ * and returns RnaSeqDifferentialAnalytics of:
  *
- * 1422201_at, g2_g1, 0.1, 0.2, 0.3
- * 1422201_at, g2_g4, 0.7, 0.8, 0.9
+ * ENSMUSG00000041538, g2_g1, 0.1, 0.3
+ * ENSMUSG00000041538, g2_g4, 0.7, 0.9
  *
- * If any of p-value, t-statistic, or log2foldchange contain NA then skip to next set
+ * If any of p-value or log2foldchange contain NA then skip to next set
  * "inf" in any value is interpreted as Double.POSITIVE_INFINITY
  * "-inf" in any value is interpreted as Double.NEAGTIVE_INFINITY
  */
-public class MicroarrayDifferentialAnalyticsInputStream implements ObjectInputStream<MicroarrayDifferentialAnalytics> {
+public class RnaSeqDifferentialAnalyticsInputStream implements ObjectInputStream<RnaSeqDifferentialAnalytics> {
 
-    private static final Logger LOGGER = Logger.getLogger(MicroarrayDifferentialAnalyticsInputStream.class);
+    private static final Logger LOGGER = Logger.getLogger(RnaSeqDifferentialAnalyticsInputStream.class);
 
-    private static final int DESIGN_ELEMENT_INDEX = 2;
-    private static final int FIRST_CONTRAST_INDEX = 3;
+    private static final int GENE_ID_INDEX = 0;
+    private static final int FIRST_CONTRAST_INDEX = 2;
 
     private final CSVReader csvReader;
-    private final Queue<MicroarrayDifferentialAnalytics> queue = new LinkedList<>();
+    private final Queue<RnaSeqDifferentialAnalytics> queue = new LinkedList<>();
     private final ImmutableList<String> contrastIds;
 
-    public MicroarrayDifferentialAnalyticsInputStream(CSVReader csvReader) {
+    public RnaSeqDifferentialAnalyticsInputStream(CSVReader csvReader) {
         this.csvReader = csvReader;
         String[] headers = readCsvLine();
         String[] contrastHeaders = (String[]) ArrayUtils.subarray(headers, FIRST_CONTRAST_INDEX, headers.length);
@@ -63,9 +62,9 @@ public class MicroarrayDifferentialAnalyticsInputStream implements ObjectInputSt
     }
 
     @Override
-    public MicroarrayDifferentialAnalytics readNext() {
+    public RnaSeqDifferentialAnalytics readNext() {
         if (queue.isEmpty()) {
-            ImmutableList<MicroarrayDifferentialAnalytics> analytics = readNextContrasts();
+            ImmutableList<RnaSeqDifferentialAnalytics> analytics = readNextContrasts();
 
             if (analytics == null) {
                 //EOF
@@ -78,42 +77,38 @@ public class MicroarrayDifferentialAnalyticsInputStream implements ObjectInputSt
         return queue.remove();
     }
 
-    private ImmutableList<MicroarrayDifferentialAnalytics> readNextContrasts() {
+    private ImmutableList<RnaSeqDifferentialAnalytics> readNextContrasts() {
         String[] line = readCsvLine();
         if (line == null) {
             // EOF
             return null;
         }
 
-        String designElement = line[DESIGN_ELEMENT_INDEX];
+        String geneId = line[GENE_ID_INDEX];
         String[] contrastAnalyticsArray = (String[]) ArrayUtils.subarray(line, FIRST_CONTRAST_INDEX, line.length);
         ImmutableList<String> contrastAnalyticsList = ImmutableList.<String>builder().add(contrastAnalyticsArray).build();
         UnmodifiableIterator<String> contrastAnalytics = contrastAnalyticsList.iterator();
 
-        ImmutableList.Builder<MicroarrayDifferentialAnalytics> builder = ImmutableList.builder();
+        ImmutableList.Builder<RnaSeqDifferentialAnalytics> builder = ImmutableList.builder();
 
         for (String contrastId : contrastIds) {
 
-            checkState(contrastAnalytics.hasNext(), String.format("missing p-value for design element %s, contrast %s", designElement,contrastId));
+            checkState(contrastAnalytics.hasNext(), String.format("missing p-value for gene %s, contrast %s", geneId,contrastId));
             String pValueString = contrastAnalytics.next();
 
-            checkState(contrastAnalytics.hasNext(), String.format("missing t-statistic for design element %s, contrast %s", designElement,contrastId));
-            String tStatisticString = contrastAnalytics.next();
-
-            checkState(contrastAnalytics.hasNext(), String.format("missing log2foldchange for design element %s, contrast %s", designElement,contrastId));
+            checkState(contrastAnalytics.hasNext(), String.format("missing log2foldchange for gene %s, contrast %s", geneId,contrastId));
             String foldChangeString = contrastAnalytics.next();
 
-            if (!("NA".equalsIgnoreCase(pValueString) || "NA".equalsIgnoreCase(tStatisticString) || "NA".equalsIgnoreCase(foldChangeString))) {
+            if (!("NA".equalsIgnoreCase(pValueString) || "NA".equalsIgnoreCase(foldChangeString))) {
                 double pValue = DifferentialTsvFileParsingUtil.parseDouble(pValueString);
-                double tStatistic = DifferentialTsvFileParsingUtil.parseDouble(tStatisticString);
                 double foldChange = DifferentialTsvFileParsingUtil.parseDouble(foldChangeString);
-                MicroarrayDifferentialAnalytics dto = new MicroarrayDifferentialAnalytics(designElement, contrastId, pValue, foldChange, tStatistic);
+                RnaSeqDifferentialAnalytics dto = new RnaSeqDifferentialAnalytics(geneId, contrastId, pValue, foldChange);
                 builder.add(dto);
             }
 
         }
 
-        ImmutableList<MicroarrayDifferentialAnalytics> analytics = builder.build();
+        ImmutableList<RnaSeqDifferentialAnalytics> analytics = builder.build();
 
         if (analytics.isEmpty()) {
             // read next line if no analytics extracted from this line
