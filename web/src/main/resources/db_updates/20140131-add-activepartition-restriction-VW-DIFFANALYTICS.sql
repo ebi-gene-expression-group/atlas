03e2eea7b35252c69545fb586cf2a16bebfcbd86
barrera <barrera@ebi.ac.uk>
2014-01-31 14:03:37 +0000

    DATABASE CHANGE: removed restriction reporting only genes that have the same species

--- web/src/main/resources/db_updates/20140131-add-activepartition-restriction-VW-DIFFANALYTICS.sql
+++ web/src/main/resources/db_updates/20140131-add-activepartition-restriction-VW-DIFFANALYTICS.sql
@@ -10,7 +10,6 @@ from MICROARRAY_DIFF_ANALYTICS subpartition( BELOW_DEFAULT_FDR ) mda
 join DESIGNELEMENT_MAPPING dem on mda.designelement=dem.designelement and mda.arraydesign = dem.arraydesign
 join BIOENTITY_NAME bn on dem.identifier=bn.identifier
 join BIOENTITY_ORGANISM o on bn.organismid = o.organismid
-join EXPERIMENT_ORGANISM eo on o.name = eo.bioentity_organism and eo.experiment = mda.experiment
 where mda.ISACTIVE='T'
 union all
 select rda.IDENTIFIER, null, bn.NAME AS NAME, o.name AS ORGANISM, rda.EXPERIMENT, rda.CONTRASTID, rda.ISACTIVE, rda.PVAL, rda.LOG2FOLD, null
