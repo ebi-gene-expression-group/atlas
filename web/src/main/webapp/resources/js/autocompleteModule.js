315b6cc6da6de4614b9cc7908722baa7c56657da
Oliver Mannion <omannion@ebi.ac.uk>
2014-03-12 10:27:07 +0000

    automatically add space after selecting autocomplete suggestion

--- web/src/main/webapp/resources/js/autocompleteModule.js
+++ web/src/main/webapp/resources/js/autocompleteModule.js
@@ -43,7 +43,7 @@ var autocompleteModule = (function($) {
                 select: function( event, ui ) {
                     var selectedValue = quoteTextThatContainsMoreThanOneWord(ui.item.value.trim()),
                         lastItem = extractLast(this.value);
-                    this.value = this.value.substr(0, this.value.length - lastItem.length).concat(selectedValue);
+                    this.value = this.value.substr(0, this.value.length - lastItem.length).concat(selectedValue) + " ";
                     return false;
                 },
                 source:function (request, response) {
