b8a4c2fa0f4174afe4caf4d677cb77771ce699de
Oliver Mannion <omannion@ebi.ac.uk>
2014-03-14 18:52:55 +0000

    migration script for story "In differential cross-experiment view display the highest log2fold change for a given gene identifier-contrast (and hide design element accessions)"

---
+++ web/src/main/resources/db_updates/20140314-display-highest-logfoldchange-only.sql
@@ -0,0 +1,25 @@
+DROP MATERIALIZED VIEW VW_DIFFANALYTICS;
+CREATE MATERIALIZED VIEW VW_DIFFANALYTICS
+NOLOGGING PARALLEL 16
+BUILD DEFERRED
+REFRESH COMPLETE ON DEMAND
+ENABLE QUERY REWRITE AS
+select IDENTIFIER, NAME, ORGANISM, EXPERIMENT, CONTRASTID, PVAL, LOG2FOLD, TSTAT
+from (
+select dem.IDENTIFIER, bn.NAME AS NAME, o.NAME AS ORGANISM, mda.EXPERIMENT, mda.CONTRASTID, mda.ISACTIVE, mda.PVAL, mda.LOG2FOLD, mda.TSTAT
+,rank() over(partition by mda.EXPERIMENT, mda.CONTRASTID, dem.IDENTIFIER order by(abs(mda.LOG2FOLD)) desc) as lfrank
+from MICROARRAY_DIFF_ANALYTICS subpartition( BELOW_DEFAULT_FDR ) mda
+join DESIGNELEMENT_MAPPING dem on mda.designelement=dem.designelement and mda.arraydesign = dem.arraydesign
+join BIOENTITY_NAME bn on dem.identifier=bn.identifier
+join BIOENTITY_ORGANISM o on bn.organismid = o.organismid
+) where lfrank = 1
+union all
+select rda.IDENTIFIER, bn.NAME AS NAME, o.name AS ORGANISM, rda.EXPERIMENT, rda.CONTRASTID, rda.PVAL, rda.LOG2FOLD, null
+from RNASEQ_DIFF_ANALYTICS subpartition( BELOW_DEFAULT_FDR ) rda
+join BIOENTITY_NAME bn on rda.IDENTIFIER=bn.identifier
+join BIOENTITY_ORGANISM o on bn.organismid = o.organismid
+join EXPERIMENT_ORGANISM eo on o.name = eo.bioentity_organism and eo.experiment = rda.experiment;
+
+-- run to here in first step, then run the following in a separate second step:
+
+exec dbms_mview.refresh( 'VW_DIFFANALYTICS', 'C' );
