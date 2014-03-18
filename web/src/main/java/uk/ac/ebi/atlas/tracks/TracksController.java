2c1acda3824f9b825f98ab3cb93aadba4ba5ff21
Oliver Mannion <omannion@ebi.ac.uk>
2014-03-18 13:58:18 +0000

    tidy

--- web/src/main/java/uk/ac/ebi/atlas/tracks/TracksController.java
+++ web/src/main/java/uk/ac/ebi/atlas/tracks/TracksController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.io.IOException;
-import java.text.MessageFormat;
 
 @Controller
 @Scope("singleton")
--- web/src/main/java/uk/ac/ebi/atlas/web/controllers/page/experimentdesign/DifferentialDesignPageController.java
+++ web/src/main/java/uk/ac/ebi/atlas/web/controllers/page/experimentdesign/DifferentialDesignPageController.java
@@ -31,7 +31,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import uk.ac.ebi.atlas.model.Experiment;
 import uk.ac.ebi.atlas.model.differential.Contrast;
 import uk.ac.ebi.atlas.model.differential.DifferentialExperiment;
 import uk.ac.ebi.atlas.model.differential.microarray.MicroarrayExperiment;
