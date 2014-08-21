f66f4b5200f1f387717b2623d5568e0348e97651
Oliver Mannion <omannion@ebi.ac.uk>
2014-08-21 14:15:49 +0100

    add test

---
+++ web/src/test/java/uk/ac/ebi/atlas/widget/HeatmapWidgetControllerMultiSpeciesExperimentSIT.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2008-2013 Microarray Informatics Team, EMBL-European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ * For further details of the Gene Expression Atlas project, including source code,
+ * downloads and documentation, please see:
+ *
+ * http://gxa.github.com/gxa
+ */
+
+package uk.ac.ebi.atlas.widget;
+
+import org.junit.Before;
+import org.junit.Test;
+import uk.ac.ebi.atlas.acceptance.selenium.fixture.SeleniumFixture;
+import uk.ac.ebi.atlas.acceptance.selenium.pages.HeatmapTableWidgetPage;
+
+import static org.hamcrest.CoreMatchers.endsWith;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.startsWith;
+import static org.hamcrest.core.StringContains.containsString;
+
+public class HeatmapWidgetControllerMultiSpeciesExperimentSIT extends SeleniumFixture {
+
+    private static final String GENE_IN_MULTI_SPECIES_EXPERIMENT = "ENSGALG00000006591";
+
+    private HeatmapTableWidgetPage heatmapTablePage;
+
+    @Before
+    public void initPage(){
+        heatmapTablePage = new HeatmapTableWidgetPage(driver, "geneQuery=" + GENE_IN_MULTI_SPECIES_EXPERIMENT + "&propertyType=bioentity_identifier");
+        heatmapTablePage.get();
+    }
+
+    @Test
+    public void header() {
+        assertThat(heatmapTablePage.getAnatomogram().isDisplayed(), is(true));
+        assertThat(heatmapTablePage.getGeneCount(), containsString("of 1"));
+
+        String experimentDescription = heatmapTablePage.getExperimentDescription();
+        assertThat(experimentDescription, startsWith("RNA-seq of poly-A enriched total RNA of brain, liver, kidney, heart and skeletal muscle samples from 5 vertebrate species: mouse, chicken, frog, lizard and pufferfish"));
+
+        String experimentDescriptionLink = heatmapTablePage.getExperimentDescriptionLink();
+        assertThat(experimentDescriptionLink, endsWith("/experiments/E-GEOD-41338?geneQuery=ENSGALG00000006591&serializedFilterFactors=ORGANISM:Gallus%20gallus"));
+    }
+
+    @Test
+    public void heatmap() {
+        assertThat(heatmapTablePage.getGeneNames(), hasSize(1));
+
+        String firstGeneName = heatmapTablePage.getGeneNames().get(0);
+        assertThat(firstGeneName, is("TNNI2"));
+    }
+
+}
