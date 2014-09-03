2f6b0fca270833cc66764d8f55af407253c560d1
Oliver Mannion <omannion@ebi.ac.uk>
2014-09-03 16:58:34 +0100

    fix test due to change in uniprot mapping for ENSP00000355434

--- web/src/test/java/uk/ac/ebi/atlas/bioentity/ProteinPageControllerSIT.java
+++ web/src/test/java/uk/ac/ebi/atlas/bioentity/ProteinPageControllerSIT.java
@@ -26,7 +26,6 @@ import org.junit.Test;
 import uk.ac.ebi.atlas.acceptance.selenium.fixture.SinglePageSeleniumFixture;
 import uk.ac.ebi.atlas.acceptance.selenium.pages.BioEntityPage;
 
-import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -44,50 +43,26 @@ public class ProteinPageControllerSIT extends SinglePageSeleniumFixture {
         subject.get();
     }
 
-    @Test
-    public void checkGeneCardTitle() {
-        assertThat(subject.getBioEntityCardTitle(), is("Q8N349 Homo sapiens olfactory receptor, family 2, subfamily L, member 13"));
-    }
-
     //This will fail with PhantomJS
     @Test
     public void checkCardExpansion() throws UnknownHostException {
         assertThat(subject.isInfoCardExpanded(), is(true));
-
-        String hostName = InetAddress.getLocalHost().getHostName();
-
-        //TODO: fix when running on lime
-        if (!hostName.equals("coconut.ebi.ac.uk")) {
-            subject.clickInfoCard(false);
-            assertThat(subject.isInfoCardExpanded(), is(false));
-        }
+        subject.clickInfoCard(false);
+        assertThat(subject.isInfoCardExpanded(), is(false));
     }
 
     @Test
-    public void checkTableSize() {
+    public void bioEntityPropertyPane() {
+        assertThat(subject.getBioEntityCardTitle(), is("Q8N349 Homo sapiens olfactory receptor, family 2, subfamily L, member 13"));
         assertThat(subject.getPropertiesTableSize(), is(6));
-    }
-
-    @Test
-    public void checkTableRows() {
         assertThat(subject.getPropertiesTableRow(0), hasItems("Ensembl Protein", "ENSP00000355434"));
         assertThat(subject.getPropertiesTableRow(1), hasItems("Ensembl Transcript"));
         assertThat(subject.getPropertiesTableRow(1).get(1), containsString("ENST00000366478"));
         assertThat(subject.getPropertiesTableRow(3), hasItems("UniProt", "Q8N349"));
-        assertThat(subject.getPropertiesTableRow(5), hasItems("Reactome", "Signal Transduction"));
-    }
-
-    @Test
-    public void checkLinksInTable() throws UnknownHostException {
+        assertThat(subject.getPropertiesTableRow(5), hasItems("Reactome", "Olfactory Signaling Pathway"));
         assertThat(subject.getLinksInTableRow(1).get(0), containsString("http://www.ensemblgenomes.org/id/ENST00000366478"));
         assertThat(subject.getLinksInTableRow(2).get(0), startsWith("http://www.ensemblgenomes.org/id-gene/ENSG00000196071"));
         assertThat(subject.getLinksInTableRow(3).get(0), startsWith("http://www.uniprot.org/uniprot/Q8N349"));
-
-        String hostName = InetAddress.getLocalHost().getHostName();
-
-        //TODO: fix when running on lime
-        if (!hostName.equals("coconut.ebi.ac.uk")) {
-            assertThat(subject.getLinksInTableRow(5).get(0), is("http://www.reactome.org/cgi-bin/eventbrowser_st_id?ST_ID=REACT_111102"));
-        }
+        assertThat(subject.getLinksInTableRow(5).get(0), is("http://www.reactome.org/cgi-bin/eventbrowser_st_id?ST_ID=REACT_15488"));
     }
 }
