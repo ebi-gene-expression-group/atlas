411d47419d66e83e8f6865bbdd277349061222f6
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2014-12-01 11:39:43 +0000

    Stub for Pubmed mined ids

---
+++ web/src/main/webapp/resources/js/pubmedMinedBioentitiesModule.js
@@ -0,0 +1,13 @@
+var pubmedMinedBioentitiesModule = (function($) {
+    "use strict";
+
+    function fetchPubmedMinedBioentities(pubmedId, callback) {
+
+
+        callback(["id1", "id2"]);
+    }
+
+    return {
+        fetchPubmedMinedBioentities: fetchPubmedMinedBioentities
+    };
+} (jQuery));
