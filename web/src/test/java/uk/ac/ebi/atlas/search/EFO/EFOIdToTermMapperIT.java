327a1d19e1ee07a07dd11d93ec60df49b97c6102
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2014-12-19 16:03:20 +0000

    Relaxed test to pass with version 2.54

--- web/src/test/java/uk/ac/ebi/atlas/search/EFO/EFOIdToTermMapperIT.java
+++ web/src/test/java/uk/ac/ebi/atlas/search/EFO/EFOIdToTermMapperIT.java
@@ -9,6 +9,7 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import javax.inject.Inject;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -21,6 +22,6 @@ public class EFOIdToTermMapperIT {
 
     @Test
     public void getIdsFromTerm() {
-        assertThat(subject.getIdsFromTerm("cancer").size(), is(34));
+        assertThat(subject.getIdsFromTerm("cancer").size(), is(greaterThanOrEqualTo(44)));
     }
 }
