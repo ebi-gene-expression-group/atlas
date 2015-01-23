c0d965b47226a4efad42e1aa4c31a3dbab96b958
Oliver Mannion <omannion@ebi.ac.uk>
2015-01-23 12:26:41 +0100

    add current solr_dev.sh and solr_test.sh scripts add _rest_managed.json (created by solr hs)

---
+++ solr/scripts/solr_dev.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# description: Starts and stops Solr production
+set -e
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+SOLR_DIR=/nfs/public/rw/fg/atlas/solr_dev/example
+JAVA_OPTIONS="-Dsolr.indexes.dir=/srv/gxa/solr -Dsolr.solr.home=./solr -Djava.util.logging.config.file=solr_dev/logging.properties -server -DSTOP.PORT=8079 -DSTOP.KEY=stopkey -Xmx8192M -Xms1024M -jar start.jar"
+JAVA=/nfs/public/rw/webadmin/java/jdks/jdk1.7.0_04/bin/java
+LOG_FILE="/srv/gxa/solr/log/console.log"
+case $1 in
+	start)
+		echo "Starting Solr"
+		cd $SOLR_DIR
+		nohup $JAVA $JAVA_OPTIONS 2> $LOG_FILE &
+		echo "ok - remember it may take a minute or two before Solr responds on requests"
+		;;
+	stop)
+		echo "Stopping Solr"
+		cd $SOLR_DIR
+		$JAVA $JAVA_OPTIONS --stop
+    		echo "ok"
+		;;
+	restart)
+		$0 stop
+		sleep 3
+		$0 start
+		;;
+	*)
+		echo "Usage: $0 {start|stop|restart}" >&2
+		exit 1
+		;;
+esac
