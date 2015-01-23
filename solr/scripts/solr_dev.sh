1cc4e6bd64d2af40d293bb48c58eed8fe169147e
Oliver Mannion <omannion@ebi.ac.uk>
2015-01-23 17:08:51 +0100

    split out config directory, update to use log4j

--- solr/scripts/solr_dev.sh
+++ solr/scripts/solr_dev.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 # description: Starts and stops Solr production
+SOLR_DIST=/nfs/public/rw/fg/atlas/solr_dev/
+SOLR_INDEXES_DIR=/srv/gxa/solr
+SOLR_CONF=/nfs/public/rw/fg/atlas/solr_dev_conf
+SOLR_LOG=/srv/gxa/solr/log/
+SOLR_BIN=${SOLR_DIST}/example
+JAVA="/nfs/public/rw/webadmin/java/jdks/jdk1.7.0_04/bin/java"
+JAVA_OPTIONS="-Dsolr.indexes.dir=$SOLR_INDEXES_DIR -Dsolr.solr.home=$SOLR_CONF -Dsolr.log=$SOLR_LOG -Dlog4j.configuration=file:$SOLR_CONF/log4j.properties -server -DSTOP.PORT=8079 -DSTOP.KEY=stopkey -Xmx8192M -Xms1024M -jar start.jar"
+CONSOLE_LOG=/srv/gxa/solr/log/console.log
 set -e
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-SOLR_DIR=/nfs/public/rw/fg/atlas/solr_dev/example
-JAVA_OPTIONS="-Dsolr.indexes.dir=/srv/gxa/solr -Dsolr.solr.home=./solr -Djava.util.logging.config.file=solr_dev/logging.properties -server -DSTOP.PORT=8079 -DSTOP.KEY=stopkey -Xmx8192M -Xms1024M -jar start.jar"
-JAVA=/nfs/public/rw/webadmin/java/jdks/jdk1.7.0_04/bin/java
-LOG_FILE="/srv/gxa/solr/log/console.log"
 case $1 in
 	start)
 		echo "Starting Solr"
-		cd $SOLR_DIR
-		nohup $JAVA $JAVA_OPTIONS 2> $LOG_FILE &
+		cd $SOLR_BIN
+		nohup $JAVA $JAVA_OPTIONS 2> $CONSOLE_LOG &
 		echo "ok - remember it may take a minute or two before Solr responds on requests"
 		;;
 	stop)
 		echo "Stopping Solr"
-		cd $SOLR_DIR
+		cd $SOLR_BIN
 		$JAVA $JAVA_OPTIONS --stop
     		echo "ok"
 		;;
