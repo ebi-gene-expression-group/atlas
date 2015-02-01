3db72c38c701524f89cfc98ab2daacc852dfe20e
Oliver Mannion <omannion@ebi.ac.uk>
2015-02-01 20:04:53 +0100

    remove page.js and use history API directly

--- web/src/main/webapp/resources/jsx/heatmapsRouter.jsx
+++ web/src/main/webapp/resources/jsx/heatmapsRouter.jsx
@@ -1,109 +1,121 @@
 //var React = require('react');
-//var page = require('page');
 
 var HeatmapsRouter = (function (React, page, queryString, facetsModule, Heatmaps) {
 
-  var Facets = facetsModule.build().Facets;
+    var Facets = facetsModule.build().Facets;
 
-  return function (facetsElement, heatmapsElement, facetData) {
+    return function (facetsElement, heatmapsElement, facetData) {
 
-    page('*', parseQueryString);
-    page('*', render);
-    page.start();
+        //TODO: add this outside the module, when module is first loaded
+        window.addEventListener('popstate', onPopState, false);
 
-    function parseQueryString(ctx, next) {
-      ctx.query = queryString.parse(ctx.querystring);
-      ctx.query.select = ctx.query.select && JSON.parse(ctx.query.select);
-      next();
-    }
+        renderMain();
 
-    function render(ctx) {
-      React.renderComponent(Facets({facets: facetData, checkedFacets: ctx.query.select, setChecked: setChecked}),
-          facetsElement
-      );
-
-      React.renderComponent(Heatmaps({geneQuery: ctx.query.geneQuery, heatmaps: queryToHeatmaps(ctx.query)}),
-          heatmapsElement
-      );
-
-      function setChecked(checked, species, factor) {
-        console.log(ctx.query);
-        console.log("setChecked " + species + " " + factor + ": " + checked);
-
-        var newSelect = checked ? addSelection(ctx.query.select, species, factor) : removeSelection(ctx.query.select, species, factor);
-        var newQueryString = "?geneQuery=" + ctx.query.geneQuery + "&select=" + JSON.stringify(newSelect);
-        console.log(newQueryString);
-        page(ctx.pathname + newQueryString);
-      }
-
-
-      function addSelection(select, species, factor) {
-        if (!select) {
-          select = {};
+        function onPopState() {
+            renderMain();
         }
 
-        if (!select[species]) {
-          select[species] = {};
+        function renderMain() {
+            var ctx = {};
+            ctx.pathname = window.location.pathname;
+            ctx.query = queryString.parse(window.location.search.slice(1));
+            ctx.query.select = ctx.query.select && JSON.parse(ctx.query.select);
+            render(ctx);
         }
-        select[species][factor] = true;
-        return select;
-      }
 
-      function removeSelection(select, species, factor) {
-        select[species][factor] = false;
-        return select;
-      }
+        function render(ctx) {
+            React.renderComponent(Facets({facets: facetData, checkedFacets: ctx.query.select, setChecked: setChecked}),
+                facetsElement
+            );
 
-    }
+            React.renderComponent(Heatmaps({geneQuery: ctx.query.geneQuery, heatmaps: queryToHeatmaps(ctx.query)}),
+                heatmapsElement
+            );
 
-    function queryToHeatmaps (query) {
-      /* eg:
-        query.geneQuery=blood
-        query.select={ "homo sapiens" : { "CELL_LINE": true, "ORGANISM_PART": true } }
+            function setChecked(checked, species, factor) {
+                console.log(ctx.query);
+                console.log("setChecked " + species + " " + factor + ": " + checked);
 
-       ->
-
-       [
-         {
-           "geneQuery": "blood",
-           "species": "Homo sapiens",
-           "factor": "ORGANISM_PART"
-         },
-         {
-           "geneQuery": "blood",
-           "species": "Mus musculus",
-           "factor": "CELL_LINE"
-         }
-       ]
-
-        */
-      var select = query.select;
-      var geneQuery = query.geneQuery;
-      var heatmaps = [];
-
-      for (var species in select) {
-        if (select.hasOwnProperty(species)) {
-
-          var factors = select[species];
-
-          for (var factor in factors) {
-            if (factors.hasOwnProperty(factor)) {
-              if (factors[factor]) {
-                heatmaps.push({
-                  "geneQuery": geneQuery,
-                  "species": species,
-                  "factor": factor
-                });
-              }
+                var newSelect = checked ? addSelection(ctx.query.select, species, factor) : removeSelection(ctx.query.select, species, factor);
+                var newQueryString = "?geneQuery=" + ctx.query.geneQuery + "&select=" + JSON.stringify(newSelect);
+                console.log(newQueryString);
+                navigateTo(ctx.pathname + newQueryString);
             }
-          }
+
+            function navigateTo(url) {
+                var state, title;
+                history.pushState(state, title, url);
+                renderMain();
+            }
+
+
+            function addSelection(select, species, factor) {
+                if (!select) {
+                    select = {};
+                }
+
+                if (!select[species]) {
+                    select[species] = {};
+                }
+                select[species][factor] = true;
+                return select;
+            }
+
+            function removeSelection(select, species, factor) {
+                select[species][factor] = false;
+                return select;
+            }
+
         }
-      }
 
-      return heatmaps;
+        function queryToHeatmaps(query) {
+            /* eg:
+             query.geneQuery=blood
+             query.select={ "homo sapiens" : { "CELL_LINE": true, "ORGANISM_PART": true } }
+
+             ->
+
+             [
+             {
+             "geneQuery": "blood",
+             "species": "Homo sapiens",
+             "factor": "ORGANISM_PART"
+             },
+             {
+             "geneQuery": "blood",
+             "species": "Mus musculus",
+             "factor": "CELL_LINE"
+             }
+             ]
+
+             */
+            var select = query.select;
+            var geneQuery = query.geneQuery;
+            var heatmaps = [];
+
+            for (var species in select) {
+                if (select.hasOwnProperty(species)) {
+
+                    var factors = select[species];
+
+                    for (var factor in factors) {
+                        if (factors.hasOwnProperty(factor)) {
+                            if (factors[factor]) {
+                                heatmaps.push({
+                                    "geneQuery": geneQuery,
+                                    "species": species,
+                                    "factor": factor
+                                });
+                            }
+                        }
+                    }
+                }
+            }
+
+            return heatmaps;
+        }
+
+
     }
-
-
-  }
 
 })(React, page, window.queryString, facetsModule, Heatmaps);
