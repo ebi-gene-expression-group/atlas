02560199c10471e91b407af3a848bcd2f7bdf1ab
Oliver Mannion <omannion@ebi.ac.uk>
2015-02-02 13:41:56 +0100

    add logging

--- web/src/main/java/uk/ac/ebi/atlas/commons/efo/EFOTreeNodesTrader.java
+++ web/src/main/java/uk/ac/ebi/atlas/commons/efo/EFOTreeNodesTrader.java
@@ -1,8 +1,7 @@
 package uk.ac.ebi.atlas.commons.efo;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import uk.ac.ebi.arrayexpress.utils.efo.EFOLoader;
 import uk.ac.ebi.arrayexpress.utils.efo.EFONode;
@@ -13,9 +12,12 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Map;
 
+
 @Named
 @Scope("singleton")
 public class EFOTreeNodesTrader {
+
+    private static final Logger LOGGER = Logger.getLogger(EFOTreeNodesTrader.class);
 
     private String efoOwlFilePath;
     private Map<String, EFONode> urlToEFONode;
@@ -29,6 +31,7 @@ public class EFOTreeNodesTrader {
         if (urlToEFONode == null) {
             try {
                 EFOLoader efoLoader = new EFOLoader();
+                LOGGER.debug("efoLoader.load " + efoOwlFilePath);
                 urlToEFONode = efoLoader.load(new FileInputStream(efoOwlFilePath)).getMap();
             } catch (FileNotFoundException e) {
                 throw new EFOTreeNodesTraderException(e);
