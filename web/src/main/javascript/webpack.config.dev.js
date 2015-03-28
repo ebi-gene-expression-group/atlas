2e45deb31d91f7060fcf44ae68a6cdae5faccd2f
Oliver Mannion <omannion@ebi.ac.uk>
2015-03-28 14:57:14 +0100

    add all files in test directory

--- web/src/main/javascript/webpack.config.dev.js
+++ web/src/main/javascript/webpack.config.dev.js
@@ -1,5 +1,12 @@
 var webpack = require("webpack");
 var path = require("path");
+var globule = require("globule");
+
+function mochaloader(files) { return files.map(function (path) {return 'mocha!./' + path;}) }
+
+var testfiles = globule.find('test/**/*-test.js');
+
+console.log("Bundling test files " + testfiles);
 
 module.exports = {
     cache: true,
@@ -7,9 +14,7 @@ module.exports = {
 
     entry: {
         // to run tests open http://localhost:9000/webpack-dev-server/specs
-        specs: [  "./build-util/install-source-map-support.js"
-                    , "mocha!./test/heatmap/heatmap-test.js"
-                    , "mocha!./test/search-results/differential-facets-test.js"],
+        specs: [  "./build-util/install-source-map-support.js"].concat(mochaloader(testfiles)),
         vendor: ["jquery", "react", "react-addons"]
     },
 
