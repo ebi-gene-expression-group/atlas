b1b6649607f7ded4ea038583275a4f360dd9fcc1
Oliver Mannion <omannion@ebi.ac.uk>
2015-03-30 11:06:17 +0200

    create prod setup

--- web/src/main/javascript/webpack.config.js
+++ web/src/main/javascript/webpack.config.js
@@ -3,26 +3,22 @@ var path = require("path");
 
 module.exports = {
     cache: true,
-    devtool: 'inline-source-map',
+    devtool: 'source-map',
 
     // define the bundles we want
     entry: {
-        "search-results-differential-page": "./search-results-differential/page.js"
-        ,"search-results-differential-specs": "./search-results-differential/test/specs.js"
+        "search-results-differential-page": "./src/search-results/differential-page.js"
         ,vendor: ["jquery", "react"]
-        ,"specs-vendor": ["jquery", "react", "react-addons"]
     },
 
     output: {
         path: path.resolve(__dirname, "../webapp/resources/js-bundles")
-        ,filename: '[name]-bundle.js' // [name] is substituted for the entry name, eg: specs-bundle.js
+        ,filename: '[name]-bundle.js' // [name] is substituted for the entry name, eg: search-results-differential-page-bundle.js
 
     },
 
     plugins: [
         new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor-bundle.js", ["vendor", "search-results-differential-page"])
-        //,new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"specs", /* filename= */"specs-bundle.js")
-        ,new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"specs-vendor-bundle.js", ["specs-vendor", "search-results-differential-specs"])
     ],
 
     module: {
