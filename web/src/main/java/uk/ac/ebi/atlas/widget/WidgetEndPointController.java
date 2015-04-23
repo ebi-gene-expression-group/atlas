9523cf845fcb10bfae87590890d81bf3b6e4f417
barrera <barrera@ebi.ac.uk>
2015-04-23 13:04:25 +0100

    Endpoint to request species

--- web/src/main/java/uk/ac/ebi/atlas/widget/WidgetEndPointController.java
+++ web/src/main/java/uk/ac/ebi/atlas/widget/WidgetEndPointController.java
@@ -9,6 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.json.MappingJacksonJsonView;
 import uk.ac.ebi.atlas.search.baseline.BaselineExperimentProfileSearchService;
 import uk.ac.ebi.atlas.search.baseline.BaselineExperimentSearchResult;
+import uk.ac.ebi.atlas.trader.cache.OrganismsCache;
 
 import javax.inject.Inject;
 
@@ -17,10 +18,13 @@ import javax.inject.Inject;
 public final class WidgetEndPointController {
 
     private BaselineExperimentProfileSearchService baselineExperimentProfileSearchService;
+    private OrganismsCache organismsCache;
 
     @Inject
-    public WidgetEndPointController(BaselineExperimentProfileSearchService baselineExperimentProfileSearchService) {
+    public WidgetEndPointController(BaselineExperimentProfileSearchService baselineExperimentProfileSearchService,
+                                    OrganismsCache organismsCache) {
         this.baselineExperimentProfileSearchService = baselineExperimentProfileSearchService;
+        this.organismsCache = organismsCache;
     }
 
     @RequestMapping(value = "/json/expressionData", method = RequestMethod.GET, produces = "application/json")
@@ -32,6 +36,18 @@ public final class WidgetEndPointController {
         BaselineExperimentSearchResult tissueResults = baselineExperimentProfileSearchService.query(ImmutableSet.of(geneId));
 
         mav.addObject(geneId, !tissueResults.isEmpty());
+
+        return mav;
+
+    }
+
+    @RequestMapping(value = "/json/expressionData/species", method = RequestMethod.GET, produces = "application/json")
+    @ResponseStatus(HttpStatus.OK)
+    public ModelAndView existSpeciesForGeneIdentifier() {
+
+        ModelAndView mav = new ModelAndView(new MappingJacksonJsonView());
+
+        mav.addObject("species", organismsCache.getOrganismsList());
 
         return mav;
 
