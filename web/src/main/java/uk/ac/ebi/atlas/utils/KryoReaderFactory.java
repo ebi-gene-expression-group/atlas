2f5c6799c385bb873cd82ac316976fee195f9347
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2015-06-03 18:20:15 +0100

    #91486574 Read baseline expression data from binary files instead of TSV files - Pool Kryo instances

--- web/src/main/java/uk/ac/ebi/atlas/utils/KryoReaderFactory.java
+++ web/src/main/java/uk/ac/ebi/atlas/utils/KryoReaderFactory.java
@@ -1,10 +1,12 @@
 package uk.ac.ebi.atlas.utils;
 
 import com.esotericsoftware.kryo.Kryo;
-import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.UnsafeInput;
 import org.apache.log4j.Logger;
-import uk.ac.ebi.atlas.profiles.KryoReader;
+import org.springframework.beans.factory.annotation.Required;
+import uk.ac.ebi.atlas.commons.serializers.ImmutableSetKryoSerializer;
+import uk.ac.ebi.atlas.commons.serializers.OntologyTermKryoSerializer;
+import uk.ac.ebi.atlas.profiles.BaselineExpressionsKryoReader;
 
 import javax.inject.Named;
 import java.io.*;
@@ -18,22 +20,33 @@ import static com.google.common.base.Preconditions.checkNotNull;
 public class KryoReaderFactory {
 
     private static final Logger LOGGER = Logger.getLogger(KryoReaderFactory.class);
+    private static ThreadLocal<Kryo> kryos;
 
-    public KryoReader createKryoReader(String serializedFilePath) {
+    @Required
+    public void initializeKryo() {
+        // Pool Kryo instances: https://github.com/EsotericSoftware/kryo#pooling-kryo-instances
+        kryos = new ThreadLocal<Kryo>() {
+            protected Kryo initialValue() {
+                Kryo kryo = new Kryo();
+                ImmutableSetKryoSerializer.registerSerializers(kryo);
+                OntologyTermKryoSerializer.registerSerializers(kryo);
+                return kryo;
+            }
+        };
+    }
+
+    public BaselineExpressionsKryoReader createBaselineExpressionsKryoReader(String serializedFilePath) {
         try {
             Path filePath = FileSystems.getDefault().getPath(checkNotNull(serializedFilePath));
             InputStream inputStream = Files.newInputStream(filePath);
-            //return createKryoReader(new Input(inputStream));
-            return createKryoReader(new UnsafeInput(inputStream));
+            return createBaselineExpressionsKryoReader(new UnsafeInput(inputStream));
         } catch (IOException e) {
             LOGGER.error(e.getMessage(), e);
             throw new IllegalArgumentException("Error trying to open " + serializedFilePath, e);
         }
     }
 
-    // If performance suffers, use a KryoPool https://github.com/EsotericSoftware/kryo#pooling-kryo-instances
-    public static KryoReader createKryoReader(UnsafeInput source) {
-        return new KryoReader(new Kryo(), source);
+    public static BaselineExpressionsKryoReader createBaselineExpressionsKryoReader(UnsafeInput source) {
+        return new BaselineExpressionsKryoReader(kryos.get(), source);
     }
-
 }
