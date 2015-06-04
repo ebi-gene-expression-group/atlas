c28abec3ae2760e967b904973a1f61ba93986617
barrera <barrera@ebi.ac.uk>
2015-06-04 11:51:55 +0100

    Add google analytics code to baseline expression widget

--- web/src/main/webapp/resources/jsx/heatmapContainer.js
+++ web/src/main/webapp/resources/jsx/heatmapContainer.js
@@ -66,6 +66,21 @@ var HeatmapContainer = (function (React) {
 
     return React.createClass({
 
+        componentDidMount: function() {
+            debugger;
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-37676851-1']);
+            _gaq.push(['_trackPageview']);
+            (function () {
+                var ga = document.createElement('script');
+                ga.type = 'text/javascript';
+                ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0];
+                s.parentNode.insertBefore(ga, s);
+            })();
+        },
+
         render: function () {
             var Heatmap = this.props.Heatmap;
             var heatmapClass = this.props.heatmapClass ? this.props.heatmapClass : "gxaHeatmapPosition" + (this.props.isWidget ? "-widget" : "");
