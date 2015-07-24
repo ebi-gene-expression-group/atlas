5e2598bd8849060957bdc8ef10d5a326c6561da3
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2015-07-24 10:11:55 +0100

    #99022742 Show *Resource not found: <experiment_name>* if an experiment isn’t found in the cache - Handle also UncheckedExecutionException in the cache

--- web/src/main/java/uk/ac/ebi/atlas/trader/ExperimentTrader.java
+++ web/src/main/java/uk/ac/ebi/atlas/trader/ExperimentTrader.java
@@ -71,7 +71,7 @@ public class ExperimentTrader {
         ExperimentType experimentType;
         try {
             experimentType = publicExperimentTypesCache.getExperimentType(experimentAccession);
-        } catch (NullPointerException e) {
+        } catch (UncheckedExecutionException | NullPointerException e) {
             throw new ResourceNotFoundException("Experiment: " + experimentAccession + " not found");
         }
 
