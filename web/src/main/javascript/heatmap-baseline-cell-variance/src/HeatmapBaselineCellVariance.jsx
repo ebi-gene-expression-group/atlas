6182bc9e22bfca5461e0143a8204679cc965f9ef
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2015-08-10 09:56:26 +0100

    #100921872 Variance charts aren’t scaled correctly

--- web/src/main/javascript/heatmap-baseline-cell-variance/src/HeatmapBaselineCellVariance.jsx
+++ web/src/main/javascript/heatmap-baseline-cell-variance/src/HeatmapBaselineCellVariance.jsx
@@ -16,90 +16,52 @@ var HeatmapBaselineCellVariance = React.createClass({
             median: React.PropTypes.number,
             upper: React.PropTypes.number,
             max: React.PropTypes.number
-        }).isRequired,
+        }).isRequired
     },
 
     render: function () {
 
-        //var highchartsOptions =
-        //{
-        //    "chart": { "type": "boxplot" },
-        //    "credits": { "enabled": false },
-        //    "title": { "text": "" },
-        //    "legend": { "enabled": false },
-        //    "xAxis": { "title": { "text": "Variance" } },
-        //    "yAxis": { "title": { "text": "" }, "gridLineColor": "#FFFFFF" },
-        //    "exporting": { "enabled": false },
-        //    "plotOptions": {
-        //        "boxplot": {
-        //            "fillColor": '#F0F0E0',
-        //            "lineWidth": 2,
-        //            "medianColor": '#0C5DA5',
-        //            "medianWidth": 3,
-        //            "stemColor": '#A63400',
-        //            "stemDashStyle": 'dot',
-        //            "stemWidth": 1,
-        //            "whiskerColor": '#3D9200',
-        //            "whiskerLength": '20%',
-        //            "whiskerWidth": 3
-        //        }
-        //    },
-        //
-        //    "tooltip": {
-        //        "headerFormat": "",
-        //        "style": {
-        //            "padding": 5,
-        //            "fontSize": '9px',
-        //            "width": 80,
-        //            "height": 100
-        //        }
-        //    },
-        //
-        //    series: [{
-        //        "name": "Observations",
-        //        "data": [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
-        //    }]
-        //};
+        var chartWidth = 110;
+        var chartHeight = 140;
+        var chartMargin = 0;
 
         var highchartsOptions = {
             credits: { enabled: false },
-            chart:   { type: 'boxplot', margin: 0, width: 100, height: 50 },
-            title:   { text: '' },
+            chart:   { type: "boxplot", width: chartWidth, height: chartHeight, margin: chartMargin},
+            title:   { text: "" },
             legend:  { enabled: false },
-            xAxis:   { title: { text: 'Variance' } },
-            yAxis:   { title: { text: 'Axpression level' } },
-            "plotOptions": {
-                "boxplot": {
-                    "fillColor": '#F0F0E0',
-                    "lineWidth": 2,
-                    "medianColor": '#0C5DA5',
-                    "medianWidth": 3,
-                    "stemColor": '#A63400',
-                    "stemDashStyle": 'dot',
-                    "stemWidth": 1,
-                    "whiskerColor": '#3D9200',
-                    "whiskerLength": '20%',
-                    "whiskerWidth": 3
+            xAxis:   { title: { text: "Variance" } },
+            yAxis:   { title: { text: "Expression level" } },
+            plotOptions: {
+                boxplot: {
+                    fillColor: "#F0F0E0",
+                    lineWidth: 2,
+                    medianColor: "#0C5DA5",
+                    medianWidth: 3,
+                    stemColor: "#A63400",
+                    stemDashStyle: "dot",
+                    stemWidth: 1,
+                    whiskerColor: "#3D9200",
+                    whiskerLength: "20%",
+                    whiskerWidth: 3
                 }
             },
             series: [{
-                name: 'Expression',
+                name: "Expression",
                 data: [
                     [this.props.quartiles.min, this.props.quartiles.lower, this.props.quartiles.median, this.props.quartiles.upper, this.props.quartiles.max]
-                ],
-                "tooltip": {
-                    "headerFormat": "",
-                    "style": {
-                        "padding": 5,
-                        "fontSize": '9px',
-                        "width": 80,
-                        "height": 100
-                    }
-                },
-            }]
+                ]
+            }],
+            tooltip: {
+                headerFormat: "",
+                style:{
+                    fontSize: "10px",
+                    padding: 5
+                }
+            }
         };
 
-        var boxPlotStyle = {"height": "50px", "margin": "0px", "minWidth": "80px", "maxWidth": "150px"};
+        var boxPlotStyle = {width: chartWidth, height: chartHeight, margin: chartMargin};
         return (
             <td>
                 <div id="container" ref="container" style={boxPlotStyle}>
