6189e67759fdfec04fad4031213a117f88ccad9f
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2015-08-12 12:11:12 +0100

    Clean up default search query syntax and remove unneeded local variables

--- web/src/main/java/uk/ac/ebi/atlas/search/analyticsindex/AnalyticsSearchDao.java
+++ web/src/main/java/uk/ac/ebi/atlas/search/analyticsindex/AnalyticsSearchDao.java
@@ -17,7 +17,10 @@ import java.io.IOException;
 
 @Named
 public class AnalyticsSearchDao {
-    public static final String ABOVE_CUTOFF = "(((experimentType:rnaseq_mrna_baseline OR experimentType:proteomics_baseline) AND expressionLevel:[0.5 TO *]) OR ((experimentType:rnaseq_mrna_differential OR experimentType:microarray_1colour_mrna_differential or experimentType:microarray_2colour_mrna_differential or experimentType:microarray_1colour_microrna_differential) AND foldChange:[1.0 TO *]))";
+    public static final String ABOVE_CUTOFF = "(" +
+                                               "(experimentType:(rnaseq_mrna_baseline OR proteomics_baseline) AND expressionLevel:[0.5 TO *]) OR " +
+                                               "(experimentType:(rnaseq_mrna_differential OR microarray_1colour_mrna_differential OR microarray_2colour_mrna_differential OR microarray_1colour_microrna_differential) AND foldChange:[1.0 TO *])" +
+                                              ")";
 
     private static final Logger LOGGER = Logger.getLogger(AnalyticsSearchDao.class);
 
@@ -33,16 +36,12 @@ public class AnalyticsSearchDao {
 
         QueryResponse queryResponse = query(buildQuery(geneQuery));
 
-        ImmutableSet<String> experimentTypes = SolrUtil.extractFirstFacetValues(queryResponse);
-
-        return experimentTypes;
+        return SolrUtil.extractFirstFacetValues(queryResponse);
     }
 
     public QueryResponse query(SolrQuery solrQuery) {
         try {
-            QueryResponse queryResponse = analyticsSolrClient.query(solrQuery);
-            //LOGGER.debug("<query> Solr query time: " + queryResponse.getQTime() + "ms, status code: " + queryResponse.getStatus());
-            return queryResponse;
+            return analyticsSolrClient.query(solrQuery);
         } catch (SolrServerException | IOException e) {
             LOGGER.error(e.getMessage(), e);
             throw new SolrException(SolrException.ErrorCode.UNKNOWN, e);
