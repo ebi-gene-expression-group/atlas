f0bae0804ffbacb45a2d5b1a7315bfb7a5b4754a
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2015-09-16 15:32:00 +0100

    A common node_modules isn’t a good idea, as version number isn’t part of the package dir name

--- web/src/main/javascript/build-bundles.sh
+++ web/src/main/javascript/build-bundles.sh
@@ -6,22 +6,16 @@ export https_proxy=$http_proxy
 export NVM_DIR="/nfs/ma/home/ma-svc/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
-mkdir -p ../node_modules
-
 for dir in `ls -d */`
 do
     if [[ $dir != node_modules/ ]]
     then
         cd $dir
-        rm -rf ./node_modules
-        ln -sf ../../node_modules ./node_modules
         npm install
         cd ..
     fi
 done
 
-rm -rf ./node_modules
-ln -sf ../node_modules ./node_modules
 npm install
 
 npm run $1
