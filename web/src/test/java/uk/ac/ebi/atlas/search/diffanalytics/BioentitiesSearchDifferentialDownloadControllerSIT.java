1f28a559a7d3c4550bef01d2204440c65da9927a
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2015-09-28 20:52:29 +0100

    Fix Selenium test

--- web/src/test/java/uk/ac/ebi/atlas/search/diffanalytics/BioentitiesSearchDifferentialDownloadControllerSIT.java
+++ web/src/test/java/uk/ac/ebi/atlas/search/diffanalytics/BioentitiesSearchDifferentialDownloadControllerSIT.java
@@ -26,9 +26,7 @@ public class BioentitiesSearchDifferentialDownloadControllerSIT extends RestAssu
 
         String[] lines = body.asString().split("\n");
         assertThat(lines.length, is(greaterThan(4)));
-        // TODO Sometimes one, sometimes the other
-        // assertThat(lines[4], startsWith("Gbp11\tMus musculus\tE-GEOD-21860\tclinical information:'non-pregnant' vs 'pregnant'\t4.44328878860028E-4\tInf\tNA"));
-        assertThat(lines[4], startsWith("Prok1\tMus musculus\tE-GEOD-21860\tclinical information:'non-pregnant' vs 'pregnant'"));
+        assertThat(lines[4], startsWith("Gbp11\tMus musculus\tE-GEOD-21860\tclinical information:'non-pregnant' vs 'pregnant'\t4.44328878860028E-4\tInf\tNA"));
     }
 
     @Test
