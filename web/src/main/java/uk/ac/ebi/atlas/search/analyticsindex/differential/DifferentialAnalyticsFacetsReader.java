e4caf0fe4cb7deadd7b1e9ee1ce74537568d6174
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2015-10-02 12:57:06 +0100

    #101964086 Sort new gene page differential results by foldchange; visualise the gradient and its contextual help - Use FoldChangeRounder to provide the values already formatted and stringified

--- web/src/main/java/uk/ac/ebi/atlas/search/analyticsindex/differential/DifferentialAnalyticsFacetsReader.java
+++ web/src/main/java/uk/ac/ebi/atlas/search/analyticsindex/differential/DifferentialAnalyticsFacetsReader.java
@@ -1,11 +1,13 @@
 package uk.ac.ebi.atlas.search.analyticsindex.differential;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.jayway.jsonpath.JsonPath;
 import com.jayway.jsonpath.ReadContext;
 import uk.ac.ebi.atlas.model.ExperimentType;
+import uk.ac.ebi.atlas.profiles.differential.viewmodel.FoldChangeRounder;
 import uk.ac.ebi.atlas.trader.ContrastTrader;
 import uk.ac.ebi.atlas.trader.ExperimentTrader;
 import uk.ac.ebi.atlas.utils.ColourGradient;
@@ -21,6 +23,7 @@ public class DifferentialAnalyticsFacetsReader {
 
     private final ExperimentTrader experimentTrader;
     private final ContrastTrader contrastTrader;
+    private final FoldChangeRounder foldChangeRounder;
 
     private static final String[] FACET_FIELDS = {"kingdom", "species", "experimentType", "factors", "numReplicates", "regulation"};
 
@@ -34,15 +37,16 @@ public class DifferentialAnalyticsFacetsReader {
     private final ColourGradient colourGradient;
 
     @Inject
-    public DifferentialAnalyticsFacetsReader(ExperimentTrader experimentTrader, ContrastTrader contrastTrader, ColourGradient colourGradient) {
+    public DifferentialAnalyticsFacetsReader(ExperimentTrader experimentTrader, ContrastTrader contrastTrader, FoldChangeRounder foldChangeRounder, ColourGradient colourGradient) {
         this.experimentTrader = experimentTrader;
         this.contrastTrader = contrastTrader;
+        this.foldChangeRounder = foldChangeRounder;
         this.colourGradient = colourGradient;
     }
 
 
     public String extractResultsAsJson(String solrResponseAsJson) {
-        Gson gson = new Gson();
+        Gson gson = new GsonBuilder().serializeSpecialFloatingPointValues().create();
 
         ReadContext jsonReadContext = JsonPath.parse(solrResponseAsJson);
 
@@ -73,6 +77,7 @@ public class DifferentialAnalyticsFacetsReader {
             double foldChange = foldChangeSymbol instanceof Double ? (double) foldChangeSymbol : Double.parseDouble((String) foldChangeSymbol);
             String colour = foldChange > 0.0 ? colourGradient.getGradientColour(foldChange, minUpLevel, maxUpLevel, "pink", "red") : colourGradient.getGradientColour(foldChange, minDownLevel, maxDownLevel, "lightGray", "blue");
 
+            document.put("foldChange", foldChangeRounder.format(foldChange));
             document.put("colour", colour);
             document.put("comparison", contrastTrader.getContrastFromCache(experimentAccession, experimentType, contrastId).getDisplayName());
             document.put("experimentName", experimentTrader.getExperimentFromCache(experimentAccession, experimentType).getDescription());
@@ -80,10 +85,10 @@ public class DifferentialAnalyticsFacetsReader {
         }
         resultsWithLevels.put("results", documents);
 
-        resultsWithLevels.put("maxDownLevel", maxDownLevel);
-        resultsWithLevels.put("minDownLevel", minDownLevel);
-        resultsWithLevels.put("minUpLevel", minUpLevel);
-        resultsWithLevels.put("maxUpLevel", maxUpLevel);
+        resultsWithLevels.put("maxDownLevel", foldChangeRounder.format(maxDownLevel));
+        resultsWithLevels.put("minDownLevel", foldChangeRounder.format(minDownLevel));
+        resultsWithLevels.put("minUpLevel", foldChangeRounder.format(minUpLevel));
+        resultsWithLevels.put("maxUpLevel", foldChangeRounder.format(maxUpLevel));
 
         return gson.toJson(resultsWithLevels);
     }
