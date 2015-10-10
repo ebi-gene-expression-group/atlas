55916621b9b4298b8f97875891ce498c0f08c27c
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2015-10-10 23:30:57 +0100

    Add instructions to `build-bundles.sh`.

--- web/src/main/javascript/build-bundles.sh
+++ web/src/main/javascript/build-bundles.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+if [ $# -ne 1 ] || [[ $1 -ne "ci" && $1 -ne "prod" ]] ; then
+        echo "Builds JavaScript modules and their dependencies (images such as PNG and CSS files)"
+        echo "into bundles with Webpack and places them in the webapp/resources/js-bundles directory."
+        echo
+        echo "The script runs in two phases:"
+        echo "1. Remove our packages from node_modules and run “npm install”, ensuring the latest"
+        echo "   version is installed."
+        echo "2. Run “npm run {ci|prod}”, which cleans the target directory and generates the bundle"
+        echo "   files. See package.json for the different parameters passed to Webpack."
+        echo
+        echo "Usage: $0 {ci|prod}"
+        exit;
+fi
+
 export http_proxy=http://www-proxy.ebi.ac.uk:3128
 export https_proxy=$http_proxy
 
