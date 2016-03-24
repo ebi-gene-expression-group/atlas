f81e6024b9e7cf0ca7e752f6397ea44d7bfee148
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2016-03-24 19:05:25 +0000

    Remove license comment block and add tooltip class to experiment header buttons for visual consistency

--- web/src/main/webapp/resources/js/experiment-page-buttons-and-tooltips.js
+++ web/src/main/webapp/resources/js/experiment-page-buttons-and-tooltips.js
@@ -1,45 +1,23 @@
-/*
- * Copyright 2008-2013 Microarray Informatics Team, EMBL-European Bioinformatics Institute
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- *
- * For further details of the Gene Expression Atlas project, including source code,
- * downloads and documentation, please see:
- *
- * http://gxa.github.com/gxa
- */
-
 function initExperimentPageButtonsAndTooltips() {
 
-    $('#display-ae').button().tooltip();
-    $('#display-analysis-methods').button().tooltip();
-    $('#display-experiment-design').button().tooltip();
-    $('#display-experiment').button().tooltip();
-    $('#display-qc-report').button().tooltip();
-    $('#display-fastqc-report').button().tooltip();
-    $('#clustering-pdf').button().tooltip();
-    if (undefined !== $('#download-raw')) $('#download-raw').button().tooltip();
-    if (undefined !== $('#download-normalized')) $('#download-normalized').button().tooltip();
-    if (undefined !== $('#download-logFold')) $('#download-logFold').button().tooltip();
-    if (undefined !== $('#download-analytics')) $('#download-analytics').button().tooltip();
-    if (undefined !== $('#download-expressions')) $('#download-expressions').button().tooltip();
-    if (undefined !== $('#download-expressions-modal')) $('#download-expressions-modal').button().tooltip();
-    $('#download-r').button().tooltip();
-    $('#download-r-modal').button().tooltip();
-    $('#goto-ae').tooltip();
-    $('#goto-experiment').tooltip();
-    $("#goto-experiment-name").tooltip();
-    $('.array-design').tooltip();
-    $('.pubmed-id').tooltip();
+    $('#display-ae').button().tooltip({tooltipClass: "gxaHelpTooltip"});
+    $('#display-analysis-methods').button().tooltip({tooltipClass: "gxaHelpTooltip"});
+    $('#display-experiment-design').button().tooltip({tooltipClass: "gxaHelpTooltip"});
+    $('#display-experiment').button().tooltip({tooltipClass: "gxaHelpTooltip"});
+    $('#display-qc-report').button().tooltip({tooltipClass: "gxaHelpTooltip"});
+    $('#display-fastqc-report').button().tooltip({tooltipClass: "gxaHelpTooltip"});
+    $('#clustering-pdf').button().tooltip({tooltipClass: "gxaHelpTooltip"});
+    if (undefined !== $('#download-raw')) $('#download-raw').button().tooltip({tooltipClass: "gxaHelpTooltip"});
+    if (undefined !== $('#download-normalized')) $('#download-normalized').button().tooltip({tooltipClass: "gxaHelpTooltip"});
+    if (undefined !== $('#download-logFold')) $('#download-logFold').button().tooltip({tooltipClass: "gxaHelpTooltip"});
+    if (undefined !== $('#download-analytics')) $('#download-analytics').button().tooltip({tooltipClass: "gxaHelpTooltip"});
+    if (undefined !== $('#download-expressions')) $('#download-expressions').button().tooltip({tooltipClass: "gxaHelpTooltip"});
+    if (undefined !== $('#download-expressions-modal')) $('#download-expressions-modal').button().tooltip({tooltipClass: "gxaHelpTooltip"});
+    $('#download-r').button().tooltip({tooltipClass: "gxaHelpTooltip"});
+    $('#download-r-modal').button().tooltip({tooltipClass: "gxaHelpTooltip"});
+    $('#goto-ae').tooltip({tooltipClass: "gxaHelpTooltip"});
+    $('#goto-experiment').tooltip({tooltipClass: "gxaHelpTooltip"});
+    $("#goto-experiment-name").tooltip({tooltipClass: "gxaHelpTooltip"});
+    $('.array-design').tooltip({tooltipClass: "gxaHelpTooltip"});
+    $('.pubmed-id').tooltip({tooltipClass: "gxaHelpTooltip"});
 }
