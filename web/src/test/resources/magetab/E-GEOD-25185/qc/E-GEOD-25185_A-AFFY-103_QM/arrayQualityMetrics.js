d30621cb43384c45a3cda5bd503a7348fe5f8160
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2016-04-13 17:23:03 +0100

    113159921 Update integration test data - 1 day: E-GEOD-25185

---
+++ web/src/test/resources/magetab/E-GEOD-25185/qc/E-GEOD-25185_A-AFFY-103_QM/arrayQualityMetrics.js
@@ -0,0 +1,197 @@
+// (C) Wolfgang Huber 2010-2011
+
+// Script parameters - these are set up by R in the function 'writeReport' when copying the 
+//   template for this script from arrayQualityMetrics/inst/scripts into the report.
+
+var highlightInitial = [ false, false, false, false, false, false, false, false, false, false, false, false ];
+var arrayMetadata    = [ [ "1", "GSM618675", "GSM618675", "2.5 milligram DON per kilogram feed for 23 days ad libitum", "GSM618675_DON2.5_sample15.CEL" ], [ "2", "GSM618674", "GSM618674", "2.5 milligram DON per kilogram feed for 23 days ad libitum", "GSM618674_DON2.5_sample13.CEL" ], [ "3", "GSM618673", "GSM618673", "2.5 milligram DON per kilogram feed for 23 days ad libitum", "GSM618673_DON2.5_sample12.CEL" ], [ "4", "GSM618670", "GSM618670", "1 milligram DON per kilogram feed for 23 days ad libitum", "GSM618670_DON1_sample7.CEL" ], [ "5", "GSM618672", "GSM618672", "1 milligram DON per kilogram feed for 23 days ad libitum", "GSM618672_DON1_sample9.CEL" ], [ "6", "GSM618671", "GSM618671", "1 milligram DON per kilogram feed for 23 days ad libitum", "GSM618671_DON1_sample8.CEL" ], [ "7", "GSM618678", "GSM618678", "5 milligram DON per kilogram feed for 23 days ad libitum", "GSM618678_DON5_sample20.CEL" ], [ "8", "GSM618677", "GSM618677", "5 milligram DON per kilogram feed for 23 days ad libitum", "GSM618677_DON5_sample19.CEL" ], [ "9", "GSM618676", "GSM618676", "5 milligram DON per kilogram feed for 23 days ad libitum", "GSM618676_DON5_sample16.CEL" ], [ "10", "GSM618668", "GSM618668", "control", "GSM618668_Control_sample2.CEL" ], [ "11", "GSM618669", "GSM618669", "control", "GSM618669_Control_sample4.CEL" ], [ "12", "GSM618667", "GSM618667", "control", "GSM618667_Control_sample1.CEL" ] ];
+var svgObjectNames   = [ "pca", "dens" ];
+
+var cssText = ["stroke-width:1; stroke-opacity:0.4",
+               "stroke-width:3; stroke-opacity:1" ];
+
+// Global variables - these are set up below by 'reportinit'
+var tables;             // array of all the associated ('tooltips') tables on the page
+var checkboxes;         // the checkboxes
+var ssrules;
+
+
+function reportinit() 
+{
+ 
+    var a, i, status;
+
+    /*--------find checkboxes and set them to start values------*/
+    checkboxes = document.getElementsByName("ReportObjectCheckBoxes");
+    if(checkboxes.length != highlightInitial.length)
+	throw new Error("checkboxes.length=" + checkboxes.length + "  !=  "
+                        + " highlightInitial.length="+ highlightInitial.length);
+    
+    /*--------find associated tables and cache their locations------*/
+    tables = new Array(svgObjectNames.length);
+    for(i=0; i<tables.length; i++) 
+    {
+        tables[i] = safeGetElementById("Tab:"+svgObjectNames[i]);
+    }
+
+    /*------- style sheet rules ---------*/
+    var ss = document.styleSheets[0];
+    ssrules = ss.cssRules ? ss.cssRules : ss.rules; 
+
+    /*------- checkboxes[a] is (expected to be) of class HTMLInputElement ---*/
+    for(a=0; a<checkboxes.length; a++)
+    {
+	checkboxes[a].checked = highlightInitial[a];
+        status = checkboxes[a].checked; 
+        setReportObj(a+1, status, false);
+    }
+
+}
+
+
+function safeGetElementById(id)
+{
+    res = document.getElementById(id);
+    if(res == null)
+        throw new Error("Id '"+ id + "' not found.");
+    return(res)
+}
+
+/*------------------------------------------------------------
+   Highlighting of Report Objects 
+ ---------------------------------------------------------------*/
+function setReportObj(reportObjId, status, doTable)
+{
+    var i, j, plotObjIds, selector;
+
+    if(doTable) {
+	for(i=0; i<svgObjectNames.length; i++) {
+	    showTipTable(i, reportObjId);
+	} 
+    }
+
+    /* This works in Chrome 10, ssrules will be null; we use getElementsByClassName and loop over them */
+    if(ssrules == null) {
+	elements = document.getElementsByClassName("aqm" + reportObjId); 
+	for(i=0; i<elements.length; i++) {
+	    elements[i].style.cssText = cssText[0+status];
+	}
+    } else {
+    /* This works in Firefox 4 */
+	var success = false;
+	i = 0; 
+	/* Some of this looping could already be cached in reportInit() */
+	while( (!success) & (i < ssrules.length) ) {
+	    selector = ssrules[i].selectorText;  // The selector 
+            if (!selector) 
+		continue; // Skip @import and other nonstyle rules
+            if (selector == (".aqm" + reportObjId)) {
+		success = true; 
+		ssrules[i].style.cssText = cssText[0+status];
+	    } else {
+		i++;
+	    }
+	}
+    }
+
+}
+
+/*------------------------------------------------------------
+   Display of the Metadata Table
+  ------------------------------------------------------------*/
+function showTipTable(tableIndex, reportObjId)
+{
+    var rows = tables[tableIndex].rows;
+    var a = reportObjId - 1;
+
+    if(rows.length != arrayMetadata[a].length)
+	throw new Error("rows.length=" + rows.length+"  !=  arrayMetadata[array].length=" + arrayMetadata[a].length);
+
+    for(i=0; i<rows.length; i++) 
+ 	rows[i].cells[1].innerHTML = arrayMetadata[a][i];
+}
+
+function hideTipTable(tableIndex)
+{
+    var rows = tables[tableIndex].rows;
+
+    for(i=0; i<rows.length; i++) 
+ 	rows[i].cells[1].innerHTML = "";
+}
+
+
+/*------------------------------------------------------------
+  From module 'name' (e.g. 'density'), find numeric index in the 
+  'svgObjectNames' array.
+  ------------------------------------------------------------*/
+function getIndexFromName(name) 
+{
+    var i;
+    for(i=0; i<svgObjectNames.length; i++)
+        if(svgObjectNames[i] == name)
+	    return i;
+
+    throw new Error("Did not find '" + name + "'.");
+}
+
+
+/*------------------------------------------------------------
+  SVG plot object callbacks
+  ------------------------------------------------------------*/
+function plotObjRespond(what, reportObjId, name)
+{
+
+    var a, i, status;
+
+    switch(what) {
+    case "show":
+	i = getIndexFromName(name);
+	showTipTable(i, reportObjId);
+	break;
+    case "hide":
+	i = getIndexFromName(name);
+	hideTipTable(i);
+	break;
+    case "click":
+        a = reportObjId - 1;
+	status = !checkboxes[a].checked;
+	checkboxes[a].checked = status;
+	setReportObj(reportObjId, status, true);
+	break;
+    default:
+	throw new Error("Invalid 'what': "+what)
+    }
+}
+
+/*------------------------------------------------------------
+  checkboxes 'onchange' event
+------------------------------------------------------------*/
+function checkboxEvent(reportObjId)
+{
+    var a = reportObjId - 1;
+    var status = checkboxes[a].checked;
+    setReportObj(reportObjId, status, true);
+}
+
+
+/*------------------------------------------------------------
+  toggle visibility
+------------------------------------------------------------*/
+function toggle(id){
+  var head = safeGetElementById(id + "-h");
+  var body = safeGetElementById(id + "-b");
+  var hdtxt = head.innerHTML;
+  var dsp;
+  switch(body.style.display){
+    case 'none':
+      dsp = 'block';
+      hdtxt = '-' + hdtxt.substr(1);
+      break;
+    case 'block':
+      dsp = 'none';
+      hdtxt = '+' + hdtxt.substr(1);
+      break;
+  }  
+  body.style.display = dsp;
+  head.innerHTML = hdtxt;
+}
