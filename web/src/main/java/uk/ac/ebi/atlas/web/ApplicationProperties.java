9aa9d1b56c9fe5bc38f6f0d3cc112ab2fef1aed8
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2016-04-19 15:39:18 +0100

    Simplify code, remove license comment block

--- web/src/main/java/uk/ac/ebi/atlas/web/ApplicationProperties.java
+++ web/src/main/java/uk/ac/ebi/atlas/web/ApplicationProperties.java
@@ -1,25 +1,3 @@
-/*
- * Copyright 2008-2013 Microarray Informatics Team, EMBL-European Bioinformatics Institute
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- *
- * For further details of the Gene Expression Atlas project, including source code,
- * downloads and documentation, please see:
- *
- * http://gxa.github.com/gxa
- */
-
 package uk.ac.ebi.atlas.web;
 
 import com.google.common.base.Joiner;
@@ -28,7 +6,6 @@ import com.google.common.collect.Sets;
 import com.google.gson.Gson;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.context.annotation.Scope;
-import org.springframework.ui.Model;
 import uk.ac.ebi.atlas.model.AnatomogramType;
 import uk.ac.ebi.atlas.model.baseline.AssayGroupFactor;
 import uk.ac.ebi.atlas.trader.ArrayDesignTrader;
@@ -181,14 +158,14 @@ public class ApplicationProperties {
         String uri = StringUtils.remove(requestUri, request.getContextPath());
 
         return Joiner.on("?").skipNulls()
-                .join(new String[]{uri + TSV_FILE_EXTENSION, queryString}).toString();
+                .join(new String[]{uri + TSV_FILE_EXTENSION, queryString});
     }
 
     public String buildDownloadURLForWidget(HttpServletRequest request, String experimentAccession) {
         // get original query string, not the one modified by ExperimentDispatcher
         String queryString = (String) request.getAttribute("javax.servlet.forward.query_string");
         return Joiner.on("?").skipNulls()
-                .join(new String[]{"/experiments/" + experimentAccession + TSV_FILE_EXTENSION, queryString}).toString();
+                .join(new String[]{"/experiments/" + experimentAccession + TSV_FILE_EXTENSION, queryString});
     }
 
     // eg: "red,green,blue"
@@ -207,7 +184,7 @@ public class ApplicationProperties {
     <c:set var="atlasHost" value="${pageContext.request.serverName.concat(serverPort)}"/>
      */
     public String buildAtlasHostURL(HttpServletRequest request){
-        String serverPort = request.getServerPort() ==80? "" : ":".concat(new Integer(request.getServerPort()).toString());
+        String serverPort = request.getServerPort() == 80 ? "" : ":".concat(Integer.toString(request.getServerPort()));
         String atlasHost = request.getServerName().concat(serverPort);
         return request.getScheme()+"://" + atlasHost;
     }
