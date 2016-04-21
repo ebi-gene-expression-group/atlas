59a7c02cc7f7caeb6a3a45f2fe4a0f37677286d8
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2016-04-21 20:09:20 +0100

    Replace shaded import with non-shaded one

--- web/src/main/java/uk/ac/ebi/atlas/commons/writers/impl/TsvWriterImpl.java
+++ web/src/main/java/uk/ac/ebi/atlas/commons/writers/impl/TsvWriterImpl.java
@@ -1,7 +1,7 @@
 package uk.ac.ebi.atlas.commons.writers.impl;
 
 import au.com.bytecode.opencsv.CSVWriter;
-import autovalue.shaded.com.google.common.common.base.Throwables;
+import com.google.common.base.Throwables;
 import uk.ac.ebi.atlas.commons.writers.TsvWriter;
 
 import java.io.IOException;
