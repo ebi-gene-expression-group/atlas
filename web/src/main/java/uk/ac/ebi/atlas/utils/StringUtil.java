91bb64cf7e044ff4cfa3a9a02700e69a77338ecf
wbazant <wbazant@ebi.ac.uk>
2016-04-29 10:34:07 +0100

    Unbreak the Spring setup

--- web/src/main/java/uk/ac/ebi/atlas/utils/StringUtil.java
+++ web/src/main/java/uk/ac/ebi/atlas/utils/StringUtil.java
@@ -1,9 +1,12 @@
 package uk.ac.ebi.atlas.utils;
 
+import javax.inject.Named;
+
 import static org.apache.commons.lang3.StringUtils.isAllUpperCase;
 import static org.apache.commons.lang3.StringUtils.lowerCase;
 import static org.apache.commons.lang3.StringUtils.wrap;
 
+@Named //used in templates
 public class StringUtil {
 
     public String lowerCaseIfNotAllUpperCase(String s) {
