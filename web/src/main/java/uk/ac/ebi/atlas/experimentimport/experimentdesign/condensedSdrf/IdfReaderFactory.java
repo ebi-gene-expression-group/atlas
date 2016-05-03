8ac3b831f1de6b1823f7c88ca71775aac49fcfb9
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2016-05-03 12:40:20 +0100

    Properly inject builders

--- web/src/main/java/uk/ac/ebi/atlas/experimentimport/experimentdesign/condensedSdrf/IdfReaderFactory.java
+++ web/src/main/java/uk/ac/ebi/atlas/experimentimport/experimentdesign/condensedSdrf/IdfReaderFactory.java
@@ -6,6 +6,7 @@ import uk.ac.ebi.atlas.commons.readers.FileTsvReaderBuilder;
 import uk.ac.ebi.atlas.commons.readers.TsvReader;
 import uk.ac.ebi.atlas.commons.readers.UrlTsvReaderBuilder;
 
+import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.IOException;
 
@@ -13,18 +14,15 @@ import java.io.IOException;
 @Scope("singleton")
 public class IdfReaderFactory {
 
-    @Value("#{configuration['experiment.magetab.idf.path.template']}")
-    private String idfPathTemplate;
-
-    @Value("#{configuration['experiment.magetab.idf.url.template']}")
-    private String idfUrlTemplate;
-
     private UrlTsvReaderBuilder urlTsvReaderBuilder;
     private FileTsvReaderBuilder fileTsvReaderBuilder;
 
-    public IdfReaderFactory() {
-        this.urlTsvReaderBuilder = new UrlTsvReaderBuilder().forTsvFileUrlTemplate(idfUrlTemplate);
-        this.fileTsvReaderBuilder = new FileTsvReaderBuilder().forTsvFilePathTemplate(idfPathTemplate);
+    @Inject
+    public IdfReaderFactory(@Value("#{configuration['experiment.magetab.idf.url.template']}") String idfUrlTemplate,
+                            @Value("#{configuration['experiment.magetab.idf.path.template']}") String idfPathTemplate,
+                            UrlTsvReaderBuilder urlTsvReaderBuilder, FileTsvReaderBuilder fileTsvReaderBuilder) {
+        this.urlTsvReaderBuilder = urlTsvReaderBuilder.forTsvFileUrlTemplate(idfUrlTemplate);
+        this.fileTsvReaderBuilder = fileTsvReaderBuilder.forTsvFilePathTemplate(idfPathTemplate);
     }
 
     public TsvReader create(String experimentAccession) {
