85fbd225528ea6f2b21ec8cf02480c585dd5d618
wbazant <wbazant@ebi.ac.uk>
2016-05-16 16:18:35 +0100

    Improve correctness of a script after Alfonso spotted a typo in it

--- web/src/main/javascript/build-bundles.sh
+++ web/src/main/javascript/build-bundles.sh
@@ -20,8 +20,8 @@ export https_proxy=$http_proxy
 export NVM_DIR="/nfs/ma/home/ma-svc/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
-all_packages=(anatomogram cell-differential contrast-tooltips display-levels-button heatmap-baseline-cell-variance help-tooltips legend number-format expression-atlas-anatomogram expression-atlaas-heatmap expression-atlas-heatmap-highcharts faceted-search)
-bundled_packages=(expression-atlas-anatomogram faceted-search expression-atlas-heatmap expression-atlas-heatmap-highcharts)
+all_packages=(anatomogram cell-differential contrast-tooltips display-levels-button heatmap-baseline-cell-variance help-tooltips legend number-format expression-atlas-anatomogram expression-atlas-heatmap expression-atlas-heatmap-highcharts faceted-search)
+bundled_packages=(expression-atlas-anatomogram expression-atlas-heatmap expression-atlas-heatmap-highcharts faceted-search)
 
 for dir in ${bundled_packages[*]}
 do
