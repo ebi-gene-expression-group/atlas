e1a8cb38ed8dc7af7758c720dff1640b06e482be
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2016-05-24 18:23:09 +0100

    Clean imports

--- web/src/main/java/uk/ac/ebi/atlas/experimentimport/expressiondataserializer/ExpressionSerializerService.java
+++ web/src/main/java/uk/ac/ebi/atlas/experimentimport/expressiondataserializer/ExpressionSerializerService.java
@@ -1,31 +1,15 @@
 package uk.ac.ebi.atlas.experimentimport.expressiondataserializer;
 
-import com.esotericsoftware.kryo.Kryo;
-import com.esotericsoftware.kryo.io.UnsafeInput;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Scope;
-import uk.ac.ebi.atlas.commons.serializers.ImmutableSetKryoSerializer;
-import uk.ac.ebi.atlas.commons.serializers.OntologyTermKryoSerializer;
 import uk.ac.ebi.atlas.model.Experiment;
-import uk.ac.ebi.atlas.profiles.BaselineExpressionsKryoReader;
 import uk.ac.ebi.atlas.trader.ExperimentTrader;
 import uk.ac.ebi.atlas.trader.cache.BaselineExperimentsCache;
 
 import javax.inject.Inject;
 import javax.inject.Named;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 
-/**
- * Created by Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk> on 02/04/15.
- */
 @Named
-@Scope("singleton")
 public class ExpressionSerializerService {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(ExpressionSerializerService.class);
 
     private ExperimentTrader experimentTrader;
     private ExpressionSerializerFactory expressionSerializerFactory;
