9244a3b1d008f9fde0f4c2d7e932cb7d4237c3d3
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2016-06-01 03:29:35 +0100

    A more robust test for SuggestionService

--- web/src/test/java/uk/ac/ebi/atlas/solr/query/SuggestionServiceIT.java
+++ web/src/test/java/uk/ac/ebi/atlas/solr/query/SuggestionServiceIT.java
@@ -1,6 +1,5 @@
 package uk.ac.ebi.atlas.solr.query;
 
-import com.google.common.base.Joiner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -11,7 +10,7 @@ import uk.ac.ebi.atlas.web.SemanticQueryTerm;
 import javax.inject.Inject;
 import java.util.List;
 
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -20,17 +19,16 @@ import static org.junit.Assert.assertThat;
 public class SuggestionServiceIT {
 
     @Inject
-    private SuggestionService suggestionService;
+    private SuggestionService subject;
 
     @Test
-    public void apop() {
-        List<SemanticQueryTerm> suggestions = suggestionService.fetchTopSuggestions("apop", null);
-        System.out.println(Joiner.on(",\n").join(suggestions));
+    public void fetchTopSuggestions() {
+        List<SemanticQueryTerm> suggestions = subject.fetchTopSuggestions("apop", null);
 
-        assertThat(suggestions.get(0).value(), is("Apopt1"));
-        assertThat(suggestions.get(0).source(), is("symbol"));
-        assertThat(suggestions.get(13).value(), is("apoplast"));
-        assertThat(suggestions.get(13).source(), is(""));
+        assertThat(suggestions, hasSize(greaterThan(10)));
+        for (SemanticQueryTerm queryTerm : suggestions) {
+            assertThat(queryTerm.value().toLowerCase(), containsString("apop"));
+        }
     }
 
 }
