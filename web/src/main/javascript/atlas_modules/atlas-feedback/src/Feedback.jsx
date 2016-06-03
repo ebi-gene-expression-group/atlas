ca40e0fb8e52ad63bdc48b0e652859d1011d7ca9
wbazant <wbazant@ebi.ac.uk>
2016-06-03 10:06:43 +0100

    New smileys component

--- web/src/main/javascript/atlas_modules/atlas-feedback/src/Feedback.jsx
+++ web/src/main/javascript/atlas_modules/atlas-feedback/src/Feedback.jsx
@@ -10,6 +10,9 @@ var BootstrapButton = require('react-bootstrap/lib/Button');
 var BootstrapFormGroup = require('react-bootstrap/lib/FormGroup');
 var BootstrapFormControl = require('react-bootstrap/lib/FormControl');
 
+var EmojiSpritesFile = require('../assets/emojione.sprites.png');
+var Emoji = require('react-emojione');
+
 require("./gxaFeedback.css");
 
 //*------------------------------------------------------------------*
@@ -134,7 +137,8 @@ var FeedbackBox = React.createClass({
                   </BootstrapButton>
                 </BootstrapFormGroup>
               </form>
-            : <div>
+            :
+              <div>
                 <BootstrapButton bsStyle="default" onClick={this._submitPositiveAnswer}>Yes</BootstrapButton>
                 <BootstrapButton onClick={function(){this.setState({askingWhyTheResultsWereNotUseful:true})}.bind(this)}bsStyle="default">No</BootstrapButton>
                 <a onClick={this.props.onRequestHide} >Do not show this again</a>
@@ -144,6 +148,126 @@ var FeedbackBox = React.createClass({
     }
   });
 
+  var Smiley = React.createClass({
+    propTypes: {
+      emoji: React.PropTypes.string.isRequired,
+      value: React.PropTypes.number.isRequired,
+      onClickCallback: React.PropTypes.func.isRequired,
+      selected: React.PropTypes.bool.isRequired
+    },
+
+    _onClick: function() {
+      this.props.onClickCallback(this.props.value);
+    },
+
+    _emojifyOptions: {
+      convertShortnames: true,
+      convertUnicode: false,
+      convertAscii: true,
+      styles: {
+          backgroundImage: 'url('+EmojiSpritesFile+')',
+          width: '32px',
+          height: '32px',
+          margin: '4px'
+      },
+    },
+
+    render: function () {
+      return (
+        <span style={{padding:"6px"}} >
+          <span className={this.props.selected? "gxaSmiley gxaSmileyClicked": "gxaSmiley"} onClick={this._onClick} >
+          {Emoji.emojify(this.props.emoji,this._emojifyOptions)}
+          </span>
+        </span>
+      );
+    }
+  });
+
+  var FeedbackSmileys = React.createClass({
+      propTypes: {
+          onComplete: React.PropTypes.func.isRequired,
+          onRequestHide : React.PropTypes.func.isRequired
+      },
+
+      mixins: [TimerMixin],
+
+      getInitialState: function() {
+        return {
+          score: -1,
+          feedbackMessage: ""
+        };
+      },
+
+      _interactionHappened: function() {
+        return this.state.score !== this.getInitialState().score;
+      },
+
+      _updateStateWithFormAnswer: function(e) {
+        this.setState({feedbackMessage: e.target.value});
+      },
+
+      _smileyClicked: function(newScore) {
+        this.setState({score: newScore});
+      },
+
+      _submit: function() {
+        this.props.onComplete(this.state.score, this.state.feedbackMessage);
+      },
+
+      componentDidUpdate: function () {
+        if(this._interactionHappened() && this.state.feedbackMessage.length===0){
+          this.setTimeout(
+            function() {
+              if(this.state.feedbackMessage.length===0) {
+                this._submit();
+              }
+            }.bind(this), 5000);
+        }
+      },
+
+      render: function() { /* identifiers from http://emoji.codes/ */
+        return (
+          <div className="gxaSmileyFeedbackBox">
+          <div id="feedbackBoxCross" className="icon icon-functional" data-icon="x" onClick={this.props.onRequestHide}></div>
+            <p> Did you find these results useful?</p>
+            <div className="gxaSmileyRow">
+              {[[":frowning:", 0],[":slight_frown:", 2], [":neutral_face:", 5], [":slight_smile:",8], [":smiley:",10]].map(function(ar){
+                return (
+                  <Smiley
+                    key={ar[0]+(this.state.score===ar[1])}
+                    emoji={ar[0]}
+                    value={ar[1]}
+                    onClickCallback={this._smileyClicked}
+                    selected={this.state.score===ar[1]}
+                  />
+                );
+              }.bind(this))}
+            </div>
+            <form style={{display: this._interactionHappened() ? "block" :"none"}}>
+              <BootstrapFormGroup
+                controlId="optionalFeedback"
+              >
+                <BootstrapFormControl
+                componentClass="textarea"
+                  type="text"
+                  value={this.state.feedbackMessage}
+                  placeholder="Feedback (optional)"
+                  onChange={this._updateStateWithFormAnswer}
+                />
+                <BootstrapFormControl.Feedback />
+                <div>
+                  <BootstrapButton onClick={this._submit}>
+                  Submit
+                  </BootstrapButton>
+                </div>
+              </BootstrapFormGroup>
+            </form>
+          </div>
+        );
+      }
+
+  });
 
 
-module.exports = FeedbackPersistence(FeedbackBox);
+
+module.exports = FeedbackPersistence(FeedbackSmileys);
--- web/src/main/javascript/atlas_modules/atlas-feedback/webpack.config.js
+++ web/src/main/javascript/atlas_modules/atlas-feedback/webpack.config.js
@@ -29,9 +29,15 @@ module.exports = {
 
     module: {
         loaders: [
-            {test: /\.jsx$/, loader: 'babel'},
-            {test: /\.css$/, loader: 'style-loader!css-loader'}
-        ]
+            	{test: /\.jsx$/, loader: 'babel'},
+            	{test: /\.css$/, loader: 'style-loader!css-loader'},
+        	{test: /\.(jpe?g|png|gif|svg)$/i,
+                loaders: [
+                          'file?hash=sha512&digest=hex&name=[hash].[ext]',
+                	  'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+                ]
+            }
+	]
     },
 
     devServer: {
