def1be4082171ad42b42e4e570c87c0cc5f584e6
wbazant <wbazant@ebi.ac.uk>
2016-06-20 08:27:11 +0100

    A quick tidyup of experiment sorter

--- web/src/main/java/uk/ac/ebi/atlas/utils/ExperimentSorter.java
+++ web/src/main/java/uk/ac/ebi/atlas/utils/ExperimentSorter.java
@@ -2,6 +2,8 @@ package uk.ac.ebi.atlas.utils;
 
 import com.google.common.collect.Ordering;
 import com.google.common.collect.TreeMultimap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import uk.ac.ebi.atlas.model.ExperimentType;
@@ -18,13 +20,11 @@ import java.nio.file.Path;
 import java.text.MessageFormat;
 import java.util.Collections;
 
-/**
- * Created by Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk> on 21/07/15.
- */
-
 @Named
 @Scope("singleton")
 public class ExperimentSorter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExperimentSorter.class);
 
     @Value("#{configuration['experiment.magetab.path.template']}")
     private String baselineTsvFileTemplate;
@@ -49,47 +49,40 @@ public class ExperimentSorter {
                 ExperimentType.PROTEOMICS_BASELINE);
     }
 
-    public TreeMultimap<Long, String> reverseSortBaselineExperimentsPerSize() {
-        return reverseSortExperimentsPerSize(
-                ExperimentType.RNASEQ_MRNA_BASELINE,
-                ExperimentType.PROTEOMICS_BASELINE);
-    }
-
-    public TreeMultimap<Long, String> reverseSortDifferentialExperimentsPerSize() {
-        return reverseSortExperimentsPerSize(
-                ExperimentType.MICROARRAY_1COLOUR_MRNA_DIFFERENTIAL,
-                ExperimentType.MICROARRAY_1COLOUR_MICRORNA_DIFFERENTIAL,
-                ExperimentType.MICROARRAY_2COLOUR_MRNA_DIFFERENTIAL,
-                ExperimentType.RNASEQ_MRNA_DIFFERENTIAL);
-    }
-
     public TreeMultimap<Long, String> reverseSortExperimentsPerSize(ExperimentType... experimentTypes) {
         TreeMultimap<Long, String> fileSizeToExperimentsMap = TreeMultimap.create(Collections.reverseOrder(), Ordering.natural());
 
         for (ExperimentType experimentType : experimentTypes) {
 
-            if (experimentType.isDifferential()) {
-                for (String experimentAccession : experimentTrader.getPublicExperimentAccessions(experimentType)) {
-                    Path diffExperimentDir = FileSystems.getDefault().getPath(MessageFormat.format(differentialTsvFileTemplate, experimentAccession)).getParent();
-                    Path diffExperimentGlobPath = FileSystems.getDefault().getPath(MessageFormat.format(differentialTsvFileTemplate, experimentAccession + "*")).getFileName();
-                    try (DirectoryStream<Path> stream = Files.newDirectoryStream(diffExperimentDir, diffExperimentGlobPath.toString())) {
-                        long diffExperimentSize = 0;
-                        for (Path path : stream) {
-                            diffExperimentSize += new File(String.valueOf(path)).length();
-                        }
-                        fileSizeToExperimentsMap.put(diffExperimentSize, experimentAccession);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            } else {
-                for (String experimentAccession : experimentTrader.getPublicExperimentAccessions(experimentType)) {
-                    String tsvFilePath = MessageFormat.format(baselineTsvFileTemplate, experimentAccession);
-                    fileSizeToExperimentsMap.put(new File(tsvFilePath).length(), experimentAccession);
-                }
+            for (String experimentAccession : experimentTrader.getPublicExperimentAccessions(experimentType)) {
+                fileSizeToExperimentsMap.put
+                        (experimentType.isDifferential()
+                                ? estimateSizeOfDifferentialExperiment(experimentAccession)
+                                : estimateSizeOfBaselineExperiment(experimentAccession),
+                                experimentAccession);
             }
+
         }
 
         return fileSizeToExperimentsMap;
+    }
+
+    private long estimateSizeOfDifferentialExperiment(String experimentAccession){
+        long diffExperimentSize = 0;
+        Path diffExperimentDir = FileSystems.getDefault().getPath(MessageFormat.format(differentialTsvFileTemplate, experimentAccession)).getParent();
+        Path diffExperimentGlobPath = FileSystems.getDefault().getPath(MessageFormat.format(differentialTsvFileTemplate, experimentAccession + "*")).getFileName();
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(diffExperimentDir, diffExperimentGlobPath.toString())) {
+            for (Path path : stream) {
+                diffExperimentSize += new File(String.valueOf(path)).length();
+            }
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage());
+        }
+        return diffExperimentSize;
+    }
+
+    private long estimateSizeOfBaselineExperiment(String experimentAccession){
+        String tsvFilePath = MessageFormat.format(baselineTsvFileTemplate, experimentAccession);
+        return new File(tsvFilePath).length();
     }
 }
