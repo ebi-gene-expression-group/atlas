72486d6db08ef8f5a3c33247c5d398076ad073a7
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2016-07-12 18:30:52 +0100

    Add flash attributes to redirect of gene and gene set page; correct parsing when one gene set or one gene ID is returned

--- web/src/main/java/uk/ac/ebi/atlas/search/BioentitiesNewSearchController.java
+++ web/src/main/java/uk/ac/ebi/atlas/search/BioentitiesNewSearchController.java
@@ -1,11 +1,7 @@
 
 package uk.ac.ebi.atlas.search;
 
-import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Sets;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import org.apache.solr.common.SolrException;
 import org.springframework.context.annotation.Scope;
 import org.springframework.http.HttpStatus;
@@ -41,14 +37,32 @@ public class BioentitiesNewSearchController {
 
         checkArgument(requestParameters.hasGeneQuery() || requestParameters.hasCondition(), "Please specify a gene query or a condition.");
 
-        String selectedSpecies = requestParameters.hasOrganism() ? requestParameters.getOrganism().trim().toLowerCase() : "";
+        model.addAttribute("isSearch", true);
+        model.addAttribute("searchDescription", requestParameters.getDescription());
 
         if (requestParameters.hasGeneQuery() && !requestParameters.hasCondition()) {
 
-            if (requestParameters.getGeneQuery().size() == 1) {
-                return singleTermGeneQuery(requestParameters.getGeneQuery(), selectedSpecies, model, redirectAttributes);
-            } else {
-                return multiTermGeneQuery(requestParameters.getGeneQuery(), selectedSpecies, model, redirectAttributes);
+            String species = requestParameters.hasOrganism() ? requestParameters.getOrganism().trim().toLowerCase() : "";
+            GeneQuery geneQuery = requestParameters.getGeneQuery();
+
+            // TODO We decide it’s a gene set because of how the query *looks*, and things like GO:FOOBAR will be incorrectly redirected to /genesets/GO:FOOBAR
+            if (GeneSetUtil.isGeneSetCategoryOrMatchesGeneSetAccession(geneQuery)) {
+                setRedirectAttributes(redirectAttributes, requestParameters);
+                return "redirect:/genesets/" + geneQuery.terms().iterator().next().value();
+            }
+
+            // Resolves to one gene ID
+            ImmutableSet<String> geneIds = analyticsSearchService.searchBioentityIdentifiers(geneQuery, species);
+            if (geneIds.size() == 0) {
+                return "empty-search-page";
+            }
+            if (geneIds.size() == 1) {
+                setRedirectAttributes(redirectAttributes, requestParameters);
+                return "redirect:/genes/" + geneIds.iterator().next();
+            }
+            // Resolves to multiple IDs
+            else {
+                return "";
             }
 
         } else if (!requestParameters.hasGeneQuery() && requestParameters.hasCondition()) {
@@ -69,123 +83,15 @@ public class BioentitiesNewSearchController {
         }
     }
 
-
-    private String singleTermGeneQuery(GeneQuery geneQuery, String species, Model model, RedirectAttributes redirectAttributes) {
-        redirectAttributes.addFlashAttribute("searchDescription", geneQuery.toString());
-
-        // Gene set ID
-        if (GeneSetUtil.isGeneSetCategoryOrMatchesGeneSetAccession(geneQuery)) {
-            return "redirect:/genesets/" + geneQuery.terms().iterator().next().value();
-        }
-
-        // Resolves to one gene ID
-        Optional<ImmutableSet<String>> geneIds = analyticsSearchService.searchBioentityIdentifiers(geneQuery, species);
-        if (geneIds.isPresent() && geneIds.get().size() == 1) {
-            return "redirect:/genes/" + geneIds.get().iterator().next();
-        }
-
-        // Resolves to more than one gene ID
-        return "";
-
-
-//        model.addAttribute("geneQuery", requestParameters.getGeneQueryDeprecated());
-//        model.addAttribute("searchDescription", requestParameters.getDescription());
-//        model.addAttribute("mainTitle", "Expression summary for " + requestParameters.getDescription());
-//
-//        SortedSet<BaselineExperimentAssayGroup> baselineExperimentAssayGroups = baselineExperimentAssayGroupSearchService.query(geneQuery, species, isExactMatch);
-//
-//        boolean showWidget = hasAllSameSpecies(baselineExperimentAssayGroups) && hasAnyTissueExperiment(baselineExperimentAssayGroups);
-//
-//        if (showWidget) {
-//            model.addAttribute("widgetHasBaselineProfiles", true);
-//            model.addAttribute("species", baselineExperimentAssayGroups.iterator().next().getSpecies());
-//
-//            SortedSet<BaselineExperimentAssayGroup> nonTissueExperimentAssayGroups = selectNonTissueExperiments(baselineExperimentAssayGroups);
-//            model.addAttribute("firstBaselineCounts", removeFirstAssayGroups(nonTissueExperimentAssayGroups, DEFAULT_BASELINE_SPLIT));
-//            model.addAttribute("remainingBaselineCounts", nonTissueExperimentAssayGroups);
-//        } else {
-//            model.addAttribute("firstBaselineCounts", removeFirstAssayGroups(baselineExperimentAssayGroups, DEFAULT_BASELINE_SPLIT));
-//            model.addAttribute("remainingBaselineCounts", baselineExperimentAssayGroups);
-//        }
-//
-//        // used to populate diff-heatmap-table
-//        DiffAnalyticsList bioentityExpressions = diffAnalyticsSearchService.fetchTop(geneQuery, species, isExactMatch);
-//
-//        model.addAttribute("bioentities", bioentityExpressions);
-//        model.addAttribute("preferences", new DifferentialRequestPreferences());
-//        model.addAttribute("requestParameters", requestParameters);
-//        model.addAttribute("exactMatch", requestParameters.isExactMatch());
-//
-//        String globalSearchTerm = ebiGlobalSearchQueryBuilder.buildGlobalSearchTerm(geneQueryString, requestParameters.getConditionQuery());
-//
-//        model.addAttribute("globalSearchTerm", globalSearchTerm);
-//
-//        return "bioEntities";
-    }
-
-    private String multiTermGeneQuery(GeneQuery geneQuery, String species, Model model, RedirectAttributes redirectAttributes) {
-        return "";
-    }
-
-//    @RequestMapping(value ={"/query/results.json"}, method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
-//    @ResponseBody
-//    public String newQueryAsJson(@Valid GeneQuerySearchRequestParameters requestParameters) {
-//        checkArgument(requestParameters.hasGeneQuery() || requestParameters.hasCondition(), "Please specify a gene query or condition!");
-//
-//        String selectedSpecies = requestParameters.hasOrganism() ? requestParameters.getOrganism().trim().toLowerCase() : "";
-//
-//        if (requestParameters.hasGeneQuery() && !requestParameters.hasCondition()) {
-//
-//            if (requestParameters.getGeneQuery().size() == 1) {
-//                return singleTermGeneQueryAsJson(requestParameters.getGeneQuery(), selectedSpecies);
-//            } else {
-//                return multiTermGeneQueryAsJson(requestParameters.getGeneQuery(), selectedSpecies);
-//            }
-//
-//        } else if (!requestParameters.hasGeneQuery() && requestParameters.hasCondition()) {
-//
-//            // Only condition was specified
-//            // if (requestParameters.getConditionQuery().size() == 1) {
-//            //     return singleTermConditionQuery(requestParameters.getConditionQuery(), selectedSpecies, requestParameters.isExactMatch(), model, redirectAttributes);
-//            // } else {
-//            //     return multiTermConditionQuery(requestParameters.getConditionQuery(), selectedSpecies, requestParameters.isExactMatch(), model, redirectAttributes);
-//            // }
-//            return "";
-//
-//        } else {
-//
-//            return "";
-//            // Both gene query and condition were specified
-//
-//        }
-//    }
-
-    private String singleTermGeneQueryAsJson(GeneQuery geneQuery, String species) {
-        Gson gson = new GsonBuilder().setPrettyPrinting().serializeSpecialFloatingPointValues().create();
-
-        // Gene set ID
-        if (GeneSetUtil.isGeneSetCategoryOrMatchesGeneSetAccession(geneQuery)) {
-            return gson.toJson(Sets.newHashSet(geneQuery.terms().iterator().next().value()));
-        }
-
-        // Resolves to one gene ID
-        Optional<ImmutableSet<String>> geneIds = analyticsSearchService.searchBioentityIdentifiers(geneQuery, species);
-        if (geneIds.isPresent()) {
-            return gson.toJson(geneIds.get());
-        }
-
-        // Resolves to more than one gene ID
-        return gson.toJson(Sets.newHashSet());
-    }
-
-    private String multiTermGeneQueryAsJson(GeneQuery geneQuery, String species) {
-        return "";
+    private void setRedirectAttributes(RedirectAttributes redirectAttributes, GeneQuerySearchRequestParameters requestParameters) {
+        redirectAttributes.addFlashAttribute("isSearch", true);
+        redirectAttributes.addFlashAttribute("searchDescription", requestParameters.getDescription());
     }
 
     @ExceptionHandler(value = {MissingServletRequestParameterException.class, IllegalArgumentException.class})
     @ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY)
     public ModelAndView handleException(Exception e) {
-        ModelAndView mav = new ModelAndView("bioentities-search-results");
+        ModelAndView mav = new ModelAndView("search-error-page");
         mav.addObject("exceptionMessage", e.getMessage());
         return mav;
     }
