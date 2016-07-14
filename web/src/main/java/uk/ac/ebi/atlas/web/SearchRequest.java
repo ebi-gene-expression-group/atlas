e08504c397a4361249e85cda4154f794593574f3
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2016-07-14 14:46:18 +0100

    Remove dummy fields used for testing

--- web/src/main/java/uk/ac/ebi/atlas/web/SearchRequest.java
+++ web/src/main/java/uk/ac/ebi/atlas/web/SearchRequest.java
@@ -1,11 +1,8 @@
 package uk.ac.ebi.atlas.web;
 
-import java.util.Date;
-
 public class SearchRequest {
 
     private GeneQuery geneQuery = getDefaultGeneQuery();
-    private Date blah;
 
     protected GeneQuery getDefaultGeneQuery() {
         return GeneQuery.create();
@@ -19,22 +16,7 @@ public class SearchRequest {
         this.geneQuery = geneQuery;
     }
 
-//    public String toString() {
-//        return Objects.toStringHelper(this.getClass())
-//                .add("geneQuery", geneQuery)
-//                .toString();
-//    }
-
     public boolean hasGeneQuery() {
         return !geneQuery.isEmpty();
     }
-
-    public Date getBlah() {
-        return blah;
-    }
-
-    public void setBlah(String blahString) {
-        this.blah = new Date(blahString);
-    }
-
 }
