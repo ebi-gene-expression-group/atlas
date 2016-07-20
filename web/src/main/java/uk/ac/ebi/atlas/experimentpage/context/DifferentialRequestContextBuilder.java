41a5741933861704a0216e2b8bfbda1e465d36dd
wbazant <wbazant@ebi.ac.uk>
2016-07-20 10:53:18 +0100

    Add a hack to preserve the previous TSV description which I didn't intend to change

--- web/src/main/java/uk/ac/ebi/atlas/experimentpage/context/DifferentialRequestContextBuilder.java
+++ web/src/main/java/uk/ac/ebi/atlas/experimentpage/context/DifferentialRequestContextBuilder.java
@@ -37,7 +37,7 @@ public class DifferentialRequestContextBuilder<T extends DifferentialRequestCont
 
         requestContext.setRequestPreferences(getRequestPreferences());
 
-        requestContext.setQueryDescription(getRequestPreferences().getGeneQuery().description());
+        requestContext.setQueryDescription(getRequestPreferences().getGeneQuery().isEmpty()? "" : getRequestPreferences().getGeneQuery().description());
 
         requestContext.setSelectedQueryFactors(getSelectedQueryContrasts(experiment));
 
