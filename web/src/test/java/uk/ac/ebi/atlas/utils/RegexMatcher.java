97bd403bdc8978cf98a7f63f8b396f4027ddf253
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2016-07-25 16:49:36 +0100

    Add a regex matcher to use with assertThat

---
+++ web/src/test/java/uk/ac/ebi/atlas/utils/RegexMatcher.java
@@ -0,0 +1,25 @@
+package uk.ac.ebi.atlas.utils;
+
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+
+public class RegexMatcher extends BaseMatcher {
+    private final String regex;
+
+    public RegexMatcher(String regex){
+        this.regex = regex;
+    }
+
+    public boolean matches(Object o){
+        return ((String)o).matches(regex);
+
+    }
+
+    public void describeTo(Description description){
+        description.appendText("matches regex=");
+    }
+
+    public static RegexMatcher matches(String regex){
+        return new RegexMatcher(regex);
+    }
+}
