d8ecd6664d0d8cc4f5cb0f60eed83f16afe2a55f
wbazant <wbazant@ebi.ac.uk>
2016-08-05 17:56:21 +0100

    Bundles

--- web/src/main/webapp/resources/js-bundles/expressionAtlasHeatmapHighcharts.bundle.js
+++ web/src/main/webapp/resources/js-bundles/expressionAtlasHeatmapHighcharts.bundle.js
@@ -3024,8 +3024,12 @@ webpackJsonp_name_([3],[
 	                return {
 	                    name: e.name,
 	                    color: e.colour,
-	                    borderWidth: 1,
-	                    borderColor: "#fff",
+	                    /*For smaller experiments, separate the cells so that they look easier to distinguish
+	                      For large experiments this would make it show up as one big white block so don't do it.
+	                      Change the magic number 200 if you feel like it.
+	                      */
+	                    borderWidth: this.props.data.xAxisCategories.length > 200 ? 0 : 1,
+	                    borderColor: "white",
 	                    data: ds[ix]
 	                };
 	            }.bind(this)),
