908d5ac663b5766b0a54fbe92a209a9d3a322503
wbazant <wbazant@ebi.ac.uk>
2016-08-19 12:02:17 +0100

    Use the hot new open source package, react-ebi-species, to show more icons. Tidy up a bit of DifferentialResults.jsx.

--- web/src/main/javascript/faceted-search/src/DifferentialResults.jsx
+++ web/src/main/javascript/faceted-search/src/DifferentialResults.jsx
@@ -15,6 +15,7 @@ var Legend = require('legend').LegendDifferential;
 var CellDifferential = require('cell-differential');
 var DifferentialDownloadButton = require('./DifferentialDownloadButton.jsx');
 var FeedbackSmileys = require('atlas-feedback');
+var EbiSpeciesIcon = require('react-ebi-species').Icon;
 
 //*------------------------------------------------------------------*
 
@@ -100,14 +101,14 @@ var DifferentialResults = React.createClass({
     render: function () {
         var differentialResultRows = this.props.results.map(function (diffResult) {
             return <DifferentialResultRow
-                key={diffResult.id}
-                bioentityIdentifier={diffResult.bioentityIdentifier} colour={diffResult.colour} foldChange={diffResult.foldChange} species={diffResult.species} comparison={diffResult.comparison} experimentName={diffResult.experimentName}
-                factors={diffResult.factors} contrastId={diffResult.contrastId} experimentAccession={diffResult.experimentAccession} displayLevels={this.state.displayLevels} atlasBaseURL={this.props.host + "/gxa"}
-            />;
+                    key={diffResult.id}
+                    displayLevels={this.state.displayLevels}
+                    atlasBaseURL={this.props.host + "/gxa"}
+                    {...diffResult} />;
         }.bind(this));
 
         var feedbackSmileys = $.browser.msie ? null
-            : 
+            :
             <div style={{marginTop:"50px"}}>
                 <FeedbackSmileys
                     collectionCallback={function(score,comment) {
@@ -168,75 +169,42 @@ var DifferentialResultRow = React.createClass({
         atlasBaseURL: React.PropTypes.string.isRequired
     },
 
+    _linkToComparisonPage: function(){
+      return (
+        "experiments/" + this.props.experimentAccession
+        + "?geneQuery=" + this.props.bioentityIdentifier
+        + "&queryFactorValues=" + this.props.contrastId
+        + "&specific=false"
+      );
+    },
+
     // TODO Use this.props.contrastId and this.props.experimentAccession to add link to the relevant experiment/comparison
     render: function () {
-        var classColor="";
-
-        if (this.props.species === "homo sapiens" || this.props.species === "gallus gallus" || this.props.species === "gorilla gorilla" || this.props.species === "macaca mulatta" || this.props.species === "monodelphis domestica" || this.props.species === "mus musculus" || this.props.species === "pan paniscus" || this.props.species === "pan troglodytes" || this.props.species === "rattus norvegicus") {
-            classColor="red";
-        } else if (this.props.species == "arabidopsis thaliana" || this.props.species === "hordeum vulgare subsp. vulgare" || this.props.species === "oryza sativa japonica group" ) {
-            classColor="green";
-        } else if (this.props.species === "anolis carolinensis" || this.props.species === "drosophila melanogaster" || this.props.species === "caenorhabditis elegans" || this.props.species === "tetraodon nigroviridis" || this.props.species === "xenopus (silurana) tropicalis") {
-            classColor="blue";
-        }
-//        || this.props.species === "gallus gallus" || this.props.species === "gorilla gorilla" || this.props.species === "macaca mulatta" || this.props.species === "monodelphis domestica" ||  this.props.species === "pan paniscus" || this.props.species === "pan troglodytes" || this.props.species === "rattus norvegicus"
-//        || this.props.species === "hordeum vulgare subsp. vulgare" || this.props.species === "oryza sativa japonica group"
-//        || this.props.species === "drosophila melanogaster" || this.props.species === "caenorhabditis elegans" || this.props.species === "tetraodon nigroviridis" || this.props.species === "xenopus (silurana) tropicalis"
-        var classIcon="";
-
-        if (this.props.species === "homo sapiens") {
-            classIcon="H";
-        } else if (this.props.species == "mus musculus") {
-            classIcon="M";
-        } else if (this.props.species === "anolis carolinensis") {
-            classIcon="7";
-        } else if (this.props.species === "arabidopsis thaliana") {
-            classIcon="B";
-        } else if (this.props.species === "bos taurus") {
-            classIcon="C";
-        } else if (this.props.species === "caenorhabditis elegans") {
-            classIcon="W";
-        } else if (this.props.species === "gallus gallus") {
-            classIcon="k";
-        } else if (this.props.species === "gorilla gorilla") {
-            classIcon="G";
-        } else if (this.props.species === "hordeum vulgare subsp. vulgare") {
-            classIcon="5";
-        } else if (this.props.species === "macaca mulatta") {
-            classIcon="r";
-        } else if (this.props.species === "monodelphis domestica") {
-            classIcon="9";
-        } else if (this.props.species === "oryctolagus cuniculus") {
-            classIcon="t";
-        } else if (this.props.species === "oryza sativa japonica group") {
-            classIcon="6";
-        } else if (this.props.species === "pan paniscus" || this.props.species === "pan troglodytes" ) {
-            classIcon="i";
-        } else if (this.props.species === "papio anubis") {
-            classIcon="8";
-        } else if (this.props.species === "rattus norvegicus") {
-            classIcon="R";
-        } else if (this.props.species === "tetraodon nigroviridis") {
-            classIcon="E";
-        } else if (this.props.species === "zea mays") {
-            classIcon="5";
-        } else if (this.props.species === "xenopus (silurana) tropicalis") {
-            classIcon="f";
-        } else if (this.props.species === "xenopus tropicalis") {
-            classIcon="f";
-        } else if (this.props.species === "drosophila melanogaster") {
-            classIcon="F";
-        } else {classIcon="";}
-
         var factors = this.props.factors ? this.props.factors.toString().replace(/,/g, ", ") : "";
 
         return (
             <tr>
-                <CellDifferential colour={this.props.colour} infinity={this.props.infinity} foldChange={this.props.foldChange} displayLevels={this.props.displayLevels}/>
-                <td className="col_species"><span className={"icon icon-species " + classColor} data-icon={classIcon} style={{color: 'red'}} title={this.props.species}/></td>
-                <td ref="comparison"><a href={"experiments/" + this.props.experimentAccession + "?geneQuery=" + this.props.bioentityIdentifier + "&queryFactorValues=" + this.props.contrastId + "&specific=false"}>{this.props.comparison}</a></td>
-                <td className="gxaExperimentalVariable">{factors}</td>
-                <td><a href={"experiments/" + this.props.experimentAccession}>{this.props.experimentName}</a></td>
+              <CellDifferential
+                colour={this.props.colour}
+                infinity={this.props.infinity}
+                foldChange={this.props.foldChange}
+                displayLevels={this.props.displayLevels}/>
+              <td className="col_species">
+                <EbiSpeciesIcon species={this.props.species}/>
+              </td>
+              <td ref="comparison">
+                <a href={this._linkToComparisonPage()}>
+                  {this.props.comparison}
+                </a>
+              </td>
+              <td className="gxaExperimentalVariable">
+                {factors}
+              </td>
+              <td>
+                <a href={"experiments/" + this.props.experimentAccession}>
+                  {this.props.experimentName}
+                </a>
+              </td>
             </tr>
         );
     },
