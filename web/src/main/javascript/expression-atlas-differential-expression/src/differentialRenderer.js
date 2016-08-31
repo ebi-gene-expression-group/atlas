8b0c358d7f79348837f2b118b03383f484e4d27a
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2016-08-31 15:20:30 +0100

    Use the current protocol and host if none is specified

--- web/src/main/javascript/expression-atlas-differential-expression/src/differentialRenderer.js
+++ web/src/main/javascript/expression-atlas-differential-expression/src/differentialRenderer.js
@@ -7,7 +7,7 @@ const DifferentialRouter = require('./DifferentialRouter.jsx');
 
 //*------------------------------------------------------------------*
 
-module.exports = function ({atlasHostUrl : hostUrl = "https://www.ebi.ac.uk", geneQuery, conditionQuery, species, target = 'gxaDifferentialTab', }) {
+module.exports = function ({atlasHostUrl : hostUrl = window.location.protocol + "//" + window.location.host, geneQuery, conditionQuery, species, target = 'gxaDifferentialTab', }) {
     ReactDOM.render(
         React.createElement(DifferentialRouter, {hostUrl: hostUrl, geneQuery: geneQuery, conditionQuery: conditionQuery, species: species}),
         document.getElementById(target)
