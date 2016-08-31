556bd0ad6fa1346b67b3a8d909b59b3cd2446263
wbazant <wbazant@ebi.ac.uk>
2016-08-31 16:55:55 +0100

    Fold change values should not be string - update test setup

--- web/src/test/java/uk/ac/ebi/atlas/profiles/differential/viewmodel/DifferentialProfilesViewModelBuilderTest.java
+++ web/src/test/java/uk/ac/ebi/atlas/profiles/differential/viewmodel/DifferentialProfilesViewModelBuilderTest.java
@@ -76,14 +76,14 @@ public class DifferentialProfilesViewModelBuilderTest {
                 "        {\n" +
                 "          \"contrastName\": \"cdk8\",\n" +
                 "          \"color\": \"#C0C0C0\",\n" +
-                "          \"foldChange\": \"-0.8\",\n" +
+                "          \"foldChange\": -0.8,\n" +
                 "          \"pValue\": \"0.002\",\n" +
                 "          \"tStat\": \"-6.45\"\n" +
                 "        },\n" +
                 "        {\n" +
                 "          \"contrastName\": \"cycC\",\n" +
                 "          \"color\": \"#0000FF\",\n" +
-                "          \"foldChange\": \"-8.8\",\n" +
+                "          \"foldChange\": -8.8,\n" +
                 "          \"pValue\": \"3.24E-4\",\n" +
                 "          \"tStat\": \"-3.37\"\n" +
                 "        }\n" +
@@ -97,7 +97,7 @@ public class DifferentialProfilesViewModelBuilderTest {
                 "        {\n" +
                 "          \"contrastName\": \"cdk8\",\n" +
                 "          \"color\": \"#FF0000\",\n" +
-                "          \"foldChange\": \"1.3\",\n" +
+                "          \"foldChange\": 1.3,\n" +
                 "          \"pValue\": \"2.16E-4\",\n" +
                 "          \"tStat\": \"10.71\"\n" +
                 "        },\n" +
