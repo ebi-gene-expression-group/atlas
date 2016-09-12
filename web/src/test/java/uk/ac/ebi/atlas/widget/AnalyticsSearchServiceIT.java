63165504bc905f331462fb7fe8490bb846d96308
wbazant <wbazant@ebi.ac.uk>
2016-09-12 09:52:06 +0100

    Test that shows us which fields we have in analytics search service

--- web/src/test/java/uk/ac/ebi/atlas/widget/AnalyticsSearchServiceIT.java
+++ web/src/test/java/uk/ac/ebi/atlas/widget/AnalyticsSearchServiceIT.java
@@ -1,5 +1,7 @@
 package uk.ac.ebi.atlas.widget;
 
+import com.google.common.collect.ImmutableList;
+import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import org.hamcrest.Matchers;
@@ -18,6 +20,7 @@ import javax.inject.Inject;
 import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertTrue;
@@ -37,8 +40,9 @@ public class AnalyticsSearchServiceIT extends RestAssuredFixture {
     @Inject
     private DifferentialAnalyticsSearchService differentialAnalyticsSearchService;
 
+    //These tests are really the assertions for ExpressionDataControllerEIT.
     @Test
-    public void geneExpressedInBaselineAndDifferentialExperimentsReturnsTrue() {
+    public void geneExpressedInBaselineAndDifferentialExperiments() {
         JsonObject result = baselineAnalyticsSearchService.findFacetsForTreeSearch(SemanticQuery.create(BASELINE_GENE),
                 SemanticQuery.create(), SpeciesFactory.NULL);
         assertThat(result.entrySet(), not(Matchers.empty()));
@@ -47,7 +51,7 @@ public class AnalyticsSearchServiceIT extends RestAssuredFixture {
     }
 
     @Test
-    public void geneExpressedInDifferentialExperimentsOnlyReturnsFalse() {
+    public void geneExpressedInDifferentialExperimentsOnly() {
         assertThat(
                 baselineAnalyticsSearchService.findFacetsForTreeSearch(SemanticQuery.create(DIFFERENTIAL_GENE), SemanticQuery.create(), SpeciesFactory.NULL),
                 is(new JsonObject()));
@@ -57,7 +61,7 @@ public class AnalyticsSearchServiceIT extends RestAssuredFixture {
     }
 
     @Test
-    public void nonExistentGeneReturnsFalse() {
+    public void nonExistentGene() {
         assertThat(
                 baselineAnalyticsSearchService.findFacetsForTreeSearch(SemanticQuery.create(NON_EXISTENT_GENE), SemanticQuery.create(), SpeciesFactory.NULL),
                 is(new JsonObject()));
@@ -65,5 +69,37 @@ public class AnalyticsSearchServiceIT extends RestAssuredFixture {
                 differentialAnalyticsSearchService.fetchDifferentialResultsForSearch(SemanticQuery.create
                         (NON_EXISTENT_GENE)).get("results").getAsJsonArray().size(),
                 is(0));
+    }
+
+    @Test
+    public void differentialAnalyticsSearchServiceHasTheRightReturnFormat(){
+        JsonObject result = differentialAnalyticsSearchService.fetchDifferentialResultsForSearch(SemanticQuery.create("GO:0008150"));
+        testDifferentialResultsAreInRightFormat(result);
+    }
+
+    private ImmutableList<String> fieldsNeededInDifferentialResults = ImmutableList.of(
+            "species",
+            "kingdom",
+            "experimentType",
+            "numReplicates",
+            "regulation",
+            "factors",
+            "bioentityIdentifier",
+            "experimentAccession",
+            "experimentName",
+            "contrastId",
+            "comparison",
+            "foldChange",
+            "colour",
+            "id");
+
+    void testDifferentialResultsAreInRightFormat(JsonObject result){
+        assertTrue(new Gson().toJson(result), result.has("results"));
+        assertThat(result.get("results").getAsJsonArray().size(), greaterThan(0));
+        for(JsonElement e: result.get("results").getAsJsonArray()){
+            for(String fieldName: fieldsNeededInDifferentialResults) {
+                assertTrue("result has "+fieldName, e.getAsJsonObject().has(fieldName));
+            }
+        }
     }
 }
