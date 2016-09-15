0067287728f8c762bab6e02b6caff75883a82d87
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2016-09-15 16:04:42 +0100

    Use constructor injection instead of field injection. http://programmers.stackexchange.com/questions/300706/dependency-injection-field-injection-vs-constructor-injection http://olivergierke.de/2013/11/why-field-injection-is-evil/

--- web/src/main/java/uk/ac/ebi/atlas/trader/loader/ProteomicsBaselineExperimentsCacheLoaderFactory.java
+++ web/src/main/java/uk/ac/ebi/atlas/trader/loader/ProteomicsBaselineExperimentsCacheLoaderFactory.java
@@ -2,12 +2,9 @@ package uk.ac.ebi.atlas.trader.loader;
 
 import org.springframework.beans.factory.annotation.Value;
 import uk.ac.ebi.atlas.experimentimport.ExperimentDAO;
-import uk.ac.ebi.atlas.model.baseline.BaselineExperimentBuilder;
-import uk.ac.ebi.atlas.model.baseline.ExperimentalFactorsBuilder;
 import uk.ac.ebi.atlas.trader.ConfigurationTrader;
 import uk.ac.ebi.atlas.trader.ExperimentDesignParser;
 import uk.ac.ebi.atlas.trader.SpeciesFactory;
-import uk.ac.ebi.atlas.trader.SpeciesKingdomTrader;
 import uk.ac.ebi.atlas.utils.ArrayExpressClient;
 
 import javax.inject.Inject;
@@ -16,29 +13,33 @@ import javax.inject.Named;
 @Named
 public class ProteomicsBaselineExperimentsCacheLoaderFactory {
 
-    @Inject
     private ConfigurationTrader configurationTrader;
-
-
-    @Inject
     private SpeciesFactory speciesFactory;
-
-    @Inject
     private ProteomicsBaselineExperimentExpressionLevelFile expressionLevelFile;
-
-    @Inject
     private ArrayExpressClient arrayExpressClient;
+    private ExperimentDesignParser experimentDesignParser;
+    private String extraInfoPathTemplate;
 
     @Inject
-    private ExperimentDesignParser experimentDesignParser;
-
-    @Value("#{configuration['experiment.extra-info-image.path.template']}")
-    private String extraInfoPathTemplate;
+    public ProteomicsBaselineExperimentsCacheLoaderFactory(ConfigurationTrader configurationTrader,
+                                                           SpeciesFactory speciesFactory,
+                                                           ProteomicsBaselineExperimentExpressionLevelFile expressionLevelFile,
+                                                           ArrayExpressClient arrayExpressClient,
+                                                           ExperimentDesignParser experimentDesignParser,
+                                                           @Value("#{configuration['experiment.extra-info-image.path.template']}") String extraInfoPathTemplate) {
+        this.configurationTrader = configurationTrader;
+        this.speciesFactory = speciesFactory;
+        this.expressionLevelFile = expressionLevelFile;
+        this.arrayExpressClient = arrayExpressClient;
+        this.experimentDesignParser = experimentDesignParser;
+        this.extraInfoPathTemplate = extraInfoPathTemplate;
+    }
 
     public ProteomicsBaselineExperimentsCacheLoader create(ExperimentDAO experimentDao) {
 
-        ProteomicsBaselineExperimentsCacheLoader loader = new ProteomicsBaselineExperimentsCacheLoader
-                (expressionLevelFile, configurationTrader, speciesFactory);
+        ProteomicsBaselineExperimentsCacheLoader loader =
+                new ProteomicsBaselineExperimentsCacheLoader(
+                        expressionLevelFile, configurationTrader, speciesFactory);
 
         loader.setExtraInfoPathTemplate(extraInfoPathTemplate);
         loader.setExperimentDAO(experimentDao);
