bf3c9162a9902ccc8a1fc777771ce75ccba24f72
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2016-10-17 19:16:51 +0100

    Remove author comment block

--- web/src/main/java/uk/ac/ebi/atlas/utils/ResourceUtils.java
+++ web/src/main/java/uk/ac/ebi/atlas/utils/ResourceUtils.java
@@ -6,9 +6,6 @@ import org.springframework.core.io.Resource;
 import java.io.IOException;
 import java.io.InputStream;
 
-/**
- * Created by Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk> on 12/11/2015.
- */
 public class ResourceUtils {
     public static String readPlainTextResource(Resource resource) {
         try (InputStream inputStream = resource.getInputStream()) {
