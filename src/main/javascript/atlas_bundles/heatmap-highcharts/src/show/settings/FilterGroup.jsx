003a8db3a4597f6db988e8703af7f06231981a9b
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2016-10-27 15:16:10 +0100

    611105-131157783 Filter menu on top of heatmap: refactor expandable elements

--- src/main/javascript/atlas_bundles/heatmap-highcharts/src/show/settings/FilterGroup.jsx
+++ src/main/javascript/atlas_bundles/heatmap-highcharts/src/show/settings/FilterGroup.jsx
@@ -7,9 +7,9 @@ const FilterGroup = React.createClass({
         name: React.PropTypes.string.isRequired,
         values: React.PropTypes.arrayOf(React.PropTypes.shape({
             name: React.PropTypes.string.isRequired,
+            elements: React.PropTypes.arrayOf(React.PropTypes.string),
             selected: React.PropTypes.bool.isRequired
         })).isRequired,
-        elements: React.PropTypes.arrayOf(React.PropTypes.string).isRequired,
         onChange: React.PropTypes.func.isRequired
     },
 
@@ -49,43 +49,50 @@ const FilterGroup = React.createClass({
         this.setState({ visibleElements: newVisibleElements })
     },
 
-    _showElements(filterValueName) {
+    _areElementsVisible(filterValueName) {
         return this.state.visibleElements.find(visibleElement => visibleElement.name === filterValueName).showElements
+    },
+
+    _expandableFilterValueWithElements(filterValue) {
+        return (
+        <span style={{paddingLeft: `5px`, textTransform: `capitalize`}}>
+            <a style={{display: !this._areElementsVisible(filterValue.name) ? `inline-block` : `none`}}
+                       onClick={(event) => { this._toggleElements(event, filterValue.name)}}
+               href="#"
+            >{filterValue.name}<Glyphicon style={{fontSize: `x-small`, paddingLeft: `5px`}} glyph="menu-down"/>
+            </a>
+            <a style={{display: this._areElementsVisible(filterValue.name) ? `inline-block` : `none`}}
+                       onClick={(event) => { this._toggleElements(event, filterValue.name)}}
+               href="#"
+            >{filterValue.name}<Glyphicon style={{fontSize: `x-small`, paddingLeft: `5px`}} glyph="menu-up"/>
+            </a>
+            <ul style={{paddingLeft: `30px`, fontSize: `small`,
+                        display: this._areElementsVisible(filterValue.name) ? `block` : `none`}}
+            >{filterValue.elements.map(element =>
+                (
+                    <li key={element}>{element}</li>
+                )
+            )}
+            </ul>
+        </span>
+        );
     },
 
     render() {
         return (
             <div>
-                <h3>{this.props.name}</h3>
+                <h4>{this.props.name}</h4>
+
                     {this.props.values.map(filterValue =>
                         (
                             <div key={filterValue.name}>
-                                <label>
-                                    <input type="checkbox" value={filterValue.name} onChange={this.handleChange}
-                                           disabled={this.props.disabled}
-                                           checked={this.state.checked.includes(filterValue.name)}/>
-                                </label>
-                                <a style={{
-                                    paddingLeft: `5px`,
-                                    display: !this._showElements(filterValue.name) ? `inline-block` : `none`
-                                   }}
-                                   onClick={(event) => { this._toggleElements(event, filterValue.name) }} href="#">{filterValue.name} <Glyphicon style={{fontSize: `x-small`, paddingLeft: `5px`}} glyph="menu-down"/></a>
-                                <a style={{
-                                    paddingLeft: `5px`,
-                                    display: this._showElements(filterValue.name) ? `inline-block` : `none`
-                                   }}
-                                   onClick={(event) => { this._toggleElements(event, filterValue.name) }} href="#">{filterValue.name} <Glyphicon style={{fontSize: `x-small`, paddingLeft: `5px`}} glyph="menu-up"/></a>
-                                <div style={{
-                                    paddingLeft: `10px`,
-                                    fontSize: `small`,
-                                    display: this._showElements(filterValue.name) ? `block` : `none`
-                                  }}>
-                                    {this.props.elements.map(element =>
-                                        (
-                                            <span key={element}>{element}<br /></span>
-                                        )
-                                    )}
-                                </div>
+                                <input type="checkbox" value={filterValue.name} onChange={this.handleChange}
+                                       disabled={this.props.disabled}
+                                       checked={this.state.checked.includes(filterValue.name)}/>
+                                {filterValue.elements ?
+                                    this._expandableFilterValueWithElements(filterValue) :
+                                    <span style={{paddingLeft: `5px`}}>{filterValue.name}</span>
+                                }
                             </div>
                         )
                     )}
