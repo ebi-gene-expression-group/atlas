f4afb30df287acc1250ebe08dc5ef6a70475bd54
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2016-11-22 11:01:09 +0000

    Update resources to provide all necessary config to atlas-base classes

---
+++ gxa/src/test/java/uk/ac/ebi/atlas/solr/EmbeddedSolrServerFactory.java
@@ -0,0 +1,23 @@
+package uk.ac.ebi.atlas.solr;
+
+import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer;
+import org.apache.solr.core.CoreContainer;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.inject.Named;
+import java.io.IOException;
+
+// Bean definition in test-solrContext.xml
+@Named
+public class EmbeddedSolrServerFactory {
+
+    @Value("#{configuration['solr.location']}")
+    private String solrConfDir;
+
+    public EmbeddedSolrServer createEmbeddedSolrServerInstance() throws IOException {
+            System.setProperty("solr.indexes.dir", System.getProperty("java.io.tmpdir"));
+            CoreContainer coreContainer =  new CoreContainer(solrConfDir);
+            coreContainer.load();
+            return new EmbeddedSolrServer(coreContainer, "gxa");
+    }
+}
