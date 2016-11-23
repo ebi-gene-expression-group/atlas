f181c56c6a33ee88733fb0c8a0f019edcdabc2ab
wbazant <wbazant@ebi.ac.uk>
2016-11-23 14:46:06 +0000

    A doodle for the cell line page

--- sc/src/main/java/uk/ac/ebi/atlas/experimentpage/SingleCellExperimentPageController.java
+++ sc/src/main/java/uk/ac/ebi/atlas/experimentpage/SingleCellExperimentPageController.java
@@ -2,16 +2,13 @@ package uk.ac.ebi.atlas.experimentpage;
 
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import uk.ac.ebi.atlas.trader.ExperimentTrader;
 import uk.ac.ebi.atlas.trader.SingleCellExperimentTrader;
 
 import javax.inject.Inject;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Controller
@@ -27,11 +24,32 @@ public class SingleCellExperimentPageController extends ExperimentPageController
     @RequestMapping(value = "/experiments/{experimentAccession}")
     @ResponseBody
     public String baselineExperimentData(@PathVariable String experimentAccession,
-                                         @RequestParam(required = false) String accessKey,
-                                         Model model, HttpServletRequest request, HttpServletResponse response) {
+                                         HttpServletResponse response) {
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
 
 
         return gson.toJson(experimentTrader.getPublicExperiment(experimentAccession).getAttributes());
+    }
+
+    @RequestMapping(value = "/experiments/{experimentAccession}/assay_group/{assayGroupId}")
+    @ResponseBody
+    public String baselineExperimentAssay(@PathVariable String experimentAccession,
+                                          @PathVariable String assayGroupId,
+                                         HttpServletResponse response) {
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+/*
+        BaselineExperiment experiment = (BaselineExperiment) experimentTrader.getExperiment(experimentAccession, accessKey);
+
+        AssayGroup assayGroup = experiment.getAssayGroups().getAssayGroup(assayGroupId);
+
+        ExperimentDesign experimentDesign = experiment.getExperimentDesign();
+
+        AssayGroupSummary assayGroupSummary = new AssayGroupSummaryBuilder().withExperimentDesign(experimentDesign).forAssayGroup(assayGroup)
+                .build();
+
+        return new Gson().toJson(assayGroupSummary);*/
+
+        return "";
     }
 }
