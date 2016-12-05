0d9fb8710112c2a937cc6baf39c964ff55525f7b
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2016-12-05 13:11:19 +0000

    Fix test for build http://bar:8085/browse/ATLAS-ATLASBASE-51/test

--- base/src/test/java/uk/ac/ebi/atlas/model/experiment/baseline/ExpressionTest.java
+++ base/src/test/java/uk/ac/ebi/atlas/model/experiment/baseline/ExpressionTest.java
@@ -1,36 +1,26 @@
-
 package uk.ac.ebi.atlas.model.experiment.baseline;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
 import uk.ac.ebi.atlas.model.experiment.baseline.impl.FactorSet;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-@RunWith(MockitoJUnitRunner.class)
 public class ExpressionTest {
 
     private BaselineExpression subject;
 
     @Before
-    public void initSubject() {
-
+    public void setUp() {
         Factor factor = new Factor("aType", "heart");
-
         subject = new BaselineExpression(2.3, new FactorSet().add(factor));
     }
 
     @Test
-    public void testIsGreaterThan() throws Exception {
-
+    public void testIsGreaterThanOrEquals() {
         assertThat(subject.isGreaterThanOrEqual(0.0), is(true));
-
         assertThat(subject.isGreaterThanOrEqual(3.0), is(false));
-
-        assertThat(subject.isGreaterThanOrEqual(2.3), is(false));
-
+        assertThat(subject.isGreaterThanOrEqual(2.3), is(true));
     }
 }
