802b31373e897583f4accb66297cd4dcd3b250f3
wbazant <wbazant@ebi.ac.uk>
2016-12-13 17:20:12 +0000

    Rename. Fix pubmed link through exposing applicationProperties

---
+++ sc/src/test/java/uk/ac/ebi/atlas/experimentpage/ExperimentPageIT.java
@@ -0,0 +1,28 @@
+package uk.ac.ebi.atlas.experimentpage;
+
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import uk.ac.ebi.atlas.web.ApplicationProperties;
+
+import javax.inject.Inject;
+
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(locations = {"classpath:applicationContext.xml", "classpath:solrContext.xml", "classpath:oracleContext.xml"})
+public class ExperimentPageIT {
+
+    @Inject
+    ApplicationProperties applicationProperties;
+
+    @Test
+    public void pubMedIds(){
+        assertThat(applicationProperties.getPubMedURL("25867922"), Matchers.notNullValue());
+    }
+}
