611f7b3daba7a84b10a6c501dee4892552ce70dc
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-01-03 13:06:40 +0000

    Build map with @PostConstruct after bean is initialised; calculate map difference and return an informative message after refresh

--- base/src/main/java/uk/ac/ebi/atlas/species/SpeciesPropertiesTrader.java
+++ base/src/main/java/uk/ac/ebi/atlas/species/SpeciesPropertiesTrader.java
@@ -2,14 +2,17 @@ package uk.ac.ebi.atlas.species;
 
 import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableSortedMap;
-import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang3.ArrayUtils;
+import com.google.common.collect.MapDifference;
+import com.google.common.collect.Maps;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collection;
 
 @Named
@@ -18,12 +21,57 @@ public class SpeciesPropertiesTrader {
     private static final Logger LOGGER = LoggerFactory.getLogger(SpeciesPropertiesTrader.class);
 
     private SpeciesPropertiesDao speciesPropertiesDao;
-    private ImmutableSortedMap<String, SpeciesProperties> nameToSpecies;
+    private ImmutableSortedMap<String, SpeciesProperties> nameToSpecies = ImmutableSortedMap.of();
 
     @Inject
     public void setSpeciesPropertiesDao(SpeciesPropertiesDao speciesPropertiesDao) {
         this.speciesPropertiesDao = speciesPropertiesDao;
-        nameToSpecies = buildMap();
+    }
+
+    @PostConstruct
+    public String refresh() throws IOException {
+        try {
+            LOGGER.info("Reading species properties...");
+
+            ImmutableSortedMap<String, SpeciesProperties> newMap = buildMap();
+            MapDifference<String, SpeciesProperties> diff = Maps.difference(nameToSpecies, newMap);
+            nameToSpecies = newMap;
+
+            LOGGER.info("Retrieved {} species properties", nameToSpecies.size());
+            return describeMapDifference(diff);
+
+        } catch (IOException e) {
+            LOGGER.error("Error reading species properties");
+            throw e;
+        }
+    }
+
+    private String describeMapDifference(MapDifference<?, ?> mapDifference) {
+        if (mapDifference.areEqual()) {
+            return "No changes were made to the reference species";
+        } else {
+            String updatedMessage = mapDifference.entriesDiffering().isEmpty() ?
+                    null : Arrays.deepToString(mapDifference.entriesDiffering().keySet().toArray()) + " updated";
+
+            String removedMessage = mapDifference.entriesOnlyOnLeft().isEmpty() ?
+                    null : Arrays.deepToString(mapDifference.entriesOnlyOnLeft().keySet().toArray()) + " removed";
+
+            String addedMessage = mapDifference.entriesOnlyOnRight().isEmpty() ?
+                    null : Arrays.deepToString(mapDifference.entriesOnlyOnRight().keySet().toArray()) + " added";
+
+            return Joiner.on("; ").skipNulls().join(updatedMessage, removedMessage, addedMessage);
+        }
+    }
+
+    private ImmutableSortedMap<String, SpeciesProperties> buildMap() throws IOException {
+        ImmutableSortedMap.Builder<String, SpeciesProperties> nameToSpeciesPropertiesBuilder =
+                ImmutableSortedMap.naturalOrder();
+
+        for (SpeciesProperties speciesProperties : speciesPropertiesDao.fetchAll()) {
+            nameToSpeciesPropertiesBuilder.put(speciesProperties.referenceName(), speciesProperties);
+        }
+
+        return nameToSpeciesPropertiesBuilder.build();
     }
 
     public SpeciesProperties get(String speciesName) {
@@ -38,34 +86,7 @@ public class SpeciesPropertiesTrader {
     private String normalise(String str) {
         String normalisedString = str.contains("_") ? str.replace("_", " ") : str;
 
-        // Arrays.copyOf pads the array with nulls, which breaks Joiner
-        return Joiner.on(' ').join(ArrayUtils.subarray(normalisedString.toLowerCase().split(" "), 0, 2));
-    }
-
-    public void refresh() {
-        nameToSpecies = buildMap();
-    }
-
-    private ImmutableSortedMap<String, SpeciesProperties> buildMap() {
-        try {
-
-            LOGGER.info("Reading species properties...");
-
-            ImmutableSortedMap.Builder<String, SpeciesProperties> nameToSpeciesPropertiesBuilder =
-                    ImmutableSortedMap.naturalOrder();
-
-            for (SpeciesProperties speciesProperties : speciesPropertiesDao.fetchAll()) {
-                nameToSpeciesPropertiesBuilder.put(speciesProperties.referenceName(), speciesProperties);
-            }
-
-            ImmutableSortedMap<String, SpeciesProperties> nameToSpeciesProperties = nameToSpeciesPropertiesBuilder.build();
-            LOGGER.info("Retrieved {} species properties", nameToSpeciesProperties.size());
-            return nameToSpeciesProperties;
-
-        } catch (IOException e) {
-            LOGGER.error("Error reading species properties");
-            return ImmutableSortedMap.of();
-        }
+        return Joiner.on(' ').skipNulls().join(Arrays.copyOf(normalisedString.toLowerCase().split(" "), 2));
     }
 
 }
