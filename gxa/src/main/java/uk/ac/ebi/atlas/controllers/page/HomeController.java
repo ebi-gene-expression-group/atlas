e9c63b1a0b442e7e593604fefd6d365478a34c1a
wbazant <wbazant@ebi.ac.uk>
2017-01-11 17:19:02 +0000

    Latest experiments on foundation home

--- gxa/src/main/java/uk/ac/ebi/atlas/controllers/page/HomeController.java
+++ gxa/src/main/java/uk/ac/ebi/atlas/controllers/page/HomeController.java
@@ -1,23 +1,31 @@
 package uk.ac.ebi.atlas.controllers.page;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import uk.ac.ebi.atlas.experiments.ExperimentInfoListService;
+import static uk.ac.ebi.atlas.model.experiment.ExperimentType.*;
 import uk.ac.ebi.atlas.species.SpeciesProperties;
 import uk.ac.ebi.atlas.species.SpeciesPropertiesTrader;
+import uk.ac.ebi.atlas.trader.ExperimentTrader;
 
 import javax.inject.Inject;
 
 @Controller
 public class HomeController {
 
-    private SpeciesPropertiesTrader speciesPropertiesTrader;
+    private final SpeciesPropertiesTrader speciesPropertiesTrader;
+    private final ExperimentInfoListService experimentInfoListService;
 
     @Inject
-    public HomeController(SpeciesPropertiesTrader speciesPropertiesTrader) {
+    public HomeController(SpeciesPropertiesTrader speciesPropertiesTrader,ExperimentTrader experimentTrader){
         this.speciesPropertiesTrader = speciesPropertiesTrader;
+        experimentInfoListService = new ExperimentInfoListService(experimentTrader,
+                ImmutableSet.of(RNASEQ_MRNA_BASELINE, PROTEOMICS_BASELINE, RNASEQ_MRNA_DIFFERENTIAL,
+                        MICROARRAY_1COLOUR_MICRORNA_DIFFERENTIAL, MICROARRAY_1COLOUR_MRNA_DIFFERENTIAL, MICROARRAY_2COLOUR_MRNA_DIFFERENTIAL));
     }
 
     @RequestMapping(value = "/home")
@@ -33,7 +41,9 @@ public class HomeController {
     }
 
     @RequestMapping(value = "/fhome")
-    public String getFoundationHomePage() {
+
+    public String getFoundationHomePage(Model model) {
+        model.addAllAttributes(experimentInfoListService.getLatestExperimentsListAttributes());
         return "foundation-home";
     }
 
