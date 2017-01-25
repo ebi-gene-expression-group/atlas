71889c567bdc4e506988f830f94e75d0449526a9
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-01-25 16:01:28 +0000

    ES6-ify and React-ify

--- gxa/src/main/javascript/atlas_modules/cell-differential/src/CellDifferential.jsx
+++ gxa/src/main/javascript/atlas_modules/cell-differential/src/CellDifferential.jsx
@@ -1,25 +1,15 @@
-"use strict";
-
-//*------------------------------------------------------------------*
-
-var React = require('react');
-var ReactDOM = require('react-dom');
-var ReactDOMServer = require('react-dom/server');
-var $ = require('jquery');
+const React = require('react');
+const ReactDOM = require('react-dom');
+const ReactDOMServer = require('react-dom/server');
+const $ = require('jquery');
 require('jquery-ui-bundle');
 
-//*------------------------------------------------------------------*
-
-var NumberFormat = require('expression-atlas-number-format');
-
-//*------------------------------------------------------------------*
+const NumberFormat = require('expression-atlas-number-format').default;
 
 require('./gxaShowHideCell.css');
 require('./gxaDifferentialCellTooltip.css');
 
-//*------------------------------------------------------------------*
-
-var CellDifferential = React.createClass({
+const CellDifferential = React.createClass({
 
     propTypes: {
         fontSize: React.PropTypes.number,
@@ -30,82 +20,70 @@ var CellDifferential = React.createClass({
         displayLevels: React.PropTypes.bool.isRequired
     },
 
-    _hasValue: function () {
-        return (this.props.foldChange !== undefined);
+    _hasValue() {
+        return !!this.props.foldChange;
     },
 
-    _getStyle: function() {
-        var style = {};
-        if (this.props.fontSize) {
-            style.fontSize = this.props.fontSize + "px";
-        }
-
-        return style;
+    _getStyle() {
+        return this.props.fontSize ? { fontSize: `${this.props.fontSize}px` } : {};
     },
 
-    render: function () {
+    render() {
         if (!this._hasValue()) {
-            return (<td/>);
+            return <td/>;
         }
 
         return (
-            <td style={{backgroundColor: this.props.colour, verticalAlign: "middle"}}>
-                <div style={this._getStyle()} className={this.props.displayLevels ? "gxaShowCell" : "gxaHideCell"}>
+            <td style={{backgroundColor: this.props.colour, verticalAlign: `middle`}}>
+                <div style={this._getStyle()} className={this.props.displayLevels ? `gxaShowCell` : `gxaHideCell`}>
                     {this.props.foldChange}
                 </div>
             </td>
         );
     },
 
-    componentDidMount: function () {
+    componentDidMount() {
         if (this._hasValue()) {
             this._initTooltip(ReactDOM.findDOMNode(this));
         }
     },
 
-    _initTooltip: function(element) {
+    _initTooltip(element) {
 
-        //TODO - build this from a React component, like we do for FactorTooltip
         function buildHeatmapCellTooltip (pValue, tStatistic, foldChange) {
-
-            return "<table>" +
-                       "<thead>" +
-                           (pValue ?
-                               "<th>Adjusted <em>p</em>-value</th>" : "") +
-                           (tStatistic?
-                               "<th><em>t</em>-statistic</th>" : "") +
-                           "<th class='gxaHeaderCell'>Log<sub>2</sub>-fold change</th>" +
-                       "</thead>" +
-                       "<tbody>" +
-                           "<tr>" +
-                               (pValue?
-                                   "<td>" + ReactDOMServer.renderToStaticMarkup(NumberFormat.scientificNotation(pValue)) + "</td>" : "") +
-                               (tStatistic ?
-                                   "<td>" + (Math.floor(tStatistic * 1e4) / 1e4) + "</td>" : "") +
-                               "<td>" + foldChange + "</td>" +
-                           "</tr>" +
-                       "</tbody>" +
-                   "</table>";
-
+            return (
+                <table>
+                    <thead>
+                        <tr>
+                            {pValue ? <th>Adjusted <em>p</em>-value</th> : null}
+                            {tStatistic ? <th><em>t</em>-statistic</th> : null}
+                            <th>Log<sub>2</sub>-fold change</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            {pValue ? <td>{NumberFormat.scientificNotation(pValue)}</td> : null}
+                            {tStatistic ? <td>{Math.floor(tStatistic * 1e4) / 1e4}</td> : null}
+                            <td>{foldChange}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            );
         }
 
-        // Don’t use bind, tooltip uses this internally
-        var thisProps = this.props;
-
-        $(element).attr("title", "").tooltip({
-            open: function (event, ui) {
-                ui.tooltip.css("background", thisProps.colour);
+        $(element).attr(`title`, ``).tooltip({
+            open: (event, ui) => {
+                ui.tooltip.css(`background`, this.props.colour);
             },
 
-            tooltipClass: "gxaDifferentialCellTooltip",
+            tooltipClass: `gxaDifferentialCellTooltip`,
 
-            content:function () {
-                return buildHeatmapCellTooltip(thisProps.pValue, thisProps.tStat, thisProps.foldChange);
+            content: () => {
+                return ReactDOMServer.renderToStaticMarkup(
+                    buildHeatmapCellTooltip(this.props.pValue, this.props.tStat, this.props.foldChange));
             }
         });
     }
 });
-
-//*------------------------------------------------------------------*
 
 module.exports = CellDifferential;
