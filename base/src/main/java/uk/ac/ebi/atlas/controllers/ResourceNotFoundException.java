a2055f05f599e9d996d7d6e2d88f296e3bebecf1
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-02-20 19:22:36 +0000

    Add a TODO note for an enhancement of ResourceNotFoundException

--- base/src/main/java/uk/ac/ebi/atlas/controllers/ResourceNotFoundException.java
+++ base/src/main/java/uk/ac/ebi/atlas/controllers/ResourceNotFoundException.java
@@ -3,9 +3,9 @@ package uk.ac.ebi.atlas.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+//TODO Make this a subclass of MissingResourceException to include fields about the resource class and key
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public final class ResourceNotFoundException extends RuntimeException {
-
     public ResourceNotFoundException(Exception exception){
         super(exception);
     }
