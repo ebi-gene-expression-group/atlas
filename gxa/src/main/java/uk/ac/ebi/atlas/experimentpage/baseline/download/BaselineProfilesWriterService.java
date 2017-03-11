cdfbd6f73856690190245fe79916e307aa01d735
wbazant <wbazant@ebi.ac.uk>
2017-03-11 20:11:50 +0000

    Profiles writer becomes a supplier

--- gxa/src/main/java/uk/ac/ebi/atlas/experimentpage/baseline/download/BaselineProfilesWriterService.java
+++ gxa/src/main/java/uk/ac/ebi/atlas/experimentpage/baseline/download/BaselineProfilesWriterService.java
@@ -1,7 +1,10 @@
 package uk.ac.ebi.atlas.experimentpage.baseline.download;
 
+import com.google.common.base.Function;
+import com.google.common.collect.ImmutableMap;
 import uk.ac.ebi.atlas.experimentpage.baseline.coexpression.CoexpressedGenesService;
 import uk.ac.ebi.atlas.experimentpage.context.BaselineRequestContext;
+import uk.ac.ebi.atlas.model.download.ExternallyAvailableContent;
 import uk.ac.ebi.atlas.model.experiment.baseline.BaselineExperiment;
 import uk.ac.ebi.atlas.profiles.baseline.BaselineProfileStreamFactory;
 import uk.ac.ebi.atlas.profiles.baseline.BaselineProfileStreamTransforms;
@@ -12,13 +15,16 @@ import uk.ac.ebi.atlas.solr.query.GeneQueryResponse;
 import uk.ac.ebi.atlas.solr.query.SolrQueryService;
 import uk.ac.ebi.atlas.web.BaselineRequestPreferences;
 
-import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.io.Writer;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Map;
 
 import static org.apache.commons.lang3.StringUtils.wrap;
 
-public class BaselineProfilesWriterService {
+public class BaselineProfilesWriterService extends ExternallyAvailableContent.Supplier<BaselineExperiment>{
 
 
     private BaselineProfileStreamFactory inputStreamFactory;
@@ -41,7 +47,7 @@ public class BaselineProfilesWriterService {
     }
 
     public long write(Writer writer, BaselineRequestPreferences preferences, BaselineExperiment experiment,
-                      Map<String, Integer> coexpressionsRequested) throws IOException {
+                      Map<String, Integer> coexpressionsRequested) {
         int totalCoexpressionsRequested = 0;
         for (Map.Entry<String, Integer> e : coexpressionsRequested.entrySet()) {
             totalCoexpressionsRequested += e.getValue();
@@ -68,5 +74,23 @@ public class BaselineProfilesWriterService {
     private String describe(SemanticQuery geneQuery, int coexpressedGenes) {
         return coexpressedGenes == 0 ? wrap(SearchDescription.get(geneQuery), "'") :
                 geneQuery.description() + " with "+ coexpressedGenes + " similarly expressed genes";
+    }
+
+    @Override
+    public Collection<ExternallyAvailableContent> get(final BaselineExperiment experiment) {
+        final BaselineRequestPreferences preferences = new BaselineRequestPreferences();
+        preferences.setCutoff(0.0d);
+        preferences.setGeneQuery(SemanticQuery.create());
+        final Map<String, Integer> coexpressionsRequested = ImmutableMap.of();
+
+        return Collections.singleton(new ExternallyAvailableContent(makeUri("tsv"),
+                ExternallyAvailableContent.Description.create("data", "link", "Expression values across all genes"), new Function<OutputStream, Void>() {
+            @Override
+            public Void apply(OutputStream outputStream) {
+                write(new PrintWriter(outputStream), preferences, experiment, coexpressionsRequested);
+                return null;
+            }
+        }));
+
     }
 }
