cbbd2305312af5bfbfa40baa6f2fe3df4cbc0358
wbazant <wbazant@ebi.ac.uk>
2017-04-07 18:09:01 +0100

    Fix single cell build

--- sc/src/main/java/uk/ac/ebi/atlas/trader/cache/loader/SingleCellRnaSeqBaselineExperimentFactory.java
+++ sc/src/main/java/uk/ac/ebi/atlas/trader/cache/loader/SingleCellRnaSeqBaselineExperimentFactory.java
@@ -1,9 +1,9 @@
 package uk.ac.ebi.atlas.trader.cache.loader;
 
+import uk.ac.ebi.atlas.model.experiment.ExperimentType;
 import uk.ac.ebi.atlas.resource.DataFileHub;
 import uk.ac.ebi.atlas.species.SpeciesFactory;
 import uk.ac.ebi.atlas.trader.ConfigurationTrader;
-import uk.ac.ebi.atlas.model.experiment.ExperimentType;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -14,6 +14,6 @@ public class SingleCellRnaSeqBaselineExperimentFactory extends BaselineExperimen
     @Inject
     public SingleCellRnaSeqBaselineExperimentFactory(ConfigurationTrader configurationTrader,
                                                      SpeciesFactory speciesFactory, DataFileHub dataFileHub) {
-        super(ExperimentType.SINGLE_CELL_RNASEQ_MRNA_BASELINE, configurationTrader, speciesFactory);
+        super(ExperimentType.SINGLE_CELL_RNASEQ_MRNA_BASELINE, configurationTrader, speciesFactory, dataFileHub);
     }
 }
