62db39b0a763955513685c21e3c3b13538954761
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-04-14 04:52:41 +0100

    Remove qualifier from annotation

--- base/src/main/java/uk/ac/ebi/atlas/bioentity/properties/BioEntityPropertyService.java
+++ base/src/main/java/uk/ac/ebi/atlas/bioentity/properties/BioEntityPropertyService.java
@@ -29,7 +29,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-@Named("bioEntityPropertyService")
+@Named
 public class BioEntityPropertyService {
 
     private UniProtClient uniProtClient;
