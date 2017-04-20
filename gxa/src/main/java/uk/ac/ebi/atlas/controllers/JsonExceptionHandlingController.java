e4cca1f81b41f9c0972fbd5aad97bfa0b9c90909
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-04-20 19:57:14 +0100

    Simplify class as much as possible - Inline the serialized error map - The controllers that inherit from this class already set the content type

--- gxa/src/main/java/uk/ac/ebi/atlas/controllers/JsonExceptionHandlingController.java
+++ gxa/src/main/java/uk/ac/ebi/atlas/controllers/JsonExceptionHandlingController.java
@@ -1,27 +1,20 @@
 package uk.ac.ebi.atlas.controllers;
 
-import com.google.common.collect.Maps;
+import com.google.common.collect.ImmutableMap;
 import com.google.gson.Gson;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import javax.servlet.http.HttpServletResponse;
-import java.util.Map;
-
+//TODO Make all controllers that produce JSON data inherit from this class
 public abstract class JsonExceptionHandlingController {
     protected final Gson gson = new Gson();
-
-    //TODO Make all controllers that produce JSON data inherit from this class
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     @ResponseBody
-    public String handleException(Exception e, HttpServletResponse response) {
-        response.setContentType("application/json;charset=UTF-8");
-        Map<String, String> bodyJsonMap = Maps.newHashMap();
-        bodyJsonMap.put("error", e.getMessage());
-        return gson.toJson(bodyJsonMap);
+    public String handleException(Exception e) {
+        return gson.toJson(ImmutableMap.of("error", e.getMessage()));
     }
 }
