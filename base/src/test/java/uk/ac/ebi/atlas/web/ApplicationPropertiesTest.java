76ec920d736487c76aa66c6e42b9dbd92624a08f
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-04-20 17:11:07 +0100

    Test ApplicationProperties.getBaselineReferenceExperimentAccession

--- base/src/test/java/uk/ac/ebi/atlas/web/ApplicationPropertiesTest.java
+++ base/src/test/java/uk/ac/ebi/atlas/web/ApplicationPropertiesTest.java
@@ -7,14 +7,19 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import uk.ac.ebi.atlas.search.SemanticQuery;
+import uk.ac.ebi.atlas.species.Species;
+import uk.ac.ebi.atlas.species.SpeciesProperties;
 import uk.ac.ebi.atlas.trader.ArrayDesignTrader;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.Matchers.startsWith;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -112,4 +117,23 @@ public class ApplicationPropertiesTest {
 
         assertThat(ApplicationProperties.buildServerURL(httpServletRequestMock), is("http://localhost:9090/gxa"));
     }
+
+    @Test
+    public void getBaselineReferenceExperimentAccession() throws Exception {
+        ImmutableMap<String, List<String>> emptySpeciesResources = ImmutableMap.of();
+        SpeciesProperties homoSapiensProperties =
+                SpeciesProperties.create("Homo_sapiens", "ORGANISM_PART", "animals", emptySpeciesResources);
+        Species species = new Species("Homo sapiens", homoSapiensProperties);
+
+        assertThat(ApplicationProperties.getBaselineReferenceExperimentAccession(species), startsWith("E-"));
+    }
+
+    @Test
+    public void getUnknownSpeciesBaselineReferenceExperimentAccession() throws Exception {
+        Species unknownSpecies = new Species("foobar", SpeciesProperties.UNKNOWN);
+
+        assertThat(unknownSpecies.isUnknown(), is (true));
+        assertThat(ApplicationProperties.getBaselineReferenceExperimentAccession(unknownSpecies), is(nullValue()));
+    }
+
 }
