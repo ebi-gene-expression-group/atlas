ab17ffb41bb51f64dcdb476f2f2fd00fd6e93e46
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-05-05 12:55:10 +0100

    New /request-diagnostics controller to inspect the request object when Atlas is deployed to wwwdev

---
+++ base/src/main/java/uk/ac/ebi/atlas/controllers/RequestDiagnosticsController.java
@@ -0,0 +1,14 @@
+package uk.ac.ebi.atlas.controllers;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Scope("request")
+public class RequestDiagnosticsController extends HtmlExceptionHandlingController {
+    @RequestMapping(value = "/request-diagnostics", produces = "text/html;charset=UTF-8")
+    public String showGenePage() {
+        return "request-diagnostics";
+    }
+}
