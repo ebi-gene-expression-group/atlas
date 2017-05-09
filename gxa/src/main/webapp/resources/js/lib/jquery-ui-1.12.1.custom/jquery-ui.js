3b8e0612bdb49e69cb2b9e7e11c1d90c33ccade3
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-05-09 11:57:11 +0100

    Create a jQuery-UI theme consistent with the current Atlas look and feel; minor tweaks to the autocomplete HTML to stretch the background colour to the right of the selected item [Fixes #143047431]

--- gxa/src/main/webapp/resources/js/lib/jquery-ui-1.12.1.custom/jquery-ui.js
+++ gxa/src/main/webapp/resources/js/lib/jquery-ui-1.12.1.custom/jquery-ui.js
@@ -1,4 +1,4 @@
-/*! jQuery UI - v1.12.1 - 2016-10-17
+/*! jQuery UI - v1.12.1 - 2017-05-09
 * http://jqueryui.com
 * Includes: widget.js, position.js, keycode.js, unique-id.js, widgets/autocomplete.js, widgets/menu.js
 * Copyright jQuery Foundation and other contributors; Licensed MIT */
