122659b773d28d92a496ff4236572dd5e495d7d0
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-05-10 16:21:41 +0100

    Remove unnecessary styles The <select> element now flows and resizes correctly together with the page width. I suspect that the latest version of DataTables fixed this issue (changing the source to the latest version from the CDN likely fixed this).

--- gxa/src/main/webapp/resources/js/foundationExperimentsPageModule.js
+++ gxa/src/main/webapp/resources/js/foundationExperimentsPageModule.js
@@ -136,7 +136,7 @@ var foundationExperimentsPageModule = (function ($) {
             "processing": true,
             "ajax":"json/experiments",
             "columns":[
-                { "title":"Type", "data":"experimentType", "className":"center", "type":"alt-string", "width": "9%",
+                { "title":"Type", "data":"experimentType", "className":"center", "type":"alt-string", "width": "7%",
                     "render": function (data, type, full) {
                         return formatExperimentType(data);
                     } },
