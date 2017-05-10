75a8339710829ce005f5df4c13b69b2c171368f1
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-05-10 17:21:31 +0100

    If exception message is null print an uninformative error message

--- base/src/main/java/uk/ac/ebi/atlas/controllers/JsonExceptionHandlingController.java
+++ base/src/main/java/uk/ac/ebi/atlas/controllers/JsonExceptionHandlingController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
 //TODO Make all controllers that produce JSON data inherit from this class
 public abstract class JsonExceptionHandlingController {
     protected final Gson gson = new GsonBuilder().setPrettyPrinting().create();
@@ -16,6 +18,9 @@ public abstract class JsonExceptionHandlingController {
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     @ResponseBody
     public String handleException(Exception e) {
-        return gson.toJson(ImmutableMap.of("error", e.getMessage()));
+        return gson.toJson(
+                ImmutableMap.of(
+                        "error",
+                        isBlank(e.getMessage()) ? "Unknown error, exception message was null" : e.getMessage()));
     }
 }
