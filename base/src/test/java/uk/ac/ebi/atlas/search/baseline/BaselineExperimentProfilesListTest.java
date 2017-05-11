ed416886a9fe959a5447a27ca584bf3ea4736012
wbazant <wbazant@ebi.ac.uk>
2017-05-11 11:17:32 +0100

    Revert previous and assert the zeros do make it into the list

--- base/src/test/java/uk/ac/ebi/atlas/search/baseline/BaselineExperimentProfilesListTest.java
+++ base/src/test/java/uk/ac/ebi/atlas/search/baseline/BaselineExperimentProfilesListTest.java
@@ -32,7 +32,7 @@ public class BaselineExperimentProfilesListTest {
     }
 
     @Test
-    public void zerosDoNotMakeItToTheList() throws Exception {
+    public void zerosDoMakeItToTheList() throws Exception {
         BaselineExperimentProfilesList result = new BaselineExperimentProfilesList();
 
         BaselineExperimentProfile p1 = new BaselineExperimentProfile(BaselineExperimentTest.mockExperiment(), new FactorSet());
@@ -40,12 +40,6 @@ public class BaselineExperimentProfilesListTest {
         p1.add(new FactorAcrossExperiments(new Factor(factorHeader, "v2")), new BaselineExpression(2.0));
         p1.add(new FactorAcrossExperiments(new Factor(factorHeader, "v3")), new BaselineExpression(0.0));
         result.add(p1);
-        assertThat(result.getFactorsAcrossExperiments().size(), is(2));
-
-        BaselineExperimentProfile p2 = new BaselineExperimentProfile(BaselineExperimentTest.mockExperiment(), new FactorSet());
-        p2.add(new FactorAcrossExperiments(new Factor(factorHeader, "v1")), new BaselineExpression(1.0));
-        p2.add(new FactorAcrossExperiments(new Factor(factorHeader, "v3")), new BaselineExpression(3.0));
-        result.add(p2);
         assertThat(result.getFactorsAcrossExperiments().size(), is(3));
     }
 
