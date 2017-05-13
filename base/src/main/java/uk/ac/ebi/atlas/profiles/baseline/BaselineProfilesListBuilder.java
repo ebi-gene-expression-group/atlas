38437d434a58e2cf292e87167a4fb9a9c97c7e3f
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-05-13 12:26:09 +0100

    Remove stale import

--- base/src/main/java/uk/ac/ebi/atlas/profiles/baseline/BaselineProfilesListBuilder.java
+++ base/src/main/java/uk/ac/ebi/atlas/profiles/baseline/BaselineProfilesListBuilder.java
@@ -3,8 +3,6 @@ package uk.ac.ebi.atlas.profiles.baseline;
 import uk.ac.ebi.atlas.model.experiment.baseline.BaselineProfilesList;
 import uk.ac.ebi.atlas.profiles.GeneProfilesListBuilder;
 
-import javax.inject.Named;
-
 public class BaselineProfilesListBuilder implements GeneProfilesListBuilder<BaselineProfilesList> {
 
     @Override
