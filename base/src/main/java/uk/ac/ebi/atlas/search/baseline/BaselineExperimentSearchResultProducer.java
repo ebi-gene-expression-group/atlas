6d18d4093cdb544e208aab8f99243942e18fc435
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-05-30 11:42:56 +0100

    Remove stale import

--- base/src/main/java/uk/ac/ebi/atlas/search/baseline/BaselineExperimentSearchResultProducer.java
+++ base/src/main/java/uk/ac/ebi/atlas/search/baseline/BaselineExperimentSearchResultProducer.java
@@ -5,7 +5,6 @@ import com.google.common.base.Optional;
 import com.google.common.base.Predicate;
 import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableList;
-import org.apache.commons.lang3.StringUtils;
 import uk.ac.ebi.atlas.model.AssayGroup;
 import uk.ac.ebi.atlas.model.FactorAcrossExperiments;
 import uk.ac.ebi.atlas.model.experiment.baseline.BaselineExperiment;
