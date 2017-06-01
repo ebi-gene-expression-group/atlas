2588fd7b74a08dbaa0b921516b78b3433c54c0b3
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-06-01 23:39:59 +0100

    Display an error page if a URL with an invalid genome browser is requested; more tests [#143403615]

--- gxa/src/test/java/uk/ac/ebi/atlas/tracks/TracksControllerWIT.java
+++ gxa/src/test/java/uk/ac/ebi/atlas/tracks/TracksControllerWIT.java
@@ -31,7 +31,15 @@ public class TracksControllerWIT {
     MockMvc mockMvc;
 
     @Before
-    public void setUp() {        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void invalidGenomeBrowserForExperimentSpecies() throws Exception {
+        this.mockMvc.perform(get("/experiments-content/E-MTAB-3827/redirect/genomeBrowser/Ensembl Plants?geneId=ENSG00000102970&trackId=g13"))
+                .andExpect(status().isBadRequest())
+                .andExpect(forwardedUrl("error-page"));
     }
 
     @Test
@@ -109,11 +117,54 @@ public class TracksControllerWIT {
         assertThat(redirectedUrl, endsWith("/experiments/E-GEOD-57252/tracks/E-GEOD-57252.g1_g6.genes.pval.bedGraph=pvalue;format=BEDGRAPH"));
     }
 
-//    @Test
-//    public void geneNotFound() throws Exception {
-//        this.mockMvc.perform(get("/experiments-content/E-GEOD-55482/redirect/genomeBrowser/Gramene?geneId=AT1G07060&trackId=g5"))
-//                .andExpect(status().isOk())
-//                .andExpect(forwardedUrl("error-page")); // Can be a view name
-//    }
+    @Test
+    public void baselineWormExperimentWormbaseParaSite() throws Exception {
+        MvcResult result = this.mockMvc.perform(get("/experiments-content/E-MTAB-2812/redirect/genomeBrowser/Wormbase ParaSite?geneId=WBGene00009892&trackId=g31"))
+                .andExpect(status().isFound())
+                .andReturn();
+
+        String redirectedUrl = result.getResponse().getRedirectedUrl();
+
+        assertThat(redirectedUrl, startsWith("http://parasite.wormbase.org/Caenorhabditis_elegans_prjna13758/Location/View?g=WBGene00009892;contigviewbottom=url:http"));
+        assertThat(redirectedUrl, endsWith("/experiments/E-MTAB-2812/tracks/E-MTAB-2812.g31.genes.expressions.bedGraph;format=BEDGRAPH"));
+    }
+
+    @Test
+    public void differentialWormExperimentWormbaseParaSite() throws Exception {
+        MvcResult result = this.mockMvc.perform(get("/experiments-content/E-MEXP-1810/redirect/genomeBrowser/Wormbase ParaSite?geneId=WBGene00003778&trackId=g3_g1"))
+                .andExpect(status().isFound())
+                .andReturn();
+
+        String redirectedUrl = result.getResponse().getRedirectedUrl();
+
+        assertThat(redirectedUrl, startsWith("http://parasite.wormbase.org/Caenorhabditis_elegans_prjna13758/Location/View?g=WBGene00003778;contigviewbottom=url:http"));
+        assertThat(redirectedUrl, containsString("/experiments/E-MEXP-1810/tracks/E-MEXP-1810.g3_g1.genes.log2foldchange.bedGraph=tiling,url:http"));
+        assertThat(redirectedUrl, endsWith("/experiments/E-MEXP-1810/tracks/E-MEXP-1810.g3_g1.genes.pval.bedGraph=pvalue;format=BEDGRAPH"));
+    }
+
+    @Test
+    public void baselineVertebrateExperimentEnsembl() throws Exception {
+        MvcResult result = this.mockMvc.perform(get("/experiments-content/E-MTAB-3827/redirect/genomeBrowser/Ensembl?geneId=ENSG00000102970&trackId=g13"))
+                .andExpect(status().isFound())
+                .andReturn();
+
+        String redirectedUrl = result.getResponse().getRedirectedUrl();
+
+        assertThat(redirectedUrl, startsWith("http://www.ensembl.org/Homo_sapiens/Location/View?g=ENSG00000102970;contigviewbottom=url:http"));
+        assertThat(redirectedUrl, endsWith("/experiments/E-MTAB-3827/tracks/E-MTAB-3827.g13.genes.expressions.bedGraph;format=BEDGRAPH"));
+    }
+
+    @Test
+    public void differentialVertebrateExperimentEnsembl() throws Exception {
+        MvcResult result = this.mockMvc.perform(get("/experiments-content/E-GEOD-22351/redirect/genomeBrowser/Ensembl?geneId=ENSMUSG00000029816&trackId=g1_g2"))
+                .andExpect(status().isFound())
+                .andReturn();
+
+        String redirectedUrl = result.getResponse().getRedirectedUrl();
+
+        assertThat(redirectedUrl, startsWith("http://www.ensembl.org/Mus_musculus/Location/View?g=ENSMUSG00000029816;contigviewbottom=url:http"));
+        assertThat(redirectedUrl, containsString("/experiments/E-GEOD-22351/tracks/E-GEOD-22351.g1_g2.genes.log2foldchange.bedGraph=tiling,url:http"));
+        assertThat(redirectedUrl, endsWith("/experiments/E-GEOD-22351/tracks/E-GEOD-22351.g1_g2.genes.pval.bedGraph=pvalue;format=BEDGRAPH"));
+    }
 
 }
