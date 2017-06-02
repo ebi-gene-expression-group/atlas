798d9dde788c6327538ea112b7d1afb757a2a843
wbazant <wbazant@ebi.ac.uk>
2017-06-02 14:10:28 +0100

    Move the reference experiment into the same controller as other baseline experiments - I think it will be easier to maintain
    (cherry picked from commit 1ac0d76)

--- gxa/src/main/java/uk/ac/ebi/atlas/experimentpage/json/JsonBaselineExperimentController.java
+++ gxa/src/main/java/uk/ac/ebi/atlas/experimentpage/json/JsonBaselineExperimentController.java
@@ -5,20 +5,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
+import uk.ac.ebi.atlas.controllers.ResourceNotFoundException;
 import uk.ac.ebi.atlas.experimentpage.baseline.BaselineExperimentPageService;
 import uk.ac.ebi.atlas.experimentpage.baseline.BaselineProfilesHeatmapsWranglerFactory;
 import uk.ac.ebi.atlas.experimentpage.baseline.BaselineRequestPreferencesValidator;
 import uk.ac.ebi.atlas.experimentpage.baseline.coexpression.CoexpressedGenesService;
+import uk.ac.ebi.atlas.model.ExpressionUnit;
 import uk.ac.ebi.atlas.model.experiment.baseline.BaselineExperiment;
 import uk.ac.ebi.atlas.profiles.stream.ProteomicsBaselineProfileStreamFactory;
 import uk.ac.ebi.atlas.profiles.stream.RnaSeqBaselineProfileStreamFactory;
+import uk.ac.ebi.atlas.search.SemanticQuery;
 import uk.ac.ebi.atlas.solr.query.SolrQueryService;
+import uk.ac.ebi.atlas.species.Species;
+import uk.ac.ebi.atlas.species.SpeciesInferrer;
 import uk.ac.ebi.atlas.trader.ExperimentTrader;
+import uk.ac.ebi.atlas.web.ApplicationProperties;
 import uk.ac.ebi.atlas.web.ProteomicsBaselineRequestPreferences;
 import uk.ac.ebi.atlas.web.RnaSeqBaselineRequestPreferences;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.text.MessageFormat;
+
+import static org.apache.commons.lang3.StringUtils.isBlank;
 
 @Controller
 @Scope("request")
@@ -31,6 +41,7 @@ public class JsonBaselineExperimentController extends JsonExperimentController {
 
     private final BaselineExperimentPageService rnaSeqBaselineExperimentPageService;
     private final BaselineExperimentPageService proteomicsBaselineExperimentPageService;
+    private final SpeciesInferrer speciesInferrer;
 
     @Inject
     public JsonBaselineExperimentController(
@@ -38,7 +49,8 @@ public class JsonBaselineExperimentController extends JsonExperimentController {
             CoexpressedGenesService coexpressedGenesService,
             SolrQueryService solrQueryService,
             RnaSeqBaselineProfileStreamFactory rnaSeqBaselineProfileStreamFactory,
-            ProteomicsBaselineProfileStreamFactory proteomicsBaselineProfileStreamFactory) {
+            ProteomicsBaselineProfileStreamFactory proteomicsBaselineProfileStreamFactory,
+            SpeciesInferrer speciesInferrer) {
         super(experimentTrader);
         this.rnaSeqBaselineExperimentPageService = new BaselineExperimentPageService(new BaselineProfilesHeatmapsWranglerFactory(
                 rnaSeqBaselineProfileStreamFactory, solrQueryService, coexpressedGenesService)
@@ -46,6 +58,7 @@ public class JsonBaselineExperimentController extends JsonExperimentController {
         this.proteomicsBaselineExperimentPageService = new BaselineExperimentPageService(new BaselineProfilesHeatmapsWranglerFactory(
                 proteomicsBaselineProfileStreamFactory, solrQueryService, coexpressedGenesService)
         );
+        this.speciesInferrer = speciesInferrer;
     }
 
     @RequestMapping(value = "/json/experiments/{experimentAccession}",
@@ -78,5 +91,41 @@ public class JsonBaselineExperimentController extends JsonExperimentController {
                 proteomicsBaselineExperimentPageService.populateModelWithHeatmapData(
                         (BaselineExperiment) experimentTrader.getExperiment(experimentAccession, accessKey),
                         preferences, model));
+    }
+
+
+
+    @RequestMapping(
+            value = "/widgets/heatmap/referenceExperiment",
+            method = RequestMethod.GET,
+            produces = "application/json;charset=UTF-8")
+    @Deprecated
+    public String oldReferenceExperiment() {
+        return "forward:/json/baseline_refexperiment";
+    }
+
+
+    @RequestMapping(
+            value = "/json/baseline_refexperiment",
+            method = RequestMethod.GET,
+            produces = "application/json;charset=UTF-8")
+    @ResponseBody
+    public String jsonBaselineRefExperiment(
+            @RequestParam(value = "geneQuery") SemanticQuery geneQuery,
+            @RequestParam(value = "species", required = false) String speciesString,
+            @ModelAttribute("preferences") @Valid RnaSeqBaselineRequestPreferences preferences, HttpServletRequest request, Model model) {
+
+        if(!request.getParameterMap().containsKey("unit")){
+            preferences.setUnit(ExpressionUnit.Absolute.Rna.FPKM);
+        }
+
+        Species species = speciesInferrer.inferSpeciesForGeneQuery(geneQuery, speciesString);
+        String experimentAccession = ApplicationProperties.getBaselineReferenceExperimentAccession(species);
+
+        if (isBlank(experimentAccession)) {
+            throw new ResourceNotFoundException("No reference baseline experiment for species " + speciesString);
+        }
+
+        return baselineRnaSeqExperimentData(preferences, experimentAccession, "", model);
     }
 }
