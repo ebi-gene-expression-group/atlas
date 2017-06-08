acc9c0045f383e535dee17ae9ad733da3495a2a2
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-06-08 15:50:28 +0100

    Remove no longer used code and classes [#145996535, #146294055]

--- base/src/main/java/uk/ac/ebi/atlas/experiments/ExperimentInfoListService.java
+++ base/src/main/java/uk/ac/ebi/atlas/experiments/ExperimentInfoListService.java
@@ -1,34 +1,24 @@
 package uk.ac.ebi.atlas.experiments;
 
-import com.google.common.collect.FluentIterable;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
-import org.joda.time.DateTime;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
 import uk.ac.ebi.atlas.model.experiment.Experiment;
 import uk.ac.ebi.atlas.model.experiment.ExperimentType;
 import uk.ac.ebi.atlas.trader.ExperimentTrader;
 import uk.ac.ebi.atlas.utils.ExperimentInfo;
 
-import java.text.NumberFormat;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Locale;
 
 public class ExperimentInfoListService {
-    public static final DateTimeFormatter DATE_FORMAT = DateTimeFormat.forPattern("dd-MM-yyyy");
-
     private final ExperimentTrader experimentTrader;
     private final Collection<ExperimentType> experimentTypes;
     private final Gson gson = new Gson();
 
-    public ExperimentInfoListService(ExperimentTrader experimentTrader, Collection<ExperimentType> experimentTypes) {
+    public ExperimentInfoListService(ExperimentTrader experimentTrader,
+                                     Collection<ExperimentType> experimentTypes) {
         this.experimentTrader = experimentTrader;
         this.experimentTypes = experimentTypes;
     }
@@ -72,34 +62,4 @@ public class ExperimentInfoListService {
         return experimentInfos;
     }
 
-    private ImmutableMap<String, Object> cached = null;
-
-    public ImmutableMap<String, Object> getLatestExperimentsListAttributes() {
-        if(cached == null) {
-            int experimentCount = fetchCount();
-            cached =
-                    ImmutableMap.of(
-                            "experimentCount", experimentCount,
-                            "formattedExperimentCount", NumberFormat.getNumberInstance(Locale.US).format(experimentCount),
-                            "latestExperiments", fetchLatest());
-        }
-        return cached;
-    }
-
-    public int fetchCount() {
-        return listPublicExperiments().size();
-    }
-
-    public List<ExperimentInfo> fetchLatest() {
-        ImmutableList<ExperimentInfo> l =
-                FluentIterable.from(listPublicExperiments()).toSortedList(new Comparator<ExperimentInfo>() {
-                    @Override
-                    public int compare(ExperimentInfo o1, ExperimentInfo o2) {
-                        return (-1) *
-                                DateTime.parse(o1.getLastUpdate(), DATE_FORMAT)
-                                        .compareTo(DateTime.parse(o2.getLastUpdate(), DATE_FORMAT));
-                    }
-                });
-        return l.subList(0, Math.min(5, l.size()));
-    }
 }
