57bde9c4e68164aa4d6ed5860ee0a4e410b2e31f
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-06-14 11:00:33 +0100

    Create JSON response as a String with @ResponseBody MappingJacksonView isn’t supported in Spring 4. Also, it’s unneeded complexity.

--- gxa/src/main/java/uk/ac/ebi/atlas/widget/ExpressionDataController.java
+++ gxa/src/main/java/uk/ac/ebi/atlas/widget/ExpressionDataController.java
@@ -1,5 +1,8 @@
 package uk.ac.ebi.atlas.widget;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.gson.Gson;
+import org.springframework.web.bind.annotation.ResponseBody;
 import uk.ac.ebi.atlas.controllers.JsonExceptionHandlingController;
 import uk.ac.ebi.atlas.search.SemanticQuery;
 import uk.ac.ebi.atlas.species.SpeciesPropertiesTrader;
@@ -10,8 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.json.MappingJacksonJsonView;
 import uk.ac.ebi.atlas.search.analyticsindex.AnalyticsSearchService;
 
 import javax.inject.Inject;
@@ -20,8 +21,9 @@ import javax.inject.Inject;
 @Scope("request")
 public final class ExpressionDataController extends JsonExceptionHandlingController {
 
-    private AnalyticsSearchService analyticsSearchService;
-    private SpeciesPropertiesTrader speciesTrader;
+    private final AnalyticsSearchService analyticsSearchService;
+    private final SpeciesPropertiesTrader speciesTrader;
+    private final Gson gson = new Gson();
 
     @Inject
     public ExpressionDataController(AnalyticsSearchService analyticsSearchService,
@@ -32,20 +34,18 @@ public final class ExpressionDataController extends JsonExceptionHandlingControl
 
     @RequestMapping(value = "/json/expressionData", method = RequestMethod.GET, produces = "application/json")
     @ResponseStatus(HttpStatus.OK)
-    public ModelAndView existGeneIdentifier(@RequestParam(value = "geneId") String geneId ) {
-        ModelAndView mav = new ModelAndView(new MappingJacksonJsonView());
+    @ResponseBody
+    public String existGeneIdentifier(@RequestParam(value = "geneId") String geneId ) {
         boolean results = analyticsSearchService.tissueExpressionAvailableFor(SemanticQuery.create(geneId));
-        mav.addObject(geneId, results);
-        return mav;
+        return gson.toJson(ImmutableMap.of(geneId, results));
     }
 
     // Wojtek: this doesn't seem right.
     @RequestMapping(value = "/json/expressionData/species", method = RequestMethod.GET, produces = "application/json")
     @ResponseStatus(HttpStatus.OK)
-    public ModelAndView existSpeciesForGeneIdentifier() {
-        ModelAndView mav = new ModelAndView(new MappingJacksonJsonView());
-        mav.addObject("species", speciesTrader.getAll());
-        return mav;
+    @ResponseBody
+    public String existSpeciesForGeneIdentifier() {
+        return gson.toJson(ImmutableMap.of("species", speciesTrader.getAll()));
     }
 
 }
