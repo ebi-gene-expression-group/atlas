13069f6c41879bfd0d8d07c39f1112fa02658441
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-06-18 02:33:17 +0100

    Replace wildcard import with single imports

--- gxa/src/main/java/uk/ac/ebi/atlas/experimentpage/baseline/coexpression/CoexpressedGenesController.java
+++ gxa/src/main/java/uk/ac/ebi/atlas/experimentpage/baseline/coexpression/CoexpressedGenesController.java
@@ -3,29 +3,32 @@ package uk.ac.ebi.atlas.experimentpage.baseline.coexpression;
 import com.google.gson.Gson;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import uk.ac.ebi.atlas.controllers.JsonExceptionHandlingController;
 
 import javax.inject.Inject;
 import java.util.Set;
 
-
 @Controller
 @RequestMapping
-public class CoexpressedGenesController {
+public class CoexpressedGenesController extends JsonExceptionHandlingController {
 
-
-    Gson gson = new Gson();
-    CoexpressedGenesDao coexpressedGenesDao;
+    private final Gson gson = new Gson();
+    private final CoexpressedGenesDao coexpressedGenesDao;
 
     @Inject
     public CoexpressedGenesController(JdbcTemplate jdbcTemplate){
         this.coexpressedGenesDao = new CoexpressedGenesDao(jdbcTemplate);
     }
 
-    @RequestMapping(value ={"/json/experiments/coexpression"}, method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+    @RequestMapping(value ={"/json/experiments/coexpression"},
+                    method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
     @ResponseBody
-    public String fetchCoexpressedGenes(@RequestParam(value = "experiment", required = true) String experiment,
-                                        @RequestParam(value = "identifier", required = true) String identifier) {
+    public String fetchCoexpressedGenes(@RequestParam(value = "experiment") String experiment,
+                                        @RequestParam(value = "identifier") String identifier) {
         return gson.toJson(coexpressedGenesDao.coexpressedGenesFor(experiment, identifier), Set.class);
     }
 }
