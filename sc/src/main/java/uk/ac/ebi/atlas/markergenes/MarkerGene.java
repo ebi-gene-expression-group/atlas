76c04431e39905ab27022a4fd0fa16a08692348d
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-06-21 16:27:47 +0100

    Add utility constructor that takes a Pair of Integers [#147104069]

--- sc/src/main/java/uk/ac/ebi/atlas/markergenes/MarkerGene.java
+++ sc/src/main/java/uk/ac/ebi/atlas/markergenes/MarkerGene.java
@@ -1,12 +1,20 @@
 package uk.ac.ebi.atlas.markergenes;
 
 import com.google.auto.value.AutoValue;
+import org.apache.commons.lang3.tuple.Pair;
 
 @AutoValue
 public abstract class MarkerGene {
-    static MarkerGene create(String geneId, String experimentAccession, int perplexity, int clusterId, double p) {
+    public static MarkerGene create(String geneId, String experimentAccession, int perplexity, int clusterId, double p) {
         return new AutoValue_MarkerGene(geneId, experimentAccession, perplexity, clusterId, p);
     }
+
+    public static MarkerGene create(String geneId, String experimentAccession,
+                                    Pair<Integer, Integer> perplexityAndClusterId, double p) {
+        return new AutoValue_MarkerGene(geneId, experimentAccession,
+                perplexityAndClusterId.getLeft(), perplexityAndClusterId.getRight(), p);
+    }
+
 
     public abstract String geneId();
     public abstract String experimentAccession();
