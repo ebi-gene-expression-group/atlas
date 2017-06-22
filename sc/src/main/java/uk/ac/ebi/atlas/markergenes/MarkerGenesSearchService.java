139f52ff9c742a2c926fd39ee30e0bd976a964c7
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-06-22 17:31:59 +0100

    The search service transforms the DTOs into model objects

--- sc/src/main/java/uk/ac/ebi/atlas/markergenes/MarkerGenesSearchService.java
+++ sc/src/main/java/uk/ac/ebi/atlas/markergenes/MarkerGenesSearchService.java
@@ -1,11 +1,25 @@
 package uk.ac.ebi.atlas.markergenes;
 
+import com.google.gson.Gson;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.lang3.tuple.Triple;
+import org.apache.http.client.utils.URIBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Named
 public class MarkerGenesSearchService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MarkerGenesSearchService.class);
 
     private final MarkerGeneDao markerGeneDao;
 
@@ -14,8 +28,45 @@ public class MarkerGenesSearchService {
         this.markerGeneDao = markerGeneDao;
     }
 
-    public List<MarkerGene> searchMarkerGenesByGeneId(String geneId) {
-        return markerGeneDao.fetchMarkerGenes(geneId);
+    public List<Pair<MarkerGene, String>> searchMarkerGenesByGeneId(String geneId) {
+        return aggregateClusterIds(markerGeneDao.fetchMarkerGenes(geneId)).stream()
+                .map(markerGene -> Pair.of(markerGene, buildUrl(markerGene)))
+                .filter(pair -> pair.getRight().isPresent())
+                .map(pair -> Pair.of(pair.getLeft(), pair.getRight().get()))
+                .collect(Collectors.toList());
     }
 
+    private List<MarkerGene> aggregateClusterIds(Collection<MarkerGeneDto> markerGeneDtos) {
+        return markerGeneDtos.stream()
+                // We could group by experimentAccession and perplexity only as we expect to have a single gene ID here
+                // but we can be a bit more defensive with very little extra effort
+                .collect(Collectors.groupingBy(mgd -> Triple.of(mgd.geneId(), mgd.experimentAccession(), mgd.perplexity())))
+                .entrySet()
+                .stream()
+                .map(entry ->
+                        MarkerGene.create(
+                                entry.getKey().getLeft(), entry.getKey().getMiddle(), entry.getKey().getRight(),
+                                entry.getValue().stream()
+                                        .map(mgd -> Pair.of(mgd.clusterId(), mgd.p()))
+                                        .collect(Collectors.toList())))
+                .collect(Collectors.toList());
+    }
+
+    private Optional<String> buildUrl(MarkerGene markerGene) {
+        int[] clusterIds = markerGene.clusterIds().stream().mapToInt(Pair::getLeft).toArray();
+
+        try {
+            return Optional.of(
+                    new URIBuilder()
+                            .setPath("/gxa_sc/experiments/" + markerGene.experimentAccession())
+                            .addParameter("k", Double.toString(markerGene.perplexity()))
+                            .addParameter("geneId", markerGene.geneId())
+                            .addParameter("clusterId", new Gson().toJson(clusterIds))
+                            .build()
+                            .toString());
+        } catch (URISyntaxException e) {
+            LOGGER.error("Unable to build URL from markerGene {}", markerGene.toString());
+            return Optional.empty();
+        }
+    }
 }
--- sc/src/test/java/uk/ac/ebi/atlas/markergenes/MarkerGenesSearchServiceIT.java
+++ sc/src/test/java/uk/ac/ebi/atlas/markergenes/MarkerGenesSearchServiceIT.java
@@ -1,5 +1,6 @@
 package uk.ac.ebi.atlas.markergenes;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -9,12 +10,16 @@ import org.springframework.test.context.web.WebAppConfiguration;
 
 import javax.inject.Inject;
 
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.startsWith;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assume.assumeThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
@@ -33,17 +38,28 @@ public class MarkerGenesSearchServiceIT {
             .map(rowMap -> (String) rowMap.get("GENE_ID"))
             .collect(Collectors.toList());
 
+        assumeThat(tenMarkerGeneIds, hasSize(10));
 
         for (String geneId : tenMarkerGeneIds) {
-            assertThat(subject.searchMarkerGenesByGeneId(geneId), hasSize(greaterThan(0)));
+            List<Pair<MarkerGene, String>> results = subject.searchMarkerGenesByGeneId(geneId);
+            assertThat(results, hasSize(greaterThan(0)));
+
+            // No exceptions thrown: URLs are well-formed
+            subject.searchMarkerGenesByGeneId(geneId).forEach(pair ->
+                    assertThat(URI.create(pair.getRight()).toString(), startsWith("/"))); // URI is relative
         }
     }
 
     @Test
     public void searchIdsOfGenesWithLowProbability() throws Exception {
-        List<String> tenNonMarkerGeneIds = jdbcTemplate.queryForList("SELECT DISTINCT(GENE_ID) FROM MARKER_GENES WHERE MARKER_PROBABILITY < 0.95 LIMIT 10").stream()
+        List<String> tenNonMarkerGeneIds = jdbcTemplate.queryForList(
+                "SELECT all_p.GENE_ID FROM MARKER_GENES all_p LEFT JOIN " +
+                "  (SELECT GENE_ID, MARKER_PROBABILITY FROM MARKER_GENES WHERE MARKER_PROBABILITY > 0.95) high_p ON all_p.GENE_ID  = high_p.GENE_ID " +
+                "WHERE high_p.GENE_ID IS NULL LIMIT 10").stream()
                 .map(rowMap -> (String) rowMap.get("GENE_ID"))
                 .collect(Collectors.toList());
+
+        assumeThat(tenNonMarkerGeneIds, hasSize(10));
 
         for (String geneId : tenNonMarkerGeneIds) {
             assertThat(subject.searchMarkerGenesByGeneId(geneId), hasSize(0));
