82d317df7b928c0f13b18a3d5df56bfe8876efd1
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-06-22 17:31:28 +0100

    Avoid producing duplicate values when populating the table with random data

--- sc/src/main/java/uk/ac/ebi/atlas/markergenes/RandomMarkerGeneInputStream.java
+++ sc/src/main/java/uk/ac/ebi/atlas/markergenes/RandomMarkerGeneInputStream.java
@@ -1,6 +1,7 @@
 package uk.ac.ebi.atlas.markergenes;
 
 import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.lang3.tuple.Triple;
 import uk.ac.ebi.atlas.commons.streams.ObjectInputStream;
 
 import java.io.IOException;
@@ -11,20 +12,28 @@ import java.util.stream.IntStream;
 
 import static org.apache.commons.lang3.StringUtils.leftPad;
 
-public class RandomMarkerGeneInputStream implements ObjectInputStream<MarkerGene> {
+public class RandomMarkerGeneInputStream implements ObjectInputStream<MarkerGeneDto> {
 
-    private final List<MarkerGene> markerGenes;
+    // Change to higher or lower values to decrease or increase, respectively, the probability of a gene being in the
+    // same experiment with different cluster IDs
+    private final int NUMBER_OF_GENES = 500;
+    private final int NUMBER_OF_EXPERIMENTS = 10;
+    private final int PERPLEXITY_VALUES = 5;
+
+    private final List<MarkerGeneDto> markerGenes;
     private int position;
 
     public RandomMarkerGeneInputStream(int size) {
         markerGenes = IntStream.rangeClosed(1, size).boxed()
-                .map(i -> MarkerGene.create(randomHumanGeneId(), randomExperimentOver9000(), randomPerplexityCluster(), ThreadLocalRandom.current().nextDouble()))
+                .map(i -> Triple.of(randomHumanGeneId(), randomExperimentOver9000(), randomPerplexityCluster()))
+                .distinct()
+                .map(triple -> MarkerGeneDto.create(triple.getLeft(), triple.getMiddle(), triple.getRight(), ThreadLocalRandom.current().nextDouble()))
                 .collect(Collectors.toList());
         position = 0;
     }
 
     @Override
-    public MarkerGene readNext() {
+    public MarkerGeneDto readNext() {
         return position < markerGenes.size() ? markerGenes.get(position++) : null;
     }
 
@@ -34,15 +43,15 @@ public class RandomMarkerGeneInputStream implements ObjectInputStream<MarkerGene
     }
 
     private String randomHumanGeneId() {
-        return "ENSG" + leftPad(Integer.toString(ThreadLocalRandom.current().nextInt(1, 999999999)), 11, '0');
+        return "ENSG" + leftPad(Integer.toString(ThreadLocalRandom.current().nextInt(1, NUMBER_OF_GENES)), 11, '0');
     }
 
     private String randomExperimentOver9000() {
-        return "E-MTAB-" + Integer.toString(ThreadLocalRandom.current().nextInt(9000, 12000));
+        return "E-MTAB-" + Integer.toString(ThreadLocalRandom.current().nextInt(9000, 9000 + NUMBER_OF_EXPERIMENTS));
     }
 
     private Pair<Integer, Integer> randomPerplexityCluster() {
-        int k = ThreadLocalRandom.current().nextInt(2, 11);
+        int k = ThreadLocalRandom.current().nextInt(2, PERPLEXITY_VALUES);
         return Pair.of(k, ThreadLocalRandom.current().nextInt(0, k));
     }
 }
