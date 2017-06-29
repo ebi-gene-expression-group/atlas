d4883184f20c2a716af0e328b860caddea16cbde
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-06-29 16:30:30 +0100

    Update to Webpack 3, rely on .babelrc to transpile ES and greatly simplify build script Using prepublish scripts and setting our own packages to latest to make it all work

--- sc/src/main/javascript/build-bundles.sh
+++ sc/src/main/javascript/build-bundles.sh
@@ -1,45 +1,25 @@
 #!/usr/bin/env bash
 
 if [ $# -ne 1 ] || [[ $1 -ne "ci" && $1 -ne "prod" ]] ; then
-        echo "Builds JavaScript modules and their dependencies (images such as PNG and CSS files)"
-        echo "into bundles with Webpack and places them in the webapp/resources/js-bundles directory."
+        echo "Bundles ES packages with Webpack and places them in the webapp/resources/js-bundles and"
+        echo "webapp/versioned-resources/js-bundles directories."
         echo
         echo "The script runs in two phases:"
-        echo "1. Remove our packages from node_modules and run “npm install”, ensuring the latest"
-        echo "   version is installed."
-        echo "2. Run “npm run {ci|prod}”, which cleans the target directory and generates the bundle"
-        echo "   files. See package.json for the different parameters passed to Webpack."
+        echo "1. Runs “npm update” in each bundle to install the latest version of our published packages."
+        echo "2. Runs “npm run {ci|prod}”, which cleans the target directory and generates the bundles."
+        echo "   See webpack.config.js for the different parameters passed to Webpack."
         echo
         echo "Usage: $0 {ci|prod}"
         exit;
 fi
 
-
-# export NVM_DIR="/nfs/ma/home/ma-svc/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
-
-atlas_modules=`ls atlas_modules`
-atlas_bundles=`ls atlas_bundles`
-
-all_packages=("${sc_modules[@]}" "${sc_bundles[@]}")
-
-for dir in sc_bundles/*
+for dir in bundles/*
 do
     pushd . > /dev/null
     cd $dir
-        for module in ${all_packages[*]}
-        do
-            rm -rf node_modules/sc-atlas-$module
-        done
-    npm install
+    npm update
     popd > /dev/null
 done
 
-for module in ${all_packages[*]}
-do
-    rm -rf node_modules/sc-atlas-$module
-done
-
 npm install
-
 npm run $1
--- sc/src/main/javascript/webpack.config.js
+++ sc/src/main/javascript/webpack.config.js
@@ -1,61 +1,96 @@
-var webpack = require('webpack');
-var path = require('path');
-var CleanWebpackPlugin = require('clean-webpack-plugin');
+var webpack = require('webpack')
+var path = require('path')
+var CleanWebpackPlugin = require('clean-webpack-plugin')
+var WebpackShellPlugin = require('webpack-shell-plugin')
 
 module.exports = {
-    // define the bundles we want
-    entry: {
-        referencePlot: './sc_modules/reference-plot'
-    },
-
-    output: {
-        libraryTarget: 'var',
-        library: '[name]',
-        path: path.resolve(__dirname, '../webapp/resources/js-bundles'),
-        filename: '[name].bundle.js'
-    },
-
-    plugins: [
-        new CleanWebpackPlugin(['webapp/resources/js-bundles'], {
-            root: path.resolve(__dirname , '..'),
-            verbose: true,
-            dry: false
-        }),
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'dependencies',
-            filename: 'vendorCommons.bundle.js',
-            minChunks: Infinity     // Explicit definition-based split. Don’t put shared modules between main and demo entries in vendor.bundle.js
-        }),
-        new webpack.DefinePlugin({
-            "process.env": {
-                NODE_ENV: process.env.NODE_ENV === 'production' ? JSON.stringify("production") : JSON.stringify("development")
-            }
-        })
+  // define the bundles we want
+  entry: {
+    atlasAutocomplete: ['./bundles/autocomplete/index.jsx'],
+    // Put dependencies one line per package
+    dependencies: [
+      'prop-types', 'react', 'react-autocomplete', 'react-dom', 'react-refetch', 'urijs'  // autocomplete
     ],
+    polyfills: ['babel-polyfill', 'whatwg-fetch']
+  },
 
-    module: {
-        loaders: [
-            {test: /\.js$/, loader: 'babel', query: {presets: ['es2015']}, exclude: /node_modules\/(?!(expression-atlas|anatomogram|react-ebi-species))/},
-            {test: /\.jsx$/, loader: 'babel', query: {presets: ['es2015', 'react']}},
-            {test: /\.css$/, loader: 'style-loader!css-loader'},
-            {test: /\.less$/, loader: 'style-loader!css-loader!less-loader'},
-            {test: /\.json$/, loader: 'json'},
-            {test: /\.(jpe?g|png|gif)$/i,
-                loaders: [
-                          'file?hash=sha512&digest=hex&name=[hash].[ext]',
-                          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
-                ]
-            },
-            {test: /\.(svg)$/i,
-                loaders: [
-                          'file?hash=sha512&digest=hex&name=[hash].[ext]'
-                ]
-            }
-        ]
+  resolve: {
+    alias: {
+      "react": path.resolve('./node_modules/react'),
+      "react-dom": path.resolve('./node_modules/react-dom')
     },
-    
-    devServer: {
-      port: 9000
-    }
-};
+  },
+
+  output: {
+    libraryTarget: 'var',
+    library: '[name]',
+    path: path.resolve(__dirname, '../webapp/resources/js-bundles'),
+    filename: '[name].bundle.js'
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(['webapp/resources/js-bundles'], {
+      root: path.resolve(__dirname, '..'),
+      verbose: true,
+      dry: false
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'dependencies',
+      filename: 'vendorCommons.bundle.js',
+      minChunks: Infinity     // Explicit definition-based split. Don’t put shared modules between main and demo entries in vendor.bundle.js
+    }),
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: process.env.NODE_ENV === 'production' ? JSON.stringify("production") : JSON.stringify("development")
+      }
+    }),
+    // Beacuse copy-webpack-plugin doesn’t wait for WebPack to finish, and the output dir is cleaned above
+    new WebpackShellPlugin({
+      onBuildStart: ['echo Cleaning versioned-resouces...', 'rm -rf ../webapp/versioned-resources/js-bundles', 'echo Done!'],
+      onBuildEnd: ['echo Populating versioned-resources...', 'cp -a ../webapp/resources/js-bundles ../webapp/versioned-resources', 'echo Done!']
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {loader: 'style-loader'},
+          {loader: 'css-loader', options: {modules: false}}
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {loader: 'style-loader'},
+          {loader: 'css-loader', options: {modules: false}},
+          {loader: 'less-loader'}
+        ]
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/i,
+        use: [
+          {loader: 'file-loader', options: {query: {name: '[hash].[ext]', hash: 'sha512', digest: 'hex'}}},
+          {loader: 'image-webpack-loader',
+            options: {
+              query: {bypassOnDebug: true, mozjpeg: {progressive: true}, gifsicle: {interlaced: true}, optipng: {optimizationLevel: 7}}
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(svg)$/i,
+        use: [
+          {loader: 'file-loader', options: {query: {name: '[hash].[ext]', hash: 'sha512', digest: 'hex'}}}
+        ]
+      },
+      {
+        test: /\.jsx?$/,
+        use: [
+          {loader: 'babel-loader'}
+        ]
+      }
+    ]
+  }
+}
