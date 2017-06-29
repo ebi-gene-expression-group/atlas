cc58677dfb5f9004f99f0e313a267f89858a0e3e
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-06-29 16:32:50 +0100

    Add resourcesVersion property to the model in controller classes Views can then point at the latest version of the static resources

--- sc/src/main/java/uk/ac/ebi/atlas/search/SearchController.java
+++ sc/src/main/java/uk/ac/ebi/atlas/search/SearchController.java
@@ -1,6 +1,7 @@
 package uk.ac.ebi.atlas.search;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import uk.ac.ebi.atlas.controllers.HtmlExceptionHandlingController;
 
@@ -8,7 +9,9 @@ import uk.ac.ebi.atlas.controllers.HtmlExceptionHandlingController;
 public class SearchController extends HtmlExceptionHandlingController {
 
     @RequestMapping(value = "/search", produces = "text/html;charset=UTF-8")
-    public String search() {
+    public String search(Model model) {
+        model.addAttribute("resourcesVersion", env.getProperty("resources.version"));
         return "search-results";
     }
+
 }
