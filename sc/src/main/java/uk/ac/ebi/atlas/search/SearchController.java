470cb99447b73b2f0a5a1b128d083c6c6c1a3590
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-06-30 02:46:27 +0100

    Add geneId to the model and use it to prepopulate the input if available [#147104069]

--- sc/src/main/java/uk/ac/ebi/atlas/search/SearchController.java
+++ sc/src/main/java/uk/ac/ebi/atlas/search/SearchController.java
@@ -3,13 +3,16 @@ package uk.ac.ebi.atlas.search;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import uk.ac.ebi.atlas.controllers.HtmlExceptionHandlingController;
 
 @Controller
 public class SearchController extends HtmlExceptionHandlingController {
 
     @RequestMapping(value = "/search", produces = "text/html;charset=UTF-8")
-    public String search(Model model) {
+    public String search(@RequestParam(value = "geneId", required = false, defaultValue = "") String geneId,
+                         Model model) {
+        model.addAttribute("geneId", geneId);
         model.addAttribute("resourcesVersion", env.getProperty("resources.version"));
         return "search-results";
     }
