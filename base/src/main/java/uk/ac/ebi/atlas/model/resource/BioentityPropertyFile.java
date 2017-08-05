1ad29ddd6306ad5926c3e7b42a67cc0f3a23bd20
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-08-05 10:56:54 +0100

    Fix regexp that cleans values Some bioentity IDs descriptions (e.g. A. thaliana AT3G06483) start with square brackets and the previous regexp would leave the value empty and therefore the bioentities index build would fail; I don’t know if this is a problem with the data...

--- base/src/main/java/uk/ac/ebi/atlas/model/resource/BioentityPropertyFile.java
+++ base/src/main/java/uk/ac/ebi/atlas/model/resource/BioentityPropertyFile.java
@@ -9,11 +9,11 @@ import java.nio.file.Path;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-
 public abstract class BioentityPropertyFile extends AtlasResource<Stream<BioentityProperty>> {
 
-
     public final Species species;
+    private static final String STUFF_IN_CURLY_BRACKETS_AT_THE_END = "\\{.*\\}$";
+    private static final String STUFF_IN_SQUARE_BRACKETS_AT_THE_END = "\\[Source.*\\]$";
 
     protected Stream<BioentityProperty> lineOfProperties(String bioentityIdentifier, String[] propertyNames, String[] propertyValues) {
         Preconditions.checkState(propertyNames.length == propertyValues.length);
@@ -33,13 +33,9 @@ public abstract class BioentityPropertyFile extends AtlasResource<Stream<Bioenti
         return new BioentityProperty(bioentityIdentifier, species.getEnsemblName(), propertyName, cleanUpPropertyValue(propertyValue));
     }
 
-    String stuffInCurlyBracketsAtTheEnd = "\\{.*\\}$";
-    String stuffInSquareBracketsAtTheEnd = "\\[.*\\]$";
-
     String cleanUpPropertyValue(String propertyValue) {
-        return propertyValue.replaceFirst(stuffInCurlyBracketsAtTheEnd, "").replaceFirst(stuffInSquareBracketsAtTheEnd, "").trim();
+        return propertyValue.replaceFirst(STUFF_IN_CURLY_BRACKETS_AT_THE_END, "").replaceFirst(STUFF_IN_SQUARE_BRACKETS_AT_THE_END, "").trim();
     }
-
 
     public BioentityPropertyFile(Path path, Species species) {
         super(path);
