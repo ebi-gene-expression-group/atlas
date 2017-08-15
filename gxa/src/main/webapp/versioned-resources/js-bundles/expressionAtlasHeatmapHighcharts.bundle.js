f203bac5634f87150810321d56045f7f00d478a1
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-08-15 14:45:48 +0100

    Production bundles Heatmap bundle without EBI Framework and better use of Bootstrap

--- gxa/src/main/webapp/versioned-resources/js-bundles/expressionAtlasHeatmapHighcharts.bundle.js
+++ gxa/src/main/webapp/versioned-resources/js-bundles/expressionAtlasHeatmapHighcharts.bundle.js
@@ -3,4 +3,4 @@ var expressionAtlasHeatmapHighcharts=webpackJsonp_name_([5],{440:/*!************
   \***************************************************/
 /*! no static exports found */
 /*! all exports used */
-function(e,r,a){"use strict";Object.defineProperty(r,"__esModule",{value:!0}),r.render=void 0;var n=a(/*! expression-atlas-heatmap-highcharts */134);r.render=n.render}},[440]);
+function(e,r,a){"use strict";Object.defineProperty(r,"__esModule",{value:!0}),r.render=void 0;var n=a(/*! expression-atlas-heatmap-highcharts */135);r.render=n.render}},[440]);
