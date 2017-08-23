7f849e409fedd04439e8dadfda81fb9c1701c573
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-08-23 17:27:42 +0100

    Fix checking if value is within min and max

--- base/src/main/java/uk/ac/ebi/atlas/utils/ColourGradient.java
+++ base/src/main/java/uk/ac/ebi/atlas/utils/ColourGradient.java
@@ -7,7 +7,9 @@ import java.awt.Color;
 public class ColourGradient {
 
     public static String getGradientColour(double value, double min, double max, String lowValueColourString, String highValueColourString) {
-        Preconditions.checkArgument(value >= min && value <= max);
+        Preconditions.checkArgument(
+                value >= 0.0 && value >= min && value <= max ||
+                value < 0 && value <= min && value >= max);
 
         Color lowValueColour = getColour(lowValueColourString);
         Color highValueColour = getColour(highValueColourString);
