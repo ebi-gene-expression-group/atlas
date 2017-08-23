1d8f72623e57a203774555569a4251f4efc9e7b0
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-08-23 15:55:09 +0100

    Inherit from HtmlExceptionHandlingController

--- gxa/src/main/java/uk/ac/ebi/atlas/controllers/rest/admin/BioentityIndexController.java
+++ gxa/src/main/java/uk/ac/ebi/atlas/controllers/rest/admin/BioentityIndexController.java
@@ -1,12 +1,10 @@
 package uk.ac.ebi.atlas.controllers.rest.admin;
 
+import uk.ac.ebi.atlas.controllers.HtmlExceptionHandlingController;
 import uk.ac.ebi.atlas.solr.admin.BioentitiesIndexer;
 import uk.ac.ebi.atlas.solr.admin.monitor.BioentityIndexMonitor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -15,9 +13,7 @@ import javax.inject.Inject;
 @Controller
 @Scope("request")
 @RequestMapping(value = "/admin")
-public class BioentityIndexController {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(BioentityIndexController.class);
+public class BioentityIndexController extends HtmlExceptionHandlingController {
 
     private BioentitiesIndexer bioentitiesIndexer;
     private BioentityIndexMonitor bioentityIndexMonitor;
@@ -28,30 +24,17 @@ public class BioentityIndexController {
         this.bioentityIndexMonitor = bioentityIndexMonitor;
     }
 
-    @ExceptionHandler(Exception.class)
-    @ResponseBody
-    public String handleException(Exception e) {
-        bioentityIndexMonitor.failed(e);
-
-        LOGGER.error(e.getMessage(),e);
-        return e.getMessage();
-    }
-
     @RequestMapping(value = "/bioentitiesIndex/buildIndex/status", produces = "text/plain;charset=UTF-8")
     @ResponseBody
     public String buildStatus() {
         return bioentityIndexMonitor.reportProgress();
     }
 
-
-    @RequestMapping(value = "/bioentitiesIndex/buildIndex",  produces = "text/plain;charset=UTF-8")
+    @RequestMapping(value = "/bioentitiesIndex/buildIndex", produces = "text/plain;charset=UTF-8")
     @ResponseBody
     public String build() {
-
         bioentitiesIndexer.rebuildIndex();
-
         return bioentityIndexMonitor.reportProgress();
-
     }
 
 }
