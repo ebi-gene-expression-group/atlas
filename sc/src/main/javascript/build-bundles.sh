a13267dde05d2735f8cb4908d9c478ef76340535
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-09-20 23:47:17 +0100

    Better bundle set-up

--- sc/src/main/javascript/build-bundles.sh
+++ sc/src/main/javascript/build-bundles.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 if [ $# -ne 1 ] || [[ $1 -ne "ci" && $1 -ne "prod" ]] ; then
-        echo "Bundles ES packages with Webpack and places them in the webapp/resources/js-bundles and"
-        echo "webapp/versioned-resources/js-bundles directories."
+        echo "Builds JavaScript modules and their dependencies (images such as PNG and CSS files)"
+        echo "into bundles with Webpack and places them in the webapp/resources/js-bundles directory."
         echo
         echo "The script runs in two phases:"
-        echo "1. Runs “npm update” in each bundle to install the latest version of our published packages."
-        echo "2. Runs “npm run {ci|prod}”, which cleans the target directory and generates the bundles."
-        echo "   See webpack.config.js for the different parameters passed to Webpack."
+        echo "1. Run “yarn upgrade”, ensuring the packages tagged with \"latest\" are updated."
+        echo "2. Run “yarn run {ci|prod}”, which cleans the target directory and generates the bundle"
+        echo "   files. See package.json for the different parameters passed to Webpack."
         echo
         echo "Usage: $0 {ci|prod}"
         exit;
@@ -16,10 +16,11 @@ fi
 for dir in bundles/*
 do
     pushd . > /dev/null
+    echo "Upgrading $dir:"
     cd $dir
-    npm update
+    yarn upgrade
     popd > /dev/null
 done
 
-npm install
-npm run $1
+yarn upgrade
+yarn run $1
