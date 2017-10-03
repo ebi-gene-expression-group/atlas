8b6e9a9c2b0c6fc8b6399ab624dc056feb4c8a8f
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-10-03 21:54:32 +0100

    Add 100% coverage test

--- base/src/test/java/uk/ac/ebi/atlas/model/BiologicalReplicateTest.java
+++ base/src/test/java/uk/ac/ebi/atlas/model/BiologicalReplicateTest.java
@@ -1,12 +1,34 @@
 package uk.ac.ebi.atlas.model;
 
+import com.google.common.collect.ImmutableSet;
 import org.junit.Test;
 
+import java.util.Collections;
+
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 public class BiologicalReplicateTest {
 
+    @Test(expected=RuntimeException.class)
+    public void replicateIdCantBeAnAssayId() throws Exception {
+        new BiologicalReplicate("Foobar", ImmutableSet.of("Foo", "Foobar", "Bar"));
+    }
+
+    @Test(expected=RuntimeException.class)
+    public void replicateNeedsAtLeastOneAssayId() throws Exception {
+        new BiologicalReplicate("Foobar", Collections.emptySet());
+    }
+
+    @Test
+    public void retrievesAssayIdsAndItself() throws Exception {
+        ImmutableSet<String> assayIds = ImmutableSet.of("assayId1", "assayId2", "assayId3");
+        BiologicalReplicate subject = new BiologicalReplicate("replicateId", assayIds);
+
+        assertThat(subject.biologicalReplicatesForThisDataColumn().iterator().next(), is(equalTo(subject)));
+        assertThat(subject.assaysAnalyzedForThisDataColumn(), is(equalTo(assayIds)));
+    }
 
     @Test
     public void toStringIsNice(){
@@ -15,5 +37,4 @@ public class BiologicalReplicateTest {
                 is("BiologicalReplicate {id=assay}")
         );
     }
-
 }
