4754e503728f1b4da9a8aab73c886331bbaae117
wbazant <wbazant@ebi.ac.uk>
2017-10-04 12:19:02 +0100

    Tidy up stdout, better error message, do not break

--- atlas-misc/scripts/warm_experiments_genedistribution_cache.sh
+++ atlas-misc/scripts/warm_experiments_genedistribution_cache.sh
@@ -17,20 +17,17 @@ PORT=8080
 ATLAS_ROOT_URL="http://${HOSTNAME}:${PORT}/gxa"
 EXPERIMENTS=${ATLAS_ROOT_URL}/json/experiments
 
-EXPERIMENTS_RESULTS=$(curl ${EXPERIMENTS})
+EXPERIMENTS_RESULTS=$(curl -s -S ${EXPERIMENTS})
 len=$(echo "$EXPERIMENTS_RESULTS" | jq .[] | jq length)
 
-curl ${ATLAS_ROOT_URL}/json/experiments | jq -r '.aaData | map(select(.experimentType=="RNASEQ_MRNA_BASELINE")) | map(.experimentAccession)[]' | while read -r experimentAccession; do {
+curl -s -S ${ATLAS_ROOT_URL}/json/experiments | jq -r '.aaData | map(select(.experimentType=="RNASEQ_MRNA_BASELINE")) | map(.experimentAccession)[]' | while read -r experimentAccession; do {
 	GENE_DISTRIBUTION=${ATLAS_ROOT_URL}/json/experiments/${experimentAccession}/genedistribution
-
     STATUS=$(curl -s -S -o /dev/null -w '%{http_code}' ${GENE_DISTRIBUTION})
-    if [ $STATUS -eq 200 ]; then
-        echo "Finished"
-        break
-    else
-        echo "Got $STATUS (error while processing" ${experimentAccession} ")"
+    if [ "$STATUS" != 200 ]; then
+        echo "Got error HTTP $STATUS for: $GENE_DISTRIBUTION " >&2
+        sleep 10
     fi
-  sleep 10
-
 }
 done
+
+echo "Finished!"
