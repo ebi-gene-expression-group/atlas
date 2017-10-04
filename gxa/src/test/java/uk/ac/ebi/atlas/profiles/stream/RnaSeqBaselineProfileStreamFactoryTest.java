55047fb9d31c2ba4728af793544c63090debdaad
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-10-04 11:44:41 +0100

    Fix test - http://bar:8085/browse/ATLAS-GXACIDEVTASKS2-5

--- gxa/src/test/java/uk/ac/ebi/atlas/profiles/stream/RnaSeqBaselineProfileStreamFactoryTest.java
+++ gxa/src/test/java/uk/ac/ebi/atlas/profiles/stream/RnaSeqBaselineProfileStreamFactoryTest.java
@@ -1,6 +1,5 @@
 package uk.ac.ebi.atlas.profiles.stream;
 
-import com.google.common.base.Predicates;
 import com.google.common.collect.ImmutableList;
 import org.hamcrest.Matchers;
 import org.junit.Before;
@@ -22,8 +21,8 @@ import uk.ac.ebi.atlas.resource.MockDataFileHub;
 import uk.ac.ebi.atlas.web.RnaSeqBaselineRequestPreferences;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 import java.util.function.Function;
 
 import static org.hamcrest.Matchers.is;
@@ -81,9 +80,9 @@ public class RnaSeqBaselineProfileStreamFactoryTest {
 
         RnaSeqBaselineRequestPreferences rnaSeqBaselineRequestPreferences = new RnaSeqBaselineRequestPreferences();
         rnaSeqBaselineRequestPreferences.setUnit(ExpressionUnit.Absolute.Rna.TPM);
-        ObjectInputStream<BaselineProfile> resultTpms = subject.create(baselineExperiment, new BaselineRequestContext<>(rnaSeqBaselineRequestPreferences, baselineExperiment), Optional.empty());
+        ObjectInputStream<BaselineProfile> resultTpms = subject.create(baselineExperiment, new BaselineRequestContext<>(rnaSeqBaselineRequestPreferences, baselineExperiment), Collections.emptySet());
         rnaSeqBaselineRequestPreferences.setUnit(ExpressionUnit.Absolute.Rna.FPKM);
-        ObjectInputStream<BaselineProfile> resultFpkms = subject.create(baselineExperiment, new BaselineRequestContext<>(rnaSeqBaselineRequestPreferences, baselineExperiment), Optional.empty());
+        ObjectInputStream<BaselineProfile> resultFpkms = subject.create(baselineExperiment, new BaselineRequestContext<>(rnaSeqBaselineRequestPreferences, baselineExperiment), Collections.emptySet());
 
         assertThat(resultTpms.readNext().getExpressionLevel(assayGroup), is(42.0));
 
@@ -103,7 +102,7 @@ public class RnaSeqBaselineProfileStreamFactoryTest {
         rnaSeqBaselineRequestPreferences.setUnit(unit);
         ObjectInputStream<BaselineProfile> result =
                 new RnaSeqBaselineProfileStreamFactory(dataFileHub).create(baselineExperiment,
-                        new BaselineRequestContext<>(rnaSeqBaselineRequestPreferences, baselineExperiment), Optional.empty());
+                        new BaselineRequestContext<>(rnaSeqBaselineRequestPreferences, baselineExperiment), Collections.emptySet());
 
 
         assertThat(result.readNext().getExpressionLevel(assayGroup), is(13.37));
@@ -116,20 +115,31 @@ public class RnaSeqBaselineProfileStreamFactoryTest {
                 new String[]{"id_1", "name_1", "1.0"},
                 new String[]{"id_2", "name_2", "2.0"},
                 new String[]{"id_3", "name_3", "3.0"}
-                ));
-
-        Function<Optional<Collection<String>>,List<BaselineProfile>> testWithGeneIds =
-                geneIdsToKeep -> ImmutableList.copyOf(new IterableObjectInputStream<>(
-                subject.create(baselineExperiment, new BaselineRequestContext<>(new RnaSeqBaselineRequestPreferences(), baselineExperiment),geneIdsToKeep)
         ));
 
-        assertThat(testWithGeneIds.apply(Optional.empty()).size(), is(3));
-        assertThat(testWithGeneIds.apply(Optional.of(ImmutableList.of())).size(), is(0));
-        assertThat(testWithGeneIds.apply(Optional.of(ImmutableList.of("id_1", "id_2","id_3"))), is(testWithGeneIds.apply(Optional.empty())));
-        assertThat(testWithGeneIds.apply(Optional.of(ImmutableList.of("id_1"))).size(), is(1));
-        assertThat(testWithGeneIds.apply(Optional.of(ImmutableList.of("id_1", "other_id"))), is(testWithGeneIds.apply(Optional.of(ImmutableList.of("id_1")))));
+        Function<Collection<String>,List<BaselineProfile>> testWithGeneIds =
+                geneIdsToKeep ->
+                        ImmutableList.copyOf(new IterableObjectInputStream<>(
+                                subject.create(
+                                        baselineExperiment,
+                                        new BaselineRequestContext<>(
+                                                new RnaSeqBaselineRequestPreferences(),
+                                                baselineExperiment
+                                        ),
+                                        geneIdsToKeep
+                                )
+                        ));
 
-        assertThat(testWithGeneIds.apply(Optional.of(ImmutableList.of("id_1", "id_2"))).size(), is(2));
+        assertThat(testWithGeneIds.apply(Collections.emptySet()).size(), is(3));
+        // This is the only change in semantics after https://github.com/gxa/atlas/pull/9/commits/a4cae7f5f9ad48ca96904db34e405176bd5c4a8c
+        // I can’t think how we can arrive at an empty collection present collection from the code, but if
+        // that’s the case revert the commit above
+        // assertThat(testWithGeneIds.apply(ImmutableList.of()).size(), is(0));
+        assertThat(testWithGeneIds.apply(ImmutableList.of("id_1", "id_2","id_3")), is(testWithGeneIds.apply(Collections.emptySet())));
+        assertThat(testWithGeneIds.apply(ImmutableList.of("id_1")).size(), is(1));
+        assertThat(testWithGeneIds.apply(ImmutableList.of("id_1", "other_id")), is(testWithGeneIds.apply(ImmutableList.of("id_1"))));
+
+        assertThat(testWithGeneIds.apply(ImmutableList.of("id_1", "id_2")).size(), is(2));
     }
 
     @Test
@@ -145,10 +155,23 @@ public class RnaSeqBaselineProfileStreamFactoryTest {
 
         RnaSeqBaselineRequestPreferences rnaSeqBaselineRequestPreferences = new RnaSeqBaselineRequestPreferences();
         rnaSeqBaselineRequestPreferences.setUnit(ExpressionUnit.Absolute.Rna.TPM);
-        BaselineProfile resultTpms = subject.create(twoAssayGroupBaselineExperiment, new BaselineRequestContext<>(rnaSeqBaselineRequestPreferences, twoAssayGroupBaselineExperiment), Optional.empty()).readNext();
+        BaselineProfile resultTpms =
+                subject.create(
+                        twoAssayGroupBaselineExperiment,
+                        new BaselineRequestContext<>(
+                                rnaSeqBaselineRequestPreferences, twoAssayGroupBaselineExperiment
+                        ),
+                        Collections.emptySet()
+                ).readNext();
         rnaSeqBaselineRequestPreferences.setUnit(ExpressionUnit.Absolute.Rna.FPKM);
-        BaselineProfile resultFpkms = subject.create(twoAssayGroupBaselineExperiment, new BaselineRequestContext<>(rnaSeqBaselineRequestPreferences, twoAssayGroupBaselineExperiment), Optional.empty()).readNext();
-
+        BaselineProfile resultFpkms =
+                subject.create(
+                        twoAssayGroupBaselineExperiment,
+                        new BaselineRequestContext<>(
+                                rnaSeqBaselineRequestPreferences,
+                                twoAssayGroupBaselineExperiment
+                        ),
+                        Collections.emptySet()).readNext();
 
         assertThat(resultTpms.getExpressionLevel(assayGroup), is(1.0));
         assertThat(resultTpms.getExpressionLevel(secondAssayGroup), is(2.0));
@@ -161,14 +184,14 @@ public class RnaSeqBaselineProfileStreamFactoryTest {
     @Test
     public void canProvideQuartiles(){
         assertThat(
-                subject.howToReadLine(twoAssayGroupBaselineExperiment, Predicates.alwaysTrue())
+                subject.howToReadLine(twoAssayGroupBaselineExperiment, x -> true)
                         .apply(new String[]{"Gene ID", "Gene name", assayGroup.getId(), secondAssayGroup.getId()})
                         .apply(new String[]{"id", "name", "1.0", "2.0"}).getExpression(assayGroup),
                 Matchers.is(new BaselineExpression(1.0))
         );
 
         assertThat(
-                subject.howToReadLine(twoAssayGroupBaselineExperiment, Predicates.alwaysTrue())
+                subject.howToReadLine(twoAssayGroupBaselineExperiment, x -> true)
                         .apply(new String[]{"Gene ID", "Gene name", assayGroup.getId(), secondAssayGroup.getId()})
                         .apply(new String[]{"id", "name", "0.1,0.2,0.3,0.4,0.5", "2.0"}).getExpression(assayGroup).toJson().get("quartiles").getAsJsonObject().size(),
                 is(5));
