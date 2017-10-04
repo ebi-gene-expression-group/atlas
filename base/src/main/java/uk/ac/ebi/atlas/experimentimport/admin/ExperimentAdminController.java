84be155c0678dfc86b51753697317a421f6a5394
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-10-04 17:04:39 +0100

    Remove stale imports (dummy commit to check out that Bamboo plan is fine)

--- base/src/main/java/uk/ac/ebi/atlas/experimentimport/admin/ExperimentAdminController.java
+++ base/src/main/java/uk/ac/ebi/atlas/experimentimport/admin/ExperimentAdminController.java
@@ -6,8 +6,6 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.stream.JsonWriter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
