4cbecf65a1557dae041ea6b81cb06b57d8aeba27
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-10-16 23:10:32 +0100

    Each test build will use its own Solr directory for the embedded Solr server [#152017524]

--- base/src/test/java/uk/ac/ebi/atlas/solr/EmbeddedSolrServerFactory.java
+++ base/src/test/java/uk/ac/ebi/atlas/solr/EmbeddedSolrServerFactory.java
@@ -1,11 +1,15 @@
 package uk.ac.ebi.atlas.solr;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer;
 import org.apache.solr.core.CoreContainer;
 import org.springframework.beans.factory.annotation.Value;
 
 import javax.inject.Named;
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Named
 public class EmbeddedSolrServerFactory {
@@ -13,7 +17,9 @@ public class EmbeddedSolrServerFactory {
     private String solrConfDir;
 
     public EmbeddedSolrServer createEmbeddedSolrServerInstance(String coreName) throws IOException {
-        CoreContainer coreContainer =  new CoreContainer(solrConfDir);
+        Path solrTempDirectory = Files.createTempDirectory("");
+        FileUtils.copyDirectory(new File(solrConfDir), solrTempDirectory.toFile());
+        CoreContainer coreContainer =  new CoreContainer(solrTempDirectory.toString());
         coreContainer.load();
         return new EmbeddedSolrServer(coreContainer, coreName);
     }
