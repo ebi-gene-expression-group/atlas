baa7982c6ff844638dbf42f3371d52834adace41
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-10-18 14:00:28 +0100

    Add method to retrieve a collection of pathway names from Reactome API /data/query/ids

--- base/src/main/java/uk/ac/ebi/atlas/utils/ReactomeClient.java
+++ base/src/main/java/uk/ac/ebi/atlas/utils/ReactomeClient.java
@@ -1,6 +1,12 @@
 package uk.ac.ebi.atlas.utils;
 
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Streams;
+import com.jayway.jsonpath.InvalidJsonException;
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.ReadContext;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestClientException;
@@ -9,12 +15,19 @@ import org.springframework.web.client.RestTemplate;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.text.MessageFormat;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Named
 public class ReactomeClient {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ReactomeClient.class);
+
+    private static final String JSON_PATH_IDS = "$[*].stId";
+    private static final String JSON_PATH_NAMES = "$[*].displayName";
 
     private final RestTemplate restTemplate;
 
@@ -23,6 +36,33 @@ public class ReactomeClient {
         this.restTemplate = restTemplate;
     }
 
+    public Set<Pair<String, String>> fetchPathwayNames(Collection<String> reactomeIds) {
+        String url = "https://reactome.org/ContentService/data/query/ids";
+        String body = reactomeIds.stream().collect(Collectors.joining(","));
+
+        try {
+
+            String response = restTemplate.postForObject(url, body, String.class);
+
+            ReadContext jsonReadContext = JsonPath.parse(response);
+            Collection<String> ids = jsonReadContext.read(JSON_PATH_IDS);
+            Collection<String> displayNames = jsonReadContext.read(JSON_PATH_NAMES);
+
+            Preconditions.checkArgument(ids.size() == displayNames.size(), "Retrieved pathway IDs did not match retrieved pathway names");
+
+            return Streams.zip(ids.stream(), displayNames.stream(), Pair::of).collect(Collectors.toSet());
+
+        } catch (InvalidJsonException e) {
+            LOGGER.warn("Invalid JSON returned from Reactome API");
+            return Collections.emptySet();
+        } catch (RestClientException e) {
+            LOGGER.error("There was an error retrieving pathway names from Reactome API");
+            return Collections.emptySet();
+        }
+    }
+
+    // Use fetchPathwayNames above instead
+    @Deprecated
     public Optional<String> fetchPathwayNameFailSafe(String reactomeId) {
         String reactomeURL = "https://reactome.org/ContentService/data/query/{0}/displayName";
         String url = MessageFormat.format(reactomeURL, reactomeId);
--- base/src/test/java/uk/ac/ebi/atlas/utils/ReactomeClientTest.java
+++ base/src/test/java/uk/ac/ebi/atlas/utils/ReactomeClientTest.java
@@ -1,5 +1,8 @@
 package uk.ac.ebi.atlas.utils;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import org.apache.commons.lang3.tuple.Pair;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -7,27 +10,82 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Collection;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ReactomeClientTest {
 
-  @Mock
-  RestTemplate restTemplateMock;
+    @Mock
+    RestTemplate restTemplateMock;
 
-  ReactomeClient subject;
+    ReactomeClient subject;
 
-  @Test
-  public void returnEmptyStringIfRestTemplateThrows() throws Exception {
-      when(restTemplateMock.getForObject(anyString(), any())).thenThrow(new RestClientException("Timeout!"));
-      subject = new ReactomeClient(restTemplateMock);
+    @Test
+    public void returnEmptyStringIfRestTemplateThrows() throws Exception {
+        when(restTemplateMock.getForObject(anyString(), any())).thenThrow(new RestClientException("Timeout!"));
+        subject = new ReactomeClient(restTemplateMock);
 
-      assertThat(subject.fetchPathwayNameFailSafe("foobar"), is(Optional.empty()));
-  }
+        assertThat(subject.fetchPathwayNameFailSafe("foobar"), is(Optional.empty()));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void throwIfNumberOfNamesDoesNotMatchNumberOfIds() throws Exception {
+        when(restTemplateMock.postForObject(anyString(), anyString(), any()))
+                .thenReturn("[{\"stId\":\"R-FOO-123\", \"displayName\":\"Bar signal\"},{\"stId\":\"R-FOO-321\"}]");
+        subject = new ReactomeClient(restTemplateMock);
+        subject.fetchPathwayNames(ImmutableSet.of("R-FOO-123", "R-FOO-321"));
+    }
+
+    @Test
+    public void returnEmptyCollectionIfSomethingGoesWrong() throws Exception {
+        when(restTemplateMock.postForObject(anyString(), anyString(), any())).thenThrow(new RestClientException("Timeout!"));
+        subject = new ReactomeClient(restTemplateMock);
+
+        assertThat(subject.fetchPathwayNames(ImmutableSet.of("R-FOO-123")).isEmpty(), is(true));
+    }
+
+    @Test
+    public void returnEmptyCollectionIfBadResponse() throws Exception {
+        when(restTemplateMock.postForObject(anyString(), anyString(), any())).thenReturn("[{\"foo\":\"bar\"}]");
+        subject = new ReactomeClient(restTemplateMock);
+
+        assertThat(subject.fetchPathwayNames(ImmutableSet.of("R-FOO-123")).isEmpty(), is(true));
+    }
+
+    @Test
+    public void returnEmptyCollectionIfInvalidResponse() throws Exception {
+        when(restTemplateMock.postForObject(anyString(), anyString(), any())).thenReturn("[{foobar]");
+        subject = new ReactomeClient(restTemplateMock);
+
+        assertThat(subject.fetchPathwayNames(ImmutableSet.of("R-FOO-123")).isEmpty(), is(true));
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void fetchPathways() throws Exception {
+        ImmutableSet<String> pathwayIds = ImmutableSet.of("R-FOO-111", "R-FOO-222", "R-FOO-333");
+
+        when(restTemplateMock.postForObject(anyString(), eq(pathwayIds.stream().collect(Collectors.joining(","))), any()))
+                .thenReturn("[" +
+                        "{\"stId\":\"R-FOO-111\", \"displayName\":\"Bar signal\"}," +
+                        "{\"stId\":\"R-FOO-222\", \"displayName\":\"Bar receptor\"}," +
+                        "{\"stId\":\"R-FOO-333\", \"displayName\":\"Bar regulator\"}]"
+                );
+        subject = new ReactomeClient(restTemplateMock);
+        Collection<Pair<String, String>> result = subject.fetchPathwayNames(pathwayIds);
+
+        assertThat(ImmutableList.copyOf(result), hasSize(3));
+        assertThat(result, containsInAnyOrder(Pair.of("R-FOO-111", "Bar signal"), Pair.of("R-FOO-222", "Bar receptor"), Pair.of("R-FOO-333", "Bar regulator")));
+    }
 }
