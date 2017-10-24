00ab832ca8a911069c9d51d6e44ac2721f43e385
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-10-24 15:02:09 +0100

    Remove unused SolrQueryBuilder We’re only using its only subclass, BioentityIdentifierQueryBuilder, and none of the base class code

--- base/src/main/java/uk/ac/ebi/atlas/solr/query/builders/BioentityIdentifierQueryBuilder.java
+++ base/src/main/java/uk/ac/ebi/atlas/solr/query/builders/BioentityIdentifierQueryBuilder.java
@@ -4,14 +4,21 @@ import org.apache.solr.client.solrj.SolrQuery;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import uk.ac.ebi.atlas.search.SemanticQueryTerm;
+import uk.ac.ebi.atlas.species.Species;
 
-
-public class BioentityIdentifierQueryBuilder extends SolrQueryBuilder<BioentityIdentifierQueryBuilder>{
+/**
+ * This is a builder, keep always in mind that builders are potentially stateful.
+ * If you need to build different query strings within the same client process
+ * you must use a new instance of builder for each query.
+ */
+public class BioentityIdentifierQueryBuilder {
     private static final Logger LOGGER = LoggerFactory.getLogger(BioentityIdentifierQueryBuilder.class);
 
     private static final String BIOENTITY_IDENTIFIER_FIELD = "bioentity_identifier";
+    private static final String SPECIES_FIELD = "species";
     private static final int MAX_GENE_IDS_TO_FETCH = 1000000;
 
+    private StringBuilder queryStringBuilder = new StringBuilder();
     private String queryString;
 
     public BioentityIdentifierQueryBuilder forTerm(SemanticQueryTerm term){
@@ -19,15 +26,20 @@ public class BioentityIdentifierQueryBuilder extends SolrQueryBuilder<BioentityI
         return this;
     }
 
+    public BioentityIdentifierQueryBuilder withSpecies(Species species){
+        // No need to lowercase the species field, it is of type lowercase in Solr
+        if (!species.isUnknown()){
+            queryStringBuilder.append(" AND " + SPECIES_FIELD + ":\"").append(species.getEnsemblName()).append("\"");
+        }
+        return this;
+    }
+
     public SolrQuery build() {
         // use default "lucene" parser and set default operator to OR and default field to propertyName
         StringBuilder queryConditionBuilder = new StringBuilder("{!lucene q.op=OR df=property_value_search}");
-
         queryConditionBuilder.append(String.format("(%s)", queryString));
 
-
         queryStringBuilder.insert(0, queryConditionBuilder);
-
         return buildQueryObject(queryStringBuilder.toString());
     }
 
@@ -44,8 +56,4 @@ public class BioentityIdentifierQueryBuilder extends SolrQueryBuilder<BioentityI
         return solrQuery;
     }
 
-    @Override
-    protected BioentityIdentifierQueryBuilder getThis() {
-        return this;
-    }
 }
--- base/src/test/java/uk/ac/ebi/atlas/solr/query/builders/BioentityIdentifierQueryBuilderTest.java
+++ base/src/test/java/uk/ac/ebi/atlas/solr/query/builders/BioentityIdentifierQueryBuilderTest.java
@@ -1,7 +1,6 @@
 package uk.ac.ebi.atlas.solr.query.builders;
 
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Sets;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,16 +8,12 @@ import uk.ac.ebi.atlas.search.SemanticQueryTerm;
 import uk.ac.ebi.atlas.species.Species;
 import uk.ac.ebi.atlas.species.SpeciesProperties;
 
-import java.util.Set;
-
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class BioentityIdentifierQueryBuilderTest {
 
     private static final String QUERY_STRING = "A QUERY STRING";
-    private static final String PROPERTY_NAME_1 = "PROPERTY_NAME_1";
-    private static final String PROPERTY_NAME_2 = "PROPERTY_NAME_2";
     private static final String SPECIES_ENSEMBL_NAME = "Species_specii";
     private static final Species SPECIES =
             new Species("Species specii",
@@ -27,7 +22,6 @@ public class BioentityIdentifierQueryBuilderTest {
     private static final String EXPECTED_QUERY_STRING_WITH_OR_OPERATOR_APPLIED =
                     "{!lucene q.op=OR df=property_value_search}" +
                     "(property_value:\"A QUERY STRING\") " +
-                    "AND (property_name:\"PROPERTY_NAME_1\" OR property_name:\"PROPERTY_NAME_2\") " +
                     "AND species:\"" + SPECIES_ENSEMBL_NAME +"\"";
 
 
@@ -41,7 +35,6 @@ public class BioentityIdentifierQueryBuilderTest {
     @Test
     public void shouldApplyOrOperatorOnQueryStringContent(){
         SolrQuery solrQuery = subject.forTerm(SemanticQueryTerm.create(QUERY_STRING))
-                .withPropertyNames(PROPERTY_NAME_1, PROPERTY_NAME_2)
                 .withSpecies(SPECIES)
                 .build();
 
