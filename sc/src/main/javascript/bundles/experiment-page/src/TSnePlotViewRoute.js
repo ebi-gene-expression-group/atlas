b56d209ad2dd2854165ec516a1288764c18ec674
barrera <barrera@ebi.ac.uk>
2017-11-03 14:37:07 +0000

    Integrate atlas-bioentity-information in single cell experiment page view

--- sc/src/main/javascript/bundles/experiment-page/src/TSnePlotViewRoute.js
+++ sc/src/main/javascript/bundles/experiment-page/src/TSnePlotViewRoute.js
@@ -3,36 +3,95 @@ import PropTypes from 'prop-types'
 import URI from 'urijs'
 
 import TSnePlotView from 'expression-atlas-experiment-page-tsne-plot'
+import BioentityInformation from 'sc-atlas-bioentity-information'
 
-const TSnePlotViewRoute = (props) => {
+const fetchResponseJson = async (base, endpoint) => {
+    const response = await fetch(URI(endpoint, base).toString())
+    const responseJson = await response.json()
+    return responseJson
+}
 
-  const {location, history} = props
+class TSnePlotViewRoute extends React.Component {
 
-  const updateUrlSearch = (parameter) => {
-    history.push(URI(location.search).setSearch(parameter.name, parameter.value).toString())
+  constructor(props) {
+    super(props)
+    this.state = {
+      geneId: null,
+      bioentityProperties: [],
+      errorMessage: null,
+    }
   }
 
-  const {atlasUrl, resourcesUrl} = props
-  const {suggesterEndpoint, experimentAccession, ks, perplexities} = props
-  const search = URI(location.search).search(true)
+  _fetchAndSetState() {
+    const {atlasUrl} = this.props;
+    const {geneId} = this.state;
+    const search = URI(location.search).search(true)
 
-  return (
-    <TSnePlotView atlasUrl={atlasUrl}
-                  suggesterEndpoint={suggesterEndpoint}
-                  experimentAccession={experimentAccession}
-                  ks={ks}
-                  k={Number(search.k) || props.ks[0]}
-                  highlightClusters={search.clusterId ? JSON.parse(search.clusterId) : []}
-                  perplexities={perplexities}
-                  perplexity={Number(search.perplexity) || props.perplexities[0]}
-                  geneId={search.geneId || ``}
-                  height={600}
-                  resourcesUrl={resourcesUrl}
-                  onSelectGeneId={ (geneId) => { updateUrlSearch({ name: `geneId`, value: geneId }) } }
-                  onChangeK={ (k) => { updateUrlSearch({ name: `k`, value: k }) } }
-                  onChangePerplexity={ (perplexity) => { updateUrlSearch({ name: `perplexity`, value: perplexity }) } }
-    />
-  )
+    const gene = geneId ? geneId : search.geneId
+
+    const bioentityEndPoint = `json/bioEntityInformation/${gene}`;
+
+    if(gene) {
+      return fetchResponseJson(atlasUrl, bioentityEndPoint)
+        .then((responseJson) => {
+            this.setState({
+                geneId : gene,
+                bioentityProperties: responseJson.bioentityProperties,
+                errorMessage: null,
+            })
+        })
+        .catch((reason) => {
+            this.setState({
+                errorMessage: `${reason.name}: ${reason.message}`,
+            })
+        })
+    }
+  }
+
+  componentDidMount() {
+      return this._fetchAndSetState()
+  }
+
+  render() {
+    const {location, history} = this.props
+    const {bioentityProperties} = this.state
+    const updateUrlSearch = (parameter) => {
+        history.push(URI(location.search).setSearch(parameter.name, parameter.value).toString())
+        this._fetchAndSetState()
+    }
+    const {atlasUrl, resourcesUrl} = this.props
+    const {suggesterEndpoint, experimentAccession, ks, perplexities} = this.props
+    const search = URI(location.search).search(true)
+
+    return (
+      <div>
+        <TSnePlotView atlasUrl={atlasUrl}
+                    suggesterEndpoint={suggesterEndpoint}
+                    experimentAccession={experimentAccession}
+                    ks={ks}
+                    k={Number(search.k) || ks[0]}
+                    highlightClusters={search.clusterId ? JSON.parse(search.clusterId) : []}
+                    perplexities={perplexities}
+                    perplexity={Number(search.perplexity) || perplexities[0]}
+                    geneId={search.geneId || ``}
+                    height={600}
+                    resourcesUrl={resourcesUrl}
+                    onSelectGeneId={(geneId) => {
+                        updateUrlSearch({name: `geneId`, value: geneId})
+                    }}
+                    onChangeK={(k) => {
+                        updateUrlSearch({name: `k`, value: k})
+                    }}
+                    onChangePerplexity={(perplexity) => {
+                        updateUrlSearch({name: `perplexity`, value: perplexity})
+                    }}
+        />
+
+        <BioentityInformation bioentityProperties={bioentityProperties}/>
+      </div>
+
+    )
+  }
 }
 
 TSnePlotViewRoute.propTypes = {
