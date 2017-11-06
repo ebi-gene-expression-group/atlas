3d38c737b49c82980b368fe28f7cd00273071a24
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-11-06 16:47:32 +0000

    Code clean up

--- base/src/main/java/uk/ac/ebi/atlas/utils/ResourceUtils.java
+++ base/src/main/java/uk/ac/ebi/atlas/utils/ResourceUtils.java
@@ -5,11 +5,12 @@ import org.springframework.core.io.Resource;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 public class ResourceUtils {
     public static String readPlainTextResource(Resource resource) {
         try (InputStream inputStream = resource.getInputStream()) {
-            return IOUtils.toString(inputStream);
+            return IOUtils.toString(inputStream, StandardCharsets.UTF_8);
         } catch (IOException e) {
             throw new IllegalStateException(e);
         }
