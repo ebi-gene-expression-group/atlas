a2dcd6da39a813f679681f647cca651a32a90415
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-11-18 10:53:35 +0000

    Remove dead code and add another convenience method to escape double quotes

--- base/src/test/java/uk/ac/ebi/atlas/utils/StringArrayUtilTest.java
+++ base/src/test/java/uk/ac/ebi/atlas/utils/StringArrayUtilTest.java
@@ -8,42 +8,29 @@ import static org.junit.Assert.assertThat;
 public class StringArrayUtilTest {
     @Test
     public void filterBySubstring() {
-        assertThat(StringArrayUtil.filterBySubstring(new String[]{"aa", "ab", "ba", "aba", "cc"}, "b"), is(new String[] {"ab", "ba", "aba"}));
+        assertThat(
+                StringArrayUtil.filterBySubstring(new String[]{"aa", "ab", "ba", "aba", "cc"}, "b"),
+                is(new String[] {"ab", "ba", "aba"}));
     }
 
     @Test
     public void substringBefore() {
-        assertThat(StringArrayUtil.substringBefore(new String[]{"foo", "g1.one", "g2.two", "bar"}, "."), is(new String[] {"foo", "g1", "g2", "bar"}));
+        assertThat(
+                StringArrayUtil.substringBefore(new String[]{"foo", "g1.one", "g2.two", "bar"}, "."),
+                is(new String[] {"foo", "g1", "g2", "bar"}));
     }
 
     @Test
     public void indicesOf() {
-        assertThat(StringArrayUtil.indicesOf(new String[]{"foo", "g1.one", "g2.two", "bar"}, "."), is(new int[]{1, 2}));
+        assertThat(
+                StringArrayUtil.indicesOf(new String[]{"foo", "g1.one", "g2.two", "bar"}, "."),
+                is(new int[]{1, 2}));
     }
 
     @Test
     public void indicesOfNoMatch() {
-        assertThat(StringArrayUtil.indicesOf(new String[]{"foo", "g1.one", "g2.two", "bar"}, "z"), is(new int[] {}));
-    }
-
-    @Test
-    public void filterByIndices() {
-        assertThat(StringArrayUtil.filterByIndices(new String[]{"foo", "g1.one", "g2.two", "bar"}, new int[] {0,2,3}),
-                is(new String[] {"foo", "g2.two", "bar"}));
-    }
-
-    @Test
-    public void removeSurroundingQuotes() {
         assertThat(
-                StringArrayUtil.removeSurroundingQuotes(new String[]{"\"foo bar\"", "foo bar", "\"foobar\"", "foobar"}),
-                is(new String[]{"foo bar", "foo bar", "foobar", "foobar"}));
-    }
-
-    @Test
-    public void quotePhrases() {
-        assertThat(
-                StringArrayUtil.quotePhrases(new String[]{"foo bar", "foobar"}),
-                is(new String[]{"\"foo bar\"", "foobar"}));
-
+                StringArrayUtil.indicesOf(new String[]{"foo", "g1.one", "g2.two", "bar"}, "z"),
+                is(new int[] {}));
     }
 }
--- base/src/test/java/uk/ac/ebi/atlas/utils/StringUtilTest.java
+++ base/src/test/java/uk/ac/ebi/atlas/utils/StringUtilTest.java
@@ -1,6 +1,9 @@
 package uk.ac.ebi.atlas.utils;
 
+import org.apache.commons.lang.RandomStringUtils;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -9,15 +12,25 @@ public class StringUtilTest {
     private static final String UBERON_2000098_URL = "http://purl.obolibrary.org/obo/UBERON_2000098";
 
     @Test
-    public void splitAtLastSlash() throws Exception {
+    public void splitAtLastSlash() {
         assertThat(StringUtil.splitAtLastSlash(UBERON_2000098_URL), is(new String[]{"http://purl.obolibrary.org/obo/", "UBERON_2000098"}));
         assertThat(StringUtil.splitAtLastSlash("foobar"), is(new String[]{"foobar"}));
     }
 
     @Test
-    public void quoteIfMoreThanOneWord() throws Exception {
+    public void quoteIfMoreThanOneWord() {
         assertThat(StringUtil.quoteIfMoreThanOneWord("foo bar"), is("\"foo bar\""));
         assertThat(StringUtil.quoteIfMoreThanOneWord("foobar"), is("foobar"));
     }
 
+    @Test
+    public void escapeDoubleQuotes() {
+        String s;
+        do {
+            s = RandomStringUtils.random(20);
+        } while(!s.contains("\""));
+
+        Arrays.stream(StringUtil.escapeDoubleQuotes(s).split("\""))
+                .forEach(str -> assertThat(str.charAt(str.length() - 1), is('\\')));
+    }
 }
