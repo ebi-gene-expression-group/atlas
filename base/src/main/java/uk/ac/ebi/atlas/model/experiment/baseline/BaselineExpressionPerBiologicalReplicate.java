40f0b853f705fb25a7ad6585619e4fb21039c12e
wbazant <wbazant@ebi.ac.uk>
2017-11-29 10:54:41 +0000

    Add more stats to the expression payload. We'll use it to draw beatiful beautiful heatmaps. Note this breaks heatmap 3.7.7 : what's in the value field has just changed.

--- base/src/main/java/uk/ac/ebi/atlas/model/experiment/baseline/BaselineExpressionPerBiologicalReplicate.java
+++ base/src/main/java/uk/ac/ebi/atlas/model/experiment/baseline/BaselineExpressionPerBiologicalReplicate.java
@@ -5,12 +5,14 @@ import com.google.gson.JsonObject;
 
 import org.apache.commons.math.stat.descriptive.rank.Median;
 import org.apache.commons.math.stat.descriptive.rank.Percentile;
+import org.apache.commons.math.util.MathUtils;
 import uk.ac.ebi.atlas.model.BiologicalReplicate;
 import uk.ac.ebi.atlas.model.Expression;
 
 import java.text.MessageFormat;
 import java.util.Map;
 import java.util.Objects;
+import java.util.stream.DoubleStream;
 
 public class BaselineExpressionPerBiologicalReplicate implements Expression {
 
@@ -21,7 +23,11 @@ public class BaselineExpressionPerBiologicalReplicate implements Expression {
     }
 
     private double[] expressionValuesSorted() {
-        return data.values().stream().mapToDouble(BaselineExpression::getLevel).sorted().toArray();
+        return expressionValues().sorted().toArray();
+    }
+
+    private DoubleStream expressionValues() {
+        return data.values().stream().mapToDouble(BaselineExpression::getLevel);
     }
 
     @Override
@@ -31,14 +37,14 @@ public class BaselineExpressionPerBiologicalReplicate implements Expression {
 
     @Override
     public JsonObject toJson() {
-        /*
-        WIP
-        Indicate when
-         */
+        if (data.size() == 0) return new JsonObject();
         JsonObject result = new JsonObject();
 
         double[] expressionValuesSorted = expressionValuesSorted();
+        double total = expressionValues().sum();
+
         JsonObject stats = new JsonObject();
+        stats.addProperty("total", total); //not corresponding to any UI feature but will be helpful
         stats.addProperty("min", expressionValuesSorted[0]);
         stats.addProperty("lower_quartile", new Percentile(0.25).evaluate(expressionValuesSorted));
         stats.addProperty("median", new Median().evaluate(expressionValuesSorted));
@@ -53,7 +59,13 @@ public class BaselineExpressionPerBiologicalReplicate implements Expression {
             JsonArray assays = new JsonArray();
             e.getKey().assaysAnalyzedForThisDataColumn().stream().sorted().forEach(assays::add);
             o.add("assays", assays);
-            o.addProperty("value", e.getValue().getLevel());
+            JsonObject value = new JsonObject();
+            double expressionLevel = e.getValue().getLevel();
+            value.addProperty("expression_absolute_units", expressionLevel);
+            value.addProperty("expression_as_fraction_of_total",(total == 0) ? 0 : MathUtils.round(expressionLevel / total,3));
+            value.addProperty("expression_dominance", dominanceAmongRelatedValues(expressionValuesSorted, expressionLevel).name());
+
+            o.add("value", value);
             return o;
         }).forEach(values::add);
         result.add("values", values);
@@ -77,5 +89,29 @@ public class BaselineExpressionPerBiologicalReplicate implements Expression {
     @Override
     public String toString() {
         return MessageFormat.format("BaselineExpressionPerBiologicalReplicate{data={0}}", data);
+    }
+
+    /*
+    assumes allValues contains value
+     */
+    DOMINANCE_AMONG_RELATED_VALUES dominanceAmongRelatedValues(double[] allValuesSortedInIncreasingOrder, double value){
+        if(allValuesSortedInIncreasingOrder.length == 0 || value == 0) return DOMINANCE_AMONG_RELATED_VALUES.absent;
+        if(allValuesSortedInIncreasingOrder.length == 1) return DOMINANCE_AMONG_RELATED_VALUES.dominant;
+
+        double topValue = allValuesSortedInIncreasingOrder[allValuesSortedInIncreasingOrder.length - 1];
+        double valueFollowingTopValue = allValuesSortedInIncreasingOrder[allValuesSortedInIncreasingOrder.length - 2];
+        /*
+        Definition quoted by Nuno: a transcript is dominant if it is twice as expressed as the next value
+         */
+        if(value == topValue && value > valueFollowingTopValue * 2 ) return DOMINANCE_AMONG_RELATED_VALUES.dominant;
+
+        return DOMINANCE_AMONG_RELATED_VALUES.present;
+    }
+
+    /*
+    Please do not rename my values without checking if there is a dependency on name() values in the UI
+     */
+    enum DOMINANCE_AMONG_RELATED_VALUES {
+        dominant, present, absent
     }
 }
