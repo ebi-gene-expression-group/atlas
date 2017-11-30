ba7742fc558ab1d42af954bbb55045e195ebf83b
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-11-30 15:10:43 +0000

    Remove scxa_analytics primary key [#152395202]

--- atlas-misc/scripts/db_updates/single-cell/20170607-create-analytics-table.sql
+++ atlas-misc/scripts/db_updates/single-cell/20170607-create-analytics-table.sql
@@ -4,6 +4,7 @@ CREATE TABLE scxa_analytics (
     experiment_accession VARCHAR(255) NOT NULL,
     gene_id VARCHAR(255) NOT NULL,
     cell_id  VARCHAR(255) NOT NULL,
-    expression_level FLOAT(53),
-    PRIMARY KEY (experiment_accession, gene_id, cell_id)
+    expression_level DOUBLE PRECISION
 );
+
+-- CREATE UNIQUE INDEX scxa_analytics__uindex ON scxa_analytics (experiment_accession, gene_id, cell_id); --
