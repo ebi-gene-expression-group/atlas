53120f69c3997800b5dfa985be72367cf9b01c7a
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-11-30 00:50:19 +0000

    Revert: Remove expression level and log2-fold change/p-value threshold validation

--- base/src/main/java/uk/ac/ebi/atlas/model/analyticsindex/DifferentialExperimentDataPoint.java
+++ base/src/main/java/uk/ac/ebi/atlas/model/analyticsindex/DifferentialExperimentDataPoint.java
@@ -15,9 +15,7 @@ public class DifferentialExperimentDataPoint extends ExperimentDataPoint{
         super(differentialAnalytics.getGeneId(), experiment,conditionSearch);
 
         propertyMap.put("factors", experiment.getExperimentDesign().getFactorHeaders());
-        if (differentialAnalytics.getFoldChange() != 0.0) {
-            propertyMap.put("regulation", Regulation.valueOf(differentialAnalytics.getFoldChange()).toString());
-        }
+        propertyMap.put("regulation", Regulation.valueOf(differentialAnalytics.getFoldChange()).toString());
         propertyMap.put("contrast_id", differentialAnalytics.getContrastId());
         propertyMap.put("num_replicates", numReplicates);
         propertyMap.put("fold_change", differentialAnalytics.getFoldChange());
