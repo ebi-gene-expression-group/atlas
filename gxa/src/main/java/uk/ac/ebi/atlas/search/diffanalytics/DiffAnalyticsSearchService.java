421a6dee43dee6f7e6b46a30870eb19fb3538644
Haider Iqbal <haideri@ebi.ac.uk>
2017-11-30 16:44:04 +0000

    Revert "Task 148227051 (Update the /das/s4/features endpoint) (#43)" (#46)
    This reverts commit f34cc46d773087d3f34e6cd3b8d4180921e907cf.

--- gxa/src/main/java/uk/ac/ebi/atlas/search/diffanalytics/DiffAnalyticsSearchService.java
+++ gxa/src/main/java/uk/ac/ebi/atlas/search/diffanalytics/DiffAnalyticsSearchService.java
@@ -1,11 +1,13 @@
 package uk.ac.ebi.atlas.search.diffanalytics;
 
+import org.springframework.context.annotation.Scope;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.util.List;
 
 @Named
+@Scope("prototype")
 public class DiffAnalyticsSearchService {
 
     private final DiffAnalyticsDao diffAnalyticsDao;
