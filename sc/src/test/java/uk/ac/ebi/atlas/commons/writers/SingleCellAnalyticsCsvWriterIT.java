94a8132976bea2811da1a859f62f7ccd211ea842
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-12-01 13:10:44 +0000

    Use E-MTAB-5061 from now on as our poster-child SC experiment; fix test set-up and have at least one so that Maven won’t complain about it

--- sc/src/test/java/uk/ac/ebi/atlas/commons/writers/SingleCellAnalyticsCsvWriterIT.java
+++ sc/src/test/java/uk/ac/ebi/atlas/commons/writers/SingleCellAnalyticsCsvWriterIT.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import uk.ac.ebi.atlas.experimentimport.analytics.singlecell.SingleCellAnalyticsStream;
 import uk.ac.ebi.atlas.resource.DataFileHub;
 
@@ -13,13 +14,19 @@ import javax.inject.Inject;
 import java.io.FileWriter;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("classpath:applicationContext.xml")
+@WebAppConfiguration
+@ContextConfiguration(locations = {"classpath:applicationContext.xml", "classpath:dispatcher-servlet.xml"})
 public class SingleCellAnalyticsCsvWriterIT {
 
     @Inject
     DataFileHub dataFileHub;
 
     SingleCellAnalyticsCsvWriter subject;
+
+    @Test
+    public void test() {
+
+    }
 
     @Ignore
     public void write() throws Exception {
