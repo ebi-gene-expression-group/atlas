405d44cbdece09db9dc7517846f1a35aca7f03ed
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2018-01-09 14:00:04 +0100

    Test SolrCloudCollectionProxyFactory

---
+++ base/src/test/java/uk/ac/ebi/atlas/solr/util/SolrCloudCollectionProxyFactoryIT.java
@@ -0,0 +1,26 @@
+package uk.ac.ebi.atlas.solr.util;
+
+import org.apache.solr.client.solrj.SolrQuery;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.inject.Inject;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext.xml")
+public class SolrCloudCollectionProxyFactoryIT {
+
+    @Inject
+    private SolrCloudCollectionProxyFactory subject;
+
+    @Test
+    public void createAnalyticsCollectionProxy() {
+        assertThat(subject.createAnalyticsCollectionProxy().query(new SolrQuery("*:*")).getResults().size())
+                .isGreaterThan(0);
+    }
+}
