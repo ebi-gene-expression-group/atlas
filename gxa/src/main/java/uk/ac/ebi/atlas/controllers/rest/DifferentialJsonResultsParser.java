0059a3d7815a6e78603dc4787683cc849d00c20d
Haider Iqbal <haideri@ebi.ac.uk>
2018-01-30 10:19:31 +0000

    Some minor fixes suggested in PR review

--- gxa/src/main/java/uk/ac/ebi/atlas/controllers/rest/DifferentialJsonResultsParser.java
+++ gxa/src/main/java/uk/ac/ebi/atlas/controllers/rest/DifferentialJsonResultsParser.java
@@ -4,6 +4,9 @@ import com.google.common.collect.ImmutableList;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import uk.ac.ebi.atlas.controllers.JsonExceptionHandlingController;
 import uk.ac.ebi.atlas.model.experiment.differential.Contrast;
 import uk.ac.ebi.atlas.model.experiment.differential.DifferentialExpression;
@@ -13,6 +16,7 @@ import uk.ac.ebi.atlas.trader.ContrastTrader;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.validation.constraints.Null;
 import java.util.Optional;
 import java.util.function.Function;
 
@@ -21,6 +25,8 @@ public class DifferentialJsonResultsParser extends JsonExceptionHandlingControll
 
     private ContrastTrader contrastTrader;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(DifferentialJsonResultsParser.class);
+
     @Inject
     public DifferentialJsonResultsParser(ContrastTrader contrastTrader) {
         this.contrastTrader = contrastTrader;
@@ -28,56 +34,71 @@ public class DifferentialJsonResultsParser extends JsonExceptionHandlingControll
 
     public ImmutableList<DiffAnalytics> parseDifferentialResults(JsonObject obj) {
 
-        ImmutableList.Builder<DiffAnalytics> diffAnalyticsListBuilder = ImmutableList.builder();
-        JsonArray results = obj.getAsJsonArray("results");
+        try {
+            ImmutableList.Builder<DiffAnalytics> diffAnalyticsListBuilder = ImmutableList.builder();
+            JsonArray results = obj.getAsJsonArray("results");
 
-        for (JsonElement e : results) {
+            for (JsonElement e : results) {
 
-            DiffAnalytics diffAnalytics = buildDiffAnalyticsObject(e);
+                DiffAnalytics diffAnalytics = buildDiffAnalyticsObject(e);
 
-            if (Optional.ofNullable(diffAnalytics).isPresent()) {
-                diffAnalyticsListBuilder.add(diffAnalytics);
+                if (Optional.ofNullable(diffAnalytics).isPresent()) {
+                    diffAnalyticsListBuilder.add(diffAnalytics);
+                }
             }
+
+            return diffAnalyticsListBuilder.build();
         }
-
-        return diffAnalyticsListBuilder.build();
-
+        catch(JsonParseException e) {
+            throw new RuntimeException("Input Json is missing a results field OR results field is not an Array",e);
+        }
     }
 
     private DiffAnalytics buildDiffAnalyticsObject(JsonElement element) {
 
-        JsonObject resultObj = element.getAsJsonObject();
+        try {
 
-        String experimentAccession = resultObj.get("experiment_accession").getAsString();
+            JsonObject resultObj = element.getAsJsonObject();
 
-        String bioentityIdentifier = resultObj.get("bioentity_identifier").getAsString();
+            String experimentAccession = resultObj.get("experiment_accession").getAsString();
 
-        String contrastId = resultObj.get("contrast_id").getAsString();
+            String bioentityIdentifier = resultObj.get("bioentity_identifier").getAsString();
 
-        Double pValue = resultObj.get("p_value").getAsDouble();
+            String contrastId = resultObj.get("contrast_id").getAsString();
 
-        Double foldChange = resultObj.get("fold_change").getAsDouble();
+            Double pValue = resultObj.get("p_value").getAsDouble();
 
-        String keywordSymbol = Optional.of(resultObj.get("keyword_symbol")).map(JsonElement::getAsString).orElse(bioentityIdentifier);
+            Double foldChange = resultObj.get("fold_change").getAsDouble();
 
-        Optional<Double> tStatistic =
-                Optional.ofNullable(resultObj.get("t_statistic"))
-                        .map(JsonElement::getAsDouble);
+            String keywordSymbol = Optional.of(resultObj.get("keyword_symbol")).map(JsonElement::getAsString).orElse(bioentityIdentifier);
 
-        Function<Double, DifferentialExpression> microarrayExpressionMapper = ts -> new MicroarrayExpression(pValue, foldChange, ts);
-        DifferentialExpression expression = tStatistic.map(microarrayExpressionMapper).orElse(new DifferentialExpression(pValue, foldChange));
+            Optional<Double> tStatistic =
+                    Optional.ofNullable(resultObj.get("t_statistic"))
+                            .map(JsonElement::getAsDouble);
 
-        Contrast contrast = contrastTrader.getContrast(experimentAccession, contrastId);
+            Function<Double, DifferentialExpression> microarrayExpressionMapper = ts -> new MicroarrayExpression(pValue, foldChange, ts);
+            DifferentialExpression expression = tStatistic.map(microarrayExpressionMapper).orElse(new DifferentialExpression(pValue, foldChange));
+
+            Contrast contrast = contrastTrader.getContrast(experimentAccession, contrastId);
 
 
             /*  Data coming from database has species name following Binomial nomenclature notation for naming species i.e. first character of species to be capital
                 but data in solr doesn't follow this notation so to make it consistent first character of species is uppercase
             */
 
-        String species = resultObj.get("species").getAsString();
-        species = species.substring(0, 1).toUpperCase() + species.substring(1);
+            String species = resultObj.get("species").getAsString();
+            species = species.substring(0, 1).toUpperCase() + species.substring(1);
 
-        return new DiffAnalytics(bioentityIdentifier, keywordSymbol, experimentAccession, expression, species, contrast);
+            return new DiffAnalytics(bioentityIdentifier, keywordSymbol, experimentAccession, expression, species, contrast);
+        }
+
+        /*NullPointerException is catched here to handle the case where invalid
+          Json object is skipped and the program execution runs as usual*/
+
+        catch (NullPointerException e) {
+            LOGGER.error("Error adding differential result: {}", element);
+            return null;
+        }
     }
 
 
--- gxa/src/test/java/uk/ac/ebi/atlas/controllers/rest/DifferentialJsonResultsParserTest.java
+++ gxa/src/test/java/uk/ac/ebi/atlas/controllers/rest/DifferentialJsonResultsParserTest.java
@@ -24,11 +24,9 @@ import static org.junit.Assert.assertThat;
 @RunWith(MockitoJUnitRunner.class)
 public class DifferentialJsonResultsParserTest {
 
-    private DifferentialJsonResultsParser subject;
-
     @Rule
-    public ExpectedException thrown= ExpectedException.none();
-
+    public ExpectedException thrown = ExpectedException.none();
+    private DifferentialJsonResultsParser subject;
     @Mock
     private ContrastTrader contrastTraderMock;
 
@@ -44,7 +42,7 @@ public class DifferentialJsonResultsParserTest {
         thrown.expect(RuntimeException.class);
 
         JsonObject result = new JsonObject();
-        result.addProperty("results",1);
+        result.addProperty("results", 1);
 
         List<DiffAnalytics> expression = subject.parseDifferentialResults(result);
     }
@@ -52,33 +50,33 @@ public class DifferentialJsonResultsParserTest {
     @Test
     public void emptyJsonResultArrayProducesEmptyList() throws Exception {
 
-        JsonObject result = buildJsonObject(0);
+        JsonObject result = buildJsonObject(0,false);
 
         List<DiffAnalytics> expression = subject.parseDifferentialResults(result);
-        assertThat(expression,hasSize(0));
+        assertThat(expression, hasSize(0));
     }
 
     @Test
     public void numberOfJsonObjectsInResultArrayProducesEqualLengthList() throws Exception {
 
-        JsonObject result = buildJsonObject(3);
+        JsonObject result = buildJsonObject(3,false);
 
         List<DiffAnalytics> expression = subject.parseDifferentialResults(result);
-        assertThat(expression,hasSize(3));
+        assertThat(expression, hasSize(3));
     }
 
     @Test
     public void secondJsonObjectsInResultArrayEqualsSecondElementOfList() throws Exception {
 
-        JsonObject result = buildJsonObject(4);
+        JsonObject result = buildJsonObject(4, false);
 
         List<DiffAnalytics> expression = subject.parseDifferentialResults(result);
 
-        assertThat(expression.get(1).getExperimentAccession(),isA(String.class));
-        assertThat(expression.get(1).getBioentityId(),isA(String.class));
-        assertThat(expression.get(1).getSpecies(),isA(String.class));
-        assertThat(expression.get(1).getExpression().getPValue(),isA(Double.class));
-        assertThat(expression.get(1).getExpression().getFoldChange(),isA(Double.class));
+        assertThat(expression.get(1).getExperimentAccession(), isA(String.class));
+        assertThat(expression.get(1).getBioentityId(), isA(String.class));
+        assertThat(expression.get(1).getSpecies(), isA(String.class));
+        assertThat(expression.get(1).getExpression().getPValue(), isA(Double.class));
+        assertThat(expression.get(1).getExpression().getFoldChange(), isA(Double.class));
     }
 
     @Test
@@ -86,10 +84,10 @@ public class DifferentialJsonResultsParserTest {
 
         JsonObject testObject = new JsonObject();
 
-        testObject.addProperty("experiment_accession","e"+4);
-        testObject.addProperty("contrast_id", "c"+4);
-        testObject.addProperty("bioentity_identifier", "id"+4);
-        testObject.addProperty("species", "org"+4);
+        testObject.addProperty("experiment_accession", "e" + 4);
+        testObject.addProperty("contrast_id", "c" + 4);
+        testObject.addProperty("bioentity_identifier", "id" + 4);
+        testObject.addProperty("species", "org" + 4);
         testObject.addProperty("p_value", 1.14);
         testObject.addProperty("fold_change", -1.14);
         testObject.addProperty("keyword_symbol", 4);
@@ -104,28 +102,52 @@ public class DifferentialJsonResultsParserTest {
 
         List<DiffAnalytics> expression = subject.parseDifferentialResults(result);
 
-        assertThat(expression,hasSize(1));
+        assertThat(expression, hasSize(1));
     }
 
-    private JsonObject buildJsonObject(int arraySize) {
+    @Test
+    public void rejectInvalidJsonObjectsInResultArray() throws Exception {
+
+        JsonObject result = buildJsonObject(3, true);
+
+        List<DiffAnalytics> expression = subject.parseDifferentialResults(result);
+
+        assertThat(expression, hasSize(3));
+    }
+
+    private JsonObject buildJsonObject(int arraySize, boolean addInvalidElement) {
 
         JsonObject result = new JsonObject();
         JsonArray results = new JsonArray();
+        JsonObject testObject = new JsonObject();
         RandomStringUtils randomString = new RandomStringUtils();
         Random random = new Random();
 
-        for(int i = 1; i<=arraySize; i++) {
+        for (int i = 1; i <= arraySize; i++) {
 
             JsonObject element = new JsonObject();
-            element.addProperty("experiment_accession", randomString.random(5)+ i);
-            element.addProperty("contrast_id", randomString.random(7)+ i);
-            element.addProperty("bioentity_identifier", randomString.random(3)+ i);
-            element.addProperty("species", randomString.random(4)+ i);
+            element.addProperty("experiment_accession", randomString.random(5) + i);
+            element.addProperty("contrast_id", randomString.random(7) + i);
+            element.addProperty("bioentity_identifier", randomString.random(3) + i);
+            element.addProperty("species", randomString.random(4) + i);
             element.addProperty("p_value", random.nextDouble());
             element.addProperty("fold_change", random.nextDouble());
             element.addProperty("t_statistic", random.nextDouble());
             element.addProperty("keyword_symbol", i);
             results.add(element);
+        }
+
+        if (addInvalidElement) {
+            testObject.addProperty("experiment accession", randomString.random(5));
+            testObject.addProperty("contrast_id", randomString.random(10));
+            testObject.addProperty("bioentity_identifier", randomString.random(15));
+            testObject.addProperty("species", randomString.random(6));
+            testObject.addProperty("p_value", random.nextDouble());
+            testObject.addProperty("fold_change", random.nextDouble());
+            testObject.addProperty("t_statistic", random.nextDouble());
+            testObject.addProperty("keyword_symbol", 4);
+
+            results.add(testObject);
         }
 
         result.add("results", results);
