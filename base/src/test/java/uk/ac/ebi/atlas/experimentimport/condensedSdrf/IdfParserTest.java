ccd8dfd93b71e8d1264d811fb23b29992013ac68
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2018-02-12 16:34:42 +0000

    Use streams sensibly :/ Fixes build http://bar:8085/browse/ATLAS-BASECIDEVTASKS0-4

--- base/src/test/java/uk/ac/ebi/atlas/experimentimport/condensedSdrf/IdfParserTest.java
+++ base/src/test/java/uk/ac/ebi/atlas/experimentimport/condensedSdrf/IdfParserTest.java
@@ -2,6 +2,7 @@ package uk.ac.ebi.atlas.experimentimport.condensedSdrf;
 
 import com.google.common.collect.ImmutableSet;
 import org.apache.commons.lang3.tuple.Pair;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -14,14 +15,15 @@ import java.util.stream.Stream;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
-import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class IdfParserTest {
 
     private static final String E_MTAB_513 = "E-MTAB-513";
 
-    private static final String E_MTAB_513_TITLE = "RNA-Seq of human individual tissues and mixture of 16 tissues (Illumina Body Map)";
+    private static final String E_MTAB_513_TITLE =
+            "RNA-Seq of human individual tissues and mixture of 16 tissues (Illumina Body Map)";
     private static final String[] E_MTAB_513_PUBMED_IDS_ARRAY = {"22496456", "22955988", "23258890"};
     private static final ImmutableSet<String> E_MTAB_513_PUBMED_IDS = ImmutableSet.copyOf(E_MTAB_513_PUBMED_IDS_ARRAY);
 
@@ -31,7 +33,10 @@ public class IdfParserTest {
     };
 
     @Mock
-    TsvReader tsvReaderMock;
+    private TsvReader titleIdfReaderMock;
+
+    @Mock
+    private TsvReader pubmedsIdfReaderMock;
 
     @Mock
     private IdfReaderFactory idfReaderFactoryMock;
@@ -39,59 +44,52 @@ public class IdfParserTest {
     @InjectMocks
     private IdfParser subject;
 
+    @Before
+    public void setUp() {
+        when(idfReaderFactoryMock.create(E_MTAB_513)).thenReturn(titleIdfReaderMock, pubmedsIdfReaderMock);
+    }
+
     @Test
     public void parse() {
-        //given
-        given(idfReaderFactoryMock.create(E_MTAB_513)).willReturn(tsvReaderMock);
-        given(tsvReaderMock.stream()).willReturn(Stream.of(E_MTAB_513_IDF_TXT));
+        when(titleIdfReaderMock.stream()).thenReturn(Stream.of(E_MTAB_513_IDF_TXT));
+        when(pubmedsIdfReaderMock.stream()).thenReturn(Stream.of(E_MTAB_513_IDF_TXT));
 
-        //when
         Pair<String, ImmutableSet<String>> idfParserOutput = subject.parse(E_MTAB_513);
 
-        //then
         assertThat(idfParserOutput.getLeft(), is(E_MTAB_513_TITLE));
         assertThat(idfParserOutput.getRight(), is(E_MTAB_513_PUBMED_IDS));
     }
 
     @Test
     public void parseNoPubmedIds() {
-        //given
-        given(idfReaderFactoryMock.create(E_MTAB_513)).willReturn(tsvReaderMock);
-        // https://stackoverflow.com/questions/48625611/how-can-i-create-a-streamstring-with-only-one-element-with-stream-of
-        given(tsvReaderMock.stream()).willReturn(Stream.<String[]>of(E_MTAB_513_IDF_TXT[0]));
+        when(titleIdfReaderMock.stream()).thenReturn(Stream.<String[]>of(E_MTAB_513_IDF_TXT[0]));
+        when(pubmedsIdfReaderMock.stream()).thenReturn(Stream.<String[]>of(E_MTAB_513_IDF_TXT[0]));
 
-        //when
         Pair<String, ImmutableSet<String>> idfParserOutput = subject.parse(E_MTAB_513);
 
-        //then
         assertThat(idfParserOutput.getLeft(), is(E_MTAB_513_TITLE));
         assertThat(idfParserOutput.getRight(), is(empty()));
     }
 
     @Test
     public void parseNoTitle() {
-        //given
-        given(idfReaderFactoryMock.create(E_MTAB_513)).willReturn(tsvReaderMock);
-        given(tsvReaderMock.stream()).willReturn(Stream.<String[]>of(E_MTAB_513_IDF_TXT[1]));
+        when(titleIdfReaderMock.stream()).thenReturn(Stream.<String[]>of(E_MTAB_513_IDF_TXT[1]));
+        when(pubmedsIdfReaderMock.stream()).thenReturn(Stream.<String[]>of(E_MTAB_513_IDF_TXT[1]));
 
-        //when
         Pair<String, ImmutableSet<String>> idfParserOutput = subject.parse(E_MTAB_513);
 
-        //then
         assertThat(idfParserOutput.getLeft().isEmpty(), is(true));
         assertThat(idfParserOutput.getRight(), is(E_MTAB_513_PUBMED_IDS));
     }
 
     @Test
     public void parseNothing() {
-        //given
-        given(idfReaderFactoryMock.create(E_MTAB_513)).willReturn(tsvReaderMock);
-        given(tsvReaderMock.stream()).willReturn(Stream.empty());
+        when(titleIdfReaderMock.stream()).thenReturn(Stream.empty());
+        when(pubmedsIdfReaderMock.stream()).thenReturn(Stream.empty());
 
-        //when
+
         Pair<String, ImmutableSet<String>> idfParserOutput = subject.parse(E_MTAB_513);
 
-        //then
         assertThat(idfParserOutput.getLeft().isEmpty(), is(true));
         assertThat(idfParserOutput.getRight(), is(empty()));
     }
--- base/src/test/java/uk/ac/ebi/atlas/trader/ExperimentDesignParserTest.java
+++ base/src/test/java/uk/ac/ebi/atlas/trader/ExperimentDesignParserTest.java
@@ -23,23 +23,49 @@ import static uk.ac.ebi.atlas.trader.ExperimentDesignParser.extractMatchingConte
 public class ExperimentDesignParserTest {
 
     private static final String EXPERIMENT_ACCESSION = "ACCESSION";
+    private static final String ASSAY = "Assay";
+    private static final String ARRAY = "Array";
+
     private static final String ASSAY_ACCESSION_1 = "C1";
+    private static final String A_AFFY_35 = "A-AFFY-35";
     private static final String RD_INSTAR_LARVA = "3rd instar larva";
+    private static final String SPECIES_1 = "Drosophila melanogaster";
+    private static final String CYC_C_MUTANT = "cycC mutant";
+
     private static final String ASSAY_ACCESSION_2 = "WT3";
     private static final String DUMMY = "dummy";
     private static final String SAMPLE_NAME_1 = "DevelopmentalStage";
     private static final String SAMPLE_NAME_2 = "StrainOrLine";
     private static final String GENOTYPE = "GENOTYPE";
     private static final String OREGON_R = "Oregon R";
-    private static final String CYC_C_MUTANT = "cycC mutant";
-    private static final String A_AFFY_35 = "A-AFFY-35";
-    private static final String ASSAY = "Assay";
-    private static final String ARRAY = "Array";
-    private static final String SPECIES_1 = "Drosophila melanogaster";
 
-    private static final String[] HEADER_LINE = new String[]{ASSAY, ARRAY, "Sample Characteristic[DevelopmentalStage]", "Sample Characteristic[Genotype]", "Sample Characteristic[Organism]", "Sample Characteristic[StrainOrLine]", "Factor Value[GENOTYPE]"};
-    private static final String[] FIRST_LINE = new String[]{ASSAY_ACCESSION_1, A_AFFY_35, RD_INSTAR_LARVA, "w1118; +; cycCY5", SPECIES_1, "", CYC_C_MUTANT};
-    private static final String[] LAST_LINE = new String[]{ASSAY_ACCESSION_2, A_AFFY_35, RD_INSTAR_LARVA, "wild_type", SPECIES_1, OREGON_R, "wild_type"};
+    private static final String[] HEADER_LINE = new String[] {
+            ASSAY,
+            ARRAY,
+            "Sample Characteristic[DevelopmentalStage]",
+            "Sample Characteristic[Genotype]",
+            "Sample Characteristic[Organism]",
+            "Sample Characteristic[StrainOrLine]",
+            "Factor Value[GENOTYPE]" };
+
+    private static final String[] FIRST_LINE = new String[] {
+            ASSAY_ACCESSION_1,
+            A_AFFY_35,
+            RD_INSTAR_LARVA,
+            "w1118; +; cycCY5",
+            SPECIES_1,
+            "",
+            CYC_C_MUTANT };
+
+    private static final String[] LAST_LINE = new String[] {
+            ASSAY_ACCESSION_2,
+            A_AFFY_35,
+            RD_INSTAR_LARVA,
+            "wild_type",
+            SPECIES_1,
+            OREGON_R,
+            "wild_type" };
+
     private static final List<String[]> DATA = Lists.newArrayList(HEADER_LINE, FIRST_LINE, LAST_LINE);
 
     private static MockDataFileHub dataFileHub;
@@ -47,25 +73,25 @@ public class ExperimentDesignParserTest {
     private ExperimentDesignParser subject;
 
     @BeforeClass
-    public static void setUpClass() throws Exception {
+    public static void setUpClass() {
         dataFileHub = MockDataFileHub.create();
     }
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         dataFileHub.addExperimentDesignFile(EXPERIMENT_ACCESSION, DATA);
         subject = new ExperimentDesignParser(dataFileHub);
     }
 
     @Test
-    public void testParseHeaders() throws Exception {
+    public void testParseHeaders() {
         ExperimentDesign experimentDesign = subject.parse(EXPERIMENT_ACCESSION);
         assertThat(experimentDesign.getFactorHeaders(), IsIterableContainingInOrder.contains(GENOTYPE));
         assertThat(experimentDesign.getSampleHeaders(), IsIterableContainingInOrder.contains(SAMPLE_NAME_1, "Genotype", "Organism", SAMPLE_NAME_2));
     }
 
     @Test
-    public void testParseFactors() throws Exception {
+    public void testParseFactors() {
         ExperimentDesign experimentDesign = subject.parse(EXPERIMENT_ACCESSION);
         assertThat(experimentDesign.getFactorValue(ASSAY_ACCESSION_1, GENOTYPE), is(CYC_C_MUTANT));
         assertThat(experimentDesign.getFactorValue(ASSAY_ACCESSION_1, DUMMY), is(nullValue()));
@@ -73,7 +99,7 @@ public class ExperimentDesignParserTest {
     }
 
     @Test
-    public void testParseSamples() throws Exception {
+    public void testParseSamples() {
         ExperimentDesign experimentDesign = subject.parse(EXPERIMENT_ACCESSION);
         assertThat(experimentDesign.getSampleCharacteristicValue(ASSAY_ACCESSION_1, SAMPLE_NAME_1), is(RD_INSTAR_LARVA));
         assertThat(experimentDesign.getSampleCharacteristicValue(ASSAY_ACCESSION_2, SAMPLE_NAME_1), is(RD_INSTAR_LARVA));
@@ -84,33 +110,33 @@ public class ExperimentDesignParserTest {
     }
 
     @Test
-    public void testAssays() throws Exception {
+    public void testAssays() {
         ExperimentDesign experimentDesign = subject.parse(EXPERIMENT_ACCESSION);
         assertThat(experimentDesign.getArrayDesign(ASSAY_ACCESSION_1), is(A_AFFY_35));
         assertThat(experimentDesign.getArrayDesign(ASSAY_ACCESSION_2), is(A_AFFY_35));
     }
 
     @Test
-    public void testAssayHeaders() throws Exception {
+    public void testAssayHeaders() {
         ExperimentDesign experimentDesign = subject.parse(EXPERIMENT_ACCESSION);
         assertThat(experimentDesign.getAssayHeaders(), IsIterableContainingInOrder.contains(ASSAY, ARRAY));
     }
 
     @Test
-    public void testGetAllRunOrAssay() throws Exception {
+    public void testGetAllRunOrAssay() {
         ExperimentDesign experimentDesign = subject.parse(EXPERIMENT_ACCESSION);
         assertThat(experimentDesign.getAllRunOrAssay(), IsIterableContainingInOrder.contains(ASSAY_ACCESSION_1, ASSAY_ACCESSION_2));
     }
 
     @Test
-    public void testGetSpeciesForAssays() throws Exception {
+    public void testGetSpeciesForAssays() {
         ExperimentDesign experimentDesign = subject.parse(EXPERIMENT_ACCESSION);
         String species = experimentDesign.getSpeciesForAssays(Sets.newHashSet(ASSAY_ACCESSION_1, ASSAY_ACCESSION_2));
         assertThat(species, is(SPECIES_1));
     }
 
     @Test
-    public void testExtractGroup1Match() throws Exception {
+    public void testExtractGroup1Match() {
         String matchingString = extractMatchingContent("Assay Bello", SAMPLE_COLUMN_HEADER_PATTERN);
         assertThat(matchingString, is(nullValue()));
 
