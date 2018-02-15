90fc43fbc20172a2212562523ebb77c01dc2f929
Haider Iqbal <haideri@ebi.ac.uk>
2018-02-15 15:53:19 +0000

    Add support for Plant Reactome Ids

--- base/src/test/java/uk/ac/ebi/atlas/species/SpeciesInferrerIT.java
+++ base/src/test/java/uk/ac/ebi/atlas/species/SpeciesInferrerIT.java
@@ -18,13 +18,16 @@ public class SpeciesInferrerIT {
 
     static final String HOMO_SAPIENS = "homo sapiens";
     static final String ARABIDOPSIS_THALIANA = "arabidopsis thaliana";
+    static final String GLYCINE_MAX = "glycine max";
 
     static final SemanticQueryTerm HUMAN_REACTOME_TERM = SemanticQueryTerm.create("R-HSA-597592", "pathwayid");
+    static final SemanticQueryTerm PLANT_REACTOME_TERM = SemanticQueryTerm.create("R-GMA-2744341", "pathwayid");
     static final SemanticQueryTerm LEAF_TERM = SemanticQueryTerm.create("leaf");
 
     static final SemanticQuery EMPTY_QUERY = SemanticQuery.create();
     static final SemanticQuery PLANT_CONDITION_QUERY = SemanticQuery.create(LEAF_TERM);
     static final SemanticQuery HUMAN_GENE_QUERY = SemanticQuery.create(HUMAN_REACTOME_TERM);
+    static final SemanticQuery PLANT_GENE_QUERY = SemanticQuery.create(PLANT_REACTOME_TERM);
     static final SemanticQuery MIXED_SPECIES_GENE_QUERY = SemanticQuery.create(SemanticQueryTerm.create("OS03G0852700", "ensgene"), SemanticQueryTerm.create("ENSMUSG00000002055", "ensgene"));
 
     @Inject
@@ -34,9 +37,12 @@ public class SpeciesInferrerIT {
     public void inferSpeciesForGeneQuery() throws Exception {
         Species species1 = subject.inferSpeciesForGeneQuery(HUMAN_GENE_QUERY);
         Species species2 = subject.inferSpeciesForGeneQuery(HUMAN_GENE_QUERY, "");
+        Species species3 = subject.inferSpeciesForGeneQuery(PLANT_GENE_QUERY);
 
         assertThat(species1.getReferenceName(), is(HOMO_SAPIENS));
         assertThat(species2.getReferenceName(), is(HOMO_SAPIENS));
+        assertThat(species3.getReferenceName(), is(GLYCINE_MAX));
+        assertThat(species3.isPlant(),is(true));
     }
 
     @Test
