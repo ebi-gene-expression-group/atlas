f5abf5e99dc0c40e1a083489ee24c64a15379894
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2018-02-19 16:31:16 +0000

    Stub to get the gene IDs we’re interested in

--- base/src/main/java/uk/ac/ebi/atlas/solr/analytics/fullanalytics/BioentityIdentifierSearchService.java
+++ base/src/main/java/uk/ac/ebi/atlas/solr/analytics/fullanalytics/BioentityIdentifierSearchService.java
@@ -1,19 +1,36 @@
 package uk.ac.ebi.atlas.solr.analytics.fullanalytics;
 
-import org.apache.solr.client.solrj.SolrClient;
+import uk.ac.ebi.atlas.solr.cloud.SolrCloudCollectionProxyFactory;
+import uk.ac.ebi.atlas.solr.cloud.search.FacetStreamBuilder;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.Collections;
+import java.util.List;
 
 @Named
 public class BioentityIdentifierSearchService {
 
-    private final AnalyticsCollectionCloudDao analyticsCollectionCloudDao;
+    private final AnalyticsCollectionProxy analyticsCollectionProxy;
 
     @Inject
-    public BioentityIdentifierSearchService(AnalyticsCollectionCloudDao analyticsCollectionCloudDao) {
-        this.analyticsCollectionCloudDao = analyticsCollectionCloudDao;
+    public BioentityIdentifierSearchService(SolrCloudCollectionProxyFactory collectionProxyFactory) {
+        analyticsCollectionProxy = collectionProxyFactory.createAnalyticsCollectionProxy();
     }
 
+    List<String> mostSpecificGenes(String experimentAccession, int limit) {
+        return Collections.emptyList();
+    }
 
+    List<String> mostSpecificGenes(String experimentAccession, double expressionLevel, int limit) {
+        return Collections.emptyList();
+    }
+
+    List<String> mostExpressedGenes(String experimentAccession, int limit) {
+        return Collections.emptyList();
+    }
+
+    List<String> mostExpressedGenes(String experimentAccession, double expressionLevel, int limit) {
+        return Collections.emptyList();
+    }
 }
