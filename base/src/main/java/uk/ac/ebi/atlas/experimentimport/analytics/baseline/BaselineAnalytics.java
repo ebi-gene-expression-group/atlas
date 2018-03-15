5ac05d183b65174d09fba39e750790392bf3f02a
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2018-03-15 12:57:24 +0000

    Add quartiles to the analytics collection [#155590435]

--- base/src/main/java/uk/ac/ebi/atlas/experimentimport/analytics/baseline/BaselineAnalytics.java
+++ base/src/main/java/uk/ac/ebi/atlas/experimentimport/analytics/baseline/BaselineAnalytics.java
@@ -4,12 +4,22 @@ import com.google.auto.value.AutoValue;
 
 @AutoValue
 public abstract class BaselineAnalytics {
-    public static BaselineAnalytics create(String geneId, String assayGroupId, double expressionLevel, double expressionLevelFpkm) {
-        return new AutoValue_BaselineAnalytics(geneId, assayGroupId, expressionLevel, expressionLevelFpkm);
+    public static BaselineAnalytics create(String geneId,
+                                           String assayGroupId,
+                                           double expressionLevel,
+                                           double expressionLevelFpkm,
+                                           double[] expressionLevels,
+                                           double[] expressionLevelsFpkm) {
+        return new AutoValue_BaselineAnalytics(
+                geneId, assayGroupId, expressionLevel, expressionLevelFpkm, expressionLevels, expressionLevelsFpkm);
     }
 
     public abstract String geneId();
     public abstract String assayGroupId();
     public abstract double expressionLevel();
     public abstract double expressionLevelFpkm();
+    @SuppressWarnings("mutable")
+    public abstract double[] expressionLevels();
+    @SuppressWarnings("mutable")
+    public abstract double[] expressionLevelsFpkm();
 }
--- base/src/main/java/uk/ac/ebi/atlas/model/analyticsindex/BaselineExperimentDataPoint.java
+++ base/src/main/java/uk/ac/ebi/atlas/model/analyticsindex/BaselineExperimentDataPoint.java
@@ -1,6 +1,7 @@
 package uk.ac.ebi.atlas.model.analyticsindex;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.primitives.Doubles;
 import uk.ac.ebi.atlas.experimentimport.analytics.baseline.BaselineAnalytics;
 import uk.ac.ebi.atlas.model.experiment.baseline.BaselineExperiment;
 import uk.ac.ebi.atlas.solr.BioentityPropertyName;
@@ -18,9 +19,11 @@ public class BaselineExperimentDataPoint extends ExperimentDataPoint{
         // If there are only TPMs, FPKM will be 0, and vice versa
         if (baselineAnalytics.expressionLevel() != 0) {
             propertyMap.put("expression_level", baselineAnalytics.expressionLevel());
+            propertyMap.put("expression_levels", Doubles.asList(baselineAnalytics.expressionLevels()));
         }
         if (baselineAnalytics.expressionLevelFpkm() != 0) {
             propertyMap.put("expression_level_fpkm", baselineAnalytics.expressionLevelFpkm());
+            propertyMap.put("expression_levels_fpkm", Doubles.asList(baselineAnalytics.expressionLevelsFpkm()));
         }
     }
 
@@ -30,4 +33,3 @@ public class BaselineExperimentDataPoint extends ExperimentDataPoint{
     }
 
 }
-
