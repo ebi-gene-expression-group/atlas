abeefeca6797ef607548feb49edc240807bb5579
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2018-03-15 12:56:58 +0000

    Simplify BaselineExpression by removing unused cruft and JSON-serialize quartiles inline, no need for an additional class that does just that

--- base/src/main/java/uk/ac/ebi/atlas/model/experiment/baseline/BaselineExpression.java
+++ base/src/main/java/uk/ac/ebi/atlas/model/experiment/baseline/BaselineExpression.java
@@ -3,19 +3,20 @@ package uk.ac.ebi.atlas.model.experiment.baseline;
 import com.google.common.base.MoreObjects;
 import com.google.common.base.Objects;
 import com.google.common.collect.ImmutableList;
+import com.google.common.primitives.Doubles;
 import com.google.gson.JsonObject;
 import uk.ac.ebi.atlas.model.Expression;
 
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
+import static com.google.common.base.Preconditions.checkArgument;
 
 public class BaselineExpression implements Expression {
-    private double level;
-    private double[] quartiles;
-    private static final NumberFormat FOUR_DP = new DecimalFormat("0.####");
+    private static final ImmutableList<String> NO_DATA_CODES = ImmutableList.of("NA", "-", "NT");
+
+    private final double level;
+    private final ImmutableList<Double> quartiles;
 
     public BaselineExpression(double level) {
-        this(level, new double[]{level});
+        this(level, new double[] { level });
     }
 
     public BaselineExpression(double min,
@@ -23,52 +24,41 @@ public class BaselineExpression implements Expression {
                               double median,
                               double upperQuartile,
                               double max) {
-        this(median, new double[]{min,lowerQuartile, median, upperQuartile, max});
+        this(median, new double[] { min,lowerQuartile, median, upperQuartile, max });
     }
 
 
     private BaselineExpression(double level, double[] quartiles) {
         this.level = level;
-        this.quartiles = quartiles;
+        this.quartiles = ImmutableList.copyOf(Doubles.asList(quartiles));
     }
 
-    /*
-    NT is deprecated, shouldn't be present
-    "-" was documented as a "zero code"
-    */
-    private static final ImmutableList<String> zeros = ImmutableList.of("NA", "-", "NT");
-    public static BaselineExpression create(String expressionLevelString){
+    // NT is deprecated, shouldn't be present. "-" was documented as a "zero code. "NA" used in diff experiments.
+    public static BaselineExpression create(String expressionLevelString) {
         if (expressionLevelString == null) {
             return null;
         }
 
-        if (zeros.contains(expressionLevelString)) {
+        if (NO_DATA_CODES.contains(expressionLevelString)) {
             return new BaselineExpression(0.0);
         }
 
         if (expressionLevelString.contains(",")) {
             String[] vals = expressionLevelString.split(",", 5);
+            checkArgument(vals.length == 5, String.format("Need 5 elements but got %s", expressionLevelString));
 
-
-            double min = Double.parseDouble(vals[0]);
-            double max = Double.parseDouble(vals[4]);
-
-            return min == max
-                    ? new BaselineExpression(min)
-                    : new BaselineExpression(
-                    min,
-                    Double.parseDouble(vals[1]),
-                    Double.parseDouble(vals[2]),
-                    Double.parseDouble(vals[3]),
-                    max
-            );
+            return new BaselineExpression(Double.parseDouble(vals[0]),
+                                          Double.parseDouble(vals[1]),
+                                          Double.parseDouble(vals[2]),
+                                          Double.parseDouble(vals[3]),
+                                          Double.parseDouble(vals[4]));
         }
 
         return new BaselineExpression(Double.parseDouble(expressionLevelString));
     }
 
     public double[] getQuartiles() {
-        return quartiles;
+        return quartiles.stream().mapToDouble(Double::doubleValue).toArray();
     }
 
     public double getLevel() {
@@ -76,7 +66,7 @@ public class BaselineExpression implements Expression {
     }
 
     public boolean isGreaterThanOrEqual(double level) {
-        return Double.compare(getLevel(), level) >= 0;
+        return Double.compare(this.level, level) >= 0;
     }
 
     @Override
@@ -98,10 +88,6 @@ public class BaselineExpression implements Expression {
         return Objects.hashCode(level);
     }
 
-    static String removeTrailingZero(double value) {
-        return FOUR_DP.format(value);
-    }
-
     @Override
     public String toString() {
         return MoreObjects.toStringHelper(this)
@@ -109,19 +95,22 @@ public class BaselineExpression implements Expression {
                 .toString();
     }
 
-    /*
-    Used to be: BaselineExpressionViewModel which also provides properties the old heatmap requires
-    see atlas_heatmap:src/load/Data.js for how the properties are read in
-     */
     @Override
     public JsonObject toJson() {
         JsonObject result = new JsonObject();
 
         result.addProperty("value", level);
 
-        if(quartiles!=null && quartiles.length == 5){
-            result.add("quartiles",Quartiles.create(quartiles).toJson());
+        if (quartiles != null && quartiles.size() == 5) {
+            JsonObject quartilesJson = new JsonObject();
+            quartilesJson.addProperty("min", quartiles.get(0));
+            quartilesJson.addProperty("lower", quartiles.get(1));
+            quartilesJson.addProperty("median", quartiles.get(2));
+            quartilesJson.addProperty("upper", quartiles.get(3));
+            quartilesJson.addProperty("max", quartiles.get(4));
+            result.add("quartiles", quartilesJson);
         }
+
         return result;
     }
 }
