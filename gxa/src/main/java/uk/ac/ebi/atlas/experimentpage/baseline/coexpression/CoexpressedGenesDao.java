1ce9e654be241ce74d10e42e0fcfe5c8e38a61a0
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2018-03-19 15:40:40 +0000

    Simplified result extraction and added tests for CoexpressedGenesDao

--- gxa/src/main/java/uk/ac/ebi/atlas/experimentpage/baseline/coexpression/CoexpressedGenesDao.java
+++ gxa/src/main/java/uk/ac/ebi/atlas/experimentpage/baseline/coexpression/CoexpressedGenesDao.java
@@ -1,36 +1,31 @@
 package uk.ac.ebi.atlas.experimentpage.baseline.coexpression;
 
 import com.google.common.collect.ImmutableList;
-import org.apache.velocity.util.StringUtils;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import java.util.List;
+import javax.inject.Named;
+import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.toList;
+
+@Named
 public class CoexpressedGenesDao {
-
     private final JdbcTemplate jdbcTemplate;
+    protected final static String CE_GENES_SQL_QUERY_TEMPLATE =
+            "SELECT CE_IDENTIFIERS FROM RNASEQ_BSLN_CE_PROFILES WHERE EXPERIMENT=? AND IDENTIFIER=?";
 
     public CoexpressedGenesDao(JdbcTemplate jdbcTemplate){
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public ImmutableList<String> coexpressedGenesFor(String experiment, String identifier){
-        List<String> res = jdbcTemplate.queryForList(
-                "SELECT CE_IDENTIFIERS " +
-                "FROM RNASEQ_BSLN_CE_PROFILES " +
-                "WHERE EXPERIMENT=? AND IDENTIFIER=?",
-                new String [] {experiment,identifier},
-                String.class);
+    public ImmutableList<String> coexpressedGenesFor(String experiment, String identifier) {
+        String result = jdbcTemplate.queryForObject(CE_GENES_SQL_QUERY_TEMPLATE, String.class, experiment, identifier);
 
-        ImmutableList.Builder<String> builder = ImmutableList.builder();
-        for(String resultRow: res){
-            for(String geneId: StringUtils.split(resultRow,",")){
-                if(!geneId.equalsIgnoreCase(identifier)) {
-                    builder.add(geneId);
-                }
-            }
-        }
-        return builder.build();
+        return result == null ?
+                ImmutableList.of() :
+                ImmutableList.copyOf(
+                Stream.of(result.split(","))
+                      .filter(geneId -> !geneId.equalsIgnoreCase(identifier))
+                      .collect(toList()));
     }
-
 }
---
+++ gxa/src/test/java/uk/ac/ebi/atlas/experimentpage/baseline/coexpression/CoexpressedGenesDaoTest.java
@@ -0,0 +1,45 @@
+package uk.ac.ebi.atlas.experimentpage.baseline.coexpression;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import static uk.ac.ebi.atlas.experimentpage.baseline.coexpression.CoexpressedGenesDao.CE_GENES_SQL_QUERY_TEMPLATE;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CoexpressedGenesDaoTest {
+    @Mock
+    private JdbcTemplate jdbcTemplate;
+
+    private CoexpressedGenesDao subject;
+
+    @Before
+    public void setUp() {
+        subject = new CoexpressedGenesDao(jdbcTemplate);
+    }
+
+    @Test
+    public void noResultsReturnsEmptyList() {
+        when(jdbcTemplate.queryForObject(CE_GENES_SQL_QUERY_TEMPLATE, String.class, "E-FOOBAR", "ENSGFOOBAR"))
+                .thenReturn(null);
+
+        assertThat(subject.coexpressedGenesFor("E-FOOBAR", "ENSGFOOBAR")).isEmpty();
+    }
+
+    @Test
+    public void originalGeneIdIsFilteredOut() {
+        when(jdbcTemplate.queryForObject(CE_GENES_SQL_QUERY_TEMPLATE, String.class, "E-FOOBAR", "ENSGFOOBAR"))
+                .thenReturn("ENSGFOOBAR,ENSGFOOBAR1,ENSGFOOBAR2,ENSGFOOBAR3");
+
+        assertThat(subject.coexpressedGenesFor("E-FOOBAR", "ENSGFOOBAR"))
+                .hasSize(3)
+                .doesNotContain("ENSGFOOBAR");
+    }
+
+
+}
