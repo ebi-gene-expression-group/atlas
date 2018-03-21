85a9050867698d7ec7788068e5e3ed6728831f2a
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2018-03-21 19:54:32 +0000

    Solr-backed search service for baseline experiments and JSON-serializer

---
+++ gxa/src/main/java/uk/ac/ebi/atlas/experimentpage/baseline/BaselineExperimentProfilesListSerializer.java
@@ -0,0 +1,23 @@
+package uk.ac.ebi.atlas.experimentpage.baseline;
+
+import com.google.gson.JsonObject;
+import uk.ac.ebi.atlas.experimentpage.LinkToGene;
+import uk.ac.ebi.atlas.experimentpage.context.BaselineRequestContext;
+import uk.ac.ebi.atlas.model.AssayGroup;
+import uk.ac.ebi.atlas.model.ExpressionUnit;
+import uk.ac.ebi.atlas.model.GeneProfilesList;
+import uk.ac.ebi.atlas.model.Profile;
+import uk.ac.ebi.atlas.profiles.json.ExternallyViewableProfilesList;
+
+public class BaselineExperimentProfilesListSerializer {
+    public static JsonObject serialize(GeneProfilesList<? extends Profile<AssayGroup, ?, ?>> profilesList,
+                                       BaselineRequestContext<? extends ExpressionUnit.Absolute> requestContext) {
+        return new
+                ExternallyViewableProfilesList<>(
+                profilesList,
+                new LinkToGene<>(),
+                requestContext.getDataColumnsToReturn(),
+                baselineProfile -> requestContext.getExpressionUnit())
+                .asJson();
+    }
+}
