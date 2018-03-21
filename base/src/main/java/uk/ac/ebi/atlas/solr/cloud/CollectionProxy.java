320e4152cf18a824f673268e461032c471761ab9
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2018-03-21 15:02:25 +0000

    Remove query(SolrQuery solrQuery) as it’s redundant: SolrQuery is a subclass of SolrParams

--- base/src/main/java/uk/ac/ebi/atlas/solr/cloud/CollectionProxy.java
+++ base/src/main/java/uk/ac/ebi/atlas/solr/cloud/CollectionProxy.java
@@ -1,12 +1,13 @@
 package uk.ac.ebi.atlas.solr.cloud;
 
 import org.apache.solr.client.solrj.SolrClient;
-import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrRequest;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.request.UpdateRequest;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.client.solrj.response.UpdateResponse;
 import org.apache.solr.common.SolrInputDocument;
+import org.apache.solr.common.params.SolrParams;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,10 +29,10 @@ public abstract class CollectionProxy {
         this.nameOrAlias = nameOrAlias;
     }
 
-    public QueryResponse query(SolrQuery solrQuery) {
+    public QueryResponse query(SolrParams solrParams) {
         try {
             // Change maybe to: return new QueryRequest()
-            return solrClient.query(nameOrAlias, solrQuery);
+            return solrClient.query(nameOrAlias, solrParams, SolrRequest.METHOD.POST);
         } catch (IOException e) {
             logException(e);
             throw new UncheckedIOException(e);
