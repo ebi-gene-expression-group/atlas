00c7d60dd1c0179375266d4aaad00df526b04e6e
Monica Jianu <monica@ebi.ac.uk>
2018-03-26 10:50:28 +0100

    Extract DesignTable component into separate file. Fixed minor CSS issues with download icon. [#155186621]

--- sc/src/main/javascript/bundles/experiment-page/src/ExperimentDesignRoute.js
+++ sc/src/main/javascript/bundles/experiment-page/src/ExperimentDesignRoute.js
@@ -1,76 +1,15 @@
 import React from 'react'
-import ReactTable from 'react-table'
 import PropTypes from 'prop-types'
 import URI from 'urijs'
-import {uniq, curry} from 'lodash'
-import toPlural from 'pluralize'
-import '!style-loader!css-loader!react-table/react-table.css'
-import '!style-loader!css-loader!./experiment-table.css'
 
-//http://stackoverflow.com/questions/196972/convert-string-to-title-case-with-javascript
-const toTitleCase = (str) => str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-
-const aggregateText = (name, vals) => {
-  const xs = uniq(vals)
-  return (
-    xs.length === 1 || (xs.length < 5 && xs.join(", ").length < 30)
-      ? xs.join(", ")
-      : toPlural(name.toLowerCase() , xs.length, true)
-  )
-}
-
-const ExperimentDesign = ({
-                            data,
-                            headers,
-                            options={}
-                          }) => (
-  <ReactTable
-    columns={
-      headers.map((headerGroup,ix)=> (
-        {
-          Header: headerGroup.name,
-          columns:
-            headerGroup.values.map((header, jx) => ({
-              aggregate: curry(aggregateText, 2)(header),
-              Header: header,
-              id: ix*1000 +jx +1,
-              accessor: r => r.values[ix][jx],
-            }))
-        }
-      ))
-    }
-    className="-striped"
-    style={{
-      fontSize: "small"
-    }}
-    data={data}
-    {...options}
-  />
-)
-
-const BaselineExperimentDesign = ({
-                                    data,
-                                    headers
-                                  }) => (
-  ExperimentDesign({
-    data:
-      data
-        .map(({properties, values}) => ({
-          values: [[properties.analysed? "Yes" : "No"]].concat(values)
-        })),
-    headers:
-      [{name:"", values: ["Analysed"]}].concat(headers)
-  })
-)
-
+import BaselineExperimentDesign from './experiment-design/ExperimentDesignTable'
 
 const ExperimentDesignRoute = (props) => {
-
   return (
     <div>
       <div className="row expanded column margin-top-large">
         <a className="button float-right margin-bottom-none" href={URI(props.downloadUrl, props.atlasUrl).toString()}>
-          <span className="icon icon-functional" data-icon="="/>
+          <span className="icon icon-spacer icon-functional" data-icon="="/>
           Download
         </a>
       </div>
---
+++ sc/src/main/javascript/bundles/experiment-page/src/experiment-design/ExperimentDesignTable.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import ReactTable from 'react-table'
+import {uniq, curry} from 'lodash'
+import toPlural from 'pluralize'
+import '!style-loader!css-loader!react-table/react-table.css'
+import '!style-loader!css-loader!./experiment-table.css'
+
+const aggregateText = (name, vals) => {
+  const xs = uniq(vals)
+  return (
+    xs.length === 1 || (xs.length < 5 && xs.join(', ').length < 30)
+      ? xs.join(', ')
+      : toPlural(name.toLowerCase() , xs.length, true)
+  )
+}
+
+const ExperimentDesign = ({
+                            data,
+                            headers,
+                            options={}
+                          }) => (
+  <ReactTable
+    columns={
+      headers.map((headerGroup,ix)=> (
+        {
+          Header: headerGroup.name,
+          columns:
+            headerGroup.values.map((header, jx) => ({
+              aggregate: curry(aggregateText, 2)(header),
+              Header: header,
+              id: ix*1000 +jx +1,
+              accessor: r => r.values[ix][jx],
+            }))
+        }
+      ))
+    }
+    className='-striped'
+    style={{
+      fontSize: 'small'
+    }}
+    data={data}
+    {...options}
+  />
+)
+
+const BaselineExperimentDesign = ({
+                                    data,
+                                    headers
+                                  }) => (
+  ExperimentDesign({
+    data:
+      data
+        .map(({properties, values}) => ({
+          values: [[properties.analysed? 'Yes' : 'No']].concat(values)
+        })),
+    headers:
+      [{name:"", values: ['Analysed']}].concat(headers)
+  })
+)
+
+export default BaselineExperimentDesign
