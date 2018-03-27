faf720703a95b9acd47f34e2f5720c3644bfd202
Monica Jianu <monicajianu@users.noreply.github.com>
2018-03-27 10:45:49 +0100

    Task 1551186043 - Idf parser retrieves Array Express display name (#70)
    * Changed idf file parser so that it retrieves the Array Express title first, then the Investigation title if the first is non-existant. Fixed tests and added test case for the new functionality.
    
    * Simplified title parsing logic following code review.
    
    * Removed unusued imports.
    
    * Added handling of keys with mixed case. Added test case.

--- base/src/main/java/uk/ac/ebi/atlas/experimentimport/condensedSdrf/IdfParser.java
+++ base/src/main/java/uk/ac/ebi/atlas/experimentimport/condensedSdrf/IdfParser.java
@@ -7,11 +7,14 @@ import uk.ac.ebi.atlas.commons.readers.TsvStreamer;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Named
 public class IdfParser {
     private static final String INVESTIGATION_TITLE_ID = "Investigation Title";
     private static final String PUBMED_ID = "PubMed ID";
+    private static final String AE_EXPERIMENT_DISPLAY_NAME = "Comment[AEExperimentDisplayName]";
 
     private IdfStreamerFactory idfReaderFactory;
 
@@ -22,15 +25,16 @@ public class IdfParser {
 
     public ImmutablePair<String, ImmutableSet<String>> parse(String experimentAccession) {
 
-        try (TsvStreamer titleIdfStreamer = idfReaderFactory.create(experimentAccession);
+        try (
+             TsvStreamer titleIdfStreamer = idfReaderFactory.create(experimentAccession);
              TsvStreamer pubmedIdfStreamer = idfReaderFactory.create(experimentAccession)) {
-            String title =
-                    titleIdfStreamer.get()
-                            .filter(line -> line.length > 1)
-                            .filter(line -> INVESTIGATION_TITLE_ID.equalsIgnoreCase(line[0].trim()))
-                            .map(line -> line[1])
-                            .findFirst()
-                            .orElse("");
+
+            Map<String, String> titles = titleIdfStreamer.get()
+                    .filter(line -> line.length > 1)
+                    .filter(line -> AE_EXPERIMENT_DISPLAY_NAME.equalsIgnoreCase(line[0].trim()) || INVESTIGATION_TITLE_ID.equalsIgnoreCase(line[0].trim()))
+                    .collect(Collectors.toMap(line -> line[0].toUpperCase().trim(), line-> line[1]));
+
+            String title = titles.getOrDefault(AE_EXPERIMENT_DISPLAY_NAME.toUpperCase(), titles.getOrDefault(INVESTIGATION_TITLE_ID.toUpperCase(), ""));
 
             ImmutableSet<String> pubmedIds = ImmutableSet.copyOf(
                     pubmedIdfStreamer.get()
