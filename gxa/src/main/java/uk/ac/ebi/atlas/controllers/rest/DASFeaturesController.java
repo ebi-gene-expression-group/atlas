342b62b5dd9386a445beee88b3b4f5682167c57d
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2018-03-29 15:22:49 +0100

    Test DAS endpoint sensibly

--- gxa/src/main/java/uk/ac/ebi/atlas/controllers/rest/DASFeaturesController.java
+++ gxa/src/main/java/uk/ac/ebi/atlas/controllers/rest/DASFeaturesController.java
@@ -4,15 +4,13 @@ import com.google.common.base.Joiner;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.SetMultimap;
-import com.google.gson.JsonObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+import uk.ac.ebi.atlas.controllers.HtmlExceptionHandlingController;
 import uk.ac.ebi.atlas.model.AssayGroup;
 import uk.ac.ebi.atlas.model.experiment.Experiment;
 import uk.ac.ebi.atlas.model.experiment.baseline.Factor;
@@ -26,12 +24,12 @@ import javax.inject.Inject;
 import java.util.List;
 import java.util.Set;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+
 @Controller
 @Scope("request")
-public class DASFeaturesController {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(DASFeaturesController.class);
-
+public class DASFeaturesController extends HtmlExceptionHandlingController {
     private final ExperimentTrader experimentTrader;
     private final DifferentialAnalyticsSearchService differentialAnalyticsSearchService;
     private final DifferentialJsonResultsParser differentialJsonResultsParser;
@@ -46,27 +44,20 @@ public class DASFeaturesController {
         this.differentialJsonResultsParser = differentialJsonResultsParser;
     }
 
-    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
-    public ModelAndView InternalServerHandleException(Exception e) {
-        ModelAndView mav = new ModelAndView("error-page");
-        mav.addObject("exceptionMessage", "Couldn't fetch search results");
-        LOGGER.error("Error getting Differential json results.",e);
-        return mav;
-    }
-
     @RequestMapping(value = "/das/s4/features")
-    public String getDifferentialJsonResults(@RequestParam(value = "segment", required = false, defaultValue = "")
-                                                     String geneId,
-                                             @RequestParam(value = "conditionQuery", required = false, defaultValue = "")
-                                                     SemanticQuery conditionQuery,
-                                             Model model) {
+    public String getDifferentialJsonResults(
+            @RequestParam(value = "segment", required = false, defaultValue = "") String geneId,
+            @RequestParam(value = "conditionQuery", required = false, defaultValue = "") SemanticQuery conditionQuery,
+            Model model) {
+
+        checkArgument(isNotBlank(geneId) || conditionQuery.isNotEmpty());
 
         List<DiffAnalytics> diffAnalyticsList;
         SetMultimap<String, String> factorValuesByType = HashMultimap.create();
 
-        JsonObject result = differentialAnalyticsSearchService.fetchResults(SemanticQuery.create(geneId), conditionQuery);
-
-        diffAnalyticsList = differentialJsonResultsParser.parseDifferentialResults(result);
+        diffAnalyticsList =
+                differentialJsonResultsParser.parseDifferentialResults(
+                        differentialAnalyticsSearchService.fetchResults(SemanticQuery.create(geneId), conditionQuery));
 
         String geneName = diffAnalyticsList.isEmpty() ? geneId : diffAnalyticsList.get(0).getBioentityName();
 
@@ -74,7 +65,9 @@ public class DASFeaturesController {
             AssayGroup testAssayGroup = dbe.getContrastTestAssayGroup();
             Experiment experiment = experimentTrader.getPublicExperiment(dbe.getExperimentAccession());
 
-            FactorSet factorsForAssayGroup = FactorSet.create(experiment.getExperimentDesign().getFactorValues(testAssayGroup.getFirstAssayAccession()));
+            FactorSet factorsForAssayGroup =
+                    FactorSet.create(experiment.getExperimentDesign()
+                             .getFactorValues(testAssayGroup.getFirstAssayAccession()));
             for (Factor factor : factorsForAssayGroup) {
                 factorValuesByType.put(factor.getType(), factor.getValue());
             }
@@ -83,8 +76,12 @@ public class DASFeaturesController {
         model.addAttribute("geneId", geneId);
         model.addAttribute("geneName", geneName);
 
-        for (String factorValue: ImmutableList.of("ORGANISM_PART","DISEASE","CELL_TYPE","CELL_LINE","COMPOUND","DEVELOPMENTAL_STAGE","INFECT","PHENOTYPE")) {
-            model.addAttribute("factorValues_"+factorValue,formatFactorValuesLabel(factorValuesByType.get(factorValue)));
+        for (String factorValue:
+                ImmutableList.of("ORGANISM_PART", "DISEASE", "CELL_TYPE", "CELL_LINE", "COMPOUND",
+                                 "DEVELOPMENTAL_STAGE", "INFECT", "PHENOTYPE")) {
+            model.addAttribute(
+                    "factorValues_" + factorValue,
+                    formatFactorValuesLabel(factorValuesByType.get(factorValue)));
         }
 
         return "das-features";
