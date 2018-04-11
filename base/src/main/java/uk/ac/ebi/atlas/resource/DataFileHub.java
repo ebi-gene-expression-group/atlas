53c37ed472c7ef4541671002e7a1337eb1bce139
Monica Jianu <monica@ebi.ac.uk>
2018-04-11 13:59:09 +0100

    Renamed file path for tpm file. Added support for software.tsv file to DataFileHub.

--- base/src/main/java/uk/ac/ebi/atlas/resource/DataFileHub.java
+++ base/src/main/java/uk/ac/ebi/atlas/resource/DataFileHub.java
@@ -63,7 +63,7 @@ public class DataFileHub {
 
     final static String REACTOME_PATHWAYS_FILE_PATH_TEMPLATE = "magetab/{0}/{0}.{1}.reactome.gsea.tsv";
 
-    final static String SINGLE_CELL_MATRIX_MARKET_TPMS_FILE_PATH_TEMPLATE = "magetab/{0}/{0}.mtx";
+    final static String SINGLE_CELL_MATRIX_MARKET_TPMS_FILE_PATH_TEMPLATE = "magetab/{0}/{0}.expression_tpm.mtx";
     final static String SINGLE_CELL_MATRIX_MARKET_TPMS_GENE_IDS_FILE_PATH_TEMPLATE =
             SINGLE_CELL_MATRIX_MARKET_TPMS_FILE_PATH_TEMPLATE + "_rows";
     final static String SINGLE_CELL_MATRIX_MARKET_TPMS_CELL_IDS_FILE_PATH_TEMPLATE =
@@ -71,6 +71,7 @@ public class DataFileHub {
     final static String SINGLE_CELL_T_SNE_PLOT_FILE_PATH_TEMPLATE = "magetab/{0}/{0}.tsne_perp_{1}.tsv";
     final static String SINGLE_CELL_SDRF_FILE_PATH_TEMPLATE = "magetab/{0}/{0}.sdrf.txt";
     final static String SINGLE_CELL_CLUSTERS_FILE_PATH_TEMPLATE = "magetab/{0}/{0}.clusters.tsv";
+    final static String SINGLE_CELL_SOFTWARE_USED_FILE_PATH_TEMPLATE = "magetab/{0}/{0}.software.tsv";
 
 
     @Inject
@@ -321,6 +322,7 @@ public class DataFileHub {
     public class SingleCellExperimentFiles {
         public final ExperimentFiles experimentFiles;
 
+        public final AtlasResource<TsvStreamer> softwareUsed;
         public final AtlasResource<MatrixMarketReader> tpmsMatrix;
         public final AtlasResource<TsvStreamer> geneIdsTsv;
         public final AtlasResource<TsvStreamer> cellIdsTsv;
@@ -330,6 +332,11 @@ public class DataFileHub {
 
         SingleCellExperimentFiles(String experimentAccession) {
             experimentFiles = new ExperimentFiles(experimentAccession);
+
+            softwareUsed = new TsvFile.ReadOnly(
+                    experimentsFilesLocation,
+                    SINGLE_CELL_SOFTWARE_USED_FILE_PATH_TEMPLATE,
+                    experimentAccession);
 
             sdrf = new TsvFile.ReadOnly(
                     experimentsFilesLocation,
