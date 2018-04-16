bfbeac580cf224bb1ca9e76a4cafacd9b359ced1
Monica Jianu <monica@ebi.ac.uk>
2018-04-16 13:28:17 +0100

    Added new parsing method for idf file. Created new IdfParserOutput class that stored the information retrieved from the idf. Added integration test.

--- base/src/main/java/uk/ac/ebi/atlas/experimentimport/condensedSdrf/IdfParser.java
+++ base/src/main/java/uk/ac/ebi/atlas/experimentimport/condensedSdrf/IdfParser.java
@@ -1,12 +1,16 @@
 package uk.ac.ebi.atlas.experimentimport.condensedSdrf;
 
 import com.google.common.collect.ImmutableSet;
+import org.apache.commons.lang.math.NumberUtils;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import uk.ac.ebi.atlas.commons.readers.TsvStreamer;
+import uk.ac.ebi.atlas.experimentimport.idf.IdfParserOutput;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -14,7 +18,9 @@ import java.util.stream.Collectors;
 public class IdfParser {
     private static final String INVESTIGATION_TITLE_ID = "Investigation Title";
     private static final String PUBMED_ID = "PubMed ID";
-    private static final String AE_EXPERIMENT_DISPLAY_NAME = "Comment[AEExperimentDisplayName]";
+    private static final String PUBLICATION_TITLE_ID = "Publication Title";
+    private static final String AE_EXPERIMENT_DISPLAY_NAME_ID = "Comment[AEExperimentDisplayName]";
+    private static final String EXPECTED_CLUSTERS_ID = "Comment[ExpectedClusters]";
 
     private IdfStreamerFactory idfReaderFactory;
 
@@ -31,19 +37,67 @@ public class IdfParser {
 
             Map<String, String> titles = titleIdfStreamer.get()
                     .filter(line -> line.length > 1)
-                    .filter(line -> AE_EXPERIMENT_DISPLAY_NAME.equalsIgnoreCase(line[0].trim()) || INVESTIGATION_TITLE_ID.equalsIgnoreCase(line[0].trim()))
+                    .filter(line -> AE_EXPERIMENT_DISPLAY_NAME_ID.equalsIgnoreCase(line[0].trim()) || INVESTIGATION_TITLE_ID.equalsIgnoreCase(line[0].trim()))
                     .collect(Collectors.toMap(line -> line[0].toUpperCase().trim(), line-> line[1]));
 
-            String title = titles.getOrDefault(AE_EXPERIMENT_DISPLAY_NAME.toUpperCase(), titles.getOrDefault(INVESTIGATION_TITLE_ID.toUpperCase(), ""));
+            String title = titles.getOrDefault(AE_EXPERIMENT_DISPLAY_NAME_ID.toUpperCase(), titles.getOrDefault(INVESTIGATION_TITLE_ID.toUpperCase(), ""));
 
             ImmutableSet<String> pubmedIds = ImmutableSet.copyOf(
                     pubmedIdfStreamer.get()
                             .filter(line -> line.length > 1)
                             .filter(line -> PUBMED_ID.equalsIgnoreCase(line[0].trim()))
                             .flatMap(line -> Arrays.stream(line).skip(1))
+                            .filter(item -> !item.isEmpty())
                             .iterator());
 
             return ImmutablePair.of(title, pubmedIds);
         }
+    }
+
+    public IdfParserOutput newParse(String experimentAccession) {
+        try (TsvStreamer idfStreamer = idfReaderFactory.create(experimentAccession)) {
+
+            Map<String, List<String>> parsedIdf =
+                    idfStreamer.get()
+                            .filter(line -> line.length > 1)
+                            .filter(line -> INVESTIGATION_TITLE_ID.equalsIgnoreCase(line[0].trim()) ||
+                                        AE_EXPERIMENT_DISPLAY_NAME_ID.equalsIgnoreCase(line[0].trim()) ||
+                                        PUBMED_ID.equalsIgnoreCase(line[0].trim()) ||
+                                        PUBLICATION_TITLE_ID.equalsIgnoreCase(line[0].trim()) ||
+                                        EXPECTED_CLUSTERS_ID.equalsIgnoreCase(line[0].trim()))
+                            .collect(Collectors.toMap(
+                                    line -> line[0].toUpperCase().trim(),
+                                    line -> Arrays.stream(line)
+                                            .skip(1)
+                                            .filter(item -> !item.isEmpty())
+                                            .collect(Collectors.toList())));
+
+            String title = parsedIdf.getOrDefault(AE_EXPERIMENT_DISPLAY_NAME_ID.toUpperCase(), parsedIdf.get(INVESTIGATION_TITLE_ID.toUpperCase()))
+                    .stream()
+                    .findFirst()
+                    .orElse("");
+
+            Map<String, String> publicationDetails = getPublicationDetailsMap(parsedIdf.get(PUBMED_ID.toUpperCase()), parsedIdf.get(PUBLICATION_TITLE_ID.toUpperCase()));
+            int expectedClusters = NumberUtils.toInt(parsedIdf.get(EXPECTED_CLUSTERS_ID.toUpperCase()).stream().findFirst().orElse(""), 0);
+
+            return new IdfParserOutput(
+                    title,
+                    publicationDetails,
+                    expectedClusters
+            );
+
+        }
+    }
+
+    private Map<String, String> getPublicationDetailsMap(List<String> pubmedIds, List<String> publicationTitles) {
+        Map<String, String> publicationDetails = new HashMap<>();
+
+        if(pubmedIds.size() == publicationTitles.size()) {
+            for (int i = 0; i < pubmedIds.size(); i++) {
+                publicationDetails.put(pubmedIds.get(i), publicationTitles.get(i));
+            }
+        }
+        
+        return  publicationDetails;
     }
 }
---
+++ base/src/test/java/uk/ac/ebi/atlas/experimentimport/idf/IdfParserIT.java
@@ -0,0 +1,51 @@
+package uk.ac.ebi.atlas.experimentimport.idf;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.ac.ebi.atlas.commons.readers.ArrayExpressIdfStreamerFactory;
+import uk.ac.ebi.atlas.experimentimport.condensedSdrf.IdfParser;
+import uk.ac.ebi.atlas.experimentimport.condensedSdrf.IdfStreamerFactory;
+import uk.ac.ebi.atlas.resource.DataFileHubFactory;
+
+import javax.inject.Inject;
+
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isEmptyString;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration("classpath:applicationContext.xml")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class IdfParserIT {
+
+    private IdfParser subject;
+
+    @Inject
+    private ArrayExpressIdfStreamerFactory arrayExpressIdfStreamerFactory;
+    @Inject
+    private DataFileHubFactory dataFileHubFactory;
+
+    @BeforeAll
+    public void setUp() {
+        IdfStreamerFactory idfStreamerFactory = new IdfStreamerFactory(arrayExpressIdfStreamerFactory, dataFileHubFactory.getScxaDataFileHub());
+        this.subject = new IdfParser(idfStreamerFactory);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "E-ENAD-13", "E-GEOD-106540"})
+    public void testParserForSingleCell(String experimentAccession) {
+        IdfParserOutput result = subject.newParse(experimentAccession);
+
+        assertThat(result.getExpectedClusters(), is(greaterThanOrEqualTo(0)));
+        assertThat(result.getTitle(), is(not(isEmptyString())));
+        assertThat(result.getPublications(), is(notNullValue()));
+    }
+}
