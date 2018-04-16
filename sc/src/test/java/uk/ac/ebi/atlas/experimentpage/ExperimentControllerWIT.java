bbe2bcfb1b52bd24905dd07540bb1e94e153f867
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2018-04-16 02:07:32 +0100

    Add support for JUnit 5

--- sc/src/test/java/uk/ac/ebi/atlas/experimentpage/ExperimentControllerWIT.java
+++ sc/src/test/java/uk/ac/ebi/atlas/experimentpage/ExperimentControllerWIT.java
@@ -1,68 +1,54 @@
 package uk.ac.ebi.atlas.experimentpage;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestContextManager;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitJupiterWebConfig;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import uk.ac.ebi.atlas.model.experiment.ExperimentType;
-
-import java.util.Arrays;
+import uk.ac.ebi.atlas.trader.ExperimentTrader;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-@RunWith(Parameterized.class)
+// @SpringJUnitJupiterWebConfig(locations = {"classpath:applicationContext.xml", "classpath:dispatcher-servlet.xml"})
+// The meta annotation above includes @ExtendWith(SpringExtension.class), @WebAppConfiguration and
+// @ContextConfiguration(locations = {"classpath:applicationContext.xml", "classpath:dispatcher-servlet.xml"}) but it
+// confuses IDEA and fails to detect the component scan beans (although the tests run fine). We can wait until we move
+// to Spring 5.
+@ExtendWith(SpringExtension.class)
 @WebAppConfiguration
 @ContextConfiguration(locations = {"classpath:applicationContext.xml", "classpath:dispatcher-servlet.xml"})
+@TestInstance(Lifecycle.PER_CLASS)  // @BeforeAll and @MethodSource need not be static :)
 public class ExperimentControllerWIT {
     private final String URL = "/experiments/{experimentAccession}";
 
     @Autowired
-    WebApplicationContext wac;
+    ExperimentTrader scxaExperimentTrader;
 
     private MockMvc mockMvc;
-    private TestContextManager testContextManager;
 
-    @Parameterized.Parameter
-    public String experimentAccession;
-
-    @Parameterized.Parameters
-    public static Iterable<?> data() {
-        // I haven’t been able to think of a better way to get all public experiments from the data source configured
-        // applicationContext.xml. Since the application context isn’t loaded until @Before, we need to explicitly
-        // build it first. Interface ApplicationContextAware (as suggested, for example, in
-        // http://sujitpal.blogspot.co.uk/2007/03/accessing-spring-beans-from-legacy-code.html) doesn’t work because of
-        // the very same reason: the context, and the bean that can supply a static reference to the context, won’t be
-        // ready at this point. Fortunately Hikari doesn’t complain about opening two connection pools.
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-        // I guess we could also retrive scxaEperimentTrader
-        JdbcTemplate jdbcTemplate = (JdbcTemplate) ctx.getBean("jdbcTemplate");
-        return jdbcTemplate.queryForList("SELECT accession FROM scxa_public_experiment", String.class);
-//        return Arrays.asList("E-GEOD-106540", "E-MTAB-5061");
+    @BeforeAll
+    public void setUp(WebApplicationContext wac) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
     }
 
-    @Before
-    public void setUp() throws Exception {
-        this.testContextManager = new TestContextManager(getClass());
-        this.testContextManager.prepareTestInstance(this);
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
-    }
-
-    @Test
-    public void validExperimentAccession() throws Exception {
-            this.mockMvc.perform(get(URL, experimentAccession))
+    @ParameterizedTest
+    @MethodSource("publicExperimentsProvider")
+    public void validExperimentAccession(String experimentAccession) throws Exception {
+        mockMvc.perform(get(URL, experimentAccession))
                     .andExpect(status().isOk())
                     .andExpect(view().name("experiment-page"))
                     .andExpect(model().attribute("experimentAccession", experimentAccession))
@@ -72,8 +58,13 @@ public class ExperimentControllerWIT {
 
     @Test
     public void invalidExperimentAccession() throws Exception {
-        this.mockMvc.perform(get(URL, "FOO"))
+        mockMvc.perform(get(URL, "FOO"))
                 .andExpect(status().is(400))
                 .andExpect(view().name("error-page"));
     }
+
+    private Iterable<String> publicExperimentsProvider() {
+        return scxaExperimentTrader.getPublicExperimentAccessions(ExperimentType.SINGLE_CELL_RNASEQ_MRNA_BASELINE);
+    }
+
 }
