04a4f0f993a18b16e6330b5d0ac421e6d9ca509d
Monica Jianu <monica@ebi.ac.uk>
2018-04-17 09:32:20 +0100

    Implemented TestingUtils method that allows retrieval of all experiment accessions available in the database. Used TestingUtils in Idf parser tests.

--- base/src/test/java/uk/ac/ebi/atlas/experimentimport/idf/IdfParserIT.java
+++ base/src/test/java/uk/ac/ebi/atlas/experimentimport/idf/IdfParserIT.java
@@ -1,14 +1,14 @@
 package uk.ac.ebi.atlas.experimentimport.idf;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.ac.ebi.atlas.commons.readers.ArrayExpressIdfStreamerFactory;
 import uk.ac.ebi.atlas.resource.DataFileHubFactory;
+import uk.ac.ebi.atlas.utils.TestingUtils;
 
 import javax.inject.Inject;
 
@@ -24,26 +24,44 @@ import static org.junit.Assert.assertThat;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class IdfParserIT {
 
-    private IdfParser subject;
-
     @Inject
     private ArrayExpressIdfStreamerFactory arrayExpressIdfStreamerFactory;
     @Inject
     private DataFileHubFactory dataFileHubFactory;
+    @Inject
+    private TestingUtils testingUtils;
 
-    @BeforeAll
-    public void setUp() {
+    @ParameterizedTest
+    @MethodSource("singleCellExperimentsProvider")
+    public void testParserForSingleCell(String experimentAccession) {
         IdfStreamerFactory idfStreamerFactory = new IdfStreamerFactory(arrayExpressIdfStreamerFactory, dataFileHubFactory.getScxaDataFileHub());
-        this.subject = new IdfParser(idfStreamerFactory);
+        IdfParser idfParser = new IdfParser(idfStreamerFactory);
+
+        parseForDBExperimentAccession(idfParser, experimentAccession);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = { "E-ENAD-13", "E-GEOD-106540"})
-    public void testParserForSingleCell(String experimentAccession) {
-        IdfParserOutput result = subject.parse(experimentAccession);
+    @MethodSource("expressionAtlasExperimentsProvider")
+    public void testParserForExpressionAtlas(String experimentAccession) {
+        IdfStreamerFactory idfStreamerFactory = new IdfStreamerFactory(arrayExpressIdfStreamerFactory, dataFileHubFactory.getGxaDataFileHub());
+        IdfParser idfParser = new IdfParser(idfStreamerFactory);
+
+        parseForDBExperimentAccession(idfParser, experimentAccession);
+    }
+
+    private void parseForDBExperimentAccession(IdfParser idfParser, String experimentAccession) {
+        IdfParserOutput result = idfParser.parse(experimentAccession);
 
         assertThat(result.getExpectedClusters(), is(greaterThanOrEqualTo(0)));
         assertThat(result.getTitle(), is(not(isEmptyString())));
         assertThat(result.getPublications(), is(notNullValue()));
+    }
+
+    private Iterable<String> singleCellExperimentsProvider() {
+        return testingUtils.getSingleCellExperimentAccessions();
+    }
+
+    private Iterable<String> expressionAtlasExperimentsProvider() {
+        return testingUtils.getExpressionAtlasExperimentAccessions();
     }
 }
