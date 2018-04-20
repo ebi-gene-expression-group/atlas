c729c973062b5bfecc34c33e45496c7b4cbe2200
Monica Jianu <monica@ebi.ac.uk>
2018-04-20 14:57:38 +0100

    Added idf file to ExperimentFileType enum and the file location service. Removed unused name property for ExperimentFileTypes.

--- sc/src/main/java/uk/ac/ebi/atlas/download/ExperimentFileType.java
+++ sc/src/main/java/uk/ac/ebi/atlas/download/ExperimentFileType.java
@@ -7,23 +7,22 @@ import java.util.Arrays;
 public enum ExperimentFileType {
 
     // Could include icon name (similar to Description class in ExternallyAvailableContent)
-    EXPERIMENT_DESIGN ("experiment-design", "Experiment Design", "Experiment design file (.tsv)", IconType.EXPERIMENT_DESIGN, false),
-    SDRF("sdrf", "SDRF file", "SDRF file (.tsv)", IconType.TSV, false),
-    CLUSTERING("cluster", "Clustering file", "Clustering file (.tsv)", IconType.TSV, false),
-    QUANTIFICATION_RAW("quantification-raw", "Raw Quantification", "Raw quantification files (MatrixMarket)", IconType.TSV, true),
-    QUANTIFICATION_FILTERED("quantification-filtered", "Filtered Quantification", "Filtered quantification files (MatrixMarket)", IconType.TSV, true);
+    EXPERIMENT_DESIGN ("experiment-design", "Experiment design file (.tsv)", IconType.EXPERIMENT_DESIGN, false),
+    SDRF("sdrf", "SDRF file (.txt)", IconType.TSV, false),
+    IDF("idf", "IDF file (.txt)", IconType.TSV, false),
+    CLUSTERING("cluster", "Clustering file (.tsv)", IconType.TSV, false),
+    QUANTIFICATION_RAW("quantification-raw", "Raw quantification files (MatrixMarket)", IconType.TSV, true),
+    QUANTIFICATION_FILTERED("quantification-filtered", "Filtered quantification files (MatrixMarket)", IconType.TSV, true);
 
     // IDs should be used when generating URLs
     private final String id;
-    private final String name;
     private final String description;
     private final IconType iconType;
     // Indicates if the file type has more than one associated files, which should be served as an archive
     private final boolean isArchive;
 
-    ExperimentFileType(String id, String name, String description, IconType iconType, boolean isArchive) {
+    ExperimentFileType(String id, String description, IconType iconType, boolean isArchive) {
         this.id = id;
-        this.name = name;
         this.description = description;
         this.iconType = iconType;
         this.isArchive = isArchive;
@@ -31,10 +30,6 @@ public enum ExperimentFileType {
 
     public String getId() {
         return id;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public String getDescription() {
