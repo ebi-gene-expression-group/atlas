8a26c5c4e212c7a9345fdb783d441df42e956620
Monica Jianu <monica@ebi.ac.uk>
2018-04-23 12:58:22 +0100

    Added forgotten test.

---
+++ base/src/test/java/uk/ac/ebi/atlas/model/experiment/baseline/CellTest.java
@@ -0,0 +1,18 @@
+package uk.ac.ebi.atlas.model.experiment.baseline;
+
+import com.google.common.collect.ImmutableSet;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+
+public class CellTest {
+
+    private Cell subject = new Cell("123");
+
+    @Test
+    public void returnsValidData() {
+        assertThat(subject.assaysAnalyzedForThisDataColumn().size(), is(1));
+        assertThat(subject.assaysAnalyzedForThisDataColumn(), is(ImmutableSet.of("123")));
+        assertThat(subject.biologicalReplicatesForThisDataColumn().isEmpty(), is(true));
+    }
+}
