853d87b3f82ba5650e4d37ca0d52542fc783127f
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2018-04-25 12:21:48 +0100

    Remove streamer factory classes, they’re totally superfluous

--- base/src/main/java/uk/ac/ebi/atlas/experimentimport/analytics/singlecell/SingleCellAnalyticsLoader.java
+++ base/src/main/java/uk/ac/ebi/atlas/experimentimport/analytics/singlecell/SingleCellAnalyticsLoader.java
@@ -1,55 +1,51 @@
 package uk.ac.ebi.atlas.experimentimport.analytics.singlecell;
 
+import org.springframework.transaction.annotation.Transactional;
 import uk.ac.ebi.atlas.experimentimport.analytics.AnalyticsLoader;
 import uk.ac.ebi.atlas.experimentimport.analytics.singlecell.clusters.ClustersStreamer;
-import uk.ac.ebi.atlas.experimentimport.analytics.singlecell.clusters.ClustersStreamerFactory;
 import uk.ac.ebi.atlas.experimentimport.analytics.singlecell.clusters.ClustersDao;
 import uk.ac.ebi.atlas.experimentimport.analytics.singlecell.tsne.TSnePlotDao;
 import uk.ac.ebi.atlas.experimentimport.analytics.singlecell.tsne.TSnePlotStreamer;
-import uk.ac.ebi.atlas.experimentimport.analytics.singlecell.tsne.TSnePlotStreamerFactory;
+import uk.ac.ebi.atlas.resource.DataFileHub;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 
 @Named
 public class SingleCellAnalyticsLoader implements AnalyticsLoader {
+    private final DataFileHub dataFileHub;
     private final SingleCellAnalyticsDao analyticsDao;
-    private final SingleCellAnalyticsStreamerFactory analyticsStreamerFactory;
-
     private final TSnePlotDao tSnePlotDao;
-    private final TSnePlotStreamerFactory tSnePlotStreamFactory;
-
-
     private final ClustersDao clustersDao;
-    private final ClustersStreamerFactory clustersStreamerFactory;
 
     @Inject
-    SingleCellAnalyticsLoader(SingleCellAnalyticsDao analyticsDao,
-                              SingleCellAnalyticsStreamerFactory analyticsStreamerFactory,
+    SingleCellAnalyticsLoader(DataFileHub dataFileHub,
+                              SingleCellAnalyticsDao analyticsDao,
                               TSnePlotDao tSnePlotDao,
-                              TSnePlotStreamerFactory tSnePlotStreamFactory,
-                              ClustersDao clustersDao,
-                              ClustersStreamerFactory clustersStreamerFactory) {
+                              ClustersDao clustersDao) {
+        this.dataFileHub = dataFileHub;
         this.analyticsDao = analyticsDao;
-        this.analyticsStreamerFactory = analyticsStreamerFactory;
-
         this.tSnePlotDao = tSnePlotDao;
-        this.tSnePlotStreamFactory = tSnePlotStreamFactory;
-
         this.clustersDao = clustersDao;
-        this.clustersStreamerFactory = clustersStreamerFactory;
     }
 
+    @Transactional
     @Override
-    public void loadAnalytics(String accession) {
-        try (SingleCellAnalyticsStreamer analyticsStreamer = analyticsStreamerFactory.create(accession);
-             TSnePlotStreamer tSnePlotStreamer = tSnePlotStreamFactory.create(accession);
-             ClustersStreamer clustersStreamer = clustersStreamerFactory.create(accession)) {
-            analyticsDao.loadAnalytics(accession, analyticsStreamer.get());
+    public void loadAnalytics(String experimentAccession) {
+        DataFileHub.SingleCellExperimentFiles files = dataFileHub.getSingleCellExperimentFiles(experimentAccession);
+
+        try (SingleCellAnalyticsStreamer analyticsStreamer =
+                     new SingleCellAnalyticsStreamer(files.tpmsMatrix, files.geneIdsTsv, files.cellIdsTsv);
+             TSnePlotStreamer tSnePlotStreamer = new TSnePlotStreamer(files.tSnePlotTsvs);
+             ClustersStreamer clustersStreamer = new ClustersStreamer(files.clustersTsv)) {
+
+            analyticsDao.loadAnalytics(experimentAccession, analyticsStreamer.get());
             tSnePlotStreamer.availablePerplexities()
-                    .forEach(p -> tSnePlotDao.loadTSnePlot(accession, p, tSnePlotStreamer.stream(p)));
-            clustersStreamer.get()
-                    .forEach(pair -> clustersDao.loadClusters(accession, pair.getLeft(), pair.getRight().stream()));
+                    .forEach(p -> tSnePlotDao.loadTSnePlot(experimentAccession, p, tSnePlotStreamer.stream(p)));
+            clustersStreamer.stream()
+                    .forEach(pair ->
+                            clustersDao.loadClusters(experimentAccession, pair.getLeft(), pair.getRight().stream()));
+
         }
     }
 
