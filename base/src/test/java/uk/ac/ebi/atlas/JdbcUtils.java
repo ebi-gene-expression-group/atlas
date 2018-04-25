5d3b3f6155be67941051c82919357a34a3a1cb09
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2018-04-25 20:12:22 +0100

    Add a few more utilities to the testing DB helper class; add a couple of tests to test for data sanity supplied by TSnePlotServiceDao

--- base/src/test/java/uk/ac/ebi/atlas/JdbcUtils.java
+++ base/src/test/java/uk/ac/ebi/atlas/JdbcUtils.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+import static java.sql.Types.VARCHAR;
+
 @Component
 public class JdbcUtils {
     private JdbcTemplate jdbcTemplate;
@@ -19,5 +21,32 @@ public class JdbcUtils {
 
     public List<String> getExpressionAtlasExperimentAccessions() {
         return jdbcTemplate.queryForList("SELECT accession FROM experiment", String.class);
+    }
+
+    public String fetchRandomExperimentAccession() {
+        return jdbcTemplate.queryForObject(
+                "SELECT accession FROM scxa_experiment ORDER BY RANDOM() LIMIT 1",
+                String.class);
+    }
+
+    public String fetchRandomGeneFromExperiment(String experimentAccession) {
+         return jdbcTemplate.queryForObject(
+                 "SELECT gene_id FROM scxa_analytics WHERE experiment_accession=? ORDER BY RANDOM() LIMIT 1",
+                 String.class,
+                 experimentAccession);
+    }
+
+    public int fetchRandomPerplexityFromExperimentTSne(String experimentAccession) {
+        return jdbcTemplate.queryForObject(
+                "SELECT perplexity FROM scxa_tsne WHERE experiment_accession=? ORDER BY RANDOM() LIMIT 1",
+                Integer.class,
+                experimentAccession);
+    }
+
+    public int fetchRandomKFromCellClusters(String experimentAccession) {
+        return jdbcTemplate.queryForObject(
+                "SELECT k FROM scxa_cell_clusters WHERE experiment_accession=? ORDER BY RANDOM() LIMIT 1",
+                Integer.class,
+                experimentAccession);
     }
 }
---
+++ base/src/test/java/uk/ac/ebi/atlas/experimentpage/baseline/tsne/TSnePlotServiceDaoIT.java
@@ -0,0 +1,64 @@
+package uk.ac.ebi.atlas.experimentpage.baseline.tsne;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.ac.ebi.atlas.JdbcUtils;
+
+import javax.inject.Inject;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration("classpath:applicationContext.xml")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class TSnePlotServiceDaoIT {
+    @Inject
+    private JdbcTemplate jdbcTemplate;
+
+    @Inject
+    private JdbcUtils jdbcTestUtils;
+
+    @Inject
+    private TSnePlotServiceDao subject;
+
+    @Test
+    void testExpression() {
+        String experimentAccession = jdbcTestUtils.fetchRandomExperimentAccession();
+        String geneId = jdbcTestUtils.fetchRandomGeneFromExperiment(experimentAccession);
+        int perplexity = jdbcTestUtils.fetchRandomPerplexityFromExperimentTSne(experimentAccession);
+
+        assertThat(subject.fetchTSnePlotWithExpression(experimentAccession, perplexity, geneId))
+                .isNotEmpty()
+                .doesNotHaveDuplicates()
+                .allMatch(tSnePointDto -> tSnePointDto.expressionLevel() > 0.0)
+                .extracting("name")
+                // There are potentially more cells in the TPMs file than in the t-SNE plot files
+                .isSubsetOf(fetchCellIds(experimentAccession));
+    }
+
+    @Test
+    void testClusters() {
+        String experimentAccession = jdbcTestUtils.fetchRandomExperimentAccession();
+        int k = jdbcTestUtils.fetchRandomKFromCellClusters(experimentAccession);
+        int perplexity = jdbcTestUtils.fetchRandomPerplexityFromExperimentTSne(experimentAccession);
+
+        assertThat(subject.fetchTSnePlotWithClusters(experimentAccession, perplexity, k))
+                .isNotEmpty()
+                .doesNotHaveDuplicates()
+                .allMatch(tSnePointDto -> tSnePointDto.clusterK() <= k)
+                .extracting("name")
+                .isSubsetOf(fetchCellIds(experimentAccession));
+    }
+
+    private final static String SELECT_CELL_IDS_STATEMENT =
+            "SELECT DISTINCT(cell_id) FROM scxa_analytics WHERE experiment_accession=?";
+    private List<String> fetchCellIds(String experiment_accession) {
+        return jdbcTemplate.queryForList(SELECT_CELL_IDS_STATEMENT, String.class, experiment_accession);
+    }
+}
