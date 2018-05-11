87238e4a5457dcc6404599b854b1d8179c6c0350
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2018-05-11 23:59:00 +0100

    Remove dead code and stale imports

--- base/src/main/java/uk/ac/ebi/atlas/solr/cloud/fullanalytics/SingleCellAnalyticsCollectionProxy.java
+++ base/src/main/java/uk/ac/ebi/atlas/solr/cloud/fullanalytics/SingleCellAnalyticsCollectionProxy.java
@@ -1,10 +1,7 @@
 package uk.ac.ebi.atlas.solr.cloud.fullanalytics;
 
 import org.apache.solr.client.solrj.SolrClient;
-import org.apache.solr.client.solrj.SolrQuery;
-import org.apache.solr.client.solrj.response.FieldStatsInfo;
 import org.apache.solr.client.solrj.response.QueryResponse;
-import uk.ac.ebi.atlas.solr.BioentityPropertyName;
 import uk.ac.ebi.atlas.solr.cloud.CollectionProxy;
 import uk.ac.ebi.atlas.solr.cloud.SchemaField;
 import uk.ac.ebi.atlas.solr.cloud.search.SolrQueryBuilder;
@@ -17,9 +14,7 @@ public class SingleCellAnalyticsCollectionProxy extends CollectionProxy {
     }
 
     public static final SingleCellAnalyticsSchemaField CELL_ID = new SingleCellAnalyticsSchemaField("cell_id");
-    public static final SingleCellAnalyticsSchemaField CHARACTERISTICS = new SingleCellAnalyticsSchemaField("characteristics");
     public static final SingleCellAnalyticsSchemaField CHARACTERISTIC_INFERRED_CELL_TYPE = new SingleCellAnalyticsSchemaField("characteristic_inferred_cell_type");
-    public static final SingleCellAnalyticsSchemaField CHARACTERISTIC_INDIVIDUAL = new SingleCellAnalyticsSchemaField("characteristic_individual");
     public static final SingleCellAnalyticsSchemaField FACTORS = new SingleCellAnalyticsSchemaField("factors");
     public static final SingleCellAnalyticsSchemaField EXPERIMENT_ACCESSION = new SingleCellAnalyticsSchemaField("experiment_accession");
 
@@ -37,9 +32,5 @@ public class SingleCellAnalyticsCollectionProxy extends CollectionProxy {
 
     public QueryResponse query(SolrQueryBuilder<SingleCellAnalyticsCollectionProxy> solrQueryBuilder) {
         return query(solrQueryBuilder.build());
-    }
-
-    public FieldStatsInfo fieldStats(SchemaField<SingleCellAnalyticsCollectionProxy> field, SolrQuery solrQuery) {
-        return super.fieldStats(field.name(), solrQuery);
     }
 }
