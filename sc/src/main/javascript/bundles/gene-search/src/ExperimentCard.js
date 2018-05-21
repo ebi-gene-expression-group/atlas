08972c3b6c4ac08a73089c903e02358799baa905
Monica Jianu <monica@ebi.ac.uk>
2018-05-21 23:07:26 +0100

    Added css file for ExperimentCard component. Implemented naive string truncation for experiment description. Wrapped entire div in link tag.

--- sc/src/main/javascript/bundles/gene-search/src/ExperimentCard.js
+++ sc/src/main/javascript/bundles/gene-search/src/ExperimentCard.js
@@ -1,22 +1,42 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const ExperimentCard = ({experimentAccession, species, experimentDescription, type}) => {
+import '!style-loader!css-loader!./ExperimentCard.css'
+
+// Naive multi-line string clamping
+const truncate = (string) => {
+  if (string.length > 300) {
+    return `${string.substring(0, 300)}...`
+  }
+  else {
+    return string
+  }
+}
+
+const ExperimentCard = ({experimentAccession, url, species, experimentDescription, type, longDescription}) => {
   return (
-    <div style={{ border: `2px solid grey`, marginBottom: `0.5rem`, borderRadius: `4px`, padding: `0.25rem` }}>
-      <a href={`http://localhost:8080/scxa/experiments/${experimentAccession}`}>
-        <p>{experimentAccession}: {experimentDescription}</p>
+    <div className={"experiment-card"}>
+      <a href={url}>
+        <h5>
+            <p>{experimentAccession}: {experimentDescription}</p>
+        </h5>
+        <p><i>Species:</i> {species}</p>
+        {
+          longDescription &&
+          <p><i>Description:</i> {truncate(longDescription)}</p>
+        }
       </a>
 
-      <p>Species : {species}</p>
     </div>
   )
 }
 
 ExperimentCard.propTypes = {
   experimentAccession: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
   species: PropTypes.string.isRequired,
   experimentDescription: PropTypes.string.isRequired,
+  longDescription: PropTypes.string,
   type: PropTypes.string.isRequired
 }
 
