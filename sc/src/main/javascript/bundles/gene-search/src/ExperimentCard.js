ea051cec1b371a76d75a5bd92441018804829863
Monica Jianu <monica@ebi.ac.uk>
2018-05-22 10:41:38 +0100

    Changed ExperimentCard to extend React.Component. Added read more button on experiment description. Added handling of state when click the read more/read less button. Styled read more button.

--- sc/src/main/javascript/bundles/gene-search/src/ExperimentCard.js
+++ sc/src/main/javascript/bundles/gene-search/src/ExperimentCard.js
@@ -5,31 +5,69 @@ import '!style-loader!css-loader!./ExperimentCard.css'
 
 // Naive multi-line string clamping
 const truncate = (string) => {
-  if (string.length > 300) {
-    return `${string.substring(0, 300)}...`
+  if (isDescriptionTooLong(string)) {
+    return `${string.substring(0, 300)}... `
   }
   else {
     return string
   }
 }
 
-const ExperimentCard = ({experimentAccession, url, species, experimentDescription, type, longDescription, lastUpdated}) => {
-  return (
-    <div className={"experiment-card"}>
+const isDescriptionTooLong = (description) => {
+  return description.length > 300
+}
+
+class ExperimentCard extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      expanded: false,
+      descriptionShown: truncate(this.props.longDescription)
+    }
+
+    this._setExpanded = this._setExpanded.bind(this);
+  }
+
+  _setExpanded() {
+    this.setState(previousState => ({
+      descriptionShown: !previousState.expanded ? this.props.longDescription : truncate(this.props.longDescription),
+      expanded: !previousState.expanded
+    }))
+  }
+
+  render() {
+    const {experimentAccession, url, species, experimentDescription, longDescription, lastUpdated} = this.props
+    const {expanded, descriptionShown} = this.state
+
+    return (
+      <div className={"experiment-card"}>
       <span className={"label"}>
           {lastUpdated} | <i>{species}</i>
         </span>
-      <h5>
-        <a href={url}>{experimentAccession}: {experimentDescription}</a>
-      </h5>
+        <h5>
+          <a href={url}>{experimentAccession}: {experimentDescription}</a>
+        </h5>
 
-      {
-        longDescription &&
-        <p><i>About the experiment:</i> {truncate(longDescription)}</p>
-      }
+        {
+          longDescription &&
+          <p>
+            <i>About the experiment: </i>
+            {descriptionShown}
+            {
+              isDescriptionTooLong(longDescription) &&
+              <button className={'read-more'}  onClick={this._setExpanded}>
+                {!expanded ? `(Read more)`  : `(Read less)`}
+              </button>
+            }
 
-    </div>
-  )
+
+          </p>
+        }
+
+      </div>
+    )
+  }
 }
 
 ExperimentCard.propTypes = {
