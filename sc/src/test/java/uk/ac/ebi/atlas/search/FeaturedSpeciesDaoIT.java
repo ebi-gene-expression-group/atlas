cc112909dc0d763fe5c9e29f5ffe7c63eb31e1b4
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2018-05-24 14:36:41 +0100

    Use declarative support of SQL statements to initialize tables for testing Addresses issue raised in https://github.com/ebi-gene-expression-group/atlas/pull/97

--- sc/src/test/java/uk/ac/ebi/atlas/search/FeaturedSpeciesDaoIT.java
+++ sc/src/test/java/uk/ac/ebi/atlas/search/FeaturedSpeciesDaoIT.java
@@ -3,18 +3,14 @@ package uk.ac.ebi.atlas.search;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.support.EncodedResource;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.jdbc.JdbcTestUtils;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.inject.Inject;
-
-import java.io.LineNumberReader;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -25,33 +21,22 @@ class FeaturedSpeciesDaoIT {
     @Inject
     private JdbcTemplate jdbcTemplate;
 
-    // @Inject
-    // @Qualifier("dataSource")
-    // private DataSource dataSource;
-
     private FeaturedSpeciesDao subject;
 
     @BeforeEach
     void setUp() {
-        JdbcTestUtils.deleteFromTables(jdbcTemplate, "scxa_experiment");
         subject = new FeaturedSpeciesDao(jdbcTemplate);
     }
 
     @Test
     void whenNoExperimentsAreLoadedResultsAreEmpty() {
+        JdbcTestUtils.deleteFromTables(jdbcTemplate, "scxa_experiment");
         assertThat(subject.fetchSpeciesSortedByExperimentCount()).isEmpty();
     }
 
     @Test
-    void sortsSpeciesNamesByNumberOfExperiments() throws Exception {
-        EncodedResource resource = new EncodedResource(new ClassPathResource("uk/ac/ebi/atlas/search/scxa_experiment_fixture.sql"));
-        LineNumberReader lnr = new LineNumberReader(resource.getReader());
-        jdbcTemplate.execute(ScriptUtils.readScript(lnr, "--", ";"));
-
-        // I would’ve loved to use ScriptUtils.executeSqlScript with dataSource, see above, but when it reaches
-        // org.postgresql.core.v3.QueryExecutorImpl::processResults, it hangs in the line c = pgStream.receiveChar();
-        // Ain’t nobody got time for that!
-        // ScriptUtils.executeSqlScript(dataSource.getConnection(), resource);
+    @Sql("scxa_experiment_fixture.sql")
+    void sortsSpeciesNamesByNumberOfExperiments() {
         assertThat(subject.fetchSpeciesSortedByExperimentCount())
                 .containsExactly("Mus musculus", "Homo sapiens");
     }
