c330400f77580d160e1f277586c3fd10a45cb0c7
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2018-05-24 14:40:01 +0100

    Remove the separator from the back end and let the React component render it

--- sc/src/main/java/uk/ac/ebi/atlas/search/AutocompleteController.java
+++ sc/src/main/java/uk/ac/ebi/atlas/search/AutocompleteController.java
@@ -14,16 +14,11 @@ import uk.ac.ebi.atlas.controllers.JsonExceptionHandlingController;
 import uk.ac.ebi.atlas.solr.bioentities.query.SolrBioentitiesSuggesterService;
 import uk.ac.ebi.atlas.species.SpeciesFactory;
 
-import java.util.concurrent.ThreadLocalRandom;
-
 import static uk.ac.ebi.atlas.utils.GsonProvider.GSON;
 
 @RestController
 @Scope("request")
 public class AutocompleteController extends JsonExceptionHandlingController {
-    private static final String NORMAL_SEPARATOR = "━━━━━━━━━━━━";
-    private static final String BEST_SEPARATOR = "(╯°□°）╯︵ ┻━┻";
-
     protected static final int FEATURED_SPECIES = 0;
 
     private final SolrBioentitiesSuggesterService suggesterService;
@@ -70,7 +65,6 @@ public class AutocompleteController extends JsonExceptionHandlingController {
         return GSON.toJson(
                 ImmutableMap.of(
                         "topSpecies", topSpeciesNames.subList(0, Math.min(topSpeciesNames.size(), FEATURED_SPECIES)),
-                        "allSpecies", ImmutableList.sortedCopyOf(topSpeciesNames),
-                        "separator", ThreadLocalRandom.current().nextDouble() > 0.999 ? BEST_SEPARATOR : NORMAL_SEPARATOR));
+                        "allSpecies", ImmutableList.sortedCopyOf(topSpeciesNames)));
     }
 }
