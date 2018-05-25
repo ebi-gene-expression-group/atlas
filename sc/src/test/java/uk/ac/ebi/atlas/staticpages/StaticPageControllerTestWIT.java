cccfbee56f00d33f7f0338919102ad48cf76ad73
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2018-05-25 11:30:08 +0100

    Boilerplate code to show help pages and other static HTMLs

---
+++ sc/src/test/java/uk/ac/ebi/atlas/staticpages/StaticPageControllerTestWIT.java
@@ -0,0 +1,58 @@
+package uk.ac.ebi.atlas.staticpages;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.stream.Stream;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@ExtendWith(SpringExtension.class)
+@WebAppConfiguration
+@ContextConfiguration(locations = {"classpath:applicationContext.xml", "classpath:dispatcher-servlet.xml"})
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class StaticPageControllerTestWIT {
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @ParameterizedTest
+    @MethodSource("staticPageUrlProvider")
+    void validUrlsAreMappedToStaticView(String url) throws Exception {
+        this.mockMvc
+                .perform(get(url))
+                .andExpect(status().isOk())
+                .andExpect(view().name("static"));
+    }
+
+    @Test
+    void invalidUrlsAreMappedToErrorView() throws Exception {
+        this.mockMvc
+                .perform(get("/foo.html"))
+                .andExpect(status().isNotFound())
+                .andExpect(view().name("error-page"));
+    }
+
+    private Stream<String> staticPageUrlProvider() {
+        return Stream.of("/help.html");
+    }
+}
