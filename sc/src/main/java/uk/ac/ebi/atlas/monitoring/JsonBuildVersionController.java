5729a1689fbac9d14fcf3bcb3bc6684e8ebaa6e6
Monica Jianu <monica@ebi.ac.uk>
2018-06-04 10:19:20 +0100

    Refactored BuildVersionController: changed name and simplified map creation logic. Added integration test.

--- sc/src/main/java/uk/ac/ebi/atlas/monitoring/JsonBuildVersionController.java
+++ sc/src/main/java/uk/ac/ebi/atlas/monitoring/JsonBuildVersionController.java
@@ -1,5 +1,6 @@
 package uk.ac.ebi.atlas.monitoring;
 
+import com.google.common.collect.ImmutableMap;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,22 +9,20 @@ import org.springframework.web.bind.annotation.RestController;
 import uk.ac.ebi.atlas.controllers.JsonExceptionHandlingController;
 
 import javax.inject.Inject;
-import java.util.HashMap;
-import java.util.Map;
 
 import static uk.ac.ebi.atlas.utils.GsonProvider.GSON;
 
 @RestController
-public class BuildVersionController extends JsonExceptionHandlingController {
+public class JsonBuildVersionController extends JsonExceptionHandlingController {
 
     private String buildNumber;
     private String buildBranch;
     private String buildCommitId;
 
     @Inject
-    public BuildVersionController(@Value("#{configuration['buildNumber']}") String buildNumber,
-                                  @Value("#{configuration['buildBranch']}") String buildBranch,
-                                  @Value("#{configuration['buildCommitId']}") String buildCommitId) {
+    public JsonBuildVersionController(@Value("#{configuration['buildNumber']}") String buildNumber,
+                                      @Value("#{configuration['buildBranch']}") String buildBranch,
+                                      @Value("#{configuration['buildCommitId']}") String buildCommitId) {
         this.buildNumber = buildNumber;
         this.buildBranch = buildBranch;
         this.buildCommitId = buildCommitId;
@@ -35,12 +34,10 @@ public class BuildVersionController extends JsonExceptionHandlingController {
             produces = MediaType.APPLICATION_JSON_UTF8_VALUE
     )
     public String getBuildNumber() {
-        Map<String, String> buildInformation = new HashMap<>();
-
-        buildInformation.put("Bamboo build version", buildNumber);
-        buildInformation.put("Git branch", buildBranch);
-        buildInformation.put("Git commit ID", buildCommitId);
-
-        return GSON.toJson(buildInformation);
+        return GSON.toJson(
+                ImmutableMap.of(
+                        "bambooBuildVersion", buildNumber,
+                        "gitBranch", buildBranch,
+                        "gitCommitID", buildCommitId));
     }
 }
