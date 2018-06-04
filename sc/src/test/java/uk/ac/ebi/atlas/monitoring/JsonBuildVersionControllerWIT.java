0a4269e17d6693b6000aed7c4da6a26124d2f1b4
Monica Jianu <monica@ebi.ac.uk>
2018-06-04 16:49:42 +0100

    Build information endpoint (#106)
    * Added build information variables. Added new JSON endpoint to expose the build information.
    
    * Refactored BuildVersionController: changed name and simplified map creation logic. Added integration test.
    
    (cherry picked from commit e433c8b)

---
+++ sc/src/test/java/uk/ac/ebi/atlas/monitoring/JsonBuildVersionControllerWIT.java
@@ -0,0 +1,46 @@
+package uk.ac.ebi.atlas.monitoring;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.isA;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebAppConfiguration
+@ContextConfiguration(locations = {"classpath:applicationContext.xml", "classpath:dispatcher-servlet.xml"})
+public class JsonBuildVersionControllerWIT {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void requestReturnsBuildInfo() throws Exception {
+        mockMvc.perform(get("/json/build"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.bambooBuildVersion", isA(String.class)))
+                .andExpect(jsonPath("$.gitBranch", isA(String.class)))
+                .andExpect(jsonPath("$.gitCommitID", isA(String.class)))
+        ;
+    }
+}
