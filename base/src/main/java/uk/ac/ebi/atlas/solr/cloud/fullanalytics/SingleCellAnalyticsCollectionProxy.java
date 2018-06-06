6e4aba2aad3fc9930ae8833f5b9f099ba7908f11
Monica Jianu <monica@ebi.ac.uk>
2018-06-06 16:25:32 +0100

    Added displayName property to SingleCellAnalyticsSchemaField such that fields can be effectively renamed without changing process in the data production side.

--- base/src/main/java/uk/ac/ebi/atlas/solr/cloud/fullanalytics/SingleCellAnalyticsCollectionProxy.java
+++ base/src/main/java/uk/ac/ebi/atlas/solr/cloud/fullanalytics/SingleCellAnalyticsCollectionProxy.java
@@ -8,28 +8,36 @@ import uk.ac.ebi.atlas.solr.cloud.search.SolrQueryBuilder;
 
 public class SingleCellAnalyticsCollectionProxy extends CollectionProxy {
     public static class SingleCellAnalyticsSchemaField extends SchemaField<SingleCellAnalyticsCollectionProxy> {
-        private SingleCellAnalyticsSchemaField(String fieldName) {
+        private String displayName;
+
+        private SingleCellAnalyticsSchemaField(String fieldName, String displayName) {
             super(fieldName);
+            this.displayName = displayName;
+        }
+
+        public String displayName() {
+            return displayName;
         }
     }
 
-    public static final SingleCellAnalyticsSchemaField CELL_ID = new SingleCellAnalyticsSchemaField("cell_id");
-    public static final SingleCellAnalyticsSchemaField CHARACTERISTIC_INFERRED_CELL_TYPE = new SingleCellAnalyticsSchemaField("characteristic_inferred_cell_type");
-    public static final SingleCellAnalyticsSchemaField CHARACTERISTIC_ORGANISM_PART = new SingleCellAnalyticsSchemaField("characteristic_organism_part");
-    public static final SingleCellAnalyticsSchemaField FACTORS = new SingleCellAnalyticsSchemaField("factors");
-    public static final SingleCellAnalyticsSchemaField EXPERIMENT_ACCESSION = new SingleCellAnalyticsSchemaField("experiment_accession");
+    public static final SingleCellAnalyticsSchemaField CELL_ID = new SingleCellAnalyticsSchemaField("cell_id", "Cell ID");
+    public static final SingleCellAnalyticsSchemaField CHARACTERISTIC_INFERRED_CELL_TYPE = new SingleCellAnalyticsSchemaField("characteristic_inferred_cell_type", "Inferred cell type");
+    public static final SingleCellAnalyticsSchemaField CHARACTERISTIC_SPECIES = new SingleCellAnalyticsSchemaField("characteristic_organism", "Species");
+    public static final SingleCellAnalyticsSchemaField CHARACTERISTIC_ORGANISM_PART = new SingleCellAnalyticsSchemaField("characteristic_organism_part", "Organism part");
+    public static final SingleCellAnalyticsSchemaField FACTORS = new SingleCellAnalyticsSchemaField("factors", "Factors");
+    public static final SingleCellAnalyticsSchemaField EXPERIMENT_ACCESSION = new SingleCellAnalyticsSchemaField("experiment_accession", "Experiment accession");
 
     public SingleCellAnalyticsCollectionProxy(SolrClient solrClient) {
         // scxa-analytics is an alias that points at scxa-analytics-vX
         super(solrClient, "scxa-analytics");
     }
 
-    public static SingleCellAnalyticsCollectionProxy.SingleCellAnalyticsSchemaField characteristicAsSchemaField(String characteristic) {
-        return new SingleCellAnalyticsCollectionProxy.SingleCellAnalyticsSchemaField(String.format("characteristic_%s", characteristic));
+    public static SingleCellAnalyticsCollectionProxy.SingleCellAnalyticsSchemaField characteristicAsSchemaField(String characteristic, String displayName) {
+        return new SingleCellAnalyticsCollectionProxy.SingleCellAnalyticsSchemaField(String.format("characteristic_%s", characteristic), displayName);
     }
 
-    public static SingleCellAnalyticsCollectionProxy.SingleCellAnalyticsSchemaField factorAsSchemaField(String factor) {
-        return new SingleCellAnalyticsCollectionProxy.SingleCellAnalyticsSchemaField(String.format("factor_%s", factor));
+    public static SingleCellAnalyticsCollectionProxy.SingleCellAnalyticsSchemaField factorAsSchemaField(String factor, String displayName) {
+        return new SingleCellAnalyticsCollectionProxy.SingleCellAnalyticsSchemaField(String.format("factor_%s", factor), displayName);
     }
 
     public QueryResponse query(SolrQueryBuilder<SingleCellAnalyticsCollectionProxy> solrQueryBuilder) {
