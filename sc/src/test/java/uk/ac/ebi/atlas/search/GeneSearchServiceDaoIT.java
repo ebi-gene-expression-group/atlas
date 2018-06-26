d56e1b2242914f393185a523b29c869b1513b810
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2018-06-26 15:30:56 +0100

    Make tests parameterized to have better feedback

--- sc/src/test/java/uk/ac/ebi/atlas/search/GeneSearchServiceDaoIT.java
+++ sc/src/test/java/uk/ac/ebi/atlas/search/GeneSearchServiceDaoIT.java
@@ -2,26 +2,29 @@ package uk.ac.ebi.atlas.search;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
-import uk.ac.ebi.atlas.configuration.JdbcConfig;
-import uk.ac.ebi.atlas.configuration.SolrConfig;
-import uk.ac.ebi.atlas.configuration.WebConfig;
 import uk.ac.ebi.atlas.solr.cloud.SolrCloudCollectionProxyFactory;
 import uk.ac.ebi.atlas.testutils.JdbcUtils;
 
 import javax.inject.Inject;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Stream;
 
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.data.MapEntry.entry;
 import static uk.ac.ebi.atlas.solr.cloud.fullanalytics.SingleCellAnalyticsCollectionProxy.CHARACTERISTIC_INFERRED_CELL_TYPE;
@@ -31,12 +34,12 @@ import static uk.ac.ebi.atlas.solr.cloud.fullanalytics.SingleCellAnalyticsCollec
 @Transactional
 @WebAppConfiguration
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = {WebConfig.class})
-public class GeneSearchServiceDaoIT {
-    private static final Logger LOGGER = LoggerFactory.getLogger(GeneSearchServiceDaoIT.class);
-
+@ContextConfiguration(locations = {"classpath:applicationContext.xml", "classpath:dispatcher-servlet.xml"})
+@TestInstance(Lifecycle.PER_CLASS)
+class GeneSearchServiceDaoIT {
     @Inject
     private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
     @Inject
     private SolrCloudCollectionProxyFactory solrCloudCollectionProxyFactory;
 
@@ -46,26 +49,21 @@ public class GeneSearchServiceDaoIT {
     private GeneSearchServiceDao subject;
 
     @BeforeEach
-    public void setUp() {
+    void setUp() {
         subject = new GeneSearchServiceDao(namedParameterJdbcTemplate, solrCloudCollectionProxyFactory);
     }
 
-    @Test
-    public void validGeneIdReturnsCellIdsPerExperiment() {
-        String geneId = jdbcTestUtils.fetchRandomGene();
-
-        LOGGER.info("Retrieving cell IDs for gene ID " + geneId);
-
-        Map<String, List<String>> result = subject.fetchCellIds(geneId);
-
-        assertThat(result).isNotEmpty();
+    @ParameterizedTest
+    @MethodSource("randomGeneIdProvider")
+    void validGeneIdReturnsAtLeastOneCellId(String geneId) {
+        assertThat(subject.fetchCellIds(geneId))
+                .isNotEmpty();
     }
 
-    @Test
+    @ParameterizedTest
     @Sql({"scxa_experiment_fixture.sql", "scxa_marker_genes_fixture.sql"})
-    public void validGeneIdReturnKAndClusterIds() {
-        String geneId = "ENSG00000000009";
-
+    @ValueSource(strings = {"ENSG00000000009"})
+    void validGeneIdReturnsKAndClusterIds(String geneId) {
         Map<String, Map<Integer, List<Integer>>> result = subject.fetchKAndClusterIds(geneId);
 
         assertThat(result)
@@ -75,39 +73,52 @@ public class GeneSearchServiceDaoIT {
         // Only marker genes with probablity < 0.05 are returned
         assertThat(result.get("E-GEOD-106540"))
                 .containsOnly(
-                        entry(3, Collections.singletonList(0)),
-                        entry(2, Collections.singletonList(1)));
+                        entry(3, singletonList(0)),
+                        entry(2, singletonList(1)));
 
     }
 
-    @Test
+    @ParameterizedTest
     @Sql({"scxa_experiment_fixture.sql", "scxa_marker_genes_fixture.sql"})
-    public void searchForGeneOverProbabilityThresholdReturnsEmpty() {
-        String geneId = "ENSMUSG00000000006";
-
-        Map<String, Map<Integer, List<Integer>>> result = subject.fetchKAndClusterIds(geneId);
-
-        assertThat(result).isEmpty();
+    @ValueSource(strings = {"ENSMUSG00000000006"})
+    void searchForGeneOverProbabilityThresholdReturnsEmpty(String geneId) {
+        assertThat(subject.fetchKAndClusterIds(geneId))
+                .isEmpty();
     }
 
-    @Test
-    public void invalidGeneIdReturnsEmpty() {
-        String geneId = "FOO";
-
-        Map<String, Map<Integer, List<Integer>>> result = subject.fetchKAndClusterIds(geneId);
-
-        assertThat(result).isEmpty();
+    @ParameterizedTest
+    @ValueSource(strings = {"FOO"})
+    void invalidGeneIdReturnsEmpty(String geneId) {
+        assertThat(subject.fetchKAndClusterIds(geneId))
+                .isEmpty();
     }
 
-
-    @Test
-    public void getFacetsForValidCellIds() {
-        List<String> cellIds = jdbcTestUtils.fetchRandomListOfCells(10);
-
-        LOGGER.info("Retrieving facets for cell IDs " + String.join(", ", cellIds));
-
-        Map<String, Map<String, List<String>>> result = subject.getFacets(cellIds, CHARACTERISTIC_INFERRED_CELL_TYPE, CHARACTERISTIC_ORGANISM_PART, CHARACTERISTIC_SPECIES);
+    @ParameterizedTest
+    @MethodSource("randomCellIdsProvider")
+    void getFacetsForValidCellIds(List<String> cellIds) {
+        Map<String, Map<String, List<String>>> result =
+                subject.getFacets(
+                        cellIds,
+                        CHARACTERISTIC_INFERRED_CELL_TYPE, CHARACTERISTIC_ORGANISM_PART, CHARACTERISTIC_SPECIES);
 
         assertThat(result).isNotEmpty();
+    }
+
+    @Test
+    void getForEmptyListOfCellIdsReturnsEmpty() {
+        Map<String, Map<String, List<String>>> result =
+                subject.getFacets(
+                        emptyList(),
+                        CHARACTERISTIC_INFERRED_CELL_TYPE, CHARACTERISTIC_ORGANISM_PART, CHARACTERISTIC_SPECIES);
+
+        assertThat(result).isEmpty();
+    }
+
+    private Stream<String> randomGeneIdProvider() {
+        return Stream.of(jdbcTestUtils.fetchRandomGene());
+    }
+
+    private Stream<List<String>> randomCellIdsProvider() {
+        return Stream.of(jdbcTestUtils.fetchRandomListOfCells(ThreadLocalRandom.current().nextInt(1, 100)));
     }
 }
