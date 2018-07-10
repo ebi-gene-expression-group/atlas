2e4a0024ae3f4bc704f7dabc3891bfb2f6222860
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2018-07-10 16:53:47 +0100

    Configure web application context using @Configuration classes

---
+++ gxa/src/main/java/uk/ac/ebi/atlas/configuration/WebConfig.java
@@ -0,0 +1,74 @@
+package uk.ac.ebi.atlas.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesView;
+import uk.ac.ebi.atlas.resource.DataFileHub;
+import uk.ac.ebi.atlas.search.SemanticQuery;
+import uk.ac.ebi.atlas.search.SemanticQueryPropertyEditor;
+import uk.ac.ebi.atlas.web.interceptors.AdminInterceptor;
+import uk.ac.ebi.atlas.web.interceptors.TimingInterceptor;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@ComponentScan(basePackages = "uk.ac.ebi.atlas")
+@EnableWebMvc
+@ControllerAdvice
+public class WebConfig extends WebMvcConfigurerAdapter {
+    private final AdminInterceptor adminInterceptor;
+    private final TimingInterceptor timingInterceptor;
+    private final DataFileHub dataFileHub;
+
+    public WebConfig(AdminInterceptor adminInterceptor, TimingInterceptor timingInterceptor, DataFileHub dataFileHub) {
+        this.adminInterceptor = adminInterceptor;
+        this.timingInterceptor = timingInterceptor;
+        this.dataFileHub = dataFileHub;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/")
+                .setCacheControl(CacheControl.maxAge(1, TimeUnit.HOURS).cachePublic());
+
+        registry.addResourceHandler("/expdata/**")
+                .addResourceLocations("file:" + dataFileHub.getExperimentMageTabDirLocation());
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(adminInterceptor).addPathPatterns("/admin/**");
+        registry.addInterceptor(timingInterceptor).addPathPatterns("/**");
+    }
+
+    @Bean
+    public TilesConfigurer tilesConfigurer() {
+        TilesConfigurer configurer = new TilesConfigurer();
+        configurer.setDefinitions("/WEB-INF/tiles/errors.xml", "/WEB-INF/tiles/layout.xml", "/WEB-INF/tiles/views.xml");
+        return configurer;
+    }
+
+    @Bean
+    public UrlBasedViewResolver urlBasedViewResolver() {
+        UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+        resolver.setViewClass(TilesView.class);
+        return resolver;
+    }
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.registerCustomEditor(SemanticQuery.class, new SemanticQueryPropertyEditor());
+    }
+}
