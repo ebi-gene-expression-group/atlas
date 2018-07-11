7621aa376b025a2c3fe76c39780e2dff05b44692
Monica Jianu <monicajianu@users.noreply.github.com>
2018-07-11 10:04:42 +0100

    Bugfix #158947679 - Clusters.tsv file not closed (#118)
    * Surrounded clusters.tsv file operations with try-with-resources blocks to handle file closing.
    
    * Add a bad test
    
    * Reduce diff of open FDs to match what the CI environment expects
    See http://bar:8085/browse/ATLAS-SCXACIDEVTASKS7-3
    
    * Reword regret comment

--- sc/src/test/java/uk/ac/ebi/atlas/experimentpage/TsnePlotSettingsServiceIT.java
+++ sc/src/test/java/uk/ac/ebi/atlas/experimentpage/TsnePlotSettingsServiceIT.java
@@ -1,9 +1,12 @@
 package uk.ac.ebi.atlas.experimentpage;
 
-
+import com.sun.management.UnixOperatingSystemMXBean;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -14,7 +17,10 @@ import uk.ac.ebi.atlas.testutils.JdbcUtils;
 
 import javax.inject.Inject;
 import java.io.UncheckedIOException;
+import java.lang.management.ManagementFactory;
+import java.lang.management.OperatingSystemMXBean;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
@@ -35,13 +41,13 @@ public class TsnePlotSettingsServiceIT {
     private TsnePlotSettingsService subject;
 
     @BeforeEach
-    public void setUp() {
+    void setUp() {
         this.subject = new TsnePlotSettingsService(dataFileHub, idfParser);
     }
 
 
     @Test
-    public void getClustersForValidAccession() {
+    void getClustersForValidAccession() {
         List<Integer> result = subject.getAvailableClusters(jdbcTestUtils.fetchRandomSingleCellExperimentAccession());
 
         assertThat(result)
@@ -50,16 +56,49 @@ public class TsnePlotSettingsServiceIT {
     }
 
     @Test()
-    public void getClustersForInvalidAccessionThrowsException() {
+    void getClustersForInvalidAccessionThrowsException() {
         assertThatExceptionOfType(UncheckedIOException.class).isThrownBy(() -> subject.getAvailableClusters("FOO"));
     }
 
     @Test
-    public void getPerplexitiesForValidAccession() {
+    void getPerplexitiesForValidAccession() {
         List<Integer> result = subject.getAvailablePerplexities(jdbcTestUtils.fetchRandomSingleCellExperimentAccession());
 
         assertThat(result)
                 .isNotEmpty()
                 .doesNotHaveDuplicates();
+    }
+
+    // This is not a good test: the JVM may do things in the background we’re not aware of, or manage sockets or TCP
+    // connections differently depending on the OS or other environment-specific factors, and can end up having more
+    // open files than expected. However, because the clusters TSV file is accessed as a field without a getter, we
+    // can’t mock the following call sequence and verify that TsvStreamer::close has been called:
+    // dataFileHub.getSingleCellExperimentFiles(experimentAccession).clustersTsv.get().get()
+    // The +1 magic number accounts for open DB connections in the build environment, in my laptop it’s +4.
+    // This is the next best thing I could come up with... sorry! :(
+    @ParameterizedTest
+    @MethodSource("randomSingleCellExperimentAccessionProvider")
+    void filesClosed(String experimentAccession) {
+        long fileDescriptorsOpenBefore = getOpenFileCount();
+        subject.getAvailableClusters(experimentAccession);
+        subject.getExpectedClusters(experimentAccession);
+        long fileDescriptorsOpenAfter = getOpenFileCount();
+
+        assertThat(fileDescriptorsOpenAfter)
+                .isEqualTo(fileDescriptorsOpenBefore + 1);
+    }
+
+    // https://stackoverflow.com/questions/16360720/how-to-find-out-number-of-files-currently-open-by-java-application
+    private long getOpenFileCount() {
+        OperatingSystemMXBean os = ManagementFactory.getOperatingSystemMXBean();
+        if (os instanceof UnixOperatingSystemMXBean) {
+            return ((UnixOperatingSystemMXBean) os).getOpenFileDescriptorCount();
+        }
+
+        else return -1;
+    }
+
+    private Stream<String> randomSingleCellExperimentAccessionProvider() {
+        return Stream.of(jdbcTestUtils.fetchRandomSingleCellExperimentAccession());
     }
 }
