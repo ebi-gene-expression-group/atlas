9730e4920b883521a97f93d24f6a4c428723368e
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2018-07-17 17:30:20 +0100

    Move “special” sets of BioentityPropertyName to BioentityCollectionsProxy

--- base/src/main/java/uk/ac/ebi/atlas/solr/cloud/collections/BioentitiesCollectionProxy.java
+++ base/src/main/java/uk/ac/ebi/atlas/solr/cloud/collections/BioentitiesCollectionProxy.java
@@ -1,15 +1,36 @@
 package uk.ac.ebi.atlas.solr.cloud.collections;
 
+import com.google.common.collect.ImmutableList;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import uk.ac.ebi.atlas.search.SemanticQueryTerm;
+import uk.ac.ebi.atlas.solr.BioentityPropertyName;
 import uk.ac.ebi.atlas.solr.cloud.CollectionProxy;
 import uk.ac.ebi.atlas.solr.cloud.SchemaField;
 import uk.ac.ebi.atlas.solr.cloud.search.SolrQueryBuilder;
 
+import static uk.ac.ebi.atlas.solr.BioentityPropertyName.ENSGENE;
+import static uk.ac.ebi.atlas.solr.BioentityPropertyName.ENTREZGENE;
+import static uk.ac.ebi.atlas.solr.BioentityPropertyName.FLYBASE_GENE_ID;
+import static uk.ac.ebi.atlas.solr.BioentityPropertyName.HGNC_SYMBOL;
+import static uk.ac.ebi.atlas.solr.BioentityPropertyName.MGI_ID;
+import static uk.ac.ebi.atlas.solr.BioentityPropertyName.MGI_SYMBOL;
+import static uk.ac.ebi.atlas.solr.BioentityPropertyName.SYMBOL;
+import static uk.ac.ebi.atlas.solr.BioentityPropertyName.WBPSGENE;
 import static uk.ac.ebi.atlas.utils.StringUtil.escapeDoubleQuotes;
 
 public class BioentitiesCollectionProxy extends CollectionProxy {
+    // Where, and in what order, should we search in case of a free text query (without category)
+    public static final ImmutableList<BioentityPropertyName> ID_PROPERTY_NAMES =
+            ImmutableList.of(ENSGENE, SYMBOL, ENTREZGENE, HGNC_SYMBOL, MGI_ID, MGI_SYMBOL, FLYBASE_GENE_ID, WBPSGENE);
+
+    // These are species-specific property names that will ignore a species argument when searching. We assume that if
+    // the user chooses something like ENSG000001234 from the drop-down and Mus musculus in the species select, it is
+    // because she didn’t notice and her intent is clear (or even that she chose a species but when typing and saw the
+    // suggestions she changed her mind). After all, she’s the geneticist/biologist/bioinformatician!
+    public static final ImmutableList<BioentityPropertyName> SPECIES_OVERRIDE_PROPERTY_NAMES =
+            ImmutableList.of(ENSGENE, ENTREZGENE, HGNC_SYMBOL, MGI_ID, MGI_SYMBOL, FLYBASE_GENE_ID, WBPSGENE);
+
     public static class BioentitiesSchemaField extends SchemaField<BioentitiesCollectionProxy> {
         private BioentitiesSchemaField(String fieldName) {
             super(fieldName);
