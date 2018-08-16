6001d6643399fedf9a291bf424745cb5fba95616
Monica Jianu <monica@ebi.ac.uk>
2018-08-16 14:47:27 +0100

    Fixed failing unit test.

--- sc/src/test/java/uk/ac/ebi/atlas/monitoring/HealthCheckServiceTest.java
+++ sc/src/test/java/uk/ac/ebi/atlas/monitoring/HealthCheckServiceTest.java
@@ -10,6 +10,8 @@ import uk.ac.ebi.atlas.experimentimport.ScxaExperimentDao;
 import uk.ac.ebi.atlas.solr.cloud.admin.SolrCloudAdminProxy;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -27,42 +29,45 @@ public class HealthCheckServiceTest {
 
     private HealthCheckService subject;
 
+    private static final List<String> MOCK_SOLR_COLLECTIONS = Arrays.asList("mockCollection1", "mockCollection2");
+    private static final String MOCK_SOLR_COLLECTION_ALIAS = "mockCollectionAlias";
+
     @BeforeEach
     public void setUp() {
-        subject = new HealthCheckService(solrCloudAdminProxyMock, experimentDaoMock);
+        subject = new HealthCheckService(solrCloudAdminProxyMock);
     }
 
     @Test
     public void solrCollectionsAreUp() throws IOException, SolrServerException {
         when(solrCloudAdminProxyMock.areCollectionsUp(anyList(), any())).thenReturn(true);
 
-        assertThat(subject.isSolrUp()).isTrue();
+        assertThat(subject.isSolrUp(MOCK_SOLR_COLLECTIONS, MOCK_SOLR_COLLECTION_ALIAS)).isTrue();
     }
 
     @Test
     public void solrCollectionsAreDown() throws IOException, SolrServerException {
         when(solrCloudAdminProxyMock.areCollectionsUp(anyList(), any())).thenReturn(false);
 
-        assertThat(subject.isSolrUp()).isFalse();
+        assertThat(subject.isSolrUp(MOCK_SOLR_COLLECTIONS, MOCK_SOLR_COLLECTION_ALIAS)).isFalse();
     }
 
     @Test
     public void solrThrowsException() throws IOException, SolrServerException {
         when(solrCloudAdminProxyMock.areCollectionsUp(anyList(), any())).thenThrow(RuntimeException.class);
 
-        assertThat(subject.isSolrUp()).isFalse();
+        assertThat(subject.isSolrUp(MOCK_SOLR_COLLECTIONS, MOCK_SOLR_COLLECTION_ALIAS)).isFalse();
     }
 
     @Test
     public void experimentsDatabaseIsUp() {
         when(experimentDaoMock.countExperiments()).thenReturn(9);
 
-        assertThat(subject.isDatabaseUp()).isTrue();
+        assertThat(subject.isDatabaseUp(experimentDaoMock)).isTrue();
     }
     @Test
     public void noExperimentsInDatabase() {
         when(experimentDaoMock.countExperiments()).thenReturn(0);
 
-        assertThat(subject.isDatabaseUp()).isFalse();
+        assertThat(subject.isDatabaseUp(experimentDaoMock)).isFalse();
     }
 }
