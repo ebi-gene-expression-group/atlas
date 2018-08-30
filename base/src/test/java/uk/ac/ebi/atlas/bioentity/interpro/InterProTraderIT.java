9bc6e9dec07dce19d191e7b1741dd70c910795e0
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2018-08-30 15:09:32 +0100

    Randomize test

--- base/src/test/java/uk/ac/ebi/atlas/bioentity/interpro/InterProTraderIT.java
+++ base/src/test/java/uk/ac/ebi/atlas/bioentity/interpro/InterProTraderIT.java
@@ -1,40 +1,62 @@
 package uk.ac.ebi.atlas.bioentity.interpro;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.ac.ebi.atlas.configuration.TestConfig;
 import uk.ac.ebi.atlas.model.OntologyTerm;
 
 import javax.inject.Inject;
 
-import java.util.Optional;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Stream;
 
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = TestConfig.class)
-public class InterProTraderIT {
-    private static final OntologyTerm FIRST_TERM = OntologyTerm.create("IPR000001", "Kringle (domain)");
-    private static final OntologyTerm LAST_TERM = OntologyTerm.create("IPR029787", "Nucleotide cyclase (domain)");
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class InterProTraderIT {
+    private static final ThreadLocalRandom RNG = ThreadLocalRandom.current();
+
+    @Inject
+    private Path interProFilePath;
 
     @Inject
     private InterProTrader subject;
 
-    @Test
-    public void hasFirstTerm() {
-        assertThat(subject.get(FIRST_TERM.accession()), is(Optional.of(FIRST_TERM)));
+    @ParameterizedTest
+    @MethodSource("randomTermProvider")
+    void canReadAnyTerm(OntologyTerm interProTerm) {
+        assertThat(subject.get(interProTerm.accession())).hasValue(interProTerm);
     }
 
     @Test
-    public void hasLastTerm() {
-        assertThat(subject.get(LAST_TERM.accession()), is(Optional.of(LAST_TERM)));
+    void returnEmptyIfTermNotfound() {
+        assertThat(subject.get("IPRFOOBAR")).isEmpty();
     }
 
-    @Test
-    public void returnEmptyIfTermNotfound() {
-        assertThat(subject.get("IPRFOOBAR"), is(Optional.empty()));
+    private Stream<OntologyTerm> randomTermProvider() throws IOException {
+        long numLines = Files.lines(interProFilePath).count();
+
+        String randomLine =
+                Files.lines(interProFilePath)
+                        .skip(RNG.nextLong(0, numLines))
+                        .findFirst()
+                        .orElseThrow(RuntimeException::new);
+
+        String[] explodedLine = randomLine.split("\t");
+
+        return Stream.of(
+                OntologyTerm.create(
+                        explodedLine[1],
+                        explodedLine[0] + String.format(" (%s)", explodedLine[2].toLowerCase())));
     }
 }
