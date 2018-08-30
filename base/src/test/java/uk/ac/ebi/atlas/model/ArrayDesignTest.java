61d3894e1d5c7f21c382da9cdb0ac2db3d502077
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2018-08-30 16:42:32 +0100

    Add minimal, basic, sufficient test for ArrayDesign

---
+++ base/src/test/java/uk/ac/ebi/atlas/model/ArrayDesignTest.java
@@ -0,0 +1,14 @@
+package uk.ac.ebi.atlas.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ArrayDesignTest {
+    @Test
+    void unknownArrayDesignsArePopulatedWithTheirAccession() {
+        assertThat(ArrayDesign.createForUnknownName(randomAlphanumeric(20)))
+                .satisfies(arrayDesign -> assertThat(arrayDesign.accession()).isEqualTo(arrayDesign.name()));
+    }
+}
