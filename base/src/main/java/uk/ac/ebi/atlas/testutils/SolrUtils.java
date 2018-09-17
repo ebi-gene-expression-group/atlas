30eff24d88766d9ea8812242400ef2532723eeab
Monica Jianu <monicajianu@users.noreply.github.com>
2018-09-17 11:10:41 +0100

    Bugfix #160415041 - No info retrieved for gene IDs with dot suffixes (#163)
    * Implemented fix in gxa for path variables with dot suffixes which were truncated by Spring. Implmented test which retrieves random Arabidopsis lyrata genes. Converted test class to JUnit 5 and cleaned up.
    
    * Implemented same fix for bioentity information controller in sc. Added test case.

--- base/src/main/java/uk/ac/ebi/atlas/testutils/SolrUtils.java
+++ base/src/main/java/uk/ac/ebi/atlas/testutils/SolrUtils.java
@@ -3,11 +3,12 @@ package uk.ac.ebi.atlas.testutils;
 import org.springframework.stereotype.Component;
 import uk.ac.ebi.atlas.solr.cloud.SolrCloudCollectionProxyFactory;
 import uk.ac.ebi.atlas.solr.cloud.collections.AnalyticsCollectionProxy;
+import uk.ac.ebi.atlas.solr.cloud.collections.BioentitiesCollectionProxy;
 import uk.ac.ebi.atlas.solr.cloud.search.SolrQueryBuilder;
 
 import java.util.concurrent.ThreadLocalRandom;
 
-import static uk.ac.ebi.atlas.solr.cloud.collections.AnalyticsCollectionProxy.BIOENTITY_IDENTIFIER;
+import static uk.ac.ebi.atlas.solr.cloud.collections.BioentitiesCollectionProxy.SPECIES;
 
 @Component
 public class SolrUtils {
@@ -24,9 +25,24 @@ public class SolrUtils {
         AnalyticsCollectionProxy analyticsCollectionProxy =
                 solrCloudCollectionProxyFactory.create(AnalyticsCollectionProxy.class);
         SolrQueryBuilder<AnalyticsCollectionProxy> queryBuilder = new SolrQueryBuilder<>();
-        queryBuilder.setFieldList(BIOENTITY_IDENTIFIER);
+        queryBuilder.setFieldList(AnalyticsCollectionProxy.BIOENTITY_IDENTIFIER);
         queryBuilder.setRows(MAX_ROWS);
         return analyticsCollectionProxy
+                .query(queryBuilder)
+                .getResults()
+                .get(RNG.nextInt(MAX_ROWS))
+                .getFieldValue("bioentity_identifier")
+                .toString();
+    }
+
+    public String fetchRandomGeneOfSpecies(String species) {
+        BioentitiesCollectionProxy bioentitiesCollectionProxy =
+                solrCloudCollectionProxyFactory.create(BioentitiesCollectionProxy.class);
+        SolrQueryBuilder<BioentitiesCollectionProxy> queryBuilder = new SolrQueryBuilder<>();
+        queryBuilder.addFilterFieldByTerm(SPECIES, species);
+        queryBuilder.setFieldList(BioentitiesCollectionProxy.BIOENTITY_IDENTIFIER);
+        queryBuilder.setRows(MAX_ROWS);
+        return bioentitiesCollectionProxy
                 .query(queryBuilder)
                 .getResults()
                 .get(RNG.nextInt(MAX_ROWS))
