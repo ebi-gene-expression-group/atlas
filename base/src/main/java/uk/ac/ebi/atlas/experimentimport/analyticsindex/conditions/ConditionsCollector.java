cd96e8958a9f5dcbe67b70e5b73d6ae0ec7d8f22
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2018-09-18 23:59:05 +0100

    Rename ConditionsLookupService to LegacyConditionsLookupService; refactor design of the class to two classes with only five methods and half the code, without any transient state (i.e. builders), just pure functions

---
+++ base/src/main/java/uk/ac/ebi/atlas/experimentimport/analyticsindex/conditions/ConditionsCollector.java
@@ -0,0 +1,66 @@
+package uk.ac.ebi.atlas.experimentimport.analyticsindex.conditions;
+
+import com.google.common.collect.ImmutableSet;
+import org.springframework.stereotype.Component;
+import uk.ac.ebi.atlas.model.experiment.Experiment;
+import uk.ac.ebi.atlas.model.experiment.baseline.BaselineExperiment;
+import uk.ac.ebi.atlas.model.experiment.differential.DifferentialExperiment;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toSet;
+
+@Component
+public class ConditionsCollector {
+    private final EfoLookupService efoLookupService;
+
+    public ConditionsCollector(EfoLookupService efoLookupService) {
+        this.efoLookupService = efoLookupService;
+    }
+
+    // Given an experiment and an assay accession, retrieves the EFO terms from the experiment design and adds to the
+    // set all the parents plus the factor values and sample characteristics
+    private Set<String> collectAssayProperties(Experiment experiment, String assayAccession) {
+        ImmutableSet<String> ontologyIds =
+                efoLookupService
+                        .expandOntologyTerms(experiment.getExperimentDesign().getAllOntologyTermIdsByAssayAccession())
+                        .get(assayAccession);
+
+        return
+                Stream.of(
+                        ontologyIds,
+                        efoLookupService.getLabels(ontologyIds),
+                        experiment.getExperimentDesign().getFactorValues(assayAccession).values(),
+                        experiment.getExperimentDesign().getSampleCharacteristicsValues(assayAccession).values())
+                .flatMap(Collection::stream)
+                .collect(toSet());
+        }
+
+    // Visits all assays in each factor and builds-collects the conditions in a stream
+    public Stream<Condition> getConditions(BaselineExperiment experiment) {
+        return experiment.getDataColumnDescriptors().stream()
+                .flatMap(assayGroup ->
+                        assayGroup.assaysAnalyzedForThisDataColumn().stream()
+                                .map(assayAccession ->
+                                        new Condition(
+                                                experiment.getAccession(),
+                                                assayGroup.getId(),
+                                                collectAssayProperties(experiment, assayAccession))));
+    }
+
+    // Visits reference and test assays in each contrast and builds-collects the conditions in a stream
+    public Stream<DifferentialCondition> getConditions(DifferentialExperiment experiment) {
+        return experiment.getDataColumnDescriptors().stream()
+                .flatMap(contrast ->
+                        Stream.of(contrast.getReferenceAssayGroup(), contrast.getTestAssayGroup())
+                                .flatMap(assayGroup ->
+                                        assayGroup.assaysAnalyzedForThisDataColumn().stream()
+                                                .map(assayAccession -> new DifferentialCondition(
+                                                        experiment.getAccession(),
+                                                        assayGroup.getId(),
+                                                        contrast.getId(),
+                                                        collectAssayProperties(experiment, assayAccession)))));
+    }
+}
