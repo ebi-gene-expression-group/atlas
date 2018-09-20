caa6ae02dd6b878340a3c7ab454291dea82b4731
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2018-09-20 20:56:37 +0100

    Add Spring-profile-dependent configuration to use an embedded database when running integration tests (i.e. all tests that use TestConfig for their context configuration)

--- base/src/test/java/uk/ac/ebi/atlas/configuration/TestConfig.java
+++ base/src/test/java/uk/ac/ebi/atlas/configuration/TestConfig.java
@@ -3,13 +3,21 @@ package uk.ac.ebi.atlas.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.client.RestTemplate;
 
+import javax.servlet.ServletContext;
+
 @Configuration
-// Enabling component scanning will also load DataFilesConfig, JdbcConfig and SolrConfig, so just using this class as
+// Enabling component scanning will also load BasePathsConfig, JdbcConfig and SolrConfig, so just using this class as
 // application context is enough in integration tests
 @ComponentScan(basePackages = "uk.ac.ebi.atlas")
-public class TestConfig {
+public class TestConfig implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) {
+        servletContext.setInitParameter("spring.profiles.active", "test");
+    }
+
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
