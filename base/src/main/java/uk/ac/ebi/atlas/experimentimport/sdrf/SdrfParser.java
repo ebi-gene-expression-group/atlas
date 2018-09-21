df2909a5f970842d886b081c9f8b048f619dc5b7
Monica Jianu <monica@ebi.ac.uk>
2018-09-21 17:03:06 +0100

    Changed the data structure that holds the headers parsed from the sdrf to a LinkedHashSet. This way the uniqueness of the elements, as well as the ordering are enforced. Trimmed all whitespace from header type, as some sdrf files have 'Factor Value' and others have 'FactorValue' headers.

--- base/src/main/java/uk/ac/ebi/atlas/experimentimport/sdrf/SdrfParser.java
+++ base/src/main/java/uk/ac/ebi/atlas/experimentimport/sdrf/SdrfParser.java
@@ -1,15 +1,16 @@
 package uk.ac.ebi.atlas.experimentimport.sdrf;
 
+import org.springframework.util.StringUtils;
 import uk.ac.ebi.atlas.commons.readers.TsvStreamer;
 import uk.ac.ebi.atlas.resource.DataFileHub;
 
 import javax.inject.Inject;
 import javax.inject.Named;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
+import java.util.LinkedHashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -24,11 +25,11 @@ public class SdrfParser {
     }
 
     // Returns a map containing the header values for characteristics and factors
-    public Map<String, List<String>> parseHeader(String experimentAccession) {
+    public Map<String, Set<String>> parseHeader(String experimentAccession) {
         try (TsvStreamer sdrfStreamer = dataFileHub.getExperimentFiles(experimentAccession).sdrf.get()) {
             Pattern pattern = Pattern.compile("(.*?)(\\[)(.*?)(].*)");
 
-            Map<String, List<String>> headers = new HashMap<>();
+            Map<String, Set<String>> headers = new HashMap<>();
             Arrays.stream(
                     sdrfStreamer
                             .get()
@@ -38,12 +39,12 @@ public class SdrfParser {
                     .forEach(header -> {
                         Matcher matcher = pattern.matcher(header);
                         if (matcher.matches()) {
-                            String headerType = matcher.group(1).trim().toLowerCase(); // is the header a characteristic or a factor value?
-                            String headerValue = matcher.group(3); // what is the title of the header? e.g. organism, age, etc
+                            String headerType = StringUtils.trimAllWhitespace(matcher.group(1)).toLowerCase(); // is the header a characteristic or a factor value?
+                            String headerValue = matcher.group(3).trim().toLowerCase(); // what is the title of the header? e.g. organism, age, etc
                             if (headers.containsKey(headerType)) {
                                 headers.get(headerType).add(headerValue);
                             } else {
-                                List<String> headerValues = new ArrayList<>();
+                                Set<String> headerValues = new LinkedHashSet<>();
                                 headerValues.add(headerValue);
                                 headers.put(headerType, headerValues);
                             }
