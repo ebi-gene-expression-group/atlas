2981e08fd625a502e7f3be52dde2e272295a47b9
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2018-09-24 17:12:14 +0100

    Load all the test configuration from TestConfig instead of segregating beans in two files This is really a workaround for the fact that @ActiveProfiles is skipped in configuration classes and only works for tests. For instance, you can have a base integration test class that includes this annotation and make all other ITs inherit it.

--- base/src/test/java/uk/ac/ebi/atlas/configuration/TestConfig.java
+++ base/src/test/java/uk/ac/ebi/atlas/configuration/TestConfig.java
@@ -3,23 +3,50 @@ package uk.ac.ebi.atlas.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.WebApplicationInitializer;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.web.client.RestTemplate;
 
-import javax.servlet.ServletContext;
+import javax.sql.DataSource;
+
+import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.H2;
 
 @Configuration
 // Enabling component scanning will also load BasePathsConfig, JdbcConfig and SolrConfig, so just using this class as
 // application context is enough in integration tests
 @ComponentScan(basePackages = "uk.ac.ebi.atlas")
-public class TestConfig implements WebApplicationInitializer {
-    @Override
-    public void onStartup(ServletContext servletContext) {
-        servletContext.setInitParameter("spring.profiles.active", "test");
-    }
-
+public class TestConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .generateUniqueName(true)
+                .setType(H2)
+                .setScriptEncoding("UTF-8")
+                .ignoreFailedDrops(true)
+                .addScripts("schemas/db/shared-schema.sql", "schemas/db/gxa-schema.sql", "schemas/db/gxasc-schema.sql")
+                .build();
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
+
+    @Bean
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate() {
+        return new NamedParameterJdbcTemplate(dataSource());
+    }
+
+    @Bean
+    public PlatformTransactionManager txManager() {
+        return new DataSourceTransactionManager(dataSource());
     }
 }
