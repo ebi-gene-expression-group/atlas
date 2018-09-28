9e6e00111a4562dcc5954136e19df37e547a99fa
lingyun1010 <zhaolingyun1010@gmail.com>
2018-09-28 15:54:02 +0100

    Condense marker gene with only two entries…
    …1. the gene with lowest adjusted p value
    2. the gene is under cruator’s preferred cluster K

--- sc/src/main/java/uk/ac/ebi/atlas/search/GeneSearchDao.java
+++ sc/src/main/java/uk/ac/ebi/atlas/search/GeneSearchDao.java
@@ -8,6 +8,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import uk.ac.ebi.atlas.experimentimport.idf.IdfParser;
+import uk.ac.ebi.atlas.experimentimport.idf.IdfParserOutput;
 import uk.ac.ebi.atlas.solr.cloud.SolrCloudCollectionProxyFactory;
 import uk.ac.ebi.atlas.solr.cloud.collections.SingleCellAnalyticsCollectionProxy;
 import uk.ac.ebi.atlas.solr.cloud.collections.SingleCellAnalyticsCollectionProxy.SingleCellAnalyticsSchemaField;
@@ -32,12 +34,15 @@ public class GeneSearchDao {
 
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
     private SingleCellAnalyticsCollectionProxy singleCellAnalyticsCollectionProxy;
+    private IdfParser idfParser;
 
     public GeneSearchDao(NamedParameterJdbcTemplate namedParameterJdbcTemplate,
-                         SolrCloudCollectionProxyFactory solrCloudCollectionProxyFactory) {
+                         SolrCloudCollectionProxyFactory solrCloudCollectionProxyFactory,
+                         IdfParser idfParser) {
         this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
         this.singleCellAnalyticsCollectionProxy =
                 solrCloudCollectionProxyFactory.create(SingleCellAnalyticsCollectionProxy.class);
+        this.idfParser = idfParser;
     }
 
     private static final String SELECT_CELL_IDS_FOR_GENE_STATEMENT =
@@ -71,16 +76,61 @@ public class GeneSearchDao {
         );
     }
 
+    private static final String SELECT_PREFFERED_K_STATEMENT =
+            "SELECT experiment_accession FROM scxa_marker_genes AS markers "+
+            "JOIN scxa_experiment AS experiments ON markers.experiment_accession = experiments.accession "+
+            "WHERE gene_id=:gene_id "+
+            "GROUP BY experiment_accession";
+
+    public Map<String, Map<Integer, List<Integer>>> preferredKAndExperiment (String geneId) {
+        Map<String, Object> namedParameters =
+                ImmutableMap.of(
+                        "gene_id", geneId);
+
+        return namedParameterJdbcTemplate.query(
+                SELECT_PREFFERED_K_STATEMENT,
+                namedParameters,
+                (ResultSet resultSet)->{
+                    Map<String, Map<Integer, List<Integer>>>  result = new HashMap<>();
+
+                    while(resultSet.next()){
+                        String experimentAccession = resultSet.getString("experiment_accession");
+                        IdfParserOutput idfParserOutput = idfParser.parse(experimentAccession);
+                        Integer preferredK = idfParserOutput.getExpectedClusters();
+                        result.putAll(fetchKAndClusterIds(geneId, experimentAccession, preferredK));
+                    }
+
+                    return result;
+
+                }
+        );
+    }
+
+    //In marker gene dataset files, one and only one clusterID for each preferred cluster K value,
+    //which is supposed to have the lowest marker_probability,
+    //so we get one pair from searching (experiment_accesion, k).
+    //If dataset has changed, i.e. there are multiple gene clusterID in one preferred cluster K file,
+    //we probably need to order and limit the searching result based on marker_propability
     private static final String SELECT_K_AND_CLUSTER_ID_FOR_GENE_STATEMENT =
-            "SELECT experiment_accession, k, cluster_id FROM scxa_marker_genes AS markers " +
+            "SELECT experiment_accession, k, marker_probability, cluster_id FROM scxa_marker_genes AS markers " +
                     "JOIN scxa_experiment AS experiments ON markers.experiment_accession = experiments.accession " +
-                    "WHERE gene_id=:gene_id AND marker_probability<=:threshold AND private=FALSE";
+                    //"WHERE gene_id=:gene_id AND marker_probability<=:threshold AND private=FALSE " +
+                    "WHERE private=FALSE AND (marker_probability IN (SELECT MIN(marker_probability) "+
+                                                                    "FROM scxa_marker_genes " +
+                                                                    "WHERE gene_id=:gene_id " +
+                                                                    "GROUP BY experiment_accession) " +
+                                                "AND experiment_accession= :experiment_accession " +
+                                                "AND marker_probability<=:threshold) " +
+                    "OR ((experiment_accession, k) IN ((:experiment_accession, :experiment_and_K)) " +
+                        "AND marker_probability<=:threshold AND gene_id=:gene_id)";
     @Transactional(readOnly = true)
-    public Map<String, Map<Integer, List<Integer>>> fetchKAndClusterIds(String geneId) {
+    public Map<String, Map<Integer, List<Integer>>> fetchKAndClusterIds(String geneId, String experiment_accession, Integer experimentAndK) {
         Map<String, Object> namedParameters =
                 ImmutableMap.of(
                         "gene_id", geneId,
-                        "threshold", MARKER_GENE_P_VALUE_THRESHOLD);
+                        "threshold", MARKER_GENE_P_VALUE_THRESHOLD,
+                        "experiment_and_K", experimentAndK,
+                        "experiment_accession", experiment_accession);
 
         return namedParameterJdbcTemplate.query(
                 SELECT_K_AND_CLUSTER_ID_FOR_GENE_STATEMENT,
@@ -92,6 +142,7 @@ public class GeneSearchDao {
                         String experimentAccession = resultSet.getString("experiment_accession");
                         Integer k = resultSet.getInt("k");
                         Integer clusterId = resultSet.getInt("cluster_id");
+                        Float marker_probability = resultSet.getFloat("marker_probability");
 
                         Map<Integer, List<Integer>> kAndClusterIds =
                                 result.getOrDefault(experimentAccession, new HashMap<>());
@@ -106,7 +157,6 @@ public class GeneSearchDao {
                 }
         );
     }
-
 
     // Returns inferred cell types and organism parts for each experiment accession
     public Map<String, Map<String, List<String>>>
