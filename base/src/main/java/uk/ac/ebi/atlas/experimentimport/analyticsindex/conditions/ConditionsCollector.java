a4fd05b6c2ded05705ee5fc5010ca2e15814e658
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2018-10-03 16:58:19 +0100

    Don’t build the assay-to-conditions map every time we visit an assay, instead build it once and reuse it when building the stream elements Try to ANALYTICS_IMPORT GTEx without this... I did and it stalls for an indefinite amount of time until it starts to send documents to Solr. I waited for three hours until I decided something was wrong.

--- base/src/main/java/uk/ac/ebi/atlas/experimentimport/analyticsindex/conditions/ConditionsCollector.java
+++ base/src/main/java/uk/ac/ebi/atlas/experimentimport/analyticsindex/conditions/ConditionsCollector.java
@@ -1,6 +1,7 @@
 package uk.ac.ebi.atlas.experimentimport.analyticsindex.conditions;
 
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableSetMultimap;
 import org.springframework.stereotype.Component;
 import uk.ac.ebi.atlas.model.experiment.Experiment;
 import uk.ac.ebi.atlas.model.experiment.baseline.BaselineExperiment;
@@ -22,12 +23,9 @@ public class ConditionsCollector {
 
     // Given an experiment and an assay accession, retrieves the EFO terms from the experiment design and adds to the
     // set all the parents plus the factor values and sample characteristics
-    private Set<String> collectAssayProperties(Experiment experiment, String assayAccession) {
-        ImmutableSet<String> ontologyIds =
-                efoLookupService
-                        .expandOntologyTerms(experiment.getExperimentDesign().getAllOntologyTermIdsByAssayAccession())
-                        .get(assayAccession);
-
+    private Set<String> collectAssayProperties(Experiment experiment,
+                                               String assayAccession,
+                                               ImmutableSet<String> ontologyIds) {
         return
                 Stream.of(
                         ontologyIds,
@@ -36,10 +34,14 @@ public class ConditionsCollector {
                         experiment.getExperimentDesign().getSampleCharacteristicsValues(assayAccession).values())
                 .flatMap(Collection::stream)
                 .collect(toSet());
-        }
+    }
 
     // Visits all assays in each factor and builds-collects the conditions in a stream
     public Stream<Condition> getConditions(BaselineExperiment experiment) {
+        ImmutableSetMultimap<String, String> assayAccession2OntologyTerms =
+                efoLookupService
+                        .expandOntologyTerms(experiment.getExperimentDesign().getAllOntologyTermIdsByAssayAccession());
+
         return experiment.getDataColumnDescriptors().stream()
                 .flatMap(assayGroup ->
                         assayGroup.assaysAnalyzedForThisDataColumn().stream()
@@ -47,11 +49,18 @@ public class ConditionsCollector {
                                         new Condition(
                                                 experiment.getAccession(),
                                                 assayGroup.getId(),
-                                                collectAssayProperties(experiment, assayAccession))));
+                                                collectAssayProperties(
+                                                        experiment,
+                                                        assayAccession,
+                                                        assayAccession2OntologyTerms.get(assayAccession)))));
     }
 
     // Visits reference and test assays in each contrast and builds-collects the conditions in a stream
     public Stream<DifferentialCondition> getConditions(DifferentialExperiment experiment) {
+        ImmutableSetMultimap<String, String> assayAccession2OntologyTerms =
+                efoLookupService
+                        .expandOntologyTerms(experiment.getExperimentDesign().getAllOntologyTermIdsByAssayAccession());
+
         return experiment.getDataColumnDescriptors().stream()
                 .flatMap(contrast ->
                         Stream.of(contrast.getReferenceAssayGroup(), contrast.getTestAssayGroup())
@@ -61,6 +70,9 @@ public class ConditionsCollector {
                                                         experiment.getAccession(),
                                                         assayGroup.getId(),
                                                         contrast.getId(),
-                                                        collectAssayProperties(experiment, assayAccession)))));
+                                                        collectAssayProperties(
+                                                                experiment,
+                                                                assayAccession,
+                                                                assayAccession2OntologyTerms.get(assayAccession))))));
     }
 }
