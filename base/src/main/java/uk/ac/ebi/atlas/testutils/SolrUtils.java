29ccb7acf18e8a8a48396947cfed5f6f167407a6
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2018-10-12 14:29:12 +0100

    Add a public rawQuery method This is a small defeat in our mini SorlCloud framework since by exposing this some type-safety is lost, but now we can write better tests. We can rethink the design if we move the framework to its own project.

--- base/src/main/java/uk/ac/ebi/atlas/testutils/SolrUtils.java
+++ base/src/main/java/uk/ac/ebi/atlas/testutils/SolrUtils.java
@@ -1,33 +1,62 @@
 package uk.ac.ebi.atlas.testutils;
 
+import org.apache.solr.client.solrj.SolrQuery;
 import org.springframework.stereotype.Component;
 import uk.ac.ebi.atlas.solr.cloud.SolrCloudCollectionProxyFactory;
 import uk.ac.ebi.atlas.solr.cloud.collections.AnalyticsCollectionProxy;
+import static uk.ac.ebi.atlas.solr.cloud.collections.AnalyticsCollectionProxy.AnalyticsSchemaField;
 import uk.ac.ebi.atlas.solr.cloud.search.SolrQueryBuilder;
-
 import java.util.concurrent.ThreadLocalRandom;
 
-import static uk.ac.ebi.atlas.solr.cloud.collections.AnalyticsCollectionProxy.BIOENTITY_IDENTIFIER;
+//import static uk.ac.ebi.atlas.solr.cloud.collections.AnalyticsCollectionProxy.BIOENTITY_IDENTIFIER;
 
 @Component
 public class SolrUtils {
     private static final int MAX_ROWS = 10000;
     private static final ThreadLocalRandom RNG = ThreadLocalRandom.current();
 
-    private SolrCloudCollectionProxyFactory solrCloudCollectionProxyFactory;
+    private AnalyticsCollectionProxy analyticsCollectionProxy;
+    // private BioentitiesCollectionProxy bioentitiesCollectionProxy;
 
     public SolrUtils(SolrCloudCollectionProxyFactory solrCloudCollectionProxyFactory) {
-        this.solrCloudCollectionProxyFactory = solrCloudCollectionProxyFactory;
+        analyticsCollectionProxy = solrCloudCollectionProxyFactory.create(AnalyticsCollectionProxy.class);
+        // bioentitiesCollectionProxy = solrCloudCollectionProxyFactory.create(BioentitiesCollectionProxy.class);
     }
 
-    public String fetchRandomGeneFromAnalytics() {
-        AnalyticsCollectionProxy analyticsCollectionProxy =
-                solrCloudCollectionProxyFactory.create(AnalyticsCollectionProxy.class);
+    public String fetchRandomGeneIdFromAnalytics() {
         SolrQueryBuilder<AnalyticsCollectionProxy> queryBuilder = new SolrQueryBuilder<>();
-        queryBuilder.setFieldList(BIOENTITY_IDENTIFIER);
-        queryBuilder.setRows(MAX_ROWS);
+        queryBuilder
+                .setFieldList(AnalyticsCollectionProxy.BIOENTITY_IDENTIFIER)
+                .setRows(MAX_ROWS);
+
         return analyticsCollectionProxy
                 .query(queryBuilder)
+                .getResults()
+                .get(RNG.nextInt(MAX_ROWS))
+                .getFieldValue("bioentity_identifier")
+                .toString();
+    }
+
+    public String fetchRandomGeneIdFromAnalytics(AnalyticsSchemaField field, String term) {
+        SolrQueryBuilder<AnalyticsCollectionProxy> queryBuilder = new SolrQueryBuilder<>();
+        queryBuilder
+                .addQueryFieldByTerm(field, term)
+                .setFieldList(AnalyticsCollectionProxy.BIOENTITY_IDENTIFIER)
+                .setRows(MAX_ROWS);
+
+        return analyticsCollectionProxy
+                .query(queryBuilder)
+                .getResults()
+                .get(RNG.nextInt(MAX_ROWS))
+                .getFieldValue("bioentity_identifier")
+                .toString();
+    }
+
+    public String fetchRandomGeneWithoutSymbolFromAnalytics() {
+        SolrQuery solrQuery = new SolrQuery("-keyword_symbol:*");
+        solrQuery.setRows(MAX_ROWS);
+        return analyticsCollectionProxy
+                .rawQuery(solrQuery)
                 .getResults()
                 .get(RNG.nextInt(MAX_ROWS))
                 .getFieldValue("bioentity_identifier")
