7bbe26c39ca2b862a7ffec49d0aaf08e9e782e78
Monica Jianu <monica@ebi.ac.uk>
2018-10-19 09:39:34 +0100

    Reverted to using LazyReference for the popular species list.

--- base/src/main/java/uk/ac/ebi/atlas/species/services/PopularSpeciesService.java
+++ base/src/main/java/uk/ac/ebi/atlas/species/services/PopularSpeciesService.java
@@ -1,31 +1,37 @@
 package uk.ac.ebi.atlas.species.services;
 
+import com.atlassian.util.concurrent.LazyReference;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class PopularSpeciesService {
 
     protected final PopularSpeciesDao popularSpeciesDao;
-    protected final List<PopularSpeciesInfo> popularSpeciesInfos;
+    private final LazyReference<List<PopularSpeciesInfo>> popularSpeciesInfos = new LazyReference<List<PopularSpeciesInfo>>() {
+        @Override
+        protected List<PopularSpeciesInfo> create() {
+            return popularSpeciesDao.getExperimentCountBySpecies().stream()
+                    .sorted(PopularSpeciesInfo.BY_SIZE_DESCENDING)
+                    .collect(Collectors.toList());
+        }
+    };
 
     public PopularSpeciesService(PopularSpeciesDao popularSpeciesDao) {
         this.popularSpeciesDao = popularSpeciesDao;
-        this.popularSpeciesInfos = popularSpeciesDao.getExperimentCountBySpecies().stream()
-                .sorted(PopularSpeciesInfo.BY_SIZE_DESCENDING)
-                .collect(Collectors.toList());
     }
 
     public List<PopularSpeciesInfo> getPopularSpecies() {
-        return popularSpeciesInfos;
+        return popularSpeciesInfos.get();
     }
 
     public List<PopularSpeciesInfo> getPopularSpecies(int howMany) {
-        return getSublist(popularSpeciesInfos, howMany);
+        return getSublist(popularSpeciesInfos.get(), howMany);
     }
 
     public List<PopularSpeciesInfo> getPopularSpecies(final String kingdom, int howMany) {
         List<PopularSpeciesInfo> filteredList =
-                popularSpeciesInfos.stream()
+                popularSpeciesInfos.get().stream()
                         .filter(speciesInfo -> kingdom.equalsIgnoreCase(speciesInfo.kingdom()))
                         .collect(Collectors.toList());
 
