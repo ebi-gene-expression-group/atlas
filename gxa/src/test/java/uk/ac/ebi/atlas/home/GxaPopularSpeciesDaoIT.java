21f631aea21e2a182561ebeb0b3a8517beb6adcb
Monica Jianu <monica@ebi.ac.uk>
2018-10-24 16:24:15 +0100

    Added integration tests for PopularSpeciesDAO classes. Refactored PopularSpeciesServiceTest to test the new create method for the PopularSpeciesInfo class.

--- gxa/src/test/java/uk/ac/ebi/atlas/home/GxaPopularSpeciesDaoIT.java
+++ gxa/src/test/java/uk/ac/ebi/atlas/home/GxaPopularSpeciesDaoIT.java
@@ -24,39 +24,37 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {TestConfig.class})
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class ScxaPopularSpeciesDaoIT {
+public class GxaPopularSpeciesDaoIT {
     @Inject
     private SpeciesFactory speciesFactory;
 
     @Inject
-    private DataSource dataSource;
+    private GxaPopularSpeciesDao subject;
 
     @Inject
-    private ScxaPopularSpeciesDao subject;
+    private DataSource dataSource;
 
     @BeforeAll
     void populateDatabaseTables() {
         ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-        populator.addScripts(new ClassPathResource("fixtures/scxa_experiment-fixture.sql"));
+        populator.addScripts(new ClassPathResource("fixtures/experiment-fixture.sql"));
         populator.execute(dataSource);
     }
 
     @AfterAll
     void cleanDatabaseTables() {
         ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-        populator.addScripts(new ClassPathResource("fixtures/scxa_experiment-delete.sql"));
+        populator.addScripts(new ClassPathResource("fixtures/experiment-delete.sql"));
         populator.execute(dataSource);
     }
 
-
     @Test
-    void returnsPopularSpeciesWithExperimentCounts() {
+    public void returnsPopularSpeciesWithExperimentCounts() {
         assertThat(subject.getExperimentCountBySpecies())
                 .isNotEmpty()
+                .allMatch(species -> species.baselineExperiments() > 0 || species.differentialExperiments() > 0)
                 .allSatisfy(species -> {
                     assertThat(species.totalExperiments()).isGreaterThan(0);
-                    assertThat(species.baselineExperiments()).isEqualTo(0);
-                    assertThat(species.differentialExperiments()).isEqualTo(0);
                     assertThat(speciesFactory.create(species.species()).isUnknown()).isFalse();
                     assertThat(speciesFactory.create(species.species()).getKingdom())
                             .isEqualToIgnoringCase(species.kingdom());
