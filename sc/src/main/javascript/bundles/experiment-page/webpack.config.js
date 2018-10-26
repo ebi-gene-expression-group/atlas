3c63a672c900df5b1c4b7e625d0ed1f6b81e7f0a
Monica Jianu <monicajianu@users.noreply.github.com>
2018-10-26 13:21:39 +0100

    Chore - Build SC bundles (#184)
    * Upgraded experiment page bundle to webpack 4. Changed default experiment ID.
    
    * Updgraded gene-search bundle to Webpack 4. Removed unnecessary dependencies.
    
    * Changed build script to prevent Babel from being updated.
    
    * Fixes issue where default colour by value wasn't selected.
    
    * Built dev bundles.
    
    * Add margin to align positioning of the plots. This rule requires the latest package of the tsne plot component (not yet published).

--- sc/src/main/javascript/bundles/experiment-page/webpack.config.js
+++ sc/src/main/javascript/bundles/experiment-page/webpack.config.js
@@ -1,99 +1,110 @@
-const webpack = require(`webpack`)
 const path = require(`path`)
 const CleanWebpackPlugin = require(`clean-webpack-plugin`)
 
-config = {
-    entry: {
-        experimentPage: [`whatwg-fetch`, `babel-polyfill`, `./src/index.js`],
-        dependencies: [`prop-types`, `react`, `react-dom`, `react-router-dom`]
-    },
+const commonPublicPath = '/dist/'
 
-    output: {
-        library: `[name]`,
-        path: path.resolve(__dirname, `dist`),
-        filename: `[name].bundle.js`,
-        publicPath: `/gxa/sc/`
-    },
+module.exports = {
+  entry: {
+    experimentPageDemo: [`whatwg-fetch`, `babel-polyfill`, `./src/index.js`],
+    // dependencies: [`prop-types`, `react`, `react-dom`, `react-router-dom`]
+  },
 
-    plugins: [
-        new CleanWebpackPlugin([`dist`], {verbose: true, dry: false}),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: `dependencies`,
-            filename: `vendorCommons.bundle.js`,
-            minChunks: Infinity    // Explicit definition-based split, see dependencies entry
-        })
-    ],
+  output: {
+    library: `[name]`,
+    filename: `[name].bundle.js`,
+    publicPath: commonPublicPath
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                // exclude: /node_modules\//,
-                use: [
-                    {loader: 'style-loader'},
-                    {
-                      loader: 'css-loader',
-                      options: {
-                        modules: true,
-                        localIdentName: '[name]__[local]___[hash:base64:5]'
-                      }
-                    }
-                ]
-            },
-            {
-                test: /\.less$/i,
-                use: [`style-loader`, `css-loader`, `less-loader`]
-            },
-            {
-                test: /\.(jpe?g|png|gif)$/i,
-                use: [
-                    {
-                        loader: `file-loader`,
-                        options: {
-                            query: {
-                                name: `[hash].[ext]`,
-                                hash: `sha512`,
-                                digest: `hex`
-                            }
-                        }
-                    },
-                    {
-                        loader: `image-webpack-loader`,
-                        options: {
-                            query: {
-                                bypassOnDebug: true,
-                                mozjpeg: {
-                                    progressive: true,
-                                },
-                                gifsicle: {
-                                    interlaced: true,
-                                },
-                                optipng: {
-                                    optimizationLevel: 7,
-                                }
-                            }
-                        }
-                    }
-                ]
-            },
-          {
-            test: /\.svg$/i,
-            use: `file-loader`
-          },
-            {
-                test: /\.js$/i,
-                exclude: /node_modules\//,
-                use: `babel-loader`
-            }
-        ]
-    },
-
-    devServer: {
-        port: 9000,
-        historyApiFallback: {
-          index: `/gxa/sc/experiments/E-EHCA-2/`
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      minSize: 1,
+      cacheGroups: {
+        experimentPage: {
+          test: /[\\/]src[\\/]/,
+          name: 'experimentPage',
+          priority: -20
+        },
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          priority: -10
         }
+      }
     }
-}
+  },
 
-module.exports = config
+  plugins: [
+    new CleanWebpackPlugin([`dist`])
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        // exclude: /node_modules\//,
+        use: [
+          {loader: 'style-loader'},
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: '[name]__[local]___[hash:base64:5]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.less$/i,
+        use: [`style-loader`, `css-loader`, `less-loader`]
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/i,
+        use: [
+          {
+            loader: `file-loader`,
+            options: {
+              query: {
+                name: `[hash].[ext]`,
+                hash: `sha512`,
+                digest: `hex`
+              }
+            }
+          },
+          {
+            loader: `image-webpack-loader`,
+            options: {
+              query: {
+                bypassOnDebug: true,
+                mozjpeg: {
+                  progressive: true,
+                },
+                gifsicle: {
+                  interlaced: true,
+                },
+                optipng: {
+                  optimizationLevel: 7,
+                }
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.svg$/i,
+        use: `file-loader`
+      },
+      {
+        test: /\.js$/i,
+        exclude: /node_modules\//,
+        use: `babel-loader`
+      }
+    ]
+  },
+
+  devServer: {
+    port: 9000,
+    contentBase: path.resolve(__dirname, 'gxa/sc/experiments/E-MTAB-5061'),
+    publicPath: commonPublicPath
+  }
+}
--- sc/src/main/javascript/bundles/gene-search/webpack.config.js
+++ sc/src/main/javascript/bundles/gene-search/webpack.config.js
@@ -1,97 +1,56 @@
-const webpack = require('webpack');
-const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require(`path`)
+const CleanWebpackPlugin = require(`clean-webpack-plugin`)
 
-config = {
-    entry: {
-        geneSearch: ['whatwg-fetch', 'babel-polyfill', './src/index.js'],
-        dependencies: ['prop-types', 'react', 'react-dom', 'urijs']
-    },
+const commonPublicPath = '/dist/'
 
-    output: {
-        library: '[name]',
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js',
-        publicPath: '/html/'
-    },
+module.exports = {
+  entry: {
+    geneSearchDemo: ['whatwg-fetch', 'babel-polyfill', './src/index.js'],
+    // dependencies: ['prop-types', 'react', 'react-dom', 'urijs']
+  },
 
-    plugins: [
-        new CleanWebpackPlugin(['dist'], {verbose: true, dry: false}),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'dependencies',
-            filename: 'vendorCommons.bundle.js',
-            minChunks: Infinity     // Explicit definition-based split, see dependencies entry
-        })
-    ],
+  output: {
+    library: `[name]`,
+    filename: `[name].bundle.js`,
+    publicPath: commonPublicPath
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: [ 'style-loader', 'css-loader' ]
-            },
-            {
-                test: /\.less$/i,
-                use: [ 'style-loader', 'css-loader', 'less-loader' ]
-            },
-            {
-                test: /\.(jpe?g|png|gif)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            query: {
-                                name: '[hash].[ext]',
-                                hash: 'sha512',
-                                digest: 'hex'
-                            }
-                        }
-                    },
-                    {
-                        loader: 'image-webpack-loader',
-                        options: {
-                            query: {
-                                bypassOnDebug: true,
-                                mozjpeg: {
-                                    progressive: true,
-                                },
-                                gifsicle: {
-                                    interlaced: true,
-                                },
-                                optipng: {
-                                    optimizationLevel: 7,
-                                }
-                            }
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.svg$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            query: {
-                                name: '[hash].[ext]',
-                                hash: 'sha512',
-                                digest: 'hex'
-                            }
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.js$/i,
-                exclude: /node_modules\//,
-                use: 'babel-loader'
-            }
-        ]
-    },
-
-    devServer: {
-        port: 9000
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      minSize: 1,
+      cacheGroups: {
+        geneSearch: {
+          test: /[\\/]src[\\/]/,
+          name: 'geneSearch',
+          priority: -20
+        },
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          priority: -10
+        }
+      }
     }
-};
+  },
 
-module.exports = config
+  plugins: [
+    new CleanWebpackPlugin([`dist`])
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        exclude: /node_modules\//,
+        use: 'babel-loader'
+      }
+    ]
+  },
+
+  devServer: {
+    port: 9000,
+    contentBase: path.resolve(__dirname, 'html'),
+    publicPath: commonPublicPath
+  }
+}
