af6f354596afa51b80fab7a59bad6ed120850620
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2018-10-26 15:45:55 +0100

    Clean up and fix RandomDataTestUtils static method names

--- sc/src/test/java/uk/ac/ebi/atlas/search/GeneSearchServiceTest.java
+++ sc/src/test/java/uk/ac/ebi/atlas/search/GeneSearchServiceTest.java
@@ -9,12 +9,9 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
-import uk.ac.ebi.atlas.experimentimport.admin.Op;
 import uk.ac.ebi.atlas.experimentpage.TsnePlotSettingsService;
 import uk.ac.ebi.atlas.solr.cloud.collections.SingleCellAnalyticsCollectionProxy.SingleCellAnalyticsSchemaField;
-import uk.ac.ebi.atlas.testutils.RandomDataTestUtils;
 
-import javax.inject.Inject;
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.util.Arrays;
@@ -31,6 +28,8 @@ import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
+import static uk.ac.ebi.atlas.testutils.RandomDataTestUtils.generateRandomEnsemblGeneId;
+import static uk.ac.ebi.atlas.testutils.RandomDataTestUtils.generateRandomExperimentAccession;
 
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
@@ -50,10 +49,10 @@ class GeneSearchServiceTest {
 
     @Test
     void returnsCellIdsPerExperiment() {
-        String experimentAccession1 = RandomDataTestUtils.getRandomExperimentAccession();
-        String experimentAccession2 = RandomDataTestUtils.getRandomExperimentAccession();
-        String experimentAccession3 = RandomDataTestUtils.getRandomExperimentAccession();
-        String geneID = RandomDataTestUtils.getRandomEnsemblGeneId();
+        String experimentAccession1 = generateRandomExperimentAccession();
+        String experimentAccession2 = generateRandomExperimentAccession();
+        String experimentAccession3 = generateRandomExperimentAccession();
+        String geneID = generateRandomEnsemblGeneId();
 
         Map<String, List<String>> ensg00000104957Cells =
                     ImmutableMap.of(
@@ -75,18 +74,21 @@ class GeneSearchServiceTest {
 
     @Test
     void cellIdsPerExperimentForMultipleGeneIds() {
-        String experimentAccession1 = RandomDataTestUtils.getRandomExperimentAccession();
-        String experimentAccession2 = RandomDataTestUtils.getRandomExperimentAccession();
-        String experimentAccession3 = RandomDataTestUtils.getRandomExperimentAccession();
-        String experimentAccession4 = RandomDataTestUtils.getRandomExperimentAccession();
-        String experimentAccession5 = RandomDataTestUtils.getRandomExperimentAccession();
-        String geneID1 = RandomDataTestUtils.getRandomEnsemblGeneId();
-        String geneID2 = RandomDataTestUtils.getRandomEnsemblGeneId();
+        String experimentAccession1 = generateRandomExperimentAccession();
+        String experimentAccession2 = generateRandomExperimentAccession();
+        String experimentAccession3 = generateRandomExperimentAccession();
+        String experimentAccession4 = generateRandomExperimentAccession();
+        String experimentAccession5 = generateRandomExperimentAccession();
+        String geneID1 = generateRandomEnsemblGeneId();
+        String geneID2 = generateRandomEnsemblGeneId();
 
         Map<String, List<String>> ensfoobar1Cells = ImmutableMap.of(
-                experimentAccession1, ImmutableList.of("cell_id_1", "cell_id_2", "cell_id_3", "cell_id_4", "cell_id_5"),
-                experimentAccession2, ImmutableList.of("cell_id_6", "cell_id_7", "cell_id_8"),
-                experimentAccession3, ImmutableList.of("cell_id_9", "cell_id_10"));
+                experimentAccession1,
+                ImmutableList.of("cell_id_1", "cell_id_2", "cell_id_3", "cell_id_4", "cell_id_5"),
+                experimentAccession2,
+                ImmutableList.of("cell_id_6", "cell_id_7", "cell_id_8"),
+                experimentAccession3,
+                ImmutableList.of("cell_id_9", "cell_id_10"));
 
         Map<String, List<String>> ensfoobar2Cells = ImmutableMap.of(
                 experimentAccession4, ImmutableList.of("cell_id_11", "cell_id_12", "cell_id_13"),
@@ -101,94 +103,86 @@ class GeneSearchServiceTest {
 
     @Test
     void markerGeneProfilesForOneGeneId() {
-        String experimentAccession1 = RandomDataTestUtils.getRandomExperimentAccession();
-        String experimentAccession2 = RandomDataTestUtils.getRandomExperimentAccession();
-        String geneID = RandomDataTestUtils.getRandomEnsemblGeneId();
+        String experimentAccession1 = generateRandomExperimentAccession();
+        String experimentAccession2 = generateRandomExperimentAccession();
+        String geneID = generateRandomEnsemblGeneId();
 
         when(tsnePlotSettingsServiceMock.getExpectedClusters(experimentAccession1)).thenReturn(Optional.of(5));
         when(tsnePlotSettingsServiceMock.getExpectedClusters(experimentAccession2)).thenReturn(Optional.of(10));
 
-        when(geneSearchDaoMock.fetchClusterIdsWithPreferredKAndMinPForExperimentAccession(geneID, experimentAccession1, 5))
+        when(geneSearchDaoMock
+                .fetchClusterIdsWithPreferredKAndMinPForExperimentAccession(geneID, experimentAccession1, 5))
                 .thenReturn(ImmutableMap.of(5, ImmutableList.of(1)));
-        when(geneSearchDaoMock.fetchClusterIdsWithPreferredKAndMinPForExperimentAccession(geneID, experimentAccession2, 10))
+        when(geneSearchDaoMock
+                .fetchClusterIdsWithPreferredKAndMinPForExperimentAccession(geneID, experimentAccession2, 10))
                 .thenReturn(ImmutableMap.of(10, ImmutableList.of(1)));
 
-        when(geneSearchDaoMock.experimentAccessionsForGeneId(geneID)).thenReturn(ImmutableList.of(experimentAccession1, experimentAccession2));
+        when(geneSearchDaoMock
+                .experimentAccessionsForGeneId(geneID))
+                .thenReturn(ImmutableList.of(experimentAccession1, experimentAccession2));
 
-
-        Map<String, Map<String, Map<Integer, List<Integer>>>> result =
-                subject.getMarkerGeneProfile(geneID);
+        Map<String, Map<String, Map<Integer, List<Integer>>>> result = subject.getMarkerGeneProfile(geneID);
 
         assertThat(result)
                 .isNotEmpty()
                 .containsOnlyKeys(geneID)
                 .containsAllEntriesOf(ImmutableMap.of(geneID,
                         ImmutableMap.of(
-                                experimentAccession1,
-                            ImmutableMap.of(
-                                    5, ImmutableList.of(1)),
-                                experimentAccession2,
-                            ImmutableMap.of(
-                                    10, ImmutableList.of(1))))
-                );
+                                experimentAccession1, ImmutableMap.of(5, ImmutableList.of(1)),
+                                experimentAccession2, ImmutableMap.of(10, ImmutableList.of(1)))));
     }
 
     @Test
     void markerGeneProfilesForMultipleGeneIds() {
-        String experimentAccession1 = RandomDataTestUtils.getRandomExperimentAccession();
-        String experimentAccession2 = RandomDataTestUtils.getRandomExperimentAccession();
-        String experimentAccession3 = RandomDataTestUtils.getRandomExperimentAccession();
-        String experimentAccession4 = RandomDataTestUtils.getRandomExperimentAccession();
-        String geneID1 = RandomDataTestUtils.getRandomEnsemblGeneId();
-        String geneID2 = RandomDataTestUtils.getRandomEnsemblGeneId();
+        String experimentAccession1 = generateRandomExperimentAccession();
+        String experimentAccession2 = generateRandomExperimentAccession();
+        String experimentAccession3 = generateRandomExperimentAccession();
+        String experimentAccession4 = generateRandomExperimentAccession();
+        String geneID1 = generateRandomEnsemblGeneId();
+        String geneID2 = generateRandomEnsemblGeneId();
 
         when(tsnePlotSettingsServiceMock.getExpectedClusters(experimentAccession1)).thenReturn(Optional.of(5));
         when(tsnePlotSettingsServiceMock.getExpectedClusters(experimentAccession2)).thenReturn(Optional.of(10));
 
-        when(geneSearchDaoMock.fetchClusterIdsWithPreferredKAndMinPForExperimentAccession(geneID1, experimentAccession1, 5))
+        when(geneSearchDaoMock
+                .fetchClusterIdsWithPreferredKAndMinPForExperimentAccession(geneID1, experimentAccession1, 5))
                 .thenReturn(ImmutableMap.of(5, ImmutableList.of(1)));
-        when(geneSearchDaoMock.fetchClusterIdsWithPreferredKAndMinPForExperimentAccession(geneID1, experimentAccession2, 10))
+        when(geneSearchDaoMock
+                .fetchClusterIdsWithPreferredKAndMinPForExperimentAccession(geneID1, experimentAccession2, 10))
                 .thenReturn(ImmutableMap.of(10, ImmutableList.of(1)));
 
         when(tsnePlotSettingsServiceMock.getExpectedClusters(experimentAccession3)).thenReturn(Optional.of(2));
         when(tsnePlotSettingsServiceMock.getExpectedClusters(experimentAccession4)).thenReturn(Optional.of(4));
 
-        when(geneSearchDaoMock.fetchClusterIdsWithPreferredKAndMinPForExperimentAccession(geneID2, experimentAccession3, 2))
+        when(geneSearchDaoMock
+                .fetchClusterIdsWithPreferredKAndMinPForExperimentAccession(geneID2, experimentAccession3, 2))
                 .thenReturn(ImmutableMap.of(2, ImmutableList.of(1)));
-        when(geneSearchDaoMock.fetchClusterIdsWithPreferredKAndMinPForExperimentAccession(geneID2, experimentAccession4, 4))
+        when(geneSearchDaoMock
+                .fetchClusterIdsWithPreferredKAndMinPForExperimentAccession(geneID2, experimentAccession4, 4))
                 .thenReturn(ImmutableMap.of(4, ImmutableList.of(1)));
 
-
-        when(geneSearchDaoMock.experimentAccessionsForGeneId(geneID1)).thenReturn(ImmutableList.of(experimentAccession1, experimentAccession2));
-        when(geneSearchDaoMock.experimentAccessionsForGeneId(geneID2)).thenReturn(ImmutableList.of(experimentAccession3, experimentAccession4));
+        when(geneSearchDaoMock.experimentAccessionsForGeneId(geneID1))
+                .thenReturn(ImmutableList.of(experimentAccession1, experimentAccession2));
+        when(geneSearchDaoMock.experimentAccessionsForGeneId(geneID2))
+                .thenReturn(ImmutableList.of(experimentAccession3, experimentAccession4));
 
         assertThat(subject.getMarkerGeneProfile(geneID1, geneID2))
                 .containsAllEntriesOf(
                         ImmutableMap.of(
                                 geneID1,
                                 ImmutableMap.of(
-                                        experimentAccession1,
-                                        ImmutableMap.of(
-                                                5, ImmutableList.of(1)),
-                                        experimentAccession2,
-                                        ImmutableMap.of(
-                                                10, ImmutableList.of(1))),
+                                        experimentAccession1, ImmutableMap.of(5, ImmutableList.of(1)),
+                                        experimentAccession2, ImmutableMap.of(10, ImmutableList.of(1))),
                                 geneID2,
                                 ImmutableMap.of(
-                                        experimentAccession3,
-                                        ImmutableMap.of(
-                                                2, ImmutableList.of(1)),
-                                        experimentAccession4,
-                                        ImmutableMap.of(
-                                                4, ImmutableList.of(1)))
-                        )
-                );
+                                        experimentAccession3, ImmutableMap.of(2, ImmutableList.of(1)),
+                                        experimentAccession4, ImmutableMap.of(4, ImmutableList.of(1)))));
     }
 
     @Test
     void returnsFacets() {
-        String experimentAccession1 = RandomDataTestUtils.getRandomExperimentAccession();
-        String experimentAccession2 = RandomDataTestUtils.getRandomExperimentAccession();
+        String experimentAccession1 = generateRandomExperimentAccession();
+        String experimentAccession2 = generateRandomExperimentAccession();
 
         when(geneSearchDaoMock.getFacets(anyList(), any(SingleCellAnalyticsSchemaField.class)))
                 .thenReturn(ImmutableMap.of(
@@ -216,6 +210,7 @@ class GeneSearchServiceTest {
         doThrow(new UncheckedIOException(new IOException())).when(geneSearchDaoMock).fetchCellIds(anyString());
 
         assertThatExceptionOfType(RuntimeException.class).isThrownBy(
-                () -> subject.getCellIdsInExperiments("ENSFOOBAR1")).withCauseInstanceOf(ExecutionException.class);
+                () -> subject.getCellIdsInExperiments(generateRandomExperimentAccession()))
+                .withCauseInstanceOf(ExecutionException.class);
     }
 }
