952ffef12da71eeb70a1f110cb45d100227e6df3
lingyun1010 <zhaolingyun1010@gmail.com>
2018-10-29 15:31:30 +0000

    Adjust Dao response to Json schema for a new card
    This is a JSON response for grouped experiment accession query in the SC landing page

--- base/src/main/java/uk/ac/ebi/atlas/model/card/CardModelFactory.java
+++ base/src/main/java/uk/ac/ebi/atlas/model/card/CardModelFactory.java
@@ -6,6 +6,8 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import uk.ac.ebi.atlas.species.services.PopularSpeciesInfo;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 public class CardModelFactory {
@@ -22,11 +24,34 @@ public class CardModelFactory {
                 );
     }
 
+    public static CardModel createLandingPageCard(Map<String, Object> landingPageCardInfo) {
+        String experimentAccession = String.valueOf(landingPageCardInfo.get("experimentAccession"));
+        Map<String, String> imageSrc = new HashMap<>();
+        imageSrc.put("E-EHCA", "https://www.ebi.ac.uk/gxa/sc/resources/images/logos/human_cell_atlas.png");
+        imageSrc.put("E-GEOD", "https://www.ebi.ac.uk/gxa/sc/resources/images/home/hero_cell.png");
+        return CardModel.create(
+                "experiments",
+                imageSrc.get(experimentAccession.substring(0,6)),
+                experimentAccession,
+                Collections.singletonList(
+                        Pair.of(String.valueOf(landingPageCardInfo.get("experimentDescription")),
+                            Optional.of(getExperimentPageUrlByAccession(experimentAccession))))
+        );
+    }
+
     private static String getExperimentsFilteredBySpeciesUrl(String species) {
         return ServletUriComponentsBuilder.fromCurrentContextPath()
                 .path("/experiments")
                 .query("species={species}")
                 .buildAndExpand(species)
+                .encode()
+                .toUriString();
+    }
+
+    private static String getExperimentPageUrlByAccession(String accession) {
+        return ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/experiments/{accession}")
+                .buildAndExpand(accession)
                 .encode()
                 .toUriString();
     }
--- sc/src/main/java/uk/ac/ebi/atlas/landingpage/LandingCardController.java
+++ sc/src/main/java/uk/ac/ebi/atlas/landingpage/LandingCardController.java
@@ -13,6 +13,9 @@ import uk.ac.ebi.atlas.controllers.HtmlExceptionHandlingController;
 import uk.ac.ebi.atlas.experimentimport.ExperimentDTO;
 import uk.ac.ebi.atlas.experimentimport.ScxaExperimentDao;
 import uk.ac.ebi.atlas.experimentpage.ExperimentAttributesService;
+import uk.ac.ebi.atlas.model.card.CardModel;
+import uk.ac.ebi.atlas.model.card.CardModelAdapter;
+import uk.ac.ebi.atlas.model.card.CardModelFactory;
 import uk.ac.ebi.atlas.model.experiment.Experiment;
 import uk.ac.ebi.atlas.model.experiment.baseline.Cell;
 import uk.ac.ebi.atlas.trader.ScxaExperimentTrader;
@@ -36,7 +39,7 @@ public class LandingCardController  extends HtmlExceptionHandlingController {
 
     @Inject
     public LandingCardController(ScxaExperimentTrader experimentTrader,
-                                ExperimentAttributesService experimentAttributesService,
+                                 ExperimentAttributesService experimentAttributesService,
                                  ScxaExperimentDao experimentDao) {
         this.experimentTrader = experimentTrader;
         this.experimentAttributesService = experimentAttributesService;
@@ -44,20 +47,28 @@ public class LandingCardController  extends HtmlExceptionHandlingController {
     }
 
 
-    @RequestMapping(value = {"/landingpage/{experimentAccession}","/landingpage/{experimentAccession}/**"},
-            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @RequestMapping(value = {"/json/landingpage/{experimentAccession}"},
+                produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public String showExperimentPage(Model model,
                                      @PathVariable String experimentAccession,
                                      @RequestParam(defaultValue = "") String accessKey) {
         Set<String> allLikeExperiment = experimentDao.getExperimentByGroupAccession(experimentAccession);
 
-        List<Experiment<Cell>> allExperiment = allLikeExperiment.stream().map(likeExperiment ->
-                experimentTrader.getExperiment(likeExperiment, accessKey)).collect(Collectors.toList());
+        List<Experiment<Cell>> allExperiment = allLikeExperiment.stream()
+                .map(likeExperiment -> experimentTrader.getExperiment(likeExperiment, accessKey))
+                .collect(Collectors.toList());
         //model.addAllAttributes(experimentAttributesService.getAttributes(experiment));
 
-        List<Map<String, Object>> attributes = allExperiment.stream().map(experiment -> experimentAttributesService.getAttributes(experiment)).collect(Collectors.toList());
+        List<Map<String, Object>> attributes = allExperiment.stream()
+                .map(experiment -> experimentAttributesService.getAttributes(experiment))
+                .collect(Collectors.toList());
 
-        return GSON.toJson(attributes);
+        List<CardModel> cardModels = attributes
+                .stream()
+                .map(CardModelFactory::createLandingPageCard)
+                .collect(Collectors.toList());
+
+        return CardModelAdapter.serialize(cardModels).toString();
     }
 
 }
