0e0a1dc8428635ae6c0c227965f41107cb8c0dea
lingyun1010 <zhaolingyun1010@gmail.com>
2018-10-31 14:09:31 +0000

    Recover homecontroller

--- sc/src/main/java/uk/ac/ebi/atlas/home/HomeController.java
+++ sc/src/main/java/uk/ac/ebi/atlas/home/HomeController.java
@@ -1,13 +1,9 @@
 package uk.ac.ebi.atlas.home;
 
-import com.google.common.collect.ImmutableMap;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import uk.ac.ebi.atlas.controllers.HtmlExceptionHandlingController;
-import uk.ac.ebi.atlas.species.services.PopularSpeciesService;
 import uk.ac.ebi.atlas.trader.ScxaExperimentTrader;
 
 import javax.inject.Inject;
