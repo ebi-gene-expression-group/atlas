9bd8a50046e655e7690650c9c03229ccaf3165a9
lingyun1010 <zhaolingyun1010@gmail.com>
2018-11-02 16:01:28 +0000

    Add sql query for a subset of experiments …
    by a  part of experiment accesion

--- sc/src/main/java/uk/ac/ebi/atlas/experimentimport/ScxaExperimentDao.java
+++ sc/src/main/java/uk/ac/ebi/atlas/experimentimport/ScxaExperimentDao.java
@@ -19,7 +19,7 @@ public class ScxaExperimentDao extends ExperimentDao {
     // Create
     private static final String INSERT_NEW_EXPERIMENT =
             "INSERT INTO scxa_experiment " +
-            "(accession, type, species, private, access_key, pubmed_ids, dois, title) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+                    "(accession, type, species, private, access_key, pubmed_ids, dois, title) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
     // Read
     private static final String SELECT_EXPERIMENT_AS_ADMIN_BY_ACCESSION =
             "SELECT * FROM scxa_experiment WHERE accession=?";
@@ -28,6 +28,9 @@ public class ScxaExperimentDao extends ExperimentDao {
     private static final String SELECT_PUBLIC_EXPERIMENTS = "SELECT accession FROM scxa_public_experiment";
     private static final String SELECT_ALL_EXPERIMENTS_AS_ADMIN = "SELECT * FROM scxa_experiment";
     private static final String COUNT_EXPERIMENTS = "SELECT COUNT(*) FROM scxa_experiment";
+
+    private static final String SELECT_EXPERIMENT_BY_GROUP_ACCESSION =
+            "SELECT accession FROM scxa_experiment WHERE accession LIKE ?";
     // Update
     private static final String UPDATE_EXPERIMENT = "UPDATE scxa_experiment SET private=? WHERE accession=?";
     // Delete
@@ -114,4 +117,13 @@ public class ScxaExperimentDao extends ExperimentDao {
                         Collectors.joining(", "),
                         (result) -> result.isEmpty() ? null : result));
     }
+
+    public HashSet<String> getExperimentByGroupAccession(String experimentAccessionLike){
+        return Sets.newHashSet(jdbcTemplate.queryForList(
+                SELECT_EXPERIMENT_BY_GROUP_ACCESSION,
+                String.class,
+                new String[] {experimentAccessionLike + "%"})
+        );
+    }
+
 }
