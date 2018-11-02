7c4c78d8fe0958ed49e1e3dc55a7b8eba0e626f3
Haider Iqbal <haideri@ebi.ac.uk>
2018-11-02 12:40:53 +0000

    - Format code according to style guidelines

--- sc/src/main/java/uk/ac/ebi/atlas/search/FacetsToTooltipMapping.java
+++ sc/src/main/java/uk/ac/ebi/atlas/search/FacetsToTooltipMapping.java
@@ -1,9 +1,9 @@
 package uk.ac.ebi.atlas.search;
 
 public enum FacetsToTooltipMapping {
-    INFERRED_CELL_TYPE("Inferred cell type","Submitter defined cell identity for a cell based on post-sequencing expression profile."),
-    MARKER_GENE("Marker genes","A gene that comprises part of the specific expression profile for that cluster."),
-    ORGANISM_PART("Organism part","The tissue from which the sample is originally derived e.g lung.");
+    INFERRED_CELL_TYPE("Inferred cell type", "Submitter-defined cell identity for a cell based on post-sequencing expression profile"),
+    MARKER_GENE("Marker genes", "A gene that comprises part of the specific expression profile for that cluster"),
+    ORGANISM_PART("Organism part", "The tissue from which the sample is originally derived, e.g lung");
 
     String tooltip;
     String title;
