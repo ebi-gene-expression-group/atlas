9c01b3c75295815af84fa222eb3e93e1676783d7
lingyun1010 <zhaolingyun1010@gmail.com>
2018-11-02 16:01:58 +0000

    Modify the CardModelFactory for json payload type

--- base/src/main/java/uk/ac/ebi/atlas/model/card/CardModelFactory.java
+++ base/src/main/java/uk/ac/ebi/atlas/model/card/CardModelFactory.java
@@ -6,6 +6,8 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import uk.ac.ebi.atlas.home.PopularSpeciesInfo;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 public class CardModelFactory {
@@ -27,6 +29,31 @@ public class CardModelFactory {
                 .path("/experiments")
                 .query("species={species}")
                 .buildAndExpand(species)
+                .encode()
+                .toUriString();
+    }
+
+    public static CardModel createLandingPageCard(Map<String, Object> landingPageCardInfo) {
+        String experimentAccession = String.valueOf(landingPageCardInfo.get("experimentAccession"));
+        Map<String, String> imageSrc = new HashMap<>();
+        imageSrc.put("E-EHCA", "https://www.ebi.ac.uk/gxa/sc/resources/images/logos/human_cell_atlas.png");
+        imageSrc.put("E-GEOD", "https://www.ebi.ac.uk/gxa/sc/resources/images/home/hero_cell.png");
+        imageSrc.put("E-ENAD", "https://www.ebi.ac.uk/gxa/sc/resources/images/home/hero_cell.png");
+        imageSrc.put("E-MTAB", "https://www.ebi.ac.uk/gxa/sc/resources/images/home/hero_cell.png");
+        return CardModel.create(
+                "experiments",
+                imageSrc.get(experimentAccession.substring(0,6)),
+                experimentAccession,
+                Collections.singletonList(
+                        Pair.of(String.valueOf(landingPageCardInfo.get("experimentDescription")),
+                                Optional.of(getExperimentPageUrlByAccession(experimentAccession))))
+        );
+    }
+
+    private static String getExperimentPageUrlByAccession(String accession) {
+        return ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/experiments/{accession}")
+                .buildAndExpand(accession)
                 .encode()
                 .toUriString();
     }
