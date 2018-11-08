339e99892090b6e940db6a40e06a7695c2f4aab6
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2018-11-08 12:55:50 +0000

    Update browse-by-species bundle to the latest and greatest set of dependencies

---
+++ gxa/src/main/javascript/bundles/browse-by-species/src/BrowseBySpecies.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import SpeciesItem from './SpeciesItem'
+
+const BrowseBySpecies = props => {
+  const speciesItems = props.speciesInfoList.map(speciesInfo =>
+    <SpeciesItem
+      key={speciesInfo.species}
+      atlasUrl={props.atlasUrl}
+      {...speciesInfo}/>)
+
+  return (
+    <div className={`row small-up-2 medium-up-3`}>
+      {speciesItems}
+    </div>
+  )
+}
+
+BrowseBySpecies.propTypes = {
+  atlasUrl: PropTypes.string.isRequired,
+  speciesInfoList: PropTypes.arrayOf(PropTypes.shape({
+    species: PropTypes.string.isRequired,
+    totalExperiments: PropTypes.number.isRequired,
+    baselineExperiments: PropTypes.number.isRequired,
+    differentialExperiments: PropTypes.number.isRequired
+  })).isRequired,
+}
+
+export default BrowseBySpecies
--- gxa/src/main/javascript/bundles/browse-by-species/webpack.config.js
+++ gxa/src/main/javascript/bundles/browse-by-species/webpack.config.js
@@ -1,7 +1,53 @@
-module.exports = Object.assign(require('../../webpack.config.package-test-build.js'),
-    {
-        entry: {
-            browseBySpecies: './index.js',
-            dependencies: ['react', 'react-dom']
+const path = require(`path`)
+const CleanWebpackPlugin = require(`clean-webpack-plugin`)
+
+const commonPublicPath = `/dist/`
+const vendorsBundleName = `vendors`
+
+module.exports = {
+  entry: {
+    browseBySpecies: `./index.js`
+  },
+
+  plugins: [
+    new CleanWebpackPlugin([`dist`])
+  ],
+
+  output: {
+    library: `[name]`,
+    filename: `[name].bundle.js`,
+    publicPath: commonPublicPath
+  },
+
+  optimization: {
+    runtimeChunk: {
+      name: vendorsBundleName
+    },
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: vendorsBundleName,
+          chunks: 'all'
         }
-    });
+      }
+    }
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        exclude: /node_modules\//,
+        use: `babel-loader`
+      }
+    ]
+  },
+
+  devServer: {
+    port: 9000,
+    contentBase: path.resolve(__dirname, `html`),
+    publicPath: commonPublicPath
+  }
+}
+
