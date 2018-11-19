e188da815bc15f606f838a1bcbc245023fe5e3b6
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2018-11-19 17:23:17 +0000

    Migrate test to JUnit 5

--- gxa/src/test/java/uk/ac/ebi/atlas/search/baseline/BaselineExperimentSearchResultProducerIT.java
+++ gxa/src/test/java/uk/ac/ebi/atlas/search/baseline/BaselineExperimentSearchResultProducerIT.java
@@ -1,10 +1,15 @@
 package uk.ac.ebi.atlas.search.baseline;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import uk.ac.ebi.atlas.configuration.TestConfig;
 import uk.ac.ebi.atlas.model.AssayGroup;
@@ -12,6 +17,7 @@ import uk.ac.ebi.atlas.model.experiment.baseline.BaselineExperiment;
 import uk.ac.ebi.atlas.trader.ExperimentTrader;
 
 import javax.inject.Inject;
+import javax.sql.DataSource;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,23 +25,40 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.lessThan;
 import static org.junit.Assert.assertThat;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@ExtendWith(SpringExtension.class)
 @WebAppConfiguration
 @ContextConfiguration(classes = TestConfig.class)
-public class BaselineExperimentSearchResultProducerIT {
-    private BaselineExperimentSearchResultProducer subject;
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class BaselineExperimentSearchResultProducerIT {
+    @Inject
+    private DataSource dataSource;
 
     @Inject
     private ExperimentTrader experimentTrader;
 
-    @Before
-    public void setUp() {
+    private BaselineExperimentSearchResultProducer subject;
+
+    @BeforeAll
+    void populateDatabaseTables() {
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScripts(new ClassPathResource("fixtures/experiment-fixture.sql"));
+        populator.execute(dataSource);
+    }
+
+    @AfterAll
+    void cleanDatabaseTables() {
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScripts(new ClassPathResource("fixtures/experiment-delete.sql"));
+        populator.execute(dataSource);
+    }
+
+    @BeforeEach
+    void setUp() {
         subject = new BaselineExperimentSearchResultProducer(experimentTrader);
     }
 
-
     @Test
-    public void weHaveTwoColumnsCorrespondingToAdultAndFetus() {
+    void weHaveTwoColumnsCorrespondingToAdultAndFetus() {
         BaselineExperiment eProt1 = (BaselineExperiment) experimentTrader.getPublicExperiment("E-PROT-1");
 
         Map<String, Map<String, Double>> expressionsPerColumnPerExperiment = new HashMap<>();
