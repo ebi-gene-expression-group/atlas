236aa13b23fa0c8bc9acad6136aa28a6404b51dd
lingyun1010 <zhaolingyun1010@gmail.com>
2018-11-26 11:44:36 +0000

    Clean up codes

--- sc/src/main/java/uk/ac/ebi/atlas/home/JsonLatestExperimentController.java
+++ sc/src/main/java/uk/ac/ebi/atlas/home/JsonLatestExperimentController.java
@@ -1,11 +1,11 @@
 package uk.ac.ebi.atlas.home;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import uk.ac.ebi.atlas.controllers.JsonExceptionHandlingController;
 import uk.ac.ebi.atlas.trader.ScxaExperimentTrader;
 import javax.inject.Inject;
+
 @RestController
 public class JsonLatestExperimentController extends JsonExceptionHandlingController {
     private final LatestExperimentsService latestExperimentsService;
