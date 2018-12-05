a63b6ee47255452701ef735f5da0e8c3f1fc858b
lingyun1010 <zhaolingyun1010@gmail.com>
2018-12-05 11:12:51 +0000

    Update homepage panels bundle with the new latest
    experiment package

--- sc/src/main/javascript/bundles/scxa-homepage-panels/index.js
+++ sc/src/main/javascript/bundles/scxa-homepage-panels/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom'
 
 import HomePagePanel from 'scxa-homepage-panels'
 import {SceaHomepageSpeciesContainer} from 'atlas-homepage-cards'
-
+import {SceaHomePageLatestExperimentContainer} from 'atlas-latest-experiments-cards'
 const render = (options, target) => {
-    ReactDOM.render(<HomePagePanel {...options} CardType={SceaHomepageSpeciesContainer} />, document.getElementById(target))
+  ReactDOM.render(<HomePagePanel {...options} SpeciesCardType={SceaHomepageSpeciesContainer}
+                                 LatestExperimentCardType={SceaHomePageLatestExperimentContainer} />, document.getElementById(target))
 }
 
 export {render}
