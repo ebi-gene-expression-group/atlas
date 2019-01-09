3f738b468ad3cecb4a51e1f89d27fdcb961df771
lingyun1010 <zhaolingyun1010@gmail.com>
2019-01-09 10:47:26 +0000

    Update homepage card bundle with new export

--- gxa/src/main/javascript/bundles/atlas-homepage-card/index.js
+++ gxa/src/main/javascript/bundles/atlas-homepage-card/index.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import { SceaHomepageSpeciesContainer, HcaLandingPageContainer } from 'atlas-homepage-cards'
+import { SceaHomepageSpeciesContainer, EaHomepageSpeciesContainer, HcaLandingPageContainer } from 'atlas-homepage-cards'
 
 const renderSceaHomepageSpeciesContainer = (options, target) => {
   ReactDOM.render(<SceaHomepageSpeciesContainer {...options} />, document.getElementById(target))
+}
+
+const renderEaHomepageSpeciesContainer = (options, target) => {
+  ReactDOM.render(<EaHomepageSpeciesContainer {...options} />, document.getElementById(target))
 }
 
 const renderHcaLandingPageContainer = (options, target) => {
   ReactDOM.render(<HcaLandingPageContainer {...options} />, document.getElementById(target))
 }
 
-export { renderSceaHomepageSpeciesContainer, renderHcaLandingPageContainer }
+
+export { renderSceaHomepageSpeciesContainer, renderHcaLandingPageContainer, renderEaHomepageSpeciesContainer}
