2482bb17902f6607894fb303869720d309520fd3
lingyun1010 <zhaolingyun1010@gmail.com>
2019-01-31 16:55:11 +0000

    Rewrite data in json file, clearer for curators
    {
      "ensembl": "94",
      "ensembl_genomes": "41",
      "wormbase_parasite": "11",
      "efo": "2.98"
    }

--- base/src/main/java/uk/ac/ebi/atlas/species/AtlasInformationDataType.java
+++ base/src/main/java/uk/ac/ebi/atlas/species/AtlasInformationDataType.java
@@ -1,13 +1,9 @@
 package uk.ac.ebi.atlas.species;
 
-import uk.ac.ebi.atlas.controllers.ResourceNotFoundException;
-
-import java.util.Arrays;
-
 public enum AtlasInformationDataType {
   ENSEMBL("ensembl"),
-  GENOMES("genomes"),
-  PARASITE("paraSite"),
+  GENOMES("ensembl_genomes"),
+  PARASITE("wormbase_parasite"),
   EFO("efo");
 
   private final String id;
