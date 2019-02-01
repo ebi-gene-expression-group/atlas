32c58a56f13a60785076670c8c4c02a9d9f61aed
Monica Jianu <monica@ebi.ac.uk>
2019-02-01 13:22:57 +0000

    Replaced Magellan with side tabs powered by React Router.

--- sc/src/main/javascript/bundles/experiment-page/src/TSnePlotViewRoute.js
+++ sc/src/main/javascript/bundles/experiment-page/src/TSnePlotViewRoute.js
@@ -1,19 +1,54 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import URI from 'urijs'
+import {BrowserRouter, Route, NavLink, Switch, Redirect, withRouter} from 'react-router-dom'
 
 import HeatmapView from 'scxa-marker-gene-heatmap'
 import TSnePlotView from 'expression-atlas-experiment-page-tsne-plot'
 import BioentityInformation from 'sc-atlas-bioentity-information'
 
+const RedirectWithSearchAndHash = (props) =>
+  <Redirect to={{ pathname: props.pathname, search: props.location.search, hash: props.location.hash}} />
+
+RedirectWithSearchAndHash.propTypes = {
+  pathname: PropTypes.string.isRequired,
+  location: PropTypes.shape({
+    search: PropTypes.string.isRequired,
+    hash: PropTypes.string.isRequired
+  }).isRequired
+}
+
+const RedirectWithLocation = withRouter(RedirectWithSearchAndHash)
+
+const GeneInfoRoute = (props) => {
+  return (
+    <section className={`margin-top-large`}>
+      <h4 key={`title`}>Information about gene {props.geneId}</h4>
+      <BioentityInformation key={`gene-information`} atlasUrl={props.atlasUrl} geneId={props.geneId} />
+    </section>
+  )
+}
+
+const TsnePlotRoute = (props) => {
+  return (
+    <section>
+      <TSnePlotView {...props}/>
+    </section>
+  )
+}
+
+const MarkerGeneHeatmapRoute = (props) => {
+  return (
+    <section>
+      <HeatmapView {...props}
+      />
+    </section>
+  )
+}
+
 class TSnePlotViewRoute extends React.Component {
   constructor(props) {
     super(props)
-  }
-
-  componentDidMount() {
-    // Required for the magellan element to be initialised. Even if this is called in the JSP, it doesn't seem to affect the element inside a React component
-    window.$(document).foundation()
   }
 
   render() {
@@ -21,6 +56,92 @@ class TSnePlotViewRoute extends React.Component {
     const {atlasUrl, suggesterEndpoint} = this.props
     const {species, experimentAccession, ks, perplexities, metadata} = this.props
     const search = URI(location.search).search(true)
+
+    const routes = [
+      {
+        path: `/tsne`,
+        title: `Clusters`,
+        main: () => <TsnePlotRoute
+          atlasUrl={atlasUrl}
+          suggesterEndpoint={suggesterEndpoint}
+          wrapperClassName={`row expanded`}
+          clusterPlotClassName={`small-12 large-6 columns`}
+          expressionPlotClassName={`small-12 large-6 columns`}
+          speciesName={species}
+          experimentAccession={experimentAccession}
+          ks={ks}
+          metadata={metadata}
+          selectedColourBy={search.k || search.metadata || preferredK}
+          selectedColourByCategory={search.colourBy || `clusters`} // Is the plot coloured by clusters or metadata
+          highlightClusters={search.clusterId ? JSON.parse(search.clusterId) : []}
+          perplexities={perplexitiesOrdered}
+          selectedPerplexity={Number(search.perplexity) || perplexitiesOrdered[Math.round((perplexitiesOrdered.length - 1) / 2)]}
+          geneId={search.geneId || ``}
+          height={800}
+          onSelectGeneId={
+            (geneId) => {
+              const query = new URLSearchParams(history.location.search)
+              query.set(`geneId`, geneId)
+              resetHighlightClusters(query)
+              updateUrlWithParams(query)
+            }
+          }
+          onChangePerplexity={
+            (perplexity) => {
+              const query = new URLSearchParams(history.location.search)
+              query.set(`perplexity`, perplexity)
+              updateUrlWithParams(query)
+            }
+          }
+          onChangeColourBy={
+            (colourByCategory, colourByValue) => {
+              const query = new URLSearchParams(history.location.search)
+              query.set(`colourBy`, colourByCategory)
+              if(colourByCategory === `clusters`) {
+                query.set(`k`, colourByValue)
+                query.set(`markerGeneK`, colourByValue)
+                query.delete(`metadata`)
+              }
+              else if(colourByCategory === `metadata`) {
+                query.set(`metadata`, colourByValue)
+                query.delete(`k`)
+              }
+              resetHighlightClusters(query)
+              updateUrlWithParams(query)
+            }
+          }
+        />
+      },
+      {
+        path: `/marker-genes`,
+        title: `Marker Genes`,
+        main: () => <MarkerGeneHeatmapRoute
+          host={atlasUrl}
+          resource={`json/experiments/${experimentAccession}/marker-genes/${search.markerGeneK || preferredK}`}
+          wrapperClassName={`row expanded`}
+          ks={ks}
+          selectedK={search.markerGeneK || preferredK}
+          onSelectK={
+            (k) => {
+              const query = new URLSearchParams(history.location.search)
+              query.set(`markerGeneK`, k)
+              // If tsne plot is coloured by k
+              if (!query.has(`metadata`)) {
+                query.set(`k`, k)
+                query.set(`colourBy`, `clusters`)
+              }
+              resetHighlightClusters(query)
+              updateUrlWithParams(query)
+            }
+          }
+        />
+      },
+      {
+        path: `/gene-info`,
+        title: `Information about ${search.geneId}`,
+        main: () => <GeneInfoRoute atlasUrl={atlasUrl} geneId={search.geneId}/>
+      }
+    ]
 
     const updateUrlWithParams = (query) => {
       history.push({...history.location, search: query.toString()})
@@ -37,111 +158,64 @@ class TSnePlotViewRoute extends React.Component {
 
     const preferredK = this.props.selectedK ? this.props.selectedK.toString() : this.props.ks[0].toString()
 
+    const basename = URI(`experiments/${experimentAccession}${location.pathname}`, URI(atlasUrl).path()).toString()
+
     return (
-      <div className={`margin-top-large`} id={`plotContainer`}>
-        <div className={`row expanded`}>
-          <div className="large-2 columns">
-            <nav className="sticky-container" data-sticky-container>
-              <div className="sticky experiment-page-side-nav" data-sticky data-anchor="plotContainer" data-sticky-on="large" data-margin-top="3">
-                <ul className="vertical menu" data-magellan data-events="scroll">
-                  <li><a href="#first">Marker gene heatmap</a></li>
-                  <li><a href="#second">t-SNE plots</a></li>
-                  {
-                    search.geneId && <li><a href="#third">Gene information</a></li>
-                  }
-                </ul>
-              </div>
-            </nav>
-          </div>
-          <div className="sections large-10 columns">
-            <section id="first" data-magellan-target="first" className={`margin-bottom-large`}>
-              <HeatmapView
-                host={atlasUrl}
-                resource={`json/experiments/${experimentAccession}/marker-genes/${search.markerGeneK || preferredK}`}
-                wrapperClassName={`row expanded`}
-                ks={ks}
-                selectedK={search.markerGeneK || preferredK}
-                onSelectK={
-                  (k) => {
-                    const query = new URLSearchParams(history.location.search)
-                    query.set(`markerGeneK`, k)
-                    // If tsne plot is coloured by k
-                    if(!query.has(`metadata`)) {
-                      query.set(`k`, k)
-                      query.set(`colourBy`, `clusters`)
-                    }
-                    resetHighlightClusters(query)
-                    updateUrlWithParams(query)
-                  }
-                }/>
-            </section>
-            <hr/>
-            <section id="second" data-magellan-target="second" className={`margin-top-large margin-bottom-large`}>
-              <TSnePlotView
-                atlasUrl={atlasUrl}
-                suggesterEndpoint={suggesterEndpoint}
-                wrapperClassName={`row expanded`}
-                clusterPlotClassName={`small-12 large-6 columns`}
-                expressionPlotClassName={`small-12 large-6 columns`}
-                speciesName={species}
-                experimentAccession={experimentAccession}
-                ks={ks}
-                metadata={metadata}
-                selectedColourBy={search.k || search.metadata || preferredK}
-                selectedColourByCategory={search.colourBy || `clusters`} // Is the plot coloured by clusters or metadata
-                highlightClusters={search.clusterId ? JSON.parse(search.clusterId) : []}
-                perplexities={perplexitiesOrdered}
-                selectedPerplexity={Number(search.perplexity) || perplexitiesOrdered[Math.round((perplexitiesOrdered.length - 1) / 2)]}
-                geneId={search.geneId || ``}
-                height={800}
-                onSelectGeneId={
-                  (geneId) => {
-                    const query = new URLSearchParams(history.location.search)
-                    query.set(`geneId`, geneId)
-                    resetHighlightClusters(query)
-                    updateUrlWithParams(query)
-                  }
-                }
-                onChangePerplexity={
-                  (perplexity) => {
-                    const query = new URLSearchParams(history.location.search)
-                    query.set(`perplexity`, perplexity)
-                    updateUrlWithParams(query)
-                  }
-                }
-                onChangeColourBy={
-                  (colourByCategory, colourByValue) => {
-                    const query = new URLSearchParams(history.location.search)
-                    query.set(`colourBy`, colourByCategory)
-                    if(colourByCategory === `clusters`) {
-                      query.set(`k`, colourByValue)
-                      query.set(`markerGeneK`, colourByValue)
-                      query.delete(`metadata`)
-                    }
-                    else if(colourByCategory === `metadata`) {
-                      query.set(`metadata`, colourByValue)
-                      query.delete(`k`)
-                    }
-                    resetHighlightClusters(query)
-                    updateUrlWithParams(query)
-                  }
-                }
+      <BrowserRouter basename={basename}>
+        <div style={{ display: `flex` }}>
+          <div
+            style={{
+              width: `20%`,
+              // background: `#e6e6e6`
+              background: `#ffffff`
+            }}
+          >
+            <ul className={`side-tabs`}>
+              <li title={routes[0].title} className={`side-tabs-title`}>
+                <NavLink to={{pathname:routes[0].path, search: location.search, hash: location.hash}}
+                  activeClassName={`active`}>
+                  {routes[0].title}</NavLink>
+              </li>
+              <li title={routes[1].title} className={`side-tabs-title`}>
+                <NavLink to={{pathname:routes[1].path, search: location.search, hash: location.hash}}
+                  activeClassName={`active`}>
+                  {routes[1].title}</NavLink>
+              </li>
+              {
+                search.geneId &&
+                <li title={routes[2].title} className={`side-tabs-title`}>
+                  <NavLink to={{pathname:routes[2].path, search: location.search, hash: location.hash}}
+                    activeClassName={`active`}>
+                    {routes[2].title}</NavLink>
+                </li>
+              }
+            </ul>
+
+            {routes.map((route, index) => (
+              <Route
+                key={index}
+                path={`/`}
+                component={route.sidebar}
               />
-            </section>
-            {
-              search.geneId &&
-                [
-                  <hr/>,
-                  <section id="third" data-magellan-target="third" className={`margin-top-large`}>
-                    <h4 key={`title`}>Information about gene {search.geneId}</h4>
-                    <BioentityInformation key={`gene-information`} atlasUrl={atlasUrl} geneId={search.geneId} />
-                  </section>
-                ]
-            }
+            ))}
+          </div>
+
+          <div style={{ flex: 1, padding: `10px` }}>
+            <Switch>
+              {routes.map((route, index) => (
+                <Route
+                  key={index}
+                  path={route.path}
+                  component={route.main}
+                />
+              ))}
+              <RedirectWithLocation pathname={`${routes[0].path}`} />
+            </Switch>
           </div>
         </div>
-      </div>
+      </BrowserRouter>
     )
+
   }
 }
 
