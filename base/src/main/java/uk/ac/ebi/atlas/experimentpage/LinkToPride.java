d307f3a455ea4bde6f575cf6025abbfd7668d4bb
alfonsomunozpomer <amunoz@ebi.ac.uk>
2019-03-07 16:37:53 +0000

    Pull logic from methods to private functional fields

--- base/src/main/java/uk/ac/ebi/atlas/experimentpage/LinkToPride.java
+++ base/src/main/java/uk/ac/ebi/atlas/experimentpage/LinkToPride.java
@@ -1,33 +1,33 @@
 package uk.ac.ebi.atlas.experimentpage;
 
+import org.springframework.stereotype.Component;
 import uk.ac.ebi.atlas.model.download.ExternallyAvailableContent;
 import uk.ac.ebi.atlas.model.experiment.Experiment;
 import uk.ac.ebi.atlas.model.experiment.baseline.BaselineExperiment;
 
-import javax.inject.Named;
 import java.text.MessageFormat;
 import java.util.Collection;
-import java.util.Collections;
+import java.util.function.Function;
 
-public abstract class LinkToPride<E extends Experiment> extends ExternallyAvailableContent.Supplier<E> {
+import static java.util.Collections.singleton;
+
+@Component
+public class LinkToPride extends ExternallyAvailableContent.Supplier<BaselineExperiment> {
+    private static final Function<Experiment, String> formatLabel =
+            e -> MessageFormat.format("PRIDE Archive: project {0}", e.getSecondaryAccession());
+    private static final Function<Experiment, String> formatLink =
+            e -> MessageFormat.format("https://www.ebi.ac.uk/pride/archive/projects/{0}", e.getSecondaryAccession());
+
+    private static final Function<Experiment, ExternallyAvailableContent.Description> createIcon =
+            formatLabel.andThen(label -> ExternallyAvailableContent.Description.create("icon-pride", label));
 
     @Override
-    public Collection<ExternallyAvailableContent> get(E experiment) {
-        return Collections.singleton(
-                new ExternallyAvailableContent(
-                        "https://www.ebi.ac.uk/pride/archive/projects/" + experiment.getSecondaryAccession(),
-                        ExternallyAvailableContent.Description.create(
-                                "icon-pride",
-                                MessageFormat.format("PRIDE Archive: project {0}", experiment.getSecondaryAccession()))));
+    public Collection<ExternallyAvailableContent> get(BaselineExperiment experiment) {
+        return singleton(new ExternallyAvailableContent(formatLink.apply(experiment), createIcon.apply(experiment)));
     }
 
     @Override
     public ExternallyAvailableContent.ContentType contentType() {
         return ExternallyAvailableContent.ContentType.SUPPLEMENTARY_INFORMATION;
-    }
-
-    @Named
-    public static class ProteomicsBaseline extends LinkToPride<BaselineExperiment> {
-
     }
 }
